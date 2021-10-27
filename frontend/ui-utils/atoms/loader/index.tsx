6a45ecefd8960bc2d41b5ac30182609ff3098b27
Philipp Litzenberger <philipp@bendcircular.com>
2021-10-27 16:23:06 +0200

    feat(Loader): init loader component

---
+++ frontend/ui-utils/atoms/loader/index.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx"
+import React from "react"
+import loaderAsset from "./loader.webp"
+
+interface LoaderProps {
+  isLoading: boolean
+}
+
+export const Loader: React.FC<LoaderProps> = ({ isLoading }) =>
+  isLoading ? (
+    <div className="fixed top-0 right-0 bottom-0 left-0 h-full w-ful">
+      <div className="absolute w-full h-full top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-50" />
+      <img
+        className={clsx(
+          "absolute",
+          "left-1/2 -translate-x-1/2",
+          "top-1/2 -translate-y-1/2",
+          "m-auto w-[125px] min-w-[125px] max-w-[calc(100vw-1rem)]",
+        )}
+        src={loaderAsset}
+      />
+    </div>
+  ) : null

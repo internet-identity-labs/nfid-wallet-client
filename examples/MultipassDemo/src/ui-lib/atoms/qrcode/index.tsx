827e02412165587841d645cdca02ea1abfbe4b2f
Hassen <hassen.saidi@gmail.com>
2021-10-27 11:13:07 -0700

    adding MultipassDemo to MultipassFrontend as an example add

---
+++ examples/MultipassDemo/src/ui-lib/atoms/qrcode/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import QR from 'qrcode'
+import clsx from 'clsx'
+
+interface QRCodeProps {
+  content: string
+}
+
+export const QRCode: React.FC<QRCodeProps> = ({
+  content
+}: {
+  content: string
+}) => {
+  const canvas = React.useRef<HTMLCanvasElement | null>(null)
+
+  React.useEffect(() => {
+    if (content) {
+      QR.toCanvas(canvas.current, content, function (error: any) {
+        if (error) console.error(error)
+      })
+    }
+  }, [content])
+
+  return (
+    <div className={clsx('center')} style={{ cursor: 'pointer' }}>
+      <canvas ref={canvas} />
+    </div>
+  )
+}

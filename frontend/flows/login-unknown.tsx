6036f302963c75e12ade4113b585f4340a1609d6
Philipp Litzenberger <philipp@bendcircular.com>
2021-10-29 14:39:29 +0200

    feat(unknown-device-iframe-screen): cherry pick

--- frontend/flows/login-unknown.tsx
+++ frontend/flows/login-unknown.tsx
@@ -1,4 +1,14 @@
-// TODO:
-// - render qrcode
-//
-export { }
+import { QRCode } from "frontend/ui-utils/atoms/qrcode";
+import React from "react";
+
+export const LoginUnknown: React.FC = () => {
+  const SECRET = "WILL_BE_GENERATED"
+  const url = `http://localhost:9090/register-device-prompt/${SECRET}`
+  return (
+    <div className="flex flex-col">
+      <div>LoginUnknown</div>
+      <QRCode  content={url}/>
+      <a className="my-class" href={url} target="_blank">RegisterDevicePrompt</a>
+    </div>
+  );
+};

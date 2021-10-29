1a427d0f28e3444d77ead2827751adae6c798627
Philipp Litzenberger <philipp@bendcircular.com>
2021-10-29 14:41:12 +0200

    feat(unknown-device-screen): initial frame and annotations

--- frontend/flows/login-unknown.tsx
+++ frontend/flows/login-unknown.tsx
@@ -1,14 +1,30 @@
+import { Centered } from "frontend/ui-utils/atoms/centered";
 import { QRCode } from "frontend/ui-utils/atoms/qrcode";
 import React from "react";
 
-export const LoginUnknown: React.FC = () => {
+const useUnknownDeviceConfig = () => {
+  // TODO: create custom hook to load requesting application scope
+  const SCOPE = 'DSCVR'
+  // TODO: create custom hook to generate secret
   const SECRET = "WILL_BE_GENERATED"
+  // TODO: load multipass domain from env
+  // requires #6 to be merged
+  // env var: `VITE_MULTIPASS_DOMAIN`
   const url = `http://localhost:9090/register-device-prompt/${SECRET}`
+  return { url, secred: SECRET, scope: SCOPE }
+};
+
+export const UnknownDeviceScreen: React.FC = () => {
+  const { url, scope } = useUnknownDeviceConfig()
   return (
-    <div className="flex flex-col">
-      <div>LoginUnknown</div>
-      <QRCode  content={url}/>
-      <a className="my-class" href={url} target="_blank">RegisterDevicePrompt</a>
-    </div>
+    <Centered>
+      <div className="font-medium mb-3">Sign in to {scope} with Multipass</div>
+      <a href={url} target="_blank">
+        <div className="flex flex-row">
+          <div className="mr-2">Scan code to login</div>
+          <QRCode content={url} options={{ margin: 0 }} />
+        </div>
+      </a>
+    </Centered >
   );
 };
--- frontend/ui-utils/atoms/qrcode.tsx
+++ frontend/ui-utils/atoms/qrcode.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-import QR from "qrcode";
+import QR, { QRCodeRenderersOptions } from "qrcode";
 import clsx from "clsx";
 
 interface QRCodeProps {
   content: string;
+  options?: QRCodeRenderersOptions
 }
 
-export const QRCode: React.FC<QRCodeProps> = ({ content }: QRCodeProps) => {
+export const QRCode: React.FC<QRCodeProps> = ({ content, options }: QRCodeProps) => {
   const canvas = React.useRef<HTMLCanvasElement | null>(null);
 
   React.useEffect(() => {
     if (content) {
-      QR.toCanvas(canvas.current, content, function (error: any) {
+      QR.toCanvas(canvas.current, content, options || {}, function (error: any) {
         if (error) console.error(error);
       });
     }
-  }, [content]);
+  }, [content, options]);
 
   return (
     <div className={clsx("center")} style={{ cursor: "pointer" }}>

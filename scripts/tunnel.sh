62502ac1ac089d691ee68c1916ca62763f8cbb8e
Philipp Litzenberger <philipp@bendcircular.com>
2021-11-04 09:20:44 +0100

    chore(ngrok): Setup network configuration for local development [sc-235]

--- scripts/tunnel.sh
+++ scripts/tunnel.sh
@@ -1,9 +1,6 @@
 NGROK=./scripts/ngrok
 if [ -f "$NGROK" ]; then
-  # TODO:
-  # - [ ] get port from .env
-  # - [ ] get static configured subdomain
-  $NGROK http 9090
+  $NGROK http --hostname=multipass.ngrok.io 9090
 else
   echo "you need to download and unzip ngrok in this folder"
   echo "https://dashboard.ngrok.com/get-started/setup"

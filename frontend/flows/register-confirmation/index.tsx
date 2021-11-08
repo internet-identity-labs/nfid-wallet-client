36a4de2129d75d4ace0c5d78fbf41758090d35db
Philipp Litzenberger <philipp@bendcircular.com>
2021-11-08 10:40:06 +0100

    feat(ConfirmationScreen): setup polling
    - [sc-220]

--- frontend/flows/register-confirmation/index.tsx
+++ frontend/flows/register-confirmation/index.tsx
@@ -1,11 +1,42 @@
 import clsx from "clsx"
-import { Centered } from "frontend/ui-utils/atoms/centered"
+import { useInterval } from "frontend/hooks/use-interval"
+import { Button } from "frontend/ui-utils/atoms/button"
 import { FaceId } from "frontend/ui-utils/atoms/icons/face-id"
 import React from "react"
 
+const MAX_TRIES = 10
+const TRY_DELAY = 2000
+
+type State = "loading" | "pause" | "success" | "error"
+
 export const RegisterConfirmation = () => {
-  // TODO: get device from communication channel
+  const [status, setStatus] = React.useState<State>("loading")
+
+  // TODO:
+  // - [ ] poll for confirmation data (pubkey)
+  // - [ ] get device from communication channel
   const DEVICE = "MacBook Pro"
+
+  const handlePoll = React.useCallback(
+    (cancelPoll: () => void, totalTries: number) => {
+      if (totalTries >= MAX_TRIES) {
+        console.log(">> total tries exceeded", { totalTries })
+        cancelPoll()
+        setStatus("pause")
+        return
+      }
+    },
+    [],
+  )
+
+  const { start, resetTries } = useInterval(handlePoll, TRY_DELAY)
+
+  const handleRetry = React.useCallback(() => {
+    resetTries()
+    setStatus("loading")
+    start()
+  }, [resetTries, start])
+
   return (
     <div className={clsx("p-4 py-10 flex flex-col h-4/5")}>
       <h1 className={clsx("text-center font-bold text-3xl")}>
@@ -17,9 +48,19 @@ export const RegisterConfirmation = () => {
       <div className={clsx("flex justify-center mt-10")}>
         <FaceId />
       </div>
-      <div className={clsx("text-center mt-40")}>
-        This screen will update once you've registered your device
-      </div>
+      {status === "loading" && (
+        <div className={clsx("text-center mt-40")}>
+          This screen will update once you've registered your device
+        </div>
+      )}
+      {status === "pause" && (
+        <>
+          <div className={clsx("text-center mt-40")}>
+            Haven't received confirmation yet. Have you registered on your Mac?
+          </div>
+          <Button onClick={handleRetry}>Retry</Button>
+        </>
+      )}
     </div>
   )
 }

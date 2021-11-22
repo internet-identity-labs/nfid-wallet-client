91cee65648a12808caec7afc135f1340da314eae
Philipp Litzenberger <philipp@bendcircular.com>
2021-11-22 16:39:25 +0100

    chore(CD): check updates

--- examples/MultipassDemo/src/ui-lib/organisms/navigation/index.tsx
+++ examples/MultipassDemo/src/ui-lib/organisms/navigation/index.tsx
@@ -9,7 +9,7 @@ export const Navigation: React.FC<NavigationProps> = () => {
     <nav className="w-full flex fixed bg-white bg-opacity-40 m-auto">
       <div className="w-full md:max-w-7xl m-auto p-4 md:p-6 xl:py-8 md:m-auto lg:m-auto flex justify-between">
         <div className="uppercase font-lato font-bold tracking-widest text-lg">
-          Multipass Demo
+          Multipass Demo 1
         </div>
       </div>
     </nav>

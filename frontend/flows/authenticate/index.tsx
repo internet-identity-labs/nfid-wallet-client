26f504f841a1e9111acfc561fdc81b0ffb46873b
Philipp Litzenberger <philipp@bendcircular.com>
2021-11-25 10:19:48 +0100

    feat(style): add button look and hover state

--- frontend/flows/authenticate/index.tsx
+++ frontend/flows/authenticate/index.tsx
@@ -3,29 +3,31 @@ import { Centered } from "frontend/ui-utils/atoms/centered"
 import { TouchId } from "frontend/ui-utils/atoms/icons/touch-id"
 import { Loader } from "frontend/ui-utils/atoms/loader"
 import { useAuthentication } from "./hooks"
+import clsx from "clsx"
 
 export const Authenticate: React.FC<{ userNumber: bigint }> = ({
   userNumber,
 }) => {
-  // TODO: pull scope from backend or locastorage
+  const { isLoading, error, authenticate } = useAuthentication({
+    userNumber,
+  })
 
-  const scope = "DSCVR"
-
-  const { isLoading, error, authenticate } = useAuthentication({ userNumber })
+  const { hostname } = new URL(document.referrer)
+  const scope = `${hostname}`
 
   return (
     <Centered>
       {!error ? (
-        <div className={clsx("w-full p-4")}>
-          <div className="font-medium mb-3">
-            Sign in to {scope} with Multipass
-          </div>
+        <div className={clsx("w-full p-10")}>
+          <div className="font-medium mb-3">Sign in to {scope}</div>
           <div
-            className="flex items-center cursor-pointer"
+            className="flex items-center cursor-pointer border border-gray-200 rounded hover:shadow-sm hover:bg-gray-50"
             onClick={authenticate}
           >
-            <TouchId />
-            <div className="ml-1">Continue with TouchID as Philipp</div>
+            <div className={clsx("p-1 bg-gray-200")}>
+              <TouchId />
+            </div>
+            <div className="ml-1 p-2">Continue with TouchID as Philipp</div>
           </div>
         </div>
       ) : (

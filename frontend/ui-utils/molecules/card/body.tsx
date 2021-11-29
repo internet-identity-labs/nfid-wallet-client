5599012d457f809795e7210f9007347e32d9b18a
Mert <mertpolat.98@hotmail.com>
2021-11-29 15:20:38 +0100

    feat: added list component + small styling changes

--- frontend/ui-utils/molecules/card/body.tsx
+++ frontend/ui-utils/molecules/card/body.tsx
@@ -9,7 +9,7 @@ interface Props
 
 export const CardBody: React.FC<Props> = ({ children, className }) => {
   return (
-    <div className={clsx("px-4 py-6 bg-white sm:p-6 max-w-4xl mx-auto", className)}>
+    <div className={clsx("px-4 py-6 sm:p-6 max-w-4xl mx-auto", className)}>
       {children}
     </div>
   )

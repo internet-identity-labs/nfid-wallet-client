616f0e80fdb2e857cf4f38295b44438056ac9f95
Philipp Litzenberger <philipp@bendcircular.com>
2021-11-29 15:01:04 +0100

    chore(Tunnel): load tunnel settings from .env.local

--- scripts/tunnel.sh
+++ scripts/tunnel.sh
@@ -1,6 +1,10 @@
+#!/bin/sh
+
+. .env.local
 NGROK=./scripts/ngrok
+
 if [ -f "$NGROK" ]; then
-  $NGROK http --region=eu --hostname=multipass.eu.ngrok.io 9090
+  $NGROK http --region=eu --hostname=${VITE_MULTIPASS_DOMAIN} ${MULTIPASS_PORT}
 else
   echo "you need to download and unzip ngrok in this folder"
   echo "https://dashboard.ngrok.com/get-started/setup"

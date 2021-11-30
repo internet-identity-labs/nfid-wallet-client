487916f56955b6c2d305557e2b233c3e0165adab
Philipp Litzenberger <philipp@bendcircular.com>
2021-11-30 19:08:12 +0100

    fix(II-Domain): temp fix

--- frontend/flows/link-internet-identity/index.tsx
+++ frontend/flows/link-internet-identity/index.tsx
@@ -44,7 +44,9 @@ export const LinkInternetIdentity = () => {
     const publicKey = identity.getPublicKey().toDer()
     const rawId = blobToHex(identity.rawId)
 
-    const url = new URL("http://qjdve-lqaaa-aaaaa-aaaeq-cai.localhost:8000")
+    // const url = new URL("http://qjdve-lqaaa-aaaaa-aaaeq-cai.localhost:8000")
+    // TODO: pull from env
+    const url = new URL("https//identity.ic0.app")
     url.pathname = "/"
     url.hash = `#device=${userNumber};${blobToHex(publicKey)};${rawId}`
     const link = encodeURI(url.toString())

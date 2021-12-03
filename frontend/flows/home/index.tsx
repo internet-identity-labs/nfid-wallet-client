96f36d59240333e92d973501021f9bb2245d6c0a
Mert <mertpolat.98@hotmail.com>
2021-12-03 16:42:24 +0100

    feat: Structure home page by flows [sc-414]

--- frontend/flows/home/index.tsx
+++ frontend/flows/home/index.tsx
@@ -5,6 +5,7 @@ import { H4 } from "frontend/ui-utils/atoms/typography"
 import { HiChevronDoubleRight } from "react-icons/hi"
 import { Card } from "frontend/ui-utils/molecules/card"
 import { CardBody } from "frontend/ui-utils/molecules/card/body"
+import { Divider } from "frontend/ui-utils/atoms/divider"
 
 interface Props
   extends React.DetailedHTMLProps<
@@ -14,22 +15,37 @@ interface Props
 
 export const HomeScreen: React.FC<Props> = ({ children, className }) => {
   const routes = [
-    "/kitchen-sink",
-    "/register",
-    "/link-internet-identity",
-    "/iframe-overview",
-    "/copy-devices",
-    "/register-identity",
-    "/register-identity-name",
-    "/register-identity-phone",
-    "/register-identity-sms",
-    "/register-identity-challenge",
-    "/register-identity-persona-welcome",
-    "/register-identity-persona",
-    "/register-identity-persona-info",
-    "/register-identity-persona-success",
-    "/register-identity-persona-createkeys",
-    "/register-identity-persona-createkeys-complete",
+    {
+      title: "Common Pages",
+      items: [
+        "/register",
+        "/iframe-overview",
+        "/link-internet-identity",
+        "/copy-devices",
+        "/kitchen-sink",
+      ],
+    },
+    {
+      title: "Phone Number Verification Flow",
+      items: [
+        "/register-identity",
+        "/register-identity-name",
+        "/register-identity-phone",
+        "/register-identity-sms",
+        "/register-identity-challenge",
+      ],
+    },
+    {
+      title: "Mobile Registration Flow",
+      items: [
+        "/register-identity-persona-welcome",
+        "/register-identity-persona",
+        "/register-identity-persona-info",
+        "/register-identity-persona-success",
+        "/register-identity-persona-createkeys",
+        "/register-identity-persona-createkeys-complete",
+      ],
+    },
   ]
 
   const getRouteName = (route: string) => {
@@ -42,19 +58,29 @@ export const HomeScreen: React.FC<Props> = ({ children, className }) => {
         {process.env.NODE_ENV == "development" && (
           <Card>
             <CardBody>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
-                {routes.map((item, index) => (
-                  <a
-                    className="group hover:border-indigo-500 hover:text-indigo-500 cursor-pointer border rounded flex items-center justify-between p-2 w-full"
-                    href={item}
-                    key={index}
-                  >
-                    <span className="capitalize">{getRouteName(item)}</span>
-                    <div className="flex">
-                      <div className="border-l w-[10px] flex mr-1"></div>
-                      <HiChevronDoubleRight className="group-hover:text-indigo-500 text-lg" />
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                {routes.map((routes) => (
+                  <div className="col-span-1 sm:col-span-2 md:col-span-1">
+                    <H4 key={routes.title}>{routes.title}</H4>
+                    <Divider />
+                    <div className="space-y-3">
+                      {routes.items.map((item, index) => (
+                        <a
+                          className="group hover:border-indigo-500 hover:text-indigo-500 cursor-pointer border rounded flex items-center justify-between p-2 w-full"
+                          href={item}
+                          key={index}
+                        >
+                          <span className="capitalize">
+                            {getRouteName(item)}
+                          </span>
+                          <div className="flex">
+                            <div className="border-l w-[10px] flex mr-1"></div>
+                            <HiChevronDoubleRight className="group-hover:text-indigo-500 text-lg" />
+                          </div>
+                        </a>
+                      ))}
                     </div>
-                  </a>
+                  </div>
                 ))}
               </div>
             </CardBody>

23ca3dbabd8880d10d64b30174107db39647f23b
Philipp Litzenberger <philipp@bendcircular.com>
2021-12-10 12:42:32 +0100

    feat(CreateKeys): initialize screen

---
+++ frontend/flows/add-new-access-point/create-keys.tsx
@@ -0,0 +1,68 @@
+import React from "react"
+import clsx from "clsx"
+import { AppScreen } from "frontend/design-system/templates/AppScreen"
+import { Card } from "frontend/design-system/molecules/card"
+import { CardTitle } from "frontend/design-system/molecules/card/title"
+import { Button } from "frontend/design-system/atoms/button"
+import { Spinner } from "frontend/design-system/atoms/loader/spinner"
+import { CardAction } from "frontend/design-system/molecules/card/action"
+import { CardBody } from "frontend/design-system/molecules/card/body"
+import { HiCheckCircle } from "react-icons/hi"
+import { InputSelect } from "frontend/design-system/molecules/inputs/select"
+
+interface CreateKeysScreenProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {}
+
+export const CreateKeysScreen: React.FC<CreateKeysScreenProps> = () => {
+  const [deviceLinked, setDeviceLinked] = React.useState(false)
+  const [loading, setLoading] = React.useState(false)
+  return (
+    <AppScreen isFocused>
+      <Card className="flex flex-col h-full">
+        <CardTitle>Link another Access Point?</CardTitle>
+        <CardBody className="w-full max-w-xl">
+          <div className="mb-4">
+            <div className="flex flex-row space-x-4 items-center py-3">
+              <HiCheckCircle className={clsx("text-2xl", "text-black")} />
+              <div>Key created</div>
+            </div>
+            <div className="flex flex-row space-x-4 items-center py-3">
+              <HiCheckCircle
+                className={clsx(
+                  "text-2xl",
+                  deviceLinked ? "text-black" : "text-gray-300",
+                )}
+              />
+              <div>Device linked</div>
+            </div>
+          </div>
+          {loading ? <Spinner className="w-12 h-12" /> : null}
+        </CardBody>
+        <CardAction
+          bottom
+          className="justify-center md:flex-col md:items-center"
+        >
+          <div className="flex flex-col justify-center">
+            <InputSelect
+              id="device-select"
+              name="device"
+              options={[
+                { value: "1", label: "iPhone X", selected: true },
+                { value: "2", label: "MacBook Pro 13", selected: false },
+              ]}
+              onChange={(event) =>
+                console.log(">> ", { value: event.target.value })
+              }
+            />
+            <Button large filled onClick={() => console.log(">> click")}>
+              use Face ID to create keys
+            </Button>
+          </div>
+        </CardAction>
+      </Card>
+    </AppScreen>
+  )
+}
--- frontend/flows/add-new-access-point/index.tsx
+++ frontend/flows/add-new-access-point/index.tsx
@@ -2,14 +2,14 @@ import React from "react"
 import { Outlet, Route, Routes } from "react-router-dom"
 import { AwaitingConfirmation } from "./awaiting-confirmation"
 import { CopyLinkToChannel } from "./copy-link-to-channel"
-import { CreateKeysForNewAccessPoint } from "./register-this-device"
+import { CreateKeysScreen } from "./create-keys"
 
 export const AddNewAccessPointRoutes = () => (
   <Routes>
     <Route path="new-access-point/" element={<AddNewAccessPoint />}>
       <Route path="copy-link-to-channel" element={<CopyLinkToChannel />} />
       <Route path="awaiting-confirmation" element={<AwaitingConfirmation />} />
-      <Route path="create-keys" element={<CreateKeysForNewAccessPoint />} />
+      <Route path="create-keys" element={<CreateKeysScreen />} />
     </Route>
   </Routes>
 )

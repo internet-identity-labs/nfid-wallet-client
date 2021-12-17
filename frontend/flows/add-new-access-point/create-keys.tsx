9cae94f8677e0caf56ee44f3d35606b1267426d9
Philipp Litzenberger <philipp@bendcircular.com>
2021-12-17 09:33:36 +0100

    feat(CreateKeys): setup device select

--- frontend/flows/add-new-access-point/create-keys.tsx
+++ frontend/flows/add-new-access-point/create-keys.tsx
@@ -9,6 +9,8 @@ import { CardAction } from "frontend/design-system/molecules/card/action"
 import { CardBody } from "frontend/design-system/molecules/card/body"
 import { HiCheckCircle } from "react-icons/hi"
 import { InputSelect } from "frontend/design-system/molecules/inputs/select"
+import { useForm } from "react-hook-form"
+import { H2, H4 } from "frontend/design-system/atoms/typography"
 
 interface CreateKeysScreenProps
   extends React.DetailedHTMLProps<
@@ -17,8 +19,13 @@ interface CreateKeysScreenProps
   > {}
 
 export const CreateKeysScreen: React.FC<CreateKeysScreenProps> = () => {
+  const [keysCreated, setKeysCreated] = React.useState(false)
   const [deviceLinked, setDeviceLinked] = React.useState(false)
   const [loading, setLoading] = React.useState(false)
+
+  const { register, watch } = useForm({ defaultValues: { device: "1" } })
+  const device = watch("device")
+
   return (
     <AppScreen isFocused>
       <Card className="flex flex-col h-full">
@@ -26,7 +33,12 @@ export const CreateKeysScreen: React.FC<CreateKeysScreenProps> = () => {
         <CardBody className="w-full max-w-xl">
           <div className="mb-4">
             <div className="flex flex-row space-x-4 items-center py-3">
-              <HiCheckCircle className={clsx("text-2xl", "text-black")} />
+              <HiCheckCircle
+                className={clsx(
+                  "text-2xl",
+                  keysCreated ? "text-black" : "text-gray-300",
+                )}
+              />
               <div>Key created</div>
             </div>
             <div className="flex flex-row space-x-4 items-center py-3">
@@ -39,24 +51,27 @@ export const CreateKeysScreen: React.FC<CreateKeysScreenProps> = () => {
               <div>Device linked</div>
             </div>
           </div>
-          {loading ? <Spinner className="w-12 h-12" /> : null}
+          <div className={clsx("flex flex-col items-center")}>
+            {loading ? <Spinner className="w-12 h-12" /> : null}
+          </div>
         </CardBody>
         <CardAction
           bottom
           className="justify-center md:flex-col md:items-center"
         >
           <div className="flex flex-col justify-center">
-            <InputSelect
-              id="device-select"
-              name="device"
-              options={[
-                { value: "1", label: "iPhone X", selected: true },
-                { value: "2", label: "MacBook Pro 13", selected: false },
-              ]}
-              onChange={(event) =>
-                console.log(">> ", { value: event.target.value })
-              }
-            />
+            <H4 className={clsx("text-center")}>select device</H4>
+            <div className={clsx("py-2")}>
+              <InputSelect
+                className={clsx("w-full rounded")}
+                defaultValue={device}
+                options={[
+                  { value: "1", label: "iPhone X" },
+                  { value: "2", label: "MacBook Pro 13" },
+                ]}
+                {...register("device", { required: true })}
+              />
+            </div>
             <Button large filled onClick={() => console.log(">> click")}>
               use Face ID to create keys
             </Button>

011d9d0429548e09784762f66571850d96c73f69
Philipp Litzenberger <philipp@bendcircular.com>
2021-12-21 10:22:38 +0100

    fix(Button): sets hover state

--- examples/MultipassDemo/src/ui-lib/atoms/button/index.tsx
+++ examples/MultipassDemo/src/ui-lib/atoms/button/index.tsx
@@ -2,23 +2,24 @@ import clsx from "clsx"
 import React from "react"
 import { Loader } from "../loader"
 
-export const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> =
-  ({ onClick, children, className, ...props }) => (
-    <button
-      onClick={onClick}
-      className={clsx(
-        "transition duration-300",
-        "rounded-full border-2 border-black hover:border-white",
-        "w-full max-w-xs",
-        "py-2 px-10",
-        "font-lato tracking-widest uppercase text-lg hover:text-white",
-        className,
-      )}
-      {...props}
-    >
-      {children}
-    </button>
-  )
+export const Button: React.FC<
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+> = ({ onClick, children, className, ...props }) => (
+  <button
+    onClick={onClick}
+    className={clsx(
+      "transition duration-300",
+      "rounded-full border-2 border-black hover:shadow-lg hover:bg-gray-100",
+      "w-full max-w-xs",
+      "py-2 px-10",
+      "font-lato tracking-widest uppercase text-lg hover:text-gray-900",
+      className,
+    )}
+    {...props}
+  >
+    {children}
+  </button>
+)
 
 interface LoadingButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {

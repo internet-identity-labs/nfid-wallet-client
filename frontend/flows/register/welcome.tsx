31fafebf890a8c0ec4686253a1248a2792335bcd
Mert <mertpolat.98@hotmail.com>
2021-12-24 18:35:18 +0100

    feat: added welcome screen with swiper component

--- frontend/flows/register/welcome.tsx
+++ frontend/flows/register/welcome.tsx
@@ -1,11 +1,15 @@
 import {
   Button,
-  Card, CardAction, CardBody, CardTitle
+  Card,
+  CardAction,
+  CardBody,
+  CardTitle,
 } from "@identity-labs/ui"
 import clsx from "clsx"
 import { AppScreen } from "frontend/design-system/templates/AppScreen"
 import React from "react"
 import { Link } from "react-router-dom"
+import { Swiper, SwiperSlide } from "swiper/react"
 
 interface IdentityPersonaWelcomeScreenProps
   extends React.DetailedHTMLProps<
@@ -18,16 +22,30 @@ export const RegisterWelcome: React.FC<IdentityPersonaWelcomeScreenProps> = ({
 }) => {
   return (
     <AppScreen isFocused>
-      <Card className={clsx("h-full flex flex-col sm:block", className)}>
-        <CardTitle>Welcome</CardTitle>
-        <CardBody className="text-center max-w-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
-          doloribus?
+      <Card className={clsx("h-full block flex-col", className)}>
+        <CardTitle>{"{applicationName}"} uses NFID</CardTitle>
+        <CardBody className="text-center">
+          It's your identity. <br /> Own it.
+          <Swiper
+            className="bg-gray-50 overflow-hidden rounded-lg md:max-w-2xl mt-12"
+            pagination={true}
+            grabCursor={true}
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+          </Swiper>
         </CardBody>
         <CardAction bottom className="justify-center">
+          {/* TODO: map link to register/restore browser screen */}
+          <Link to="#" className="flex justify-center">
+            <Button block large>
+              Proceed with my NFID
+            </Button>
+          </Link>
           <Link to="/register/create-persona" className="flex justify-center">
             <Button block large filled>
-              Register
+              Create my NFID
             </Button>
           </Link>
         </CardAction>
--- frontend/main.tsx
+++ frontend/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom"
 
 // import '@identitylabs/ui/dist/identitylabs-ui.css'
 import "./index.css"
+import "swiper/css"
+import "swiper/css/pagination"
 
 import App from "./App"
+
+import SwiperCore, { Pagination } from "swiper"
+
+SwiperCore.use([Pagination])
 
 ReactDOM.render(
   <React.StrictMode>

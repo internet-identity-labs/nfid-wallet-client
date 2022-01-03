58723467080e5cfd13abcebc2362b67de232294d
Mert <mertpolat.98@hotmail.com>
2022-01-03 13:27:37 +0100

    force push changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,10 @@
 module.exports = {
-  mode: "jit",
-  purge: [
-    "./frontend/**/*.{ts,tsx}",
+  content: [
+    "./frontend/**/*.{js,jsx,ts,tsx}",
     "./examples/MultipassDemo/**/*.{ts,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      boxShadow: {
-        "bottom-sheet": "0 0 30px 8px rgba(0, 0, 0, 0.02)",
-      },
-      colors: {
-        black: "#212121",
-      },
-      fontFamily: {
-        lato: ["Lato", "sans-serif"],
-      },
-      animation: {
-        bounce200: "bounce 1s infinite 200ms",
-        bounce400: "bounce 1s infinite 400ms",
-      },
-    },
+    extend: {},
   },
-  variants: {
-    extend: {
-      backgroundColor: ["checked"],
-      borderColor: ["checked"],
-      inset: ["checked"],
-      zIndex: ["hover", "active"],
-    },
-  },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("./plugins/tailwind-ios-height"),
-  ],
+  plugins: [],
 }

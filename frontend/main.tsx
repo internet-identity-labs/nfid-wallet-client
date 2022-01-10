422946913f8c33edd0575477278697f9521ba12f
Mert <mertpolat.98@hotmail.com>
2022-01-10 14:28:10 +0100

    feat: migrated to React Router V6

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom"
 
 import App from "./App"
 
+import SwiperCore, { Pagination } from "swiper"
+import { BrowserRouter as Router } from "react-router-dom"
+import { AuthProvider } from "./flows/auth-wrapper"
+
+SwiperCore.use([Pagination])
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root"),
 )

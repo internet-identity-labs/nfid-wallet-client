37dc1545262d98d3837d9c01c42aedfae0bb0200
Mert <mertpolat.98@hotmail.com>
2022-01-10 12:09:59 +0100

    feat: remove unnecessary dark classes, fix #220

--- frontend/ui-kit/src/components/molecules/menu/menu-item.tsx
+++ frontend/ui-kit/src/components/molecules/menu/menu-item.tsx
@@ -13,7 +13,7 @@ interface Props
 export const MenuItem: React.FC<Props> = ({ icon, title, subtitle }) => {
   return (
     <div
-      className="flex items-center px-3 py-2 cursor-pointer space-x-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600"
+      className="flex items-center px-3 py-2 cursor-pointer space-x-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900"
       role="menuitem"
     >
       {icon && icon}

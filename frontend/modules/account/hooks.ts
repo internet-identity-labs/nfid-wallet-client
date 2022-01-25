e1ab35faec52de38b610eefc9768e2a9d0201c6e
Philipp Litzenberger <philipp@bendcircular.com>
2022-01-25 13:19:11 +0100

    fix(CORS): set access control header

--- frontend/modules/account/hooks.ts
+++ frontend/modules/account/hooks.ts
@@ -68,6 +68,7 @@ export const useAccount = (accountService?: AccountService) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": window.location.origin,
       },
       body: JSON.stringify({
         phoneNumber: phoneNumber,

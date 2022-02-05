c2a93701521839d2eebb4e2d784492dc948c94a6
Mert <mertpolat.98@hotmail.com>
2022-02-05 20:30:57 +0100

    feat: updated menu, button and list component

--- frontend/ui-kit/src/components/atoms/button/index.tsx
+++ frontend/ui-kit/src/components/atoms/button/index.tsx
@@ -1,25 +1,28 @@
 import React from "react"
 import clsx from "clsx"
 
-interface ButtonProps
+export interface ButtonProps
   extends React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement
   > {
-  filled?: boolean
+  primary?: boolean
+  secondary?: boolean
+  stroke?: boolean
+  error?: boolean
   block?: boolean
   text?: boolean
   large?: boolean
   disabled?: boolean
   icon?: boolean
   largeMax?: boolean
-  stroke?: boolean
 }
 
 export const Button: React.FC<ButtonProps> = ({
   children,
   className,
-  filled,
+  secondary,
+  primary,
   block,
   text,
   large,
@@ -27,16 +30,18 @@ export const Button: React.FC<ButtonProps> = ({
   icon,
   largeMax,
   stroke,
+  error,
   ...buttonProps
 }) => {
   return (
     <button
+      disabled={disabled}
       className={clsx(
         "text-center p-4 text-sm font-bold rounded-md outline-none focus:ring-2 focus:ring-offset-[3px] focus:ring-black-base first-letter:capitalize  hover:no-underline",
         !disabled && "cursor-pointer",
         text &&
-          "hover:underline text-blue-base py-2 focus:ring-blue-base outline-none",
-        filled &&
+          "hover:underline text-blue-base py-2 outline-none active:bg-gray-200 hover:bg-gray-100",
+        secondary &&
           "bg-black-base hover:bg-black-hover focus:ring-black-base text-white border-0",
         stroke &&
           "hover:bg-black-hover border border-black-base text-black-base active:bg-black-base active:text-white hover:text-white focus:text-white !focus:ring-black-hover focus:bg-black-hover",
@@ -45,6 +50,10 @@ export const Button: React.FC<ButtonProps> = ({
         disabled && "cursor-not-allowed opacity-20 pointer-events-none",
         icon && "flex items-center justify-center space-x-4",
         block && "w-full",
+        error &&
+          "text-white bg-red-base hover:bg-red-hover active:bg-red-active hover:shadow-lg hover:shadow-red-base/40 disabled:bg-black-base",
+        primary &&
+          "text-white bg-blue-base hover:bg-blue-hover active:bg-blue-active disabled:bg-black-base",
         className,
       )}
       {...buttonProps}

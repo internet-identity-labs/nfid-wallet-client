45fc1ee92661df1eb825c5140eed3720df3e6803
Philipp Litzenberger <philipp@bendcircular.com>
2022-02-07 10:49:17 +0100

    feat[sc-662]: update requirements

--- frontend/services/identity-manager/devices/hooks.ts
+++ frontend/services/identity-manager/devices/hooks.ts
@@ -1,5 +1,11 @@
+import { blobToHex } from "@dfinity/candid"
+import { WebAuthnIdentity } from "@dfinity/identity"
 import { useAuthentication } from "frontend/flows/auth-wrapper"
-import { IIConnection } from "frontend/services/internet-identity/iiConnection"
+import {
+  creationOptions,
+  IIConnection,
+} from "frontend/services/internet-identity/iiConnection"
+import { getBrowser, getPlatform } from "frontend/utils"
 import produce from "immer"
 import { useAtom } from "jotai"
 import React from "react"
@@ -39,6 +45,25 @@ export const useDevices = () => {
     [internetIdentity, userNumber],
   )
 
+  const createWebAuthNDevice = React.useCallback(
+    async (secret: string, userNumber: bigint) => {
+      const existingDevices = await IIConnection.lookupAll(userNumber)
+
+      const identity = await WebAuthnIdentity.create({
+        publicKey: creationOptions(existingDevices),
+      })
+      const publicKey = identity.getPublicKey().toDer()
+      const device = {
+        publicKey,
+        rawId: blobToHex(identity.rawId),
+        deviceName: `NFID ${getBrowser()} on ${getPlatform()}`,
+      }
+
+      return { device }
+    },
+    [],
+  )
+
   const getDevices = React.useCallback(async () => {
     await handleLoadDevices()
   }, [handleLoadDevices])
@@ -53,6 +78,7 @@ export const useDevices = () => {
 
   return {
     devices,
+    createWebAuthNDevice,
     getDevices,
     updateDevices,
     handleLoadDevices,

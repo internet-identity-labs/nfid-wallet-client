e26613a31795783e195bcccec4be15c584299723
Philipp Litzenberger <philipp@bendcircular.com>
2022-02-10 10:02:59 +0100

    chore: reverts accidential change

--- scripts/tunnel.sh
+++ scripts/tunnel.sh
@@ -4,7 +4,7 @@
 NGROK=./scripts/ngrok
 
 if [ -f "$NGROK" ]; then
-  $NGROK http --region=eu --hostname=${VITE_MULTIPASS_DOMAIN} 5001
+  $NGROK http --region=eu --hostname=${VITE_MULTIPASS_DOMAIN} ${MULTIPASS_PORT}
 else
   echo "you need to download and unzip ngrok in this folder"
   echo "https://dashboard.ngrok.com/get-started/setup"

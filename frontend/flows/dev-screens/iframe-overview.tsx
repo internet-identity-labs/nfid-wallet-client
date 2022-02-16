f3fbe554171a47d53a10032d5a12b66f6d0576a9
Mert <mertpolat.98@hotmail.com>
2022-02-16 09:17:37 +0100

    feat: update iframe overview screen

--- frontend/flows/dev-screens/iframe-overview.tsx
+++ frontend/flows/dev-screens/iframe-overview.tsx
@@ -1,8 +1,18 @@
 import clsx from "clsx"
 import { IFrameWrapper } from "components/molecules/iframe/wrapper"
+import { InputSelect } from "frontend/design-system/molecules/inputs/select"
 import { AppScreen } from "frontend/design-system/templates/AppScreen"
 import { IFrameScreen } from "frontend/design-system/templates/IFrameScreen"
-import { Button, Card, CardBody, H5, P } from "frontend/ui-kit/src"
+import {
+  Button,
+  Card,
+  CardBody,
+  DropdownMenu,
+  H5,
+  Label,
+  MenuItem,
+  P,
+} from "frontend/ui-kit/src"
 import React from "react"
 
 interface IFrameOverviewProps
@@ -15,29 +25,88 @@ export const IFrameOverview: React.FC<IFrameOverviewProps> = ({
   children,
   className,
 }) => {
+  const applicationName = "NFID Demo"
+  const accountName = "John Doe"
+  const personas = ["John Doe", "John77", "john_doe"]
+  const anchors = ["10001", "10002", "10003"]
+
   return (
     <AppScreen className={clsx("", className)}>
       <Card>
         <CardBody>
           <div className="grid grid-cols-12 gap-x-4 gap-y-12 grid-flow-row">
-            {Array.from({ length: 3}, (_, i) => (
-              <IFrameWrapper
-                className="inset-0 col-span-4 relative"
-                fixedHeight={false}
-              >
-                <IFrameScreen>
-                  <H5 className="mb-4 text-center">Welcome</H5>
-                  <P>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Ducimus, possimus. Odit, veniam tempora maiores architecto
-                    quia.
-                  </P>
-                  <Button secondary block>
-                    Next
-                  </Button>
-                </IFrameScreen>
-              </IFrameWrapper>
-            ))}
+            {/* IFrameNFIDLogin */}
+            <IFrameWrapper
+              className="inset-0 col-span-12 md:col-span-6 xl:col-span-4 relative"
+              fixedHeight={false}
+            >
+              <IFrameScreen logo>
+                <H5 className="mb-4 text-center">Unlock your NFID</H5>
+
+                <P className="text-center pb-12">
+                  The NFID on this device can only be unlocked by {accountName}.
+                </P>
+
+                <Button secondary block>
+                  Unlock as {accountName}
+                </Button>
+                <Button text block>
+                  Log in as different person
+                </Button>
+              </IFrameScreen>
+            </IFrameWrapper>
+
+            {/* IFrameAuthorizeApp */}
+            <IFrameWrapper
+              className="inset-0 col-span-12 md:col-span-6 xl:col-span-4 relative"
+              fixedHeight={false}
+            >
+              <IFrameScreen logo>
+                <H5 className="mb-4 text-center">
+                  Log in to {applicationName}
+                </H5>
+
+                <div className="mb-5">
+                  <Label>Continue as</Label>
+                  <DropdownMenu title={`Continue as ${personas[0]}`}>
+                    {(toggle) => (
+                      <>
+                        <Label menuItem>Personas</Label>
+                        {personas.map((persona) => (
+                          <MenuItem
+                            key={persona}
+                            title={persona}
+                            onClick={() => {
+                              console.log("persona clicked", persona)
+                              toggle()
+                            }}
+                          />
+                        ))}
+
+                        <Label menuItem>Anchors</Label>
+                        {anchors.map((anchor) => (
+                          <MenuItem
+                            key={anchor}
+                            title={anchor}
+                            onClick={() => {
+                              console.log("anchor clicked", anchor)
+                              toggle()
+                            }}
+                          />
+                        ))}
+                      </>
+                    )}
+                  </DropdownMenu>
+                </div>
+
+                <Button secondary block>
+                  Log in
+                </Button>
+                <Button text block>
+                  Create a new persona
+                </Button>
+              </IFrameScreen>
+            </IFrameWrapper>
           </div>
         </CardBody>
       </Card>

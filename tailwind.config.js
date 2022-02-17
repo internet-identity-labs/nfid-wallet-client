273749d7b1a59b78fc00131e0824a1c4186e9713
Mert <mertpolat.98@hotmail.com>
2022-02-17 11:23:36 +0100

    fix: tailwindcss import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   content: ["./frontend/**/*.{ts,tsx}"],
-  theme: {
-    extend: {
-      spacing: {
-        "13vw": "13vw",
-        "20vw": "20vw",
-        "16vh": "16vh",
-        "30vh": "30vh",
-      },
-    },
-  },
   plugins: [
     require("@tailwindcss/forms"),
     require("./plugins/tailwind-ios-height"),

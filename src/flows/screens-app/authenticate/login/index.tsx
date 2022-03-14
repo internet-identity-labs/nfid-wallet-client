d7c1f99f044d6003caa5f19547e5f2a81a537b3e
Philipp Litzenberger <philipp@bendcircular.com>
2022-03-14 09:42:26 +0100

    fix[sc-796]: checks account before personalize

--- src/flows/screens-app/authenticate/login/index.tsx
+++ src/flows/screens-app/authenticate/login/index.tsx
@@ -1,19 +1,30 @@
 import { AppScreen } from "frontend/design-system/templates/AppScreen"
 import { CardBody } from "frontend/ui-kit/src"
 import React from "react"
-import { AuthenticateNFIDLoginContent } from "frontend/screens/nfid-login"
+import { NFIDLogin } from "frontend/screens/nfid-login"
+import { IFrameScreen } from "frontend/design-system/templates/IFrameScreen"
 
 interface NFIDLoginProps
   extends React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLDivElement>,
     HTMLDivElement
-  > {}
+  > {
+  iframe?: boolean
+}
 
-export const AuthenticateNFIDLogin: React.FC<NFIDLoginProps> = () => {
-  return (
+export const AuthenticateNFIDLogin: React.FC<NFIDLoginProps> = (
+  { iframe } = { iframe: false },
+) => {
+  console.log(">> AuthenticateNFIDLogin", { iframe })
+
+  return iframe ? (
+    <IFrameScreen logo className="flex flex-col h-full">
+      <NFIDLogin />
+    </IFrameScreen>
+  ) : (
     <AppScreen className="flex flex-col h-full">
       <CardBody className="flex flex-col-reverse h-full justify-between lg:flex-row lg:justify-between !py-0">
-        <AuthenticateNFIDLoginContent />
+        <NFIDLogin />
       </CardBody>
     </AppScreen>
   )
--- src/flows/screens-iframe/authenticate/login/index.tsx
+++ src/flows/screens-iframe/authenticate/login/index.tsx
@@ -1,5 +1,5 @@
 import { IFrameScreen } from "frontend/design-system/templates/IFrameScreen"
-import { AuthenticateNFIDLoginContent } from "frontend/screens/nfid-login"
+import { NFIDLogin } from "frontend/screens/nfid-login"
 import React from "react"
 
 interface IFrameAuthenticateNFIDLoginProps
@@ -8,13 +8,13 @@ interface IFrameAuthenticateNFIDLoginProps
     HTMLDivElement
   > {}
 
-export const IFrameAuthenticateNFIDLogin: React.FC<
+export const IFrameNFIDLogin: React.FC<
   IFrameAuthenticateNFIDLoginProps
 > = ({ children, className }) => {
   return (
     <IFrameScreen logo>
       <div className="flex flex-col-reverse">
-        <AuthenticateNFIDLoginContent iframe />
+        <NFIDLogin iframe />
       </div>
     </IFrameScreen>
   )

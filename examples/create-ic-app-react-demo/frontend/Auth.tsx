bfe9f9b0f48692966d99fc461c66d9d90a241d1e
Pavlo Chorniy <pchorniy@proffiz.com>
2022-03-17 18:11:01 +0100

    added NFID provider

--- examples/create-ic-app-react-demo/frontend/Auth.tsx
+++ examples/create-ic-app-react-demo/frontend/Auth.tsx
@@ -5,24 +5,40 @@ import { AuthContent } from "./AuthContent"
 
 // Note: This is just a basic example to get you started
 function Auth() {
+  const [provider, setProvider] = React.useState<"II" | "NFID" | null>(null)
+
   return (
     <div className="auth-section">
-      <InternetIdentityProvider
-        authClientOptions={{
-          maxTimeToLive: BigInt(Date.now() + 7 * 24 * 60 * 60 * 1e9),
-          identityProvider: "https://identity.ic0.app/#authorize",
-          onSuccess: (principal) => {
-            console.log(">> onSuccess:", { principal })
-          },
-          onError: (e) => {
-            console.error(e)
-          },
-        }}
-      >
-        <div className="pt-8">
-          <AuthContent />
-        </div>
-      </InternetIdentityProvider>
+      {!provider || provider === "II" ? (
+        <InternetIdentityProvider
+          authClientOptions={{
+            maxTimeToLive: BigInt(Date.now() + 7 * 24 * 60 * 60 * 1e9),
+            identityProvider: "https://identity.ic0.app/#authorize",
+            onSuccess: (principal) => {
+              setProvider("II")
+            },
+          }}
+        >
+          <div className="pt-8">
+            <AuthContent reset={() => setProvider(null)} provider="II" />
+          </div>
+        </InternetIdentityProvider>
+      ) : null}
+      {!provider || provider === "NFID" ? (
+        <InternetIdentityProvider
+          authClientOptions={{
+            maxTimeToLive: BigInt(Date.now() + 7 * 24 * 60 * 60 * 1e9),
+            identityProvider: import.meta.env.VITE_NFID_PROVIDER_URL as string,
+            onSuccess: (principal) => {
+              setProvider("NFID")
+            },
+          }}
+        >
+          <div className="pt-8">
+            <AuthContent reset={() => setProvider(null)} provider="NFID" />
+          </div>
+        </InternetIdentityProvider>
+      ) : null}
     </div>
   )
 }
--- examples/create-ic-app-react-demo/frontend/AuthContent.tsx
+++ examples/create-ic-app-react-demo/frontend/AuthContent.tsx
@@ -1,9 +1,10 @@
 import { useInternetIdentity } from "@identity-labs/react-ic-ii-auth"
 import React from "react"
 
+import logo from "./assets/dfinity.svg"
 import nfid from "./assets/nfid-logo.svg"
 
-export const AuthContent = () => {
+export const AuthContent = ({ provider = "", reset = () => {} }) => {
   const { signout, authenticate, isAuthenticated, identity } =
     useInternetIdentity()
 
@@ -11,12 +12,21 @@ export const AuthContent = () => {
     await authenticate()
   }, [authenticate])
 
+  const signOut = () => {
+    signout()
+    reset()
+  }
+
   return (
     <div>
       {!isAuthenticated ? (
         <button onClick={() => handleAuthenticate()} className="auth-button">
           Sign in with
-          <img src={nfid} alt="" />
+          {provider === "NFID" ? (
+            <img src={nfid} alt="" />
+          ) : (
+            <img src={logo} alt="" />
+          )}
         </button>
       ) : null}
 
@@ -26,7 +36,7 @@ export const AuthContent = () => {
             <strong>Signed in as: </strong>
             {identity?.getPrincipal().toText()}
           </span>
-          <button onClick={signout} className="auth-button">
+          <button onClick={signOut} className="auth-button">
             Sign out
           </button>
         </div>

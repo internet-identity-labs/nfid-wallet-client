c838b73e95c8a342cb6517ae1524b08c2cb1351f
Philipp Litzenberger <philipp@bendcircular.com>
2022-03-17 22:15:55 +0100

    chore: upgrade dfx

--- examples/MultipassDemo/scripts/provision-linux.sh
+++ examples/MultipassDemo/scripts/provision-linux.sh
@@ -12,13 +12,11 @@ sudo apt-get install --yes nodejs
 rm install-node.sh
 
 # Install DFINITY SDK.
-DFX_VERSION=0.8.1 sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"
-
+DFX_VERSION=0.9.2 sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -o rustup.sh -sSf https://sh.rustup.rs
 bash rustup.sh -y
-
 
 # Exit temporary directory.
 popd

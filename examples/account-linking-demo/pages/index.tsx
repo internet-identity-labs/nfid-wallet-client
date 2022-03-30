3494fac39c467577e3e9de951ca26dfb70045fda
Philipp Litzenberger <philipp@bendcircular.com>
2022-03-30 10:05:06 +0200

    chore[sc-823]: setup sceleton

--- examples/account-linking-demo/pages/index.tsx
+++ examples/account-linking-demo/pages/index.tsx
@@ -1,53 +1,11 @@
 import React from "react"
-import { InternetIdentityProvider } from "@identity-labs/react-ic-ii-auth"
-
-import { Navigation } from "src/ui-lib/organisms/navigation"
-
-import { MPAuthComponent } from "src/components/mp-auth-component"
-import { IIAuthComponent } from "src/components/ii-auth-component"
+import { RegiserIIAccount } from "src/components/step-1-register-ii-account"
 
 function App() {
-  const [mountNFID, setMoundNFID] = React.useState(true)
-
   return (
     <div className="w-full h-screen">
-      <Navigation />
-      <div className="p-8 pt-20 md:p-8 md:w-8/12 md:m-auto md:pt-32 lg:pt-48 2xl:max-w-7xl">
-        {mountNFID && (
-          <InternetIdentityProvider
-            authClientOptions={{
-              maxTimeToLive: BigInt(Date.now() + 7 * 24 * 60 * 60 * 1e9),
-              identityProvider: process.env.NEXT_PUBLIC_MP_ID_PROVIDER_URL,
-              onSuccess: (principal) => {
-                console.log(">> onSuccess:", { principal })
-              },
-            }}
-          >
-            <div className="pt-8">
-              <MPAuthComponent />
-            </div>
-          </InternetIdentityProvider>
-        )}
-        <InternetIdentityProvider
-          authClientOptions={{
-            maxTimeToLive: BigInt(Date.now() + 7 * 24 * 60 * 60 * 1e9),
-            identityProvider:
-              process.env.NEXT_PUBLIC_ENV === "development"
-                ? process.env.NEXT_PUBLIC_II_ID_PROVIDER_URL
-                : "https://identity.ic0.app/#authorize",
-            onSuccess: (principal) => {
-              console.log(">> onSuccess:", { principal })
-            },
-          }}
-        >
-          <div className="pt-8">
-            <IIAuthComponent
-              unmountNFID={() => setMoundNFID(false)}
-              isUnmounted={!mountNFID}
-            />
-          </div>
-        </InternetIdentityProvider>
-      </div>
+      <div>NFID Account linking demo</div>
+      <RegiserIIAccount />
     </div>
   )
 }
---
+++ examples/account-linking-demo/src/components/step-1-register-ii-account/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+interface RegiserIIAccountProps {}
+
+export const RegiserIIAccount: React.FC<RegiserIIAccountProps> = ({
+  children,
+}) => {
+  return <div>RegiserIIAccount</div>
+}

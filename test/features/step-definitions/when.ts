53eba594c6bf9b34c1997127cd43c3b2015a8a91
Raghunath Venkata <sv_raghunath@hotmail.com>
2022-04-08 16:24:44 +0200

    Second commit - changes in feature file

--- test/features/step-definitions/when.ts
+++ test/features/step-definitions/when.ts
@@ -2,6 +2,6 @@ import { When } from "@cucumber/cucumber";
 import chai from "chai";
 import basePage from "../../page-objects/basePage"
 
-When(/^user clicks on our mission link on the navigation bar$/, async function () {
+When(/^user clicks on (.*) link on the navigation bar$/, async function (x) {
   await basePage.dummy();
 })

1629880e60ea8a8842ef4ccb9076badffa3e58a7
Philipp Litzenberger <philipp@bendcircular.com>
2022-04-08 23:56:06 +0200

    feat[sc-468]: include NFID_DOMAIN in bundle

--- craco.config.ts
+++ craco.config.ts
@@ -8,22 +8,28 @@ const webpack = require("webpack")
 
 loadEnv({ path: path.resolve(__dirname, ".env.local") })
 
-const caniserEnv = [
-  "INTERNET_IDENTITY_CANISTER_ID",
-  "IDENTITY_MANAGER_CANISTER_ID",
-  "PUB_SUB_CHANNEL_CANISTER_ID",
-].reduce(
-  (acc, key) => ({
-    ...acc,
-    [`process.env.${key}`]:
-      process.env.EXAMPLE_BUILD === "1"
-        ? JSON.stringify(`<${key}>`)
-        : JSON.stringify(
-            process.env[`${key}_${process.env.REACT_APP_BACKEND_MODE}`],
-          ),
-  }),
-  {},
-)
+const isExampleBuild = process.env.EXAMPLE_BUILD === "1"
+
+const FRONTEND_ENV = {
+  "process.env.NFID_DOMAIN": isExampleBuild
+    ? "<NFID_DOMAIN>"
+    : process.env.NFID_DOMAIN,
+  "process.env.INTERNET_IDENTITY_CANISTER_ID": isExampleBuild
+    ? "<INTERNET_IDENTITY_CANISTER_ID>"
+    : process.env[
+        `INTERNET_IDENTITY_CANISTER_ID_${process.env.REACT_APP_BACKEND_MODE}`
+      ],
+  "process.env.IDENTITY_MANAGER_CANISTER_ID": isExampleBuild
+    ? "<IDENTITY_MANAGER_CANISTER_ID>"
+    : process.env[
+        `INTERNET_IDENTITY_CANISTER_ID_${process.env.REACT_APP_BACKEND_MODE}`
+      ],
+  "process.env.PUB_SUB_CHANNEL_CANISTER_ID": isExampleBuild
+    ? "<PUB_SUB_CHANNEL_CANISTER_ID>"
+    : process.env[
+        `PUB_SUB_CHANNEL_CANISTER_ID_${process.env.REACT_APP_BACKEND_MODE}`
+      ],
+}
 
 // Gets the port dfx is running on from dfx.json
 const DFX_PORT = dfxJson.networks.local.bind.split(":")[1]
@@ -34,8 +40,8 @@ const config = {
       frontend: path.resolve(__dirname, "src"),
     },
     configure: (webpackConfig: any, { env, paths }: any) => {
-      console.log(">> ", { caniserEnv })
-      webpackConfig.plugins.push(new webpack.DefinePlugin(caniserEnv))
+      console.log(">> ", { FRONTEND_ENV })
+      webpackConfig.plugins.push(new webpack.DefinePlugin(FRONTEND_ENV))
       webpackConfig.plugins.push(
         new webpack.ProvidePlugin({
           Buffer: [require.resolve("buffer/"), "Buffer"],

efc8e24c91357872ca739d00257201b3d56ea97f
Philipp Litzenberger <philipp@bendcircular.com>
2022-04-12 16:39:15 +0200

    feat: structure ideas

---
+++ test/features/ThirdPartyLogin.feature
@@ -0,0 +1,3 @@
+@login @smoke @regression
+Feature: Login on ThirdParty application
+To check if user can register on NFID
--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -1,6 +1,14 @@
-import { Given } from "@cucumber/cucumber";
+import { Given } from "@cucumber/cucumber"
+
 import basePage from "../../page-objects/basePage"
 
 Given(/^User navigates to home page using$/, async function () {
-  await basePage.navigateTo('https://dq6kg-laaaa-aaaah-aaeaq-cai.raw.ic0.app/');
+  await basePage.navigateTo("https://dq6kg-laaaa-aaaah-aaeaq-cai.raw.ic0.app/")
 })
+
+Given(/^User is registered$/, async function () {})
+
+Given(
+  /^User opens third party application for the first time$/,
+  async function () {},
+)

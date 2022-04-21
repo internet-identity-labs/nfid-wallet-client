a14500692e97ee594be088ce65c9f8d09e086d8a
Philipp Litzenberger <philipp@bendcircular.com>
2022-04-21 10:51:40 +0200

    chore: use baseUrl

--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -1,6 +1,7 @@
-import { Given } from "@cucumber/cucumber";
+import { Given } from "@cucumber/cucumber"
+
 import basePage from "../../page-objects/basePage"
 
 Given(/^User navigates to home page using$/, async function () {
-  await basePage.navigateTo('https://dq6kg-laaaa-aaaah-aaeaq-cai.raw.ic0.app/');
+  await basePage.navigateTo("/")
 })

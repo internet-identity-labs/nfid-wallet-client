928c3c0a1ecabf952da03d1867c53860dc2134bf
Wisdom Tetteh <wisdom.tetteh@tech11.com>
2022-04-21 10:51:38 +0200

    reverts run .conf file

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -84,17 +84,7 @@ export const config: WebdriverIO.Config = {
       maxInstances: 5,
       browserName: 'chrome',
       acceptInsecureCerts: true,
-       "goog:chromeOptions": {
-               args: [
-                 "--headless",
-                 "--ignore-certificate-errors", // allow self-signed certificates
-                 "--disable-gpu",
-                 `--window-size=${runConfig.screenConfiguration.windowSize}`,
-                "--no-sandbox", 
-                "--single-process",
-               ],
     }
-   }
   ],
   //
   // ===================

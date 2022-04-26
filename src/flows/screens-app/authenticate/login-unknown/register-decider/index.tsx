d4bb89c68c9b33041239e3846c48c7b4d186ddee
Pavlo Chorniy <pchorniy@proffiz.com>
2022-04-26 19:07:34 +0200

    Open register decider after recovery phrase

--- src/flows/screens-app/authenticate/login-unknown/register-decider/index.tsx
+++ src/flows/screens-app/authenticate/login-unknown/register-decider/index.tsx
@@ -1,9 +1,14 @@
-import { Card, CardBody } from "@internet-identity-labs/nfid-sdk-react"
+import { Card, CardBody, Loader } from "@internet-identity-labs/nfid-sdk-react"
 import clsx from "clsx"
-import React from "react"
+import React, { useState } from "react"
+import {useNavigate} from "react-router-dom"
 
 import { AppScreen } from "frontend/design-system/templates/AppScreen"
 import { AuthorizeRegisterDecider } from "frontend/screens/authorize-register-decider"
+import { useDevices } from "frontend/services/identity-manager/devices/hooks"
+import { useAccount } from "frontend/services/identity-manager/account/hooks"
+import { useAuthentication } from "frontend/hooks/use-authentication"
+import { useUnknownDeviceConfig } from "frontend/flows/screens-iframe/authenticate/login-unknown/hooks/use-unknown-device.config"
 
 interface AuthorizeRegisterDeciderProps
   extends React.DetailedHTMLProps<
@@ -14,6 +19,15 @@ interface AuthorizeRegisterDeciderProps
 export const AppScreenAuthorizeRegisterDecider: React.FC<
   AuthorizeRegisterDeciderProps
 > = ({ children, className }) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const { createDevice } = useDevices()
+  const { readAccount } = useAccount()
+  const { identityManager } = useAuthentication()
+  const navigate = useNavigate()
+
+  const { userNumber } = useUnknownDeviceConfig()
+  const { createWebAuthNDevice } = useDevices()
+
   return (
     <AppScreen>
       <main className={clsx("flex flex-1")}>
@@ -22,15 +36,28 @@ export const AppScreenAuthorizeRegisterDecider: React.FC<
             <CardBody className="col-span-12 lg:col-span-10 xl:col-span-6">
               <AuthorizeRegisterDecider
                 iframe
-                onRegister={() => {
-                  console.log("register me")
+                onRegister={async () => {
+                    setIsLoading(true)
+                    if (!userNumber) {
+                      return console.error(`Missing userNumber: ${userNumber}`)
+                    }
+
+                    const { device } = await createWebAuthNDevice(BigInt(userNumber))
+                    await createDevice({
+                      ...device,
+                      userNumber,
+                    })
+                    await readAccount(identityManager, userNumber)
+
+                    setIsLoading(false)
+                    navigate('/profile')
                 }}
                 onLogin={() => {
-                  console.log("log me in")
+                  navigate('/profile')
                 }}
               />
 
-              {/* <Loader isLoading={isLoading} /> */}
+               <Loader isLoading={isLoading} fullscreen />
             </CardBody>
           </Card>
         </div>

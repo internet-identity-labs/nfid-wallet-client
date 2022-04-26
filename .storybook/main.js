bdb6900249594ead836548952014c807b6b279b0
Philipp Litzenberger <philipp@bendcircular.com>
2022-04-26 19:06:39 +0200

    chore: configure storybook and tailwind
    (cherry picked from commit c5f55eebe0b7db10261c28494f156b82a2b3cdd0)

---
+++ .storybook/main.js
@@ -0,0 +1,75 @@
+const path = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  typescript: {
+    check: true, // type-check stories during Storybook build
+  },
+  webpackFinal: async (config) => {
+    const canisterEnv = {
+      IC_HOST: JSON.stringify(process.env.IC_HOST),
+      II_ENV: JSON.stringify(process.env.II_MODE),
+      FRONTEND_MODE: JSON.stringify(process.env.FRONTEND_MODE),
+      USERGEEK_API_KEY: JSON.stringify(process.env.USERGEEK_API_KEY),
+      VERIFY_PHONE_NUMBER: JSON.stringify(
+        process.env.FRONTEND_MODE === "production"
+          ? process.env.AWS_VERIFY_PHONENUMBER
+          : "/verify",
+      ),
+      INTERNET_IDENTITY_CANISTER_ID: JSON.stringify(
+        process.env[
+          `INTERNET_IDENTITY_CANISTER_ID_${process.env.BACKEND_MODE}`
+        ],
+      ),
+      IDENTITY_MANAGER_CANISTER_ID: JSON.stringify(
+        process.env[`IDENTITY_MANAGER_CANISTER_ID_${process.env.BACKEND_MODE}`],
+      ),
+      PUB_SUB_CHANNEL_CANISTER_ID: JSON.stringify(
+        process.env[`PUB_SUB_CHANNEL_CANISTER_ID_${process.env.BACKEND_MODE}`],
+      ),
+    }
+
+    config.plugins.push(new webpack.DefinePlugin(canisterEnv))
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        Buffer: [require.resolve("buffer/"), "Buffer"],
+        process: require.resolve("process/browser"),
+      }),
+    )
+
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, "../src"),
+    ]
+
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          frontend: path.resolve(__dirname, "../src"),
+        },
+        fallback: {
+          ...config.resolve.fallback,
+          assert: require.resolve("assert"),
+          buffer: require.resolve("buffer"),
+          events: require.resolve("events"),
+          stream: require.resolve("stream-browserify"),
+          util: require.resolve("util"),
+        },
+      },
+    }
+  },
+}
--- src/flows/screens-app/profile/index.tsx
+++ src/flows/screens-app/profile/index.tsx
@@ -1,28 +1,11 @@
-import {
-  Button,
-  ButtonMenu,
-  Card,
-  CardBody,
-  DotsIcon,
-  H2,
-  H5,
-  Input,
-  List,
-  ListItem,
-  Loader,
-  LogoutIcon,
-  ModalAdvanced,
-  ModalAdvancedProps,
-  PlusIcon,
-  P,
-} from "@internet-identity-labs/nfid-sdk-react"
+import { ModalAdvancedProps } from "@internet-identity-labs/nfid-sdk-react"
 import React from "react"
-import { MdLaptopMac } from "react-icons/md"
 
-import { AppScreen } from "frontend/design-system/templates/AppScreen"
 import { useAuthentication } from "frontend/hooks/use-authentication"
+import { Profile } from "frontend/screens/profile"
 import { useAccount } from "frontend/services/identity-manager/account/hooks"
 import { useDevices } from "frontend/services/identity-manager/devices/hooks"
+import { PublicKey } from "frontend/services/internet-identity/generated/internet_identity_types"
 
 interface AuthenticateNFIDHomeProps
   extends React.DetailedHTMLProps<
@@ -30,12 +13,13 @@ interface AuthenticateNFIDHomeProps
     HTMLDivElement
   > {}
 
-export const AuthenticateNFIDHome: React.FC<AuthenticateNFIDHomeProps> = ({
+export const NFIDProfile: React.FC<AuthenticateNFIDHomeProps> = ({
   children,
   className,
 }) => {
   const applications: any[] = ["NFID Demo"]
 
+  const [hasPoa, setHasPoa] = React.useState(false)
   const [showModal, setShowModal] = React.useState(false)
   const [loading, setLoading] = React.useState(false)
   const [modalOptions, setModalOptions] =
@@ -43,10 +27,17 @@ export const AuthenticateNFIDHome: React.FC<AuthenticateNFIDHomeProps> = ({
 
   const { devices, deleteDevice, handleLoadDevices } = useDevices()
   const { account } = useAccount()
-  const { logout } = useAuthentication()
+  const { logout, imAddition } = useAuthentication()
+
+  React.useEffect(() => {
+    imAddition &&
+      imAddition.has_poap().then((response) => {
+        setHasPoa(response)
+      })
+  }, [imAddition])
 
   const handleDeleteDevice = React.useCallback(
-    (publicKey) => async () => {
+    (publicKey: PublicKey) => async () => {
       setLoading(true)
 
       await deleteDevice(publicKey)
@@ -59,190 +50,17 @@ export const AuthenticateNFIDHome: React.FC<AuthenticateNFIDHomeProps> = ({
   )
 
   return (
-    <AppScreen
-      bubbleOptions={{
-        showBubbles: true,
-        bubbleColors: ["#a69cff", "#4df1ffa8"],
-        bubbleClassNames: [
-          "",
-          "top-[20vh] right-[-15vw] md:top-56 md:right-[9vw]",
-        ],
-      }}
-      classNameWrapper="relative mt-[150px]"
-      navigationItems={
-        <Button text icon onClick={logout}>
-          <LogoutIcon />
-          <span className="hidden md:block">Logout</span>
-        </Button>
-      }
-    >
-      <div className="absolute top-0 left-0 w-full h-full bg-white"></div>
-
-      <Card className="relative grid grid-cols-12">
-        <CardBody className="col-span-12">
-          <div className="mt-[-150px]">
-            <H2 className="block py-2">NFID Number</H2>
-            <P className="">{account?.anchor}</P>
-          </div>
-
-          <div className="grid grid-cols-12 gap-4 py-12 md:py-14">
-            <div className="col-span-12 md:col-span-6">
-              <List>
-                <List.Header>
-                  <div className="mb-5">
-                    <H5>Applications</H5>
-                  </div>
-                </List.Header>
-                <List.Items>
-                  {applications.length > 0 &&
-                    applications.map((application, index) => (
-                      <ListItem
-                        key={index}
-                        title={application}
-                        icon={
-                          <span className="text-xl font-medium text-blue-base">
-                            {application[0]}
-                          </span>
-                        }
-                      />
-                    ))}
-                </List.Items>
-              </List>
-            </div>
-            <div className="col-span-12 md:col-span-6">
-              <List>
-                <List.Header>
-                  <div className="flex items-center justify-between mb-5">
-                    <H5>Access points</H5>
-
-                    <div className="hidden">
-                      <PlusIcon className="text-blue-base mr-[14px]" />
-                    </div>
-                  </div>
-                </List.Header>
-                <List.Items>
-                  {devices.map((device, index) => (
-                    <ListItem
-                      key={device.alias}
-                      title={device.alias}
-                      subtitle={""}
-                      icon={<MdLaptopMac className="text-xl text-blue-base" />}
-                      action={
-                        <ButtonMenu buttonElement={<DotsIcon />}>
-                          {(toggle) => (
-                            <>
-                              <li
-                                className="hover:bg-gray-200"
-                                onClick={(e) => {
-                                  e.stopPropagation()
-                                  toggle()
-                                  setShowModal(true)
-
-                                  setModalOptions({
-                                    title: "Rename access point",
-                                    children: (
-                                      <>
-                                        <Input
-                                          autoFocus
-                                          labelText="Access point name"
-                                          defaultValue={device.alias}
-                                        />
-                                      </>
-                                    ),
-                                    primaryButton: {
-                                      text: "Rename",
-                                      type: "primary",
-                                      onClick: () => {
-                                        console.log("rename")
-                                        setShowModal(false)
-                                      },
-                                    },
-                                    secondaryButton: {
-                                      text: "Cancel",
-                                      type: "secondary",
-                                      onClick: () => {
-                                        console.log("cancel")
-                                        setShowModal(false)
-                                      },
-                                    },
-                                  })
-                                }}
-                              >
-                                <div className="block px-4 py-2 text-sm">
-                                  Rename
-                                </div>
-                              </li>
-                              <li
-                                className="hover:bg-gray-200"
-                                onClick={(e) => {
-                                  e.stopPropagation()
-                                  toggle()
-                                  setShowModal(true)
-
-                                  setModalOptions({
-                                    title: "Delete access point",
-                                    children: (
-                                      <P>
-                                        Do you really want to delete{" "}
-                                        <span className="font-bold">
-                                          {device.alias}
-                                        </span>{" "}
-                                        access point? This process cannot be
-                                        undone.
-                                      </P>
-                                    ),
-                                    primaryButton: {
-                                      text: "Delete",
-                                      type: "error",
-                                      onClick: handleDeleteDevice(
-                                        device.pubkey,
-                                      ),
-                                    },
-                                    secondaryButton: {
-                                      text: "Cancel",
-                                      type: "secondary",
-                                      onClick: () => {
-                                        console.log("cancel")
-                                        setShowModal(false)
-                                      },
-                                    },
-                                  })
-                                }}
-                              >
-                                <div className="block px-4 py-2 text-sm">
-                                  Delete
-                                </div>
-                              </li>
-                            </>
-                          )}
-                        </ButtonMenu>
-                      }
-                    />
-                    // {/*
-                    //   AVAILABLE ICONS:
-                    //     MdLaptopChromebook - MdLaptopMac - MdLaptopWindows -
-                    //     MdPhoneAndroid - MdPhoneIphone -
-                    //     MdDesktopMac - MdDesktopWindows
-                    //   */}
-                  ))}
-                </List.Items>
-              </List>
-            </div>
-          </div>
-        </CardBody>
-
-        {showModal && modalOptions && (
-          <ModalAdvanced
-            title={modalOptions.title}
-            onClose={() => setShowModal(false)}
-            primaryButton={modalOptions.primaryButton}
-            secondaryButton={modalOptions.secondaryButton}
-          >
-            {modalOptions.children}
-            <Loader isLoading={loading} />
-          </ModalAdvanced>
-        )}
-      </Card>
-    </AppScreen>
+    <Profile
+      onLogout={logout}
+      account={account}
+      applications={applications}
+      onDeleteDeviceFactory={handleDeleteDevice}
+      showModal={showModal}
+      modalOptions={modalOptions}
+      setModalOptions={setModalOptions}
+      setShowModal={setShowModal}
+      devices={devices}
+      loading={loading}
+    />
   )
 }

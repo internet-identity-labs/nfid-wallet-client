ad4b5d18bce7dbd2a18a0fc1382743f348670da8
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-04-27 10:27:06 +0200

    feat[sc-1539]: layout wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}", "./public/**/*.{html,svg}"],
   plugins: [
     require("@tailwindcss/forms"),
     require("@internet-identity-labs/nfid-ui-kit-core"),
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "poa-dog-screen":
+          "url('src/screens/proof-of-attendency/image_dog.svg')",
+        "poa-dog-award":
+          "url('src/screens/proof-of-attendency-award/dog_image.svg')",
+      },
       fontFamily: {
         ...defaultTheme.fontFamily,
         sans: ["Inter", ...defaultTheme.fontFamily.sans],

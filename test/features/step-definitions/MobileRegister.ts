722579ac657e7bb4584adeca6bacf01eb37a1b18
Philipp Litzenberger <philipp@bendcircular.com>
2022-05-02 15:41:15 +0200

    feat: structure ideas

---
+++ test/features/step-definitions/MobileRegister.ts
@@ -0,0 +1,21 @@
+import { Given, When, Then } from "@cucumber/cucumber"
+
+Given(/^The user is on mobile device$/, () => {
+  console.log("implement me")
+})
+
+When(/^user clicks on register$/, () => {
+  console.log("implement me")
+})
+
+When(/^user clicks on the qrcode$/, () => {
+  console.log("implement me")
+})
+
+Then(/^the user should see something$/, () => {
+  console.log("implement me")
+})
+
+Then(/^the user should see something else$/, () => {
+  console.log("implement me")
+})
--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -10,3 +10,10 @@ const getUrl = (basePath: string) => {
 Given(/^User navigates to home page using$/, async function () {
   await basePage.navigateTo(getUrl("/"))
 })
+
+Given(/^User is registered$/, async function () {})
+
+Given(
+  /^User opens third party application for the first time$/,
+  async function () {},
+)

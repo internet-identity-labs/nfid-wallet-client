9331b095dddc8689b9a55d580ad968ef84b69e00
Wisdom Tetteh <tettehwisdom.itil@gmail.com>
2022-05-02 15:43:42 +0200

    re-structures test folder

--- test/step-definitions/when.ts
+++ test/step-definitions/when.ts
@@ -1,6 +1,14 @@
 import { When } from "@cucumber/cucumber";
-import basePage from "./basePage"
+import basePage from "../step-definitions/basePage"
 
 When(/^user clicks on (.*) link on the navigation bar$/, async function (sectionName) {
   await basePage.clickOnSection(sectionName);
+})
+
+When(/^user clicks on register$/, () => {
+  console.log("implement me")
+})
+
+When(/^user clicks on the qrcode$/, () => {
+  console.log("implement me")
 })

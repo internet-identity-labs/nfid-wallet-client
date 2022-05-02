32e91099ac6ad43d2e7b4a77c33a1cb9c25a0ec2
Wisdom Tetteh <tettehwisdom.itil@gmail.com>
2022-05-02 15:44:04 +0200

    adds scroll and checkTitle actions

--- test/step-definitions/then.ts
+++ test/step-definitions/then.ts
@@ -2,6 +2,9 @@
 import { Then } from "@cucumber/cucumber";
 import chai from "chai";
 import basePage from "../step-definitions/basePage"
+import isEnabled from "../page-objects/checks/isEnabled"
+import isDisplayed from "../page-objects/checks/isDisplayed"
+import isExisting from "../page-objects/checks/isEnabled"
 
 Then(/^user should be navigated to (.*)$/, async function (sectionName) {
   chai.expect(await basePage.getSectionName(sectionName)).to.contains(sectionName)
--- test/step-definitions/when.ts
+++ test/step-definitions/when.ts
@@ -1,5 +1,8 @@
 import { When } from "@cucumber/cucumber";
 import basePage from "../step-definitions/basePage"
+import isEnabled from "../page-objects/checks/isEnabled"
+import isDisplayed from "../page-objects/checks/isDisplayed"
+import isExisting from "../page-objects/checks/isEnabled"
 
 When(/^user clicks on (.*) link on the navigation bar$/, async function (sectionName) {
   await basePage.clickOnSection(sectionName);

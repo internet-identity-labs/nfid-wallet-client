3740d4d278f2f4e2a86a8747414c62209b9af859
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-05-04 17:29:41 +0200

    chore([sc-1344]): add specs

---
+++ test/support/action/clearInputField.spec.js
@@ -0,0 +1,20 @@
+import clearInputField from "./clearInputField"
+
+let clearElementMock
+
+describe("clearInputField", () => {
+  let element
+
+  beforeEach(() => {
+    clearElementMock = jest.fn()
+    global.$ = jest.fn().mockReturnValue({
+      clearValue: clearElementMock,
+    })
+    element = "element_selector"
+  })
+
+  it("should call clearElement on the browser", async () => {
+    await clearInputField(element)
+    expect(clearElementMock).toHaveBeenCalled()
+  })
+})
---
+++ test/support/action/closeAllButFirstTab.spec.js
@@ -0,0 +1,20 @@
+import closeAllButFirstTab from "./closeAllButFirstTab"
+
+describe("closeAllButFirstTab", () => {
+  beforeEach(() => {
+    global.browser = {
+      getWindowHandles: jest.fn().mockResolvedValue(["one", "two", "three"]),
+      switchToWindow: jest.fn().mockResolvedValue({
+        close: jest.fn().mockResolvedValue({}),
+      }),
+      closeWindow: jest.fn(),
+    }
+  })
+
+  it("should call closeAllButFirstTab on the browser", async () => {
+    await closeAllButFirstTab("")
+
+    expect(global.browser.getWindowHandles).toHaveBeenCalledTimes(1)
+    expect(global.browser.switchToWindow).toHaveBeenCalledTimes(3)
+  })
+})

ec9946ab6257a8eeb489e1cfcbde6bd900adcc84
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-05-04 17:29:41 +0200

    chore([sc-1344]): add specs

---
+++ test/support/action/focusLastOpenedWindow.spec.js
@@ -0,0 +1,18 @@
+import focusLastOpenedWindow from "./focusLastOpenedWindow"
+
+describe("focusLastOpenedWindow", () => {
+  beforeEach(() => {
+    global.browser = {
+      getWindowHandles: jest.fn().mockResolvedValue(["one", "two", "three"]),
+      switchToWindow: jest.fn(),
+    }
+  })
+
+  it("should call focusLastOpenedWindow on the browser", async () => {
+    await focusLastOpenedWindow("")
+
+    expect(global.browser.getWindowHandles).toHaveBeenCalledTimes(1)
+    expect(global.browser.switchToWindow).toHaveBeenCalledTimes(1)
+    expect(global.browser.switchToWindow).toHaveBeenCalledWith("three")
+  })
+})

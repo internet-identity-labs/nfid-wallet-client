0f602d3acd80c61a776b74061a72d3c2f46b061e
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-05-04 21:20:49 +0200

    refactor([sc-1344]): home page test suite

--- test/steps/support/action/openWebsite.ts
+++ test/steps/support/action/openWebsite.ts
@@ -1,3 +1,8 @@
+import DFX_JSON from "../../../../.dfx/local/canister_ids.json"
+
+const appendCanisterId = (basePath: string) => {
+  return `${basePath}?canisterId=${DFX_JSON.assets.local}`
+}
 /**
  * Open the given URL
  * @param  {String}   type Type of navigation (getUrl or site)
@@ -8,6 +13,7 @@ export default async (type: "url" | "site", page: string) => {
    * The URL to navigate to
    * @type {String}
    */
-  const url = type === "url" ? page : browser.options.baseUrl + page
+  const url =
+    type === "url" ? page : browser.options.baseUrl + appendCanisterId(page)
   await browser.url(url)
 }

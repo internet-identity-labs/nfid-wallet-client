4c644606bf18252a38d16f7be967b8522b5b25a8
Wisdom Tetteh <tettehwisdom.itil@gmail.com>
2022-05-04 17:29:40 +0200

    adds support actions and checks

--- test/support/check/checkTitleContains.ts
+++ test/support/check/checkTitleContains.ts
@@ -1,26 +1,27 @@
 /**
- * Check the title of the current browser window
- * @param  {Type}     falseCase     Whether to check if the title matches the
+ * Check the title of the current browser window contains expected text/title
+ * @param  {Type}     falseCase     Whether to check if the title contains the
  *                                  expected value or not
  * @param  {Type}     expectedTitle The expected title
  */
 export default async (falseCase: boolean, expectedTitle: string) => {
-    
+    /**
+     * The actual title of the current browser window
      * @type {String}
      */
     const title = await browser.getTitle();
 
     if (falseCase) {
-        expect(title).not.toEqual(
+        expect(title).not.toContain(
             expectedTitle,
             // @ts-expect-error
-            `Expected title not to be "${expectedTitle}"`
+            `Expected title not to contain "${expectedTitle}"`
         );
     } else {
-        expect(title).toEqual(
+        expect(title).toContain(
             expectedTitle,
             // @ts-expect-error
-            `Expected title to be "${expectedTitle}" but found "${title}"`
+            `Expected title to contain "${expectedTitle}" but found "${title}"`
         );
     }
 };

3eeaa3711dfb89979dab4ebafb5aa9319de90aab
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-05-04 20:33:56 +0200

    chore([sc-1344]): update folder structure

--- test/support/action/scroll.ts
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'webdriverio';
-
-/**
- * Scroll the page to the given element
- * @param  {String}   selector Element selector
- */
-export default async (selector: Selector) => {
-    await $(selector).scrollIntoView();
-};

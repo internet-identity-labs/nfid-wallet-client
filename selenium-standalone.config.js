af4337ef351dd091fb1d45d8f6a28f82866301d5
Raghunath Venkata <sv_raghunath@hotmail.com>
2022-05-08 11:12:45 +0100

    ci([sc-1344]): chrome version updated

--- selenium-standalone.config.js
+++ selenium-standalone.config.js
@@ -2,7 +2,7 @@ module.exports = {
   drivers: {
     chrome: {
       // This version needs to match the chrome version on GitHub Actions
-      version: '100.0.4896.60',
+      version: '99.0.4844.51',
       arch: process.arch,
       baseURL: 'https://chromedriver.storage.googleapis.com'
     },

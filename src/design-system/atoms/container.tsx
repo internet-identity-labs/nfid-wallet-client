47bace322d0fbe4e588670754fd9b6d977f24237
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-05-11 13:52:28 +0200

    style([sc-1546]): copy recovery phrase

--- src/design-system/atoms/container.tsx
+++ src/design-system/atoms/container.tsx
@@ -1,9 +1,10 @@
+import clsx from "clsx"
 import React from "react"
 
-export const CONTAINER_CLASSES = "container mx-auto"
+export const CONTAINER_CLASSES = "container mx-auto px-5"
 
 interface ContainerProps {}
 
 export const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div className={CONTAINER_CLASSES}>{children}</div>
+  return <div className={clsx(CONTAINER_CLASSES)}>{children}</div>
 }

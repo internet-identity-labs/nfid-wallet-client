c9ca1db2a31f3cc9663e88cd5b092f94f48176f0
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-05-18 10:05:33 +0200

    fix([sc-1495]): padding for register decider

--- src/design-system/organisms/navigation/navigation-popup/popup-register-decider.tsx
+++ src/design-system/organisms/navigation/navigation-popup/popup-register-decider.tsx
@@ -7,7 +7,6 @@ import {
 import clsx from "clsx"
 import React, { useState } from "react"
 
-import logo from "frontend/assets/logo.svg"
 import { useRegisterQRCode } from "frontend/flows/screens-app/landing-page/register-qrcode/use-register-qrcode"
 import { useAuthentication } from "frontend/hooks/use-authentication"
 import { useDeviceInfo } from "frontend/hooks/use-device-info"
@@ -62,7 +61,9 @@ export const PopupRegisterDecider: React.FC<PopupRegisterDeciderProps> = () => {
   }, [setStatus])
 
   return (
-    <div className={clsx(isLoading && "pointer-events-none")}>
+    <div
+      className={clsx(isLoading && "pointer-events-none", "pt-6 px-6 mx-auto")}
+    >
       <div
         className={clsx(
           "flex justify-center items-center",
@@ -78,7 +79,6 @@ export const PopupRegisterDecider: React.FC<PopupRegisterDeciderProps> = () => {
           fullscreen={false}
         />
       </div>
-      <img src={logo} alt="logo" className="w-20 my-8" />
       <H5 className="mb-4">Log in faster on this device</H5>
       <div>
         Trust this {device}? You can quickly and securely log in the next time

f8e346d98cf0fe895f3a8b8d726cdba0bd87e829
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-05-20 20:51:35 +0200

    feat([sc-2062]): styles loading state

--- src/design-system/molecules/challenge/index.tsx
+++ src/design-system/molecules/challenge/index.tsx
@@ -1,6 +1,8 @@
 import clsx from "clsx"
 import React from "react"
 
+import spinner from "./spinner.png"
+
 interface ChallengeProps {
   src?: string
 }
@@ -14,7 +16,18 @@ export const Challenge: React.FC<ChallengeProps> = ({ src }) => {
       )}
     >
       {!src ? (
-        <div className="my-auto text-center">Loading challenge ...</div>
+        <div className="flex flex-col h-full my-auto text-center animate-pulse center">
+          <div className="h-5 m-auto">
+            <img
+              className="inline-block animate-spin"
+              alt="spinner"
+              src={spinner}
+            />
+            <span className="inline-block ml-2 text-xs font-normal align-middle opacity-40">
+              Generating captcha, this might take a few seconds
+            </span>
+          </div>
+        </div>
       ) : (
         <img alt="captcha" src={src} className="object-contain w-full h-full" />
       )}

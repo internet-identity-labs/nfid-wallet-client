b0c6f11de846a3fa6d212edcdd6e6f45a9078e8c
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-05-31 11:06:45 +0200

    chore([sc-2027]): redesigned single account authorize app

--- src/screens/authorize-app/single-account.tsx
+++ src/screens/authorize-app/single-account.tsx
@@ -4,47 +4,29 @@ import React from "react"
 
 import { CONTAINER_CLASSES } from "frontend/design-system/atoms/container"
 import { AppScreen } from "frontend/design-system/templates/AppScreen"
+import { IFrameTemplate } from "frontend/design-system/templates/IFrameTemplate"
 import { ElementProps } from "frontend/types/react"
 
 import MobileHero from "./assets/mobile_hero.svg"
 
 interface AuthorizeAppSingleAccountProps extends ElementProps<HTMLDivElement> {
+  applicationLogo: string
   applicationName: string
   onContinueButtonClick: () => Promise<void>
 }
 
 export const AuthorizeAppSingleAccount: React.FC<
   AuthorizeAppSingleAccountProps
-> = ({ applicationName, onContinueButtonClick }) => {
+> = ({ applicationName, onContinueButtonClick, applicationLogo }) => {
   return (
-    <AppScreen isFocused showLogo>
-      <main className={clsx("flex flex-1")}>
-        <div className={clsx(CONTAINER_CLASSES)}>
-          <div className="grid h-full grid-cols-12">
-            <div className="flex flex-col col-span-12 md:col-span-11 lg:col-span-7">
-              <img
-                className="max-w-max w-[120%] -ml-[10%]"
-                src={MobileHero}
-                alt=""
-              />
-              <div>
-                <P className="text-base font-bold text-gray-400">
-                  {applicationName}
-                </P>
-                <H4>Sign in with NFID</H4>
-                <Button
-                  className="mt-8"
-                  largeMax
-                  secondary
-                  onClick={onContinueButtonClick}
-                >
-                  Continue
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-    </AppScreen>
+    <IFrameTemplate className="flex flex-col items-center">
+      <img src={applicationLogo} alt="logo" />
+      <H4 className="mt-4">Unlock NFID</H4>
+      <p className="mt-2 text-sm">to continue to {applicationName}</p>
+      <img className="w-full max-w-max" src={MobileHero} alt="" />
+      <Button className="my-6 " block secondary onClick={onContinueButtonClick}>
+        Unlock to continue
+      </Button>
+    </IFrameTemplate>
   )
 }

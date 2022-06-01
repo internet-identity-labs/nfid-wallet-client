45874dbf43ea769496bd4de525b1bc99a1438d7a
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-06-01 17:15:36 +0200

    chore([sc-1917]): implemented add recovery method modal

--- src/screens/profile/recovery-methods/index.tsx
+++ src/screens/profile/recovery-methods/index.tsx
@@ -1,9 +1,12 @@
 import clsx from "clsx"
-import React from "react"
+import React, { ReactElement, useState } from "react"
 
 import { PlusIcon } from "frontend/design-system/atoms/icons/plus"
+import { IconRecovery } from "frontend/design-system/atoms/icons/recovery"
+import { USBIcon } from "frontend/design-system/atoms/icons/usb"
 import { H5 } from "frontend/design-system/atoms/typography"
 import { List } from "frontend/design-system/molecules/list"
+import { ModalAdvanced } from "frontend/design-system/molecules/modal/advanced"
 
 import { recoveryMethod } from "../"
 import { RecoveryMethodListItem } from "./recovery-list-item"
@@ -19,14 +22,40 @@ export const RecoveryMethodsList: React.FC<RecoveryMethodsListProps> = ({
   onRecoveryUpdate,
   onRecoveryDelete,
 }) => {
+  const [isModalVisible, setIsModalVisible] = useState(true)
+
   return (
     <div className={clsx("px-5 md:px-16 pt-8", "bg-white flex-1 md:pt-16")}>
+      {isModalVisible && (
+        <ModalAdvanced
+          onClose={() => setIsModalVisible(false)}
+          title={"Account recovery"}
+          backgroundClassnames="opacity-40"
+        >
+          <p className="text-sm">
+            Create a secret phrase or add a security key as a backup plan in
+            case you lose your other devices.
+          </p>
+          <div className="mt-3 space-y-2">
+            <MethodRaw
+              title="Secret recovery phrase"
+              subtitle="A “master password” to keep offline"
+              img={<IconRecovery />}
+            />
+            <MethodRaw
+              title="Security key"
+              subtitle="A special USB stick to keep safe"
+              img={<USBIcon />}
+            />
+          </div>
+        </ModalAdvanced>
+      )}
       <List>
         <List.Header>
           <div className="flex items-center justify-between mb-3">
             <H5>Account recovery methods</H5>
 
-            <div className="">
+            <div className="" onClick={() => setIsModalVisible(true)}>
               <PlusIcon className="w-6 h-6 text-gray-500" />
             </div>
           </div>
@@ -37,16 +66,8 @@ export const RecoveryMethodsList: React.FC<RecoveryMethodsListProps> = ({
               <RecoveryMethodListItem
                 key={`${method.label}-${method.lastUsed}`}
                 recoveryMethod={method}
-                onRecoveryUpdate={function (
-                  recoveryMethod: recoveryMethod,
-                ): Promise<void> {
-                  throw new Error("Function not implemented.")
-                }}
-                onRecoveryDelete={function (
-                  recoveryMethod: recoveryMethod,
-                ): Promise<void> {
-                  throw new Error("Function not implemented.")
-                }}
+                onRecoveryUpdate={onRecoveryUpdate}
+                onRecoveryDelete={onRecoveryDelete}
               />
             ))}
           {!recoveryMethods.length && (
@@ -63,3 +84,18 @@ export const RecoveryMethodsList: React.FC<RecoveryMethodsListProps> = ({
     </div>
   )
 }
+interface MethodRawProps {
+  img: ReactElement
+  title: string
+  subtitle: string
+}
+
+const MethodRaw: React.FC<MethodRawProps> = ({ img, title, subtitle }) => (
+  <div className="flex items-center w-full px-3 py-2 border border-gray-200 rounded-md hover:border-blue-light transition-all cursor-pointer hover:bg-[#F4FAFF]">
+    <div className="w-[28px] mr-[9px]">{img}</div>
+    <div>
+      <p className="text-sm">{title}</p>
+      <p className="text-[11px] text-gray-400">{subtitle}</p>
+    </div>
+  </div>
+)

ae753ec87730cd09e91e22c8ddba637808792145
Pavlo Chorniy <pchorniy@proffiz.com>
2022-06-03 10:58:02 +0200

    chore([sc-1862]): raw authorizeapp

---
+++ src/screens/authorize-app-unknown-device-new/index.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { BrowserRouter as Router } from "react-router-dom"
+import { AuthorizeAppUnknownDevice } from "./"
+
+export default {
+  title: "Screens/AuthorizeAppUnknownDeviceNew",
+  component: AuthorizeAppUnknownDevice,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof AuthorizeAppUnknownDevice>
+
+const AppScreenRegisterDeviceDeciderTemplate: ComponentStory<
+  typeof AuthorizeAppUnknownDevice
+> = (args) => {
+  return (
+    <Router>
+      <AuthorizeAppUnknownDevice {...args} />
+    </Router>
+  )
+}
+
+export const AppScreen = AppScreenRegisterDeviceDeciderTemplate.bind({})
+
+AppScreen.args = {
+  applicationName: "My Application",
+  url: "https://nfid.one/secret/scope/my-application",
+  isLoading: false,
+  registerDeviceDeciderPath: "/register-device-decider",
+}

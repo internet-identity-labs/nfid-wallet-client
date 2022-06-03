147ec11b0daac00a3239e3569899e04df720ad11
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-06-03 10:58:22 +0200

    feat([sc-1855]): connect register intro screen

--- src/screens/authorize-app-unknown-device-new/index.tsx
+++ src/screens/authorize-app-unknown-device-new/index.tsx
@@ -1,8 +1,9 @@
-import React, { ReactElement } from "react"
+import React from "react"
 
+import { IconButton } from "frontend/design-system/atoms/button/icon-button"
 import { Input } from "frontend/design-system/atoms/input"
 import { H5 } from "frontend/design-system/atoms/typography"
-import { IFrameTemplate } from "frontend/design-system/templates/IFrameTemplate"
+import { ScreenResponsive } from "frontend/design-system/templates/screen-responsive"
 
 import QRCode from "./assets/qrcode.svg"
 import SecurityKey from "./assets/security-key.svg"
@@ -28,7 +29,7 @@ export const AuthorizeAppUnknownDevice: React.FC<
   const [isOtherMethod, setIsOtherMethod] = React.useState(false)
 
   return (
-    <IFrameTemplate className="flex flex-col items-center">
+    <ScreenResponsive className="flex flex-col items-center">
       <img src={applicationLogo} alt="" />
       <H5 className="mt-4">Sign in</H5>
       <p className="mt-3 text-center">
@@ -38,15 +39,17 @@ export const AuthorizeAppUnknownDevice: React.FC<
         <Input labelText="Your NFID number" className="w-full mt-8" />
       )}
       <div className="flex flex-col w-full mt-8 space-y-1">
-        <MethodRaw
+        <IconButton
           title="iPhone, iPad, or Android device"
           subtitle="Use passkey from a device with a camera"
           img={<img src={QRCode} alt="qrcode" />}
+          onClick={() => new Error("implement me")}
         />
-        <MethodRaw
+        <IconButton
           title="Security key"
           subtitle="Use an external security key"
           img={<img src={SecurityKey} alt="security-key" />}
+          onClick={() => new Error("implement me")}
         />
         <p
           className="pt-4 text-sm text-center cursor-pointer text-blue-base"
@@ -55,22 +58,6 @@ export const AuthorizeAppUnknownDevice: React.FC<
           {isOtherMethod ? "Back" : "Other sign in options"}
         </p>
       </div>
-    </IFrameTemplate>
+    </ScreenResponsive>
   )
 }
-
-interface MethodRawProps {
-  img: ReactElement
-  title: string
-  subtitle: string
-}
-
-const MethodRaw: React.FC<MethodRawProps> = ({ img, title, subtitle }) => (
-  <div className="flex items-center w-full px-3 py-2 border border-gray-200 rounded-md hover:border-blue-light transition-all cursor-pointer hover:bg-[#F4FAFF]">
-    <div className="w-[28px] mr-[9px]">{img}</div>
-    <div>
-      <p className="text-sm">{title}</p>
-      <p className="text-[11px] text-gray-400">{subtitle}</p>
-    </div>
-  </div>
-)

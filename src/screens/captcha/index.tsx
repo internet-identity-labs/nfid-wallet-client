6bec5aab68a3e488ca0255de1778b8dca3b9f6b8
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-06-03 10:58:26 +0200

    feat([sc-1855]): handle register same device

--- src/screens/captcha/index.tsx
+++ src/screens/captcha/index.tsx
@@ -8,22 +8,28 @@ import { H5 } from "frontend/design-system/atoms/typography"
 import { Challenge } from "frontend/design-system/molecules/challenge"
 import { ScreenResponsive } from "frontend/design-system/templates/screen-responsive"
 
+import { ProfileConstants } from "frontend/flows/screens-app/profile/routes"
+import { useAuthentication } from "frontend/hooks/use-authentication"
+import { useAuthorization } from "frontend/hooks/use-authorization"
+import { useMultipass } from "frontend/hooks/use-multipass"
+import { useNFIDNavigate } from "frontend/hooks/use-nfid-navigate"
+import { useAccount } from "frontend/services/identity-manager/account/hooks"
+import { usePersona } from "frontend/services/identity-manager/persona/hooks"
 import { ElementProps } from "frontend/types/react"
 import { captchaRules } from "frontend/utils/validations"
 
+import { useUnknownDeviceConfig } from "../remote-authorize-app-unknown-device/hooks/use-unknown-device.config"
 import { useCaptcha } from "./hook"
-import { useMultipass } from 'frontend/hooks/use-multipass'
-import { useNavigate } from 'react-router-dom'
-import { ProfileConstants } from 'frontend/flows/screens-app/profile/routes'
 
 interface CaptchaProps extends ElementProps<HTMLDivElement> {
-  successPath?: string;
+  successPath?: string
 }
 
 export const Captcha: React.FC<CaptchaProps> = ({
   className,
   successPath = `${ProfileConstants.base}`,
 }) => {
+  const { scope } = useUnknownDeviceConfig()
   const {
     register,
     formState: { errors, dirtyFields },
@@ -51,24 +57,57 @@ export const Captcha: React.FC<CaptchaProps> = ({
         })
       },
     })
-  
-  const navigate = useNavigate()
 
-  const onSubmit = React.useMemo(() => (form : any) => {
-    registerAnchor(form)
-    .then((result) => {
+  const { navigate } = useNFIDNavigate()
+
+  const { userNumber } = useAccount()
+
+  const { isAuthenticated } = useAuthentication()
+  const { authorizeApp } = useAuthorization({
+    userNumber,
+  })
+
+  const { nextPersonaId, createPersona } = usePersona()
+
+  const handleAuthorizePersona = React.useCallback(async () => {
+    setLoading(true)
+    console.log(">> handleAuthorizePersona", { scope })
+
+    const response = await createPersona({ domain: scope })
+
+    if (response?.status_code === 200) {
+      await authorizeApp({ persona_id: nextPersonaId, domain: scope })
+      navigate(successPath)
+      setLoading(false)
+    }
+    console.error(response)
+  }, [
+    authorizeApp,
+    createPersona,
+    navigate,
+    nextPersonaId,
+    scope,
+    setLoading,
+    successPath,
+  ])
+
+  const onSubmit = React.useCallback(
+    async (form: any) => {
+      const result = await registerAnchor(form)
       if (result.kind === "loginSuccess") {
-        navigate(successPath);
-      } else {
-        console.error(result)
       }
-    })
-    .catch((result) => {
-      console.error(result)
-    })
-  }, [registerAnchor]);
-  
-  const { applicationLogo, applicationName } = useMultipass();
+      console.log(">> onSubmit", { result })
+    },
+    [registerAnchor],
+  )
+
+  React.useEffect(() => {
+    if (isAuthenticated && userNumber) {
+      handleAuthorizePersona()
+    }
+  }, [handleAuthorizePersona, isAuthenticated, userNumber])
+
+  const { applicationLogo, applicationName } = useMultipass()
 
   return (
     <ScreenResponsive
@@ -77,7 +116,9 @@ export const Captcha: React.FC<CaptchaProps> = ({
     >
       {applicationLogo && <img src={applicationLogo} alt="logo" />}
       <H5 className="mt-4">Complete NFID registration</H5>
-      <p className="mt-1 text-center">to continue {applicationName && `to ${applicationName}`}</p>
+      <p className="mt-1 text-center">
+        to continue {applicationName && `to ${applicationName}`}
+      </p>
       <form className="flex flex-col w-full mt-5">
         <Challenge
           src={challenge && `data:image/png;base64,${challenge.png_base64}`}

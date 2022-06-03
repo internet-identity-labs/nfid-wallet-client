f825f1f62dbdc2d18fefedb9f3c950a96beb5c4c
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-06-03 10:58:12 +0200

    chore([sc-1946]): implemented storybookcomponent and ui state for captcha

---
+++ src/screens/captcha/index.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { BrowserRouter as Router } from "react-router-dom"
+
+import logo from "frontend/assets/distrikt.svg"
+
+import { Captcha } from "./new-captcha"
+
+export default {
+  title: "Screens/Captcha",
+  component: Captcha,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof Captcha>
+
+const CaptchaTemplate: ComponentStory<typeof Captcha> = (args) => {
+  return (
+    <Router>
+      <Captcha {...args} />
+    </Router>
+  )
+}
+
+export const CaptchaScreen = CaptchaTemplate.bind({})
+
+CaptchaScreen.args = {
+  applicationName: "Distrikt",
+  applicationLogo: logo,
+}

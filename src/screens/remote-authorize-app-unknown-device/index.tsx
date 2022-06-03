199edd36b99cfc935ddefa49886b6855a28f8e24
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-06-03 16:09:09 +0200

    fix([sc-2296]): pass applicationLogo through QRCode

--- src/screens/remote-authorize-app-unknown-device/index.tsx
+++ src/screens/remote-authorize-app-unknown-device/index.tsx
@@ -1,11 +1,10 @@
 import clsx from "clsx"
 import React from "react"
-import { Link, Navigate } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 
 import { QRCode } from "@internet-identity-labs/nfid-sdk-react"
 import { H5 } from "@internet-identity-labs/nfid-sdk-react"
 
-import { useIsIframe } from "frontend/hooks/use-is-iframe"
 import { useMultipass } from "frontend/hooks/use-multipass"
 import { useNFIDNavigate } from "frontend/hooks/use-nfid-navigate"
 
@@ -19,15 +18,9 @@ export interface AuthorizeAppUnknownDeviceProps {
 
 export const RemoteAuthorizeAppUnknownDevice: React.FC<
   AuthorizeAppUnknownDeviceProps
-> = ({
-  registerDeviceDeciderPath,
-  registerSameDevicePath,
-  url,
-  showRegister,
-}) => {
+> = ({ registerDeviceDeciderPath, url, showRegister }) => {
   const { applicationLogo, applicationName } = useMultipass()
   const { generatePath } = useNFIDNavigate()
-  const isIframe = useIsIframe()
 
   return url && !showRegister ? (
     <div className={clsx("text-center")}>
@@ -47,16 +40,7 @@ export const RemoteAuthorizeAppUnknownDevice: React.FC<
         </div>
 
         <div className="py-5 m-auto">
-          <LinkWrapper
-            isIframe={isIframe}
-            url={
-              isIframe
-                ? registerSameDevicePath
-                : generatePath(registerSameDevicePath)
-            }
-          >
-            <QRCode content={url} options={{ margin: 0 }} />
-          </LinkWrapper>
+          <QRCode content={url} options={{ margin: 0 }} />
         </div>
       </div>
     </div>
@@ -64,16 +48,3 @@ export const RemoteAuthorizeAppUnknownDevice: React.FC<
     <Navigate to={generatePath(registerDeviceDeciderPath)} />
   ) : null
 }
-
-const LinkWrapper: React.FC<{ isIframe: boolean; url: string }> = ({
-  url,
-  isIframe,
-  children,
-}) =>
-  isIframe ? (
-    <a href={url} target={isIframe ? "_blank" : "_self"} rel="noreferrer">
-      {children}
-    </a>
-  ) : (
-    <Link to={url}>{children}</Link>
-  )

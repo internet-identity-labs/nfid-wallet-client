2e283284eafcd0daebd46d954ce338d773321560
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-06-03 10:58:04 +0200

    chore([sc-1862]): updated authorizeapp according latest designs

--- src/screens/authorize-app-unknown-device-new/index.tsx
+++ src/screens/authorize-app-unknown-device-new/index.tsx
@@ -1,10 +1,9 @@
-import clsx from "clsx"
 import React, { ReactElement } from "react"
 
-import { useIsIframe } from "frontend/hooks/use-is-iframe"
-import { useNFIDNavigate } from "frontend/hooks/use-nfid-navigate"
-import { NFIDGradientBar } from "frontend/design-system/atoms/gradient-bar"
+import { Input } from "frontend/design-system/atoms/input"
 import { H5 } from "frontend/design-system/atoms/typography"
+import { IFrameTemplate } from "frontend/design-system/templates/IFrameTemplate"
+
 import QRCode from "./assets/qrcode.svg"
 import SecurityKey from "./assets/security-key.svg"
 
@@ -14,40 +13,50 @@ export interface AuthorizeAppUnknownDeviceProps {
   url: string | null
   showRegister: boolean
   applicationName?: string
+  applicationLogo?: string
 }
 
-export const AuthorizeAppUnknownDevice: React.FC<AuthorizeAppUnknownDeviceProps> = ({
-                                                                                      registerDeviceDeciderPath,
-                                                                                      registerSameDevicePath,
-                                                                                      url,
-                                                                                      showRegister,
-                                                                                      applicationName,
-                                                                                    }) => {
-  const { generatePath } = useNFIDNavigate()
-  const isIframe = useIsIframe()
+export const AuthorizeAppUnknownDevice: React.FC<
+  AuthorizeAppUnknownDeviceProps
+> = ({
+  registerDeviceDeciderPath,
+  registerSameDevicePath,
+  url,
+  applicationName,
+  applicationLogo,
+}) => {
+  const [isOtherMethod, setIsOtherMethod] = React.useState(false)
 
-  return <div>
-    <div className="relative w-80 shadow-iframe rounded-xl">
-      <NFIDGradientBar />
-      <div className="w-[90%] mx-auto py-6">
-        <H5>Sign in</H5>
-        <p className="mt-3 text-sm">Choose how you’d like to sign in to <br /> {applicationName}</p>
-        <div className="flex flex-col mt-2 space-y-1">
-          <MethodRaw
-            title="iPhone, iPad, or Android device"
-            subtitle="Use passkey from a device with a camera"
-            img={<img src={QRCode} alt="qrcode" />}
-          />
-          <MethodRaw
-            title="Security key"
-            subtitle="Use an external security key"
-            img={<img src={SecurityKey} alt="security-key" />}
-          />
-        </div>
+  return (
+    <IFrameTemplate className="flex flex-col items-center">
+      <img src={applicationLogo} alt="" />
+      <H5 className="mt-4">Sign in</H5>
+      <p className="mt-3 text-center">
+        Choose how you’d like to sign in to {applicationName}
+      </p>
+      {isOtherMethod && (
+        <Input labelText="Your NFID number" className="w-full mt-8" />
+      )}
+      <div className="flex flex-col w-full mt-8 space-y-1">
+        <MethodRaw
+          title="iPhone, iPad, or Android device"
+          subtitle="Use passkey from a device with a camera"
+          img={<img src={QRCode} alt="qrcode" />}
+        />
+        <MethodRaw
+          title="Security key"
+          subtitle="Use an external security key"
+          img={<img src={SecurityKey} alt="security-key" />}
+        />
+        <p
+          className="pt-4 text-sm text-center cursor-pointer text-blue-base"
+          onClick={() => setIsOtherMethod(!isOtherMethod)}
+        >
+          {isOtherMethod ? "Back" : "Other sign in options"}
+        </p>
       </div>
-    </div>
-  </div>
-
+    </IFrameTemplate>
+  )
 }
 
 interface MethodRawProps {
@@ -58,9 +67,7 @@ interface MethodRawProps {
 
 const MethodRaw: React.FC<MethodRawProps> = ({ img, title, subtitle }) => (
   <div className="flex items-center w-full px-3 py-2 border border-gray-200 rounded-md hover:border-blue-light transition-all cursor-pointer hover:bg-[#F4FAFF]">
-    <div className="w-[28px] mr-[9px]">
-      {img}
-    </div>
+    <div className="w-[28px] mr-[9px]">{img}</div>
     <div>
       <p className="text-sm">{title}</p>
       <p className="text-[11px] text-gray-400">{subtitle}</p>

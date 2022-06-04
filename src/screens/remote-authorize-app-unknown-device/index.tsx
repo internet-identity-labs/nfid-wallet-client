cc7491f4ef7e3dc79f28c2e9583b6790a3ce43be
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-06-04 22:35:55 +0200

    fix([sc-2326]): qrcode bg

--- src/screens/remote-authorize-app-unknown-device/index.tsx
+++ src/screens/remote-authorize-app-unknown-device/index.tsx
@@ -50,12 +50,8 @@ export const RemoteAuthorizeAppUnknownDevice: React.FC<
           Use passkey from a device with a camera to sign in to{" "}
           {applicationName}
         </P>
-        <Link to={url}>
-          <QRCode
-            className="p-6 rounded-[10px] w-48 h-48 mt-8 bg-red-300"
-            content={url}
-            options={{ width: 143 }}
-          />
+        <Link to={url} className="bg-gray-50 p-6 rounded-[10px] w-48 h-48 mt-8">
+          <QRCode content={url} options={{ width: 143 }} />
         </Link>
       </div>
     </ScreenResponsive>

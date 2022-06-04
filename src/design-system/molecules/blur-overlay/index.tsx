e5440d2c36d596cf60fa73d83933e84a79f867dd
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-06-04 22:35:55 +0200

    fix([sc-2326]): blur on firefox

--- src/design-system/molecules/blur-overlay/index.tsx
+++ src/design-system/molecules/blur-overlay/index.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx"
 import React from "react"
 
+import { useDeviceInfo } from "frontend/hooks/use-device-info"
 import { ElementProps } from "frontend/types/react"
 
 interface BlurOverlayProps extends ElementProps<HTMLDivElement> {}
@@ -9,11 +10,16 @@ export const BlurOverlay: React.FC<BlurOverlayProps> = ({
   children,
   className,
 }) => {
+  const { browser } = useDeviceInfo()
+
   return (
     <div
       className={clsx("", className)}
       style={{
-        background: "rgba(255, 255, 255, 0.1)",
+        background:
+          browser.name === "Firefox"
+            ? "rgba(255, 255, 255, 0.85)"
+            : "rgba(255, 255, 255, 0.1)",
         backdropFilter: "blur(8px)",
         WebkitBackdropFilter: "blur(8px)",
       }}

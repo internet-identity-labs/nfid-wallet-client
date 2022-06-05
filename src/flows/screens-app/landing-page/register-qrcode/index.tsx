8bd4663b084939176181e631f381e9ff1a80b24b
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-06-05 15:06:37 +0200

    fix([sc-2380]): remove QRCode link nfid landing

--- src/flows/screens-app/landing-page/register-qrcode/index.tsx
+++ src/flows/screens-app/landing-page/register-qrcode/index.tsx
@@ -1,9 +1,8 @@
-import { QRCode } from "@internet-identity-labs/nfid-sdk-react"
 import { QRCodeRenderersOptions } from "qrcode"
 import React, { CSSProperties } from "react"
-import { Link } from "react-router-dom"
 
-import { NFIDRegisterAccountConstants } from "frontend/flows/screens-app/register-account/routes"
+import { QRCode } from "@internet-identity-labs/nfid-sdk-react"
+
 import { useInterval } from "frontend/hooks/use-interval"
 
 import "./index.css"
@@ -25,13 +24,8 @@ export const RegisterQRCode: React.FC<RegisterQRCodeProps> = ({
   useInterval(handlePollForDelegate, 2000)
 
   return (
-    <Link
-      to={`${NFIDRegisterAccountConstants.base}/${NFIDRegisterAccountConstants.account}`}
-      rel="noreferrer"
-      className={className}
-      style={style}
-    >
+    <div className={className} style={style}>
       <QRCode options={options} content={url} />
-    </Link>
+    </div>
   )
 }

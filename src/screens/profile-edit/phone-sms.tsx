26779bab4b4304028a25e74a5d3282bffd3081fd
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-06-17 10:15:35 +0200

    chore([sc-2347]): implemented Mert's solution for input

--- src/screens/profile-edit/phone-sms.tsx
+++ src/screens/profile-edit/phone-sms.tsx
@@ -1,14 +1,22 @@
 import clsx from "clsx"
 import React from "react"
+import { useForm } from "react-hook-form"
 import { IoMdArrowBack } from "react-icons/io"
 import { Link } from "react-router-dom"
 import ReactCodeInput from "react-verification-code-input"
 
-import { P, Logo, H4, Button } from "@internet-identity-labs/nfid-sdk-react"
+import {
+  P,
+  Logo,
+  H4,
+  Button,
+  Input,
+} from "@internet-identity-labs/nfid-sdk-react"
 
 import { AppScreen } from "frontend/design-system/templates/AppScreen"
 
 import { useTimer } from "frontend/hooks/use-timer"
+import { isValidToken, tokenRules } from "frontend/utils/validations"
 
 interface Account {
   anchor: string
@@ -18,18 +26,112 @@ interface Account {
 interface ProfileEditPhoneSmsProps {
   account?: Account
   onResendCode: () => void
+  onSubmit: () => void
+  phone: string | number
 }
 
 export const ProfileEditPhoneSms: React.FC<ProfileEditPhoneSmsProps> = ({
   account,
   onResendCode,
+  onSubmit,
+  phone,
 }) => {
-  console.log(account)
+  const {
+    formState: { errors },
+    setError,
+    clearErrors,
+  } = useForm({
+    mode: "all",
+  })
+  const list = [...Array(6).keys()]
+  const inputItemsRef = React.useRef<Array<HTMLInputElement | null>>([])
+  const getVerificationCode = React.useCallback(
+    () => inputItemsRef.current.map((item) => item?.value).join(""),
+    [],
+  )
   const { counter, setCounter } = useTimer({ defaultCounter: 3 })
 
   const handleResend = () => {
     onResendCode && onResendCode()
     setCounter(60)
+  }
+
+  const handleKeydown = React.useCallback((event: KeyboardEvent) => {
+    if (event.key === "Backspace" || event.key === "Delete") {
+      const currentFocus = document.activeElement
+      const index = Number(currentFocus?.getAttribute("data-pin-index"))
+
+      if (index && !inputItemsRef.current[index]?.value) {
+        inputItemsRef.current[index - 1]?.focus()
+      }
+    }
+  }, [])
+
+  const handlePaste = React.useCallback(
+    (event: ClipboardEvent) => {
+      const paste = event.clipboardData?.getData("text/plain")
+
+      if (paste && isValidToken(paste)) {
+        inputItemsRef.current.forEach((item, index) => {
+          if (item) {
+            item.value = paste[index]
+            inputItemsRef.current[index]?.blur()
+          }
+        })
+
+        clearErrors("verificationCode")
+      }
+    },
+    [clearErrors],
+  )
+
+  const handleInput = (e: { target: HTMLInputElement }, index: number) => {
+    const validRegex = inputItemsRef.current[index]?.value.match(
+      e.target.pattern,
+    )
+
+    if (validRegex) {
+      if (index === list.length - 1) {
+        inputItemsRef.current[index]?.blur()
+      }
+
+      inputItemsRef.current[index + 1]?.focus()
+    } else {
+      e.target.value = e.target.value[0]
+    }
+  }
+
+  React.useEffect(() => {
+    document.addEventListener("paste", handlePaste)
+    document.addEventListener("keydown", handleKeydown)
+
+    return () => {
+      document.removeEventListener("paste", handlePaste)
+    }
+  }, [handleKeydown, handlePaste])
+
+  const validateToken = () => {
+    const verificationCode = getVerificationCode()
+
+    if (verificationCode.length !== tokenRules.minLength) {
+      return setError("verificationCode", {
+        type: "manual",
+        message: tokenRules.errorMessages.length,
+      })
+    }
+
+    if (!isValidToken(verificationCode)) {
+      return setError("verificationCode", {
+        type: "manual",
+        message: tokenRules.errorMessages.pattern,
+      })
+    }
+
+    clearErrors("verificationCode")
+  }
+
+  const handleSubmit = () => {
+    if (!errors) onSubmit()
   }
 
   return (
@@ -70,7 +172,7 @@ export const ProfileEditPhoneSms: React.FC<ProfileEditPhoneSmsProps> = ({
           </Link>
           <H4 className="mt-5 sm:mt-0">SMS verification</H4>
           <P className="mt-3 text-sm sm:mt-14">
-            Please enter the verification code that was sent to +1 234 856 7890.
+            Please enter the verification code that was sent to {phone}.
             <br />
             {counter > 0 ? (
               <P className="mt-3">Code can be resent in {counter} sec</P>
@@ -86,19 +188,41 @@ export const ProfileEditPhoneSms: React.FC<ProfileEditPhoneSmsProps> = ({
               </P>
             )}
           </P>
-          <form
+          <div
             className={clsx(
               "mt-5 flex flex-col justify-between flex-1",
               "sm:block",
             )}
           >
-            <div className="flex flex-row space-x-2.5">
-              <ReactCodeInput />
+            <div className="flex space-x-3">
+              {list.map((_, index) => (
+                <Input
+                  pin
+                  type="number"
+                  key={index}
+                  autoFocus={index === 0}
+                  data-pin-index={index}
+                  ref={(el) => (inputItemsRef.current[index] = el)}
+                  onChange={(e) => handleInput(e, index)}
+                  maxLength={1}
+                  pattern="^[0-9]{1}$"
+                />
+              ))}
             </div>
-            <Button secondary className="px-10 sm:mt-5">
+            <div className="py-1 text-sm text-red-base">
+              {errors.verificationCode?.message || errors.phonenumber?.message}
+            </div>
+            <Button
+              secondary
+              className="px-10 sm:mt-5"
+              onClick={() => {
+                validateToken()
+                handleSubmit()
+              }}
+            >
               Complete
             </Button>
-          </form>
+          </div>
         </div>
       </main>
     </AppScreen>

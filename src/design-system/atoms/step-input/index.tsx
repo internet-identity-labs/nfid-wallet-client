6d940111c6239927f14ce93ac86b82265156bcf1
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-06-17 10:15:35 +0200

    chore([sc-2347]): added step-input component

---
+++ src/design-system/atoms/step-input/index.tsx
@@ -0,0 +1,150 @@
+import clsx from "clsx"
+import React from "react"
+import { useForm } from "react-hook-form"
+
+import { isValidToken, tokenRules } from "frontend/utils/validations"
+
+import { Button } from "../button"
+import { Input } from "../input"
+
+interface StepInputProps {
+  className?: string
+  onSubmit: (value: string) => boolean
+}
+
+export const StepInput: React.FC<StepInputProps> = ({
+  className,
+  onSubmit,
+}) => {
+  const list = [...Array(6).keys()]
+  const inputItemsRef = React.useRef<Array<HTMLInputElement | null>>([])
+  const {
+    formState: { errors, isValid },
+    setError,
+    clearErrors,
+  } = useForm({
+    mode: "all",
+  })
+
+  const getVerificationCode = React.useCallback(
+    () => inputItemsRef.current.map((item) => item?.value).join(""),
+    [],
+  )
+
+  const handleKeydown = React.useCallback((event: KeyboardEvent) => {
+    if (event.key === "Backspace" || event.key === "Delete") {
+      const currentFocus = document.activeElement
+      const index = Number(currentFocus?.getAttribute("data-pin-index"))
+
+      if (index && !inputItemsRef.current[index]?.value) {
+        inputItemsRef.current[index - 1]?.focus()
+      }
+    }
+  }, [])
+
+  const handlePaste = React.useCallback(
+    (event: ClipboardEvent) => {
+      const paste = event.clipboardData?.getData("text/plain")
+
+      if (paste && isValidToken(paste)) {
+        inputItemsRef.current.forEach((item, index) => {
+          if (item) {
+            item.value = paste[index]
+            inputItemsRef.current[index]?.blur()
+          }
+        })
+
+        clearErrors("verificationCode")
+      }
+    },
+    [clearErrors],
+  )
+
+  const handleInput = (e: { target: HTMLInputElement }, index: number) => {
+    const validRegex = inputItemsRef.current[index]?.value.match(
+      e.target.pattern,
+    )
+
+    if (validRegex) {
+      if (index === list.length - 1) {
+        inputItemsRef.current[index]?.blur()
+      }
+
+      inputItemsRef.current[index + 1]?.focus()
+    } else {
+      e.target.value = e.target.value[0]
+    }
+  }
+
+  React.useEffect(() => {
+    document.addEventListener("paste", handlePaste)
+    document.addEventListener("keydown", handleKeydown)
+
+    return () => {
+      document.removeEventListener("paste", handlePaste)
+    }
+  }, [handleKeydown, handlePaste])
+
+  const validateToken = () => {
+    const verificationCode = getVerificationCode()
+
+    if (verificationCode.length !== tokenRules.minLength) {
+      return setError("verificationCode", {
+        type: "manual",
+        message: tokenRules.errorMessages.length,
+      })
+    }
+
+    if (!isValidToken(verificationCode)) {
+      return setError("verificationCode", {
+        type: "manual",
+        message: tokenRules.errorMessages.pattern,
+      })
+    }
+
+    clearErrors("verificationCode")
+  }
+
+  const handleSubmit = () => {
+    if (!isValid) return
+    if (!onSubmit(getVerificationCode())) {
+      setError("verificationCode", {
+        type: "manual",
+        message: "Incorrect verification code, please try again.",
+      })
+    }
+  }
+
+  return (
+    <div>
+      <div className={clsx("flex space-x-3", className)}>
+        {list.map((_, index) => (
+          <Input
+            pin
+            type="number"
+            key={index}
+            autoFocus={index === 0}
+            data-pin-index={index}
+            ref={(el) => (inputItemsRef.current[index] = el)}
+            onChange={(e) => handleInput(e, index)}
+            maxLength={1}
+            pattern="^[0-9]{1}$"
+          />
+        ))}
+      </div>
+      <div className="py-1 text-sm text-red-base">
+        {errors.verificationCode?.message || errors.phonenumber?.message}
+      </div>
+      <Button
+        secondary
+        className="px-10 sm:mt-5"
+        onClick={() => {
+          validateToken()
+          handleSubmit()
+        }}
+      >
+        Complete
+      </Button>
+    </div>
+  )
+}
--- src/screens/profile-edit/phone-sms.tsx
+++ src/screens/profile-edit/phone-sms.tsx
@@ -1,21 +1,14 @@
 import clsx from "clsx"
 import React from "react"
-import { useForm } from "react-hook-form"
 import { IoMdArrowBack } from "react-icons/io"
 import { Link } from "react-router-dom"
 
-import {
-  P,
-  Logo,
-  H4,
-  Button,
-  Input,
-} from "@internet-identity-labs/nfid-sdk-react"
+import { P, Logo, H4 } from "@internet-identity-labs/nfid-sdk-react"
 
+import { StepInput } from "frontend/design-system/atoms/step-input"
 import { AppScreen } from "frontend/design-system/templates/AppScreen"
 
 import { useTimer } from "frontend/hooks/use-timer"
-import { isValidToken, tokenRules } from "frontend/utils/validations"
 
 interface Account {
   anchor: string
@@ -35,108 +28,11 @@ export const ProfileEditPhoneSms: React.FC<ProfileEditPhoneSmsProps> = ({
   onSubmit,
   phone,
 }) => {
-  const {
-    formState: { errors },
-    setError,
-    clearErrors,
-  } = useForm({
-    mode: "all",
-  })
-  const list = [...Array(6).keys()]
-  const inputItemsRef = React.useRef<Array<HTMLInputElement | null>>([])
-  const getVerificationCode = React.useCallback(
-    () => inputItemsRef.current.map((item) => item?.value).join(""),
-    [],
-  )
   const { counter, setCounter } = useTimer({ defaultCounter: 3 })
 
   const handleResend = () => {
     onResendCode && onResendCode()
     setCounter(60)
-  }
-
-  const handleKeydown = React.useCallback((event: KeyboardEvent) => {
-    if (event.key === "Backspace" || event.key === "Delete") {
-      const currentFocus = document.activeElement
-      const index = Number(currentFocus?.getAttribute("data-pin-index"))
-
-      if (index && !inputItemsRef.current[index]?.value) {
-        inputItemsRef.current[index - 1]?.focus()
-      }
-    }
-  }, [])
-
-  const handlePaste = React.useCallback(
-    (event: ClipboardEvent) => {
-      const paste = event.clipboardData?.getData("text/plain")
-
-      if (paste && isValidToken(paste)) {
-        inputItemsRef.current.forEach((item, index) => {
-          if (item) {
-            item.value = paste[index]
-            inputItemsRef.current[index]?.blur()
-          }
-        })
-
-        clearErrors("verificationCode")
-      }
-    },
-    [clearErrors],
-  )
-
-  const handleInput = (e: { target: HTMLInputElement }, index: number) => {
-    const validRegex = inputItemsRef.current[index]?.value.match(
-      e.target.pattern,
-    )
-
-    if (validRegex) {
-      if (index === list.length - 1) {
-        inputItemsRef.current[index]?.blur()
-      }
-
-      inputItemsRef.current[index + 1]?.focus()
-    } else {
-      e.target.value = e.target.value[0]
-    }
-  }
-
-  React.useEffect(() => {
-    document.addEventListener("paste", handlePaste)
-    document.addEventListener("keydown", handleKeydown)
-
-    return () => {
-      document.removeEventListener("paste", handlePaste)
-    }
-  }, [handleKeydown, handlePaste])
-
-  const validateToken = () => {
-    const verificationCode = getVerificationCode()
-
-    if (verificationCode.length !== tokenRules.minLength) {
-      return setError("verificationCode", {
-        type: "manual",
-        message: tokenRules.errorMessages.length,
-      })
-    }
-
-    if (!isValidToken(verificationCode)) {
-      return setError("verificationCode", {
-        type: "manual",
-        message: tokenRules.errorMessages.pattern,
-      })
-    }
-
-    clearErrors("verificationCode")
-  }
-
-  const handleSubmit = () => {
-    if (errors) return
-    if (!onSubmit()) {
-      setError("verificationCode", {
-        type: "manual",
-        message: "Incorrect verification code, please try again.",
-      })
-    }
   }
 
   return (
@@ -199,34 +95,7 @@ export const ProfileEditPhoneSms: React.FC<ProfileEditPhoneSmsProps> = ({
               "sm:block",
             )}
           >
-            <div className="flex space-x-3">
-              {list.map((_, index) => (
-                <Input
-                  pin
-                  type="number"
-                  key={index}
-                  autoFocus={index === 0}
-                  data-pin-index={index}
-                  ref={(el) => (inputItemsRef.current[index] = el)}
-                  onChange={(e) => handleInput(e, index)}
-                  maxLength={1}
-                  pattern="^[0-9]{1}$"
-                />
-              ))}
-            </div>
-            <div className="py-1 text-sm text-red-base">
-              {errors.verificationCode?.message || errors.phonenumber?.message}
-            </div>
-            <Button
-              secondary
-              className="px-10 sm:mt-5"
-              onClick={() => {
-                validateToken()
-                handleSubmit()
-              }}
-            >
-              Complete
-            </Button>
+            <StepInput onSubmit={onSubmit} />
           </div>
         </div>
       </main>

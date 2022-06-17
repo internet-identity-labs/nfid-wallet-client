798f6205029bbc022472d327e0c4962b5abcfb40
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-06-17 18:10:39 +0200

    fix([sc-2616]): open link in new tab

--- src/flows/screens-app/landing-page/footer.tsx
+++ src/flows/screens-app/landing-page/footer.tsx
@@ -13,8 +13,10 @@ export const Footer: React.FC<FooterProps> = ({ children, className }) => {
     <footer className="flex flex-col items-center justify-between mt-[100px] md:flex-row pb-4 sm:pb-0">
       <div className="flex flex-wrap items-center justify-center mb-2 space-x-6 sm:mb-0">
         <a
-          href="https://docs.nfid.one/privacy"
           className="text-blue-base hover:underline"
+          href="https://docs.nfid.one/privacy"
+          target="_blank"
+          rel="noreferrer"
         >
           Privacy policy
         </a>

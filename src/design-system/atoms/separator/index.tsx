79f88f0797cd1eba769eb9894e7fd0c5b96e569b
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-06-19 13:33:53 +0200

    chore([sc-2644]): updated according latest design colors

--- src/design-system/atoms/separator/index.tsx
+++ src/design-system/atoms/separator/index.tsx
@@ -1,13 +1,24 @@
+import clsx from "clsx"
 import React from "react"
 
-interface SeparatorProps {}
+import { ElementProps } from "frontend/types/react"
 
-export const Separator: React.FC<SeparatorProps> = ({ children }) => {
+interface SeparatorProps extends ElementProps<HTMLDivElement> {}
+
+export const Separator: React.FC<SeparatorProps> = ({
+  children,
+  className,
+}) => {
   return (
-    <div className="flex items-center justify-between w-full text-sm text-gray-200 h-9">
-      <div className="flex w-full h-[1px] bg-gray-200" />
-      <div className="px-2">or</div>
-      <div className="flex w-full h-[1px] bg-gray-200" />
+    <div
+      className={clsx(
+        "flex items-center justify-between w-full text-sm text-gray-400 h-9",
+        className,
+      )}
+    >
+      <div className="flex w-full h-[1px] bg-gray-300" />
+      <div className="px-2">OR</div>
+      <div className="flex w-full h-[1px] bg-gray-300" />
     </div>
   )
 }

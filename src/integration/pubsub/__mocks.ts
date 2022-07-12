8ce9cd536dec96f249f9b0cc63a809494cf8b5f9
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-07-12 14:10:02 +0200

    refactor(pubsub): add mocks

---
+++ src/integration/pubsub/__mocks.ts
@@ -0,0 +1,11 @@
+import {
+  JSON_SERIALISABLE_SIGNED_DELEGATION,
+  NFID_SIGNED_DELEGATION,
+} from "../internet-identity/__mocks"
+
+export const REMOTE_LOGIN_REGISTER_MESSAGE = {
+  type: "remote-login-register",
+  userNumber: "10064",
+  nfid: NFID_SIGNED_DELEGATION,
+  ...JSON_SERIALISABLE_SIGNED_DELEGATION,
+}

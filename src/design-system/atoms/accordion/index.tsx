475be813f87e4f14354603c8862236146cb9c664
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-07-13 15:01:32 +0200

    fix([sc-2386]): fixed validate DOM nesting

--- src/design-system/atoms/accordion/index.tsx
+++ src/design-system/atoms/accordion/index.tsx
@@ -14,17 +14,27 @@ export const Accordion: React.FC<AccordionProps> = ({ title, details }) => {
 
   function toggleAccordion() {
     setActive((prevState) => !prevState)
-    setHeight(active ? "0px" : `${contentSpace.current && contentSpace.current.scrollHeight}px`)
-    setRotate(active ? "transform duration-700 ease" : "transform duration-700 ease rotate-180")
+    setHeight(
+      active
+        ? "0px"
+        : `${contentSpace.current && contentSpace.current.scrollHeight}px`,
+    )
+    setRotate(
+      active
+        ? "transform duration-700 ease"
+        : "transform duration-700 ease rotate-180",
+    )
   }
 
   return (
     <div className="flex flex-col border-b border-gray-200 font-inter">
       <button
-        className="box-border flex justify-between items-center py-6 appearance-none cursor-pointer focus:outline-none"
+        className="box-border flex items-center justify-between py-6 appearance-none cursor-pointer focus:outline-none"
         onClick={toggleAccordion}
       >
-        <p className="inline-block text-lg font-bold text-left sm:text-xl">{title}</p>
+        <p className="inline-block text-lg font-bold text-left sm:text-xl">
+          {title}
+        </p>
         <div className={`${rotate} inline-block w-[30px] h-[30px]`}>
           <svg
             width="30"
@@ -48,7 +58,9 @@ export const Accordion: React.FC<AccordionProps> = ({ title, details }) => {
         style={{ maxHeight: `${height}` }}
         className="overflow-hidden duration-700 ease-in-out transition-max-height"
       >
-        <div className="pb-10 text-base sm:text-lg tracking-[0.01em] leading-[26px]">{details}</div>
+        <div className="pb-10 text-base sm:text-lg tracking-[0.01em] leading-[26px]">
+          {details}
+        </div>
       </div>
     </div>
   )

ff3c994ffb78aaa6d4c02c403effda5a67dd2f62
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-07-13 21:03:36 +0200

    test: handle .did as files

--- jest.config.js
+++ jest.config.js
@@ -35,7 +35,7 @@ module.exports = {
   roots: ["test/", "src/"],
   moduleDirectories: ["node_modules"],
   moduleNameMapper: {
-    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|did)$":
       "<rootDir>/mocks/fileMock.js",
     "\\.(css|less)$": "<rootDir>/mocks/fileMock.js",
     "^frontend/(.*)$": "<rootDir>/src/$1",

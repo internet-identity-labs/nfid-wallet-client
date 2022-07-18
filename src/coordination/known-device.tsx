66a44c5bfb79da3339d197c56196ed704427a097
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-07-18 21:14:43 +0200

    test(knownDevice): coordinator

--- src/coordination/known-device.tsx
+++ src/coordination/known-device.tsx
@@ -6,14 +6,16 @@ import { KnownDeviceActor } from "frontend/state/machines/authentication/known-d
 export function KnownDeviceCoordinator({ actor }: Actor<KnownDeviceActor>) {
   const [state] = useActor(actor)
 
-  console.log(">>", JSON.stringify(state.value))
-
   switch (true) {
     case state.matches("Start"):
       return <div>Loading Devices</div>
     case state.matches("Authenticate"):
     case state.matches("Login"):
-      return <div>Authenticate isLoading: {state.matches("Login")}</div>
+      return state.context.isSingleAccountApplication ? (
+        <div>Authenticate SingleAccount</div>
+      ) : (
+        <div>Authenticate MultiAccount</div>
+      )
     default:
       return <div>KnownDeviceCoordinator</div>
   }

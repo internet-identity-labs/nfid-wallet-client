befe83f7c650c492dc97254d19ed2599d57bee90
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-07-20 14:22:01 +0200

    feat([sc-2602]): added other sign in options to register-account

--- src/design-system/pages/register-account-intro/screen-app.tsx
+++ src/design-system/pages/register-account-intro/screen-app.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+import { useCallback, useReducer, useEffect } from "react"
+import { useForm } from "react-hook-form"
 
 import { H5 } from "@internet-identity-labs/nfid-sdk-react"
 
@@ -9,26 +10,62 @@ import {
   SignInWithGoogle,
 } from "frontend/design-system/atoms/button/signin-with-google"
 import TouchId from "frontend/design-system/atoms/icons/touch-id.svg"
+import { Input } from "frontend/design-system/atoms/input"
 import { Separator } from "frontend/design-system/atoms/separator"
 import { ScreenResponsive } from "frontend/design-system/templates/screen-responsive"
+
+import { anchorRules } from "frontend/utils/validations"
+
+import SecurityKey from "./assets/security-key.svg"
 
 import "./index.css"
 
 interface RegisterAccountIntroProps {
   onRegister: () => void
   onSelectGoogleAuthorization: LoginEventHandler
+  onSelectSecurityKeyAuthorization: (userNumber: number) => Promise<void> | void
   applicationName?: string
   applicationLogo?: string
   isLoading: boolean
+  authError?: string
 }
 
 export const RegisterAccountIntro: React.FC<RegisterAccountIntroProps> = ({
   onRegister,
   onSelectGoogleAuthorization,
+  onSelectSecurityKeyAuthorization,
   applicationName = "this application",
   applicationLogo,
   isLoading,
+  authError,
 }) => {
+  const [showAdvancedOptions, toggleAdvancedOptions] = useReducer(
+    (state) => !state,
+    false,
+  )
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<{
+    userNumber: number
+  }>()
+
+  useEffect(() => {
+    if (authError && errors.userNumber?.message !== authError) {
+      setError("userNumber", { message: authError })
+    }
+  }, [authError, errors.userNumber, setError])
+
+  const handleSelectSecurityKeyAuthorization = useCallback(
+    async ({ userNumber }) => {
+      await onSelectSecurityKeyAuthorization(userNumber)
+    },
+    [onSelectSecurityKeyAuthorization],
+  )
+
   return (
     <ScreenResponsive
       className="flex flex-col items-center"
@@ -45,9 +82,29 @@ export const RegisterAccountIntro: React.FC<RegisterAccountIntroProps> = ({
         Choose how you'd like to sign in to {applicationName}
       </p>
       <div className="flex flex-col w-full mt-8 space-y-1">
-        <SignInWithGoogle onLogin={onSelectGoogleAuthorization} />
-
-        <Separator className="max-w-[400px]" />
+        {!showAdvancedOptions ? (
+          <>
+            <SignInWithGoogle onLogin={onSelectGoogleAuthorization} />
+            <Separator className="max-w-[400px]" />
+          </>
+        ) : (
+          <Input
+            errorText={errors.userNumber?.message}
+            labelText="Your NFID number"
+            className="w-full my-4"
+            {...register("userNumber", {
+              required: "userNumber is required",
+              pattern: {
+                value: anchorRules.regex,
+                message: anchorRules.errorMessages.pattern,
+              },
+              minLength: {
+                value: anchorRules.minLength,
+                message: anchorRules.errorMessages.length,
+              },
+            })}
+          />
+        )}
 
         <IconButton
           title="Create a new NFID"
@@ -55,6 +112,23 @@ export const RegisterAccountIntro: React.FC<RegisterAccountIntroProps> = ({
           img={<img src={TouchId} alt="passkey" />}
           onClick={onRegister}
         />
+        {showAdvancedOptions ? (
+          <>
+            <IconButton
+              title="Security key"
+              subtitle="Use a previously registered security key"
+              img={<img src={SecurityKey} alt="touch-id" />}
+              onClick={handleSubmit(handleSelectSecurityKeyAuthorization)}
+            />
+          </>
+        ) : null}
+
+        <p
+          className="py-4 text-sm text-center cursor-pointer text-blue-base"
+          onClick={toggleAdvancedOptions}
+        >
+          {showAdvancedOptions ? "Back" : "Other sign in options"}
+        </p>
       </div>
     </ScreenResponsive>
   )

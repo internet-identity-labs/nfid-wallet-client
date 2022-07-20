a1db8bb7bba9b48d43df0d90cc13c52e276ac07d
Jorgen <jorgen@buildnode.io>
2022-07-20 13:33:15 -0700

    test: mock delegation chains and webauthn identities

--- src/integration/identity/__mocks.ts
+++ src/integration/identity/__mocks.ts
@@ -1,4 +1,11 @@
+import { DerEncodedPublicKey } from "@dfinity/agent"
 import { WebAuthnIdentity } from "@dfinity/identity"
+import {
+  Delegation,
+  DelegationChain,
+  DelegationIdentity,
+  Ed25519KeyIdentity,
+} from "@dfinity/identity"
 
 export async function mockWebAuthnCreate() {
   return WebAuthnIdentity.fromJSON(
@@ -8,5 +15,37 @@ export async function mockWebAuthnCreate() {
       rawId:
         "4eafd1acf03edb17657429f253a2a217e198e71ac17d596fdbf5b70bc6751d1428c939cebd74ed7c70c9844e36fe39055018acab9185b8949213320dc1e37f51cc6310d71af4cd012bd12002de58cfa78edea4",
     }),
+  )
+}
+
+/**
+ * Turn an object into an ArrayBuffer.
+ */
+function encodeObject(object: {}): Uint8Array {
+  return Buffer.from(JSON.stringify(object))
+}
+
+export async function factoryDelegationChain(): Promise<DelegationChain> {
+  const id = Ed25519KeyIdentity.generate()
+  const delegation = new Delegation(
+    new Uint8Array(id.getPublicKey().toDer()),
+    BigInt(new Date().getTime() + 3_600_000),
+    undefined,
+  )
+  const signature = await id.sign(encodeObject(delegation))
+  const signedDelegation = {
+    delegation,
+    signature,
+  }
+  return DelegationChain.fromDelegations(
+    [signedDelegation],
+    new Uint8Array(id.getPublicKey().toDer()).buffer as DerEncodedPublicKey,
+  )
+}
+
+export async function factoryDelegationIdentity(): Promise<DelegationIdentity> {
+  return DelegationIdentity.fromDelegation(
+    Ed25519KeyIdentity.generate(),
+    await factoryDelegationChain(),
   )
 }

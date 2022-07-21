91cd4028b9e4e29d8d7c3a7b5bf00d4e4b4cd0b4
Jorgen <jorgen@buildnode.io>
2022-07-21 09:47:37 -0700

    feat: remote auth machine

---
+++ src/state/machines/authentication/trust-device.typegen.ts
@@ -0,0 +1,53 @@
+// This file was automatically generated. Edits will be overwritten
+
+export interface Typegen0 {
+  "@@xstate/typegen": true
+  eventsCausingActions: {}
+  internalEvents: {
+    "": { type: "" }
+    "xstate.init": { type: "xstate.init" }
+    "done.invoke.registerWithWebAuthn": {
+      type: "done.invoke.registerWithWebAuthn"
+      data: unknown
+      __tip: "See the XState TS docs to learn how to strongly type this."
+    }
+    "error.platform.registerWithWebAuthn": {
+      type: "error.platform.registerWithWebAuthn"
+      data: unknown
+    }
+    "done.invoke.regsiterWithSecurityKey": {
+      type: "done.invoke.regsiterWithSecurityKey"
+      data: unknown
+      __tip: "See the XState TS docs to learn how to strongly type this."
+    }
+    "error.platform.regsiterWithSecurityKey": {
+      type: "error.platform.regsiterWithSecurityKey"
+      data: unknown
+    }
+  }
+  invokeSrcNameMap: {
+    registerWithWebAuthn: "done.invoke.registerWithWebAuthn"
+    registerWithSecurityKey: "done.invoke.regsiterWithSecurityKey"
+  }
+  missingImplementations: {
+    actions: never
+    services: "registerWithWebAuthn" | "registerWithSecurityKey"
+    guards: "isMobileWebAuthn"
+    delays: never
+  }
+  eventsCausingServices: {
+    registerWithWebAuthn: ""
+    registerWithSecurityKey: ""
+  }
+  eventsCausingGuards: {
+    isMobileWebAuthn: ""
+  }
+  eventsCausingDelays: {}
+  matchesStates:
+    | "Select"
+    | "IsMobileWebAuthn"
+    | "End"
+    | "RegisterWithWebAuthn"
+    | "RegisterWithSecurityKey"
+  tags: never
+}

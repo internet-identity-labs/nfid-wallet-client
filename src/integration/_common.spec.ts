c19a95d09d61684d489269ed6eb4660586c6dfc7
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-07-22 14:17:28 +0200

    test(unpackResponse): define expectation

---
+++ src/integration/_common.spec.ts
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+import { unpackResponse } from "./_common"
+
+describe("_common test suite", () => {
+  describe("unpackResponse", () => {
+    it("should return empty array in success state", () => {
+      const response = unpackResponse<Array<string>>({
+        data: [],
+        error: [],
+        status_code: 200,
+      })
+      expect(response).toEqual([])
+    })
+  })
+})

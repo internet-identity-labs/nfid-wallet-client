184c32c5ad92b55cdf89f0fb569462cb8cdb71b7
Jorgen <jorgen@buildnode.io>
2022-07-22 12:39:11 -0700

    refactor: tidy up isMobileWithWebAuthn service

--- src/integration/device/services.ts
+++ src/integration/device/services.ts
@@ -2,10 +2,5 @@ import { fetchWebAuthnCapability, getIsMobileDeviceMatch } from "."
 
 export async function isMobileWithWebAuthn() {
   console.debug("isMobileWithWebAuthn call fetchWebAuthnCapability")
-  const hasWebAuthn = await fetchWebAuthnCapability()
-  console.debug("isMobileWithWebAuthn", {
-    hasWebAuthn,
-  })
-  if (!(hasWebAuthn && getIsMobileDeviceMatch())) throw new Error()
-  return hasWebAuthn
+  return (await fetchWebAuthnCapability()) && getIsMobileDeviceMatch()
 }

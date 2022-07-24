8fadc63766a5a6352ad842b10151b02fed7e65b6
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-07-24 12:11:32 +0200

    chore(getScope): explicit use of derivationOrigin

--- src/state/authorization.spec.ts
+++ src/state/authorization.spec.ts
@@ -7,14 +7,16 @@ describe("internet identity integration layer", () => {
   it("does not include a persona salt for the zero persona", () => {
     expect(getScope("http://test.com", 0)).toBe("http://test.com")
   })
-  it("does not include a persona salt for a null persona", () => {
-    expect(getScope("https://test.com")).toBe("https://test.com")
-  })
   it("includes persona salt for a 1+ persona", () => {
     expect(getScope("https://test.com", 1)).toBe("1@https://test.com")
   })
   it("adds https:// if no protocol was present", () => {
-    expect(getScope("test.com")).toBe("https://test.com")
+    expect(getScope("test.com", 0)).toBe("https://test.com")
     expect(getScope("test.com", 2)).toBe("2@https://test.com")
+  })
+  it("uses derivationOrigin if present", () => {
+    expect(getScope("test.com", 0, "my-other-domain.com")).toBe(
+      "https://my-other-domain.com",
+    )
   })
 })

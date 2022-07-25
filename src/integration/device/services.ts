b3e2ea0938e7b2f96fd12ccaa47f840897ae1944
Jorgen <jorgen@buildnode.io>
2022-07-25 16:56:07 -0700

    wip: scaffold device registration services

--- src/integration/device/services.ts
+++ src/integration/device/services.ts
@@ -4,3 +4,13 @@ export async function isMobileWithWebAuthn() {
   console.debug("isMobileWithWebAuthn call fetchWebAuthnCapability")
   return (await fetchWebAuthnCapability()) && getIsMobileDeviceMatch()
 }
+
+// NOTE: Maybe this should live somewhere else?
+export async function registerDeviceWithWebAuthn() {
+  throw new Error("Not implemented")
+}
+
+// NOTE: Maybe this should live somewhere else?
+export async function registerDeviceWithSecurityKey() {
+  throw new Error("Not implemented")
+}

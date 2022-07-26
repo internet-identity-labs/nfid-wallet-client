973fb0f79b7e62c05dcdd919b474b51600a79ea2
Jorgen <jorgen@buildnode.io>
2022-07-26 14:03:44 -0700

    fix: certified state / service overload retry

--- src/integration/actors.ts
+++ src/integration/actors.ts
@@ -1,6 +1,8 @@
 // A global singleton for our internet computer actors.
 import * as Agent from "@dfinity/agent"
+import { Identity, SubmitResponse } from "@dfinity/agent"
 import { InterfaceFactory } from "@dfinity/candid/lib/cjs/idl"
+import { Principal } from "@dfinity/principal"
 
 import { authState } from "frontend/integration/internet-identity"
 
@@ -53,9 +55,42 @@ export const ic = {
 // Agent //
 //////////
 
-// We share the same agent across all actors, and replace the identity when identity connection events occur.
+/** Agent which retries all failed calls in order to mitigate "certified state unavailable" and "service overload" 5XX errors. */
+class AgentWithRetry extends Agent.HttpAgent {
+  RETRY_LIMIT = 5
+  call(
+    canisterId: Principal | string,
+    options: {
+      methodName: string
+      arg: ArrayBuffer
+      effectiveCanisterId?: Principal | string
+    },
+    identity?: Identity | Promise<Identity>,
+    attempt: number = 1,
+  ) {
+    try {
+      return super.call(canisterId, options, identity)
+    } catch (e: unknown) {
+      if (attempt < this.RETRY_LIMIT) {
+        console.warn(
+          `Failed to fetch "${options.methodName}" from "${canisterId}" (attempt #${attempt})`,
+          e,
+        )
+        return new Promise<SubmitResponse>((res) => {
+          setTimeout(
+            () => res(this.call(canisterId, options, identity, attempt + 1)),
+            1000 * attempt,
+          )
+        })
+      }
+      console.error(`Failed to fetch after ${attempt} attempts`)
+      throw e
+    }
+  }
+}
 
-export const agent = new Agent.HttpAgent({ host: ic.host })
+/** We share the same agent across all actors, and replace the identity when identity connection events occur. */
+export const agent = new AgentWithRetry({ host: ic.host })
 
 /**
  * When user connects an identity, we update our agent.

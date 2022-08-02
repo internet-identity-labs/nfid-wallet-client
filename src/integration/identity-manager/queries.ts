9bed7fac8933f8f82fa47ee81d2b9aeb9c01a4f6
Jorgen <jorgen@buildnode.io>
2022-08-02 21:48:59 +0200

    fix: conflicts

--- src/integration/identity-manager/queries.ts
+++ src/integration/identity-manager/queries.ts
@@ -1,9 +1,9 @@
 import useSWR from "swr"
 
-import { fetchAccount } from "."
+import { fetchProfile } from "."
 
 export function useAccount() {
-  return useSWR("account/fetch", fetchAccount, {
+  return useSWR("account/fetch", fetchProfile, {
     dedupingInterval: 60_000,
     focusThrottleInterval: 60_000,
   })

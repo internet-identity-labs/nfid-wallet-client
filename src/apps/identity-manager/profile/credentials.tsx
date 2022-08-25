373bd57e71ec91cf825cb31fbf08e92d86077f00
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-08-25 16:27:23 +0200

    feat([sc-3691]): unhashed phone number

--- src/apps/identity-manager/profile/credentials.tsx
+++ src/apps/identity-manager/profile/credentials.tsx
@@ -1,11 +1,28 @@
 import React from "react"
+import useSWR from "swr"
+
+import { Loader } from "@internet-identity-labs/nfid-sdk-react"
 
 import { useAccount } from "frontend/integration/identity-manager/queries"
+import { authState } from "frontend/integration/internet-identity"
+import { decryptStringForIdentity } from "frontend/integration/lambda/symmetric"
 import ProfileCredentialsPage from "frontend/ui/pages/new-profile/credentials"
 
 const ProfileCredentials = () => {
   const { data } = useAccount()
-  return <ProfileCredentialsPage phone={data?.phoneNumber} />
+  const { data: decryptedPhone, isValidating } = useSWR(
+    "decryptPhoneNumber",
+    async () => {
+      const phone = data?.phoneNumber
+      const delegation = authState.get().delegationIdentity
+
+      if (!phone || !delegation) return ""
+      return await decryptStringForIdentity(phone, delegation)
+    },
+  )
+
+  if (isValidating) return <Loader isLoading={true} />
+  return <ProfileCredentialsPage phone={decryptedPhone} />
 }
 
 export default ProfileCredentials

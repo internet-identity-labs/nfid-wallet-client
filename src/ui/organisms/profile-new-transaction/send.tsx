d1a5816328cc57de1e84c4a566f03383d30ef8da
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-08-31 16:51:24 +0200

    fix: formatted files

--- src/ui/organisms/profile-new-transaction/send.tsx
+++ src/ui/organisms/profile-new-transaction/send.tsx
@@ -75,7 +75,8 @@ const TransactionSendForm: React.FC<ITransactionSendForm> = ({
 
   const onSumChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
-    if (Number(value) < 0) return setValue("sum", (Number(value) * -1).toString())
+    if (Number(value) < 0)
+      return setValue("sum", (Number(value) * -1).toString())
     setSumLength(value.length)
     if (Number(value) + walletFee > balance)
       setError("sum", {

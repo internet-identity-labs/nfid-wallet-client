b28c6edb3120562c73663b70dd788467a359f491
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-09-05 16:01:26 +0200

    fix([sc-3842]): integrate browserstack

---
+++ wdio.bs.mobile.ts
@@ -0,0 +1,40 @@
+import { config as common } from "./wdio.conf"
+
+export const config = {
+  ...common,
+  user: "philipplitzenber_bPNyfo",
+  key: "faaS9WisBd4BqfFnyoEz",
+  hostname: "hub.browserstack.com",
+  capabilities: [
+    {
+      browserName: "ios",
+      "bstack:options": {
+        deviceName: "iPhone 12 Pro Max",
+        osVersion: "16 Beta",
+      },
+    },
+    {
+      browserName: "android",
+      "bstack:options": {
+        deviceName: "Google Pixel 6 Pro",
+        osVersion: "13.0",
+      },
+    },
+    {
+      browserName: "ios",
+      "bstack:options": {
+        deviceName: "iPad Air 4",
+        osVersion: "14",
+      },
+    },
+  ],
+  commonCapabilities: {
+    "bstack:options": {
+      buildName: "browserstack-nfid-mobile",
+    },
+  },
+  cucumberOpts: {
+    ...common.cucumberOpts,
+    tagExpression: "@mobile",
+  },
+}

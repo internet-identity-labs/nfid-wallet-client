f056fcda95541092e85ec8b9b8c9f381a23a900b
Dmitrii <dmitrii@identityLabs.ooo>
2022-09-06 12:27:36 +0400

    E2E Integrate Google Registration flow into [sc-4139]

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -77,7 +77,7 @@ export const config: WebdriverIO.Config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: isDebug ? 1 : 2,
+  maxInstances: 1,
   // maxInstances: 1,
   //
   // If you have trouble getting all important capabilities together, check out the

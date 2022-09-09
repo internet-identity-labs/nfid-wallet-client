d93f75705287e87e204be1395866ba108036a0b9
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-09-09 15:41:42 +0200

    feat([sc-3497]): setup auth popup

--- src/apps/marketing/landing-page/hero-right-side.tsx
+++ src/apps/marketing/landing-page/hero-right-side.tsx
@@ -9,32 +9,30 @@ import Group from "./assets/Group.svg"
 
 interface HeroRightSideProps extends ElementProps<HTMLDivElement> {
   isUnregistered?: boolean
-  hasAccount?: boolean
 }
 
-export const HeroRightSide: React.FC<HeroRightSideProps> = ({
+export const NFIDAuthentication: React.FC<HeroRightSideProps> = ({
   isUnregistered,
-  hasAccount,
 }) => {
-  console.debug("HeroRightSide", { isUnregistered, hasAccount })
+  console.debug("HeroRightSide", { isUnregistered })
 
   return isUnregistered ? (
     <div
       className={clsx(
-        "relative m-auto sm:block z-10 bg-white p-6 rounded-b-md",
+        "relative m-auto sm:block z-10 bg-white rounded-b-md",
         "mb-[20vh] sm:mb-[60vh] max-w-[360px] drop-shadow-[0_10px_60px_rgba(48,139,245,0.30)]",
         "min-h-[400px]",
       )}
     >
       <NFIDGradientBar className="w-full h-0.5 z-20" rounded={false} />
-      <NFIDAuthenticationCoordinator />
+      <div className="flex flex-col w-full h-full p-6">
+        <NFIDAuthenticationCoordinator />
+      </div>
       <div className="z-0 bg-white"></div>
     </div>
-  ) : hasAccount ? (
+  ) : (
     <div className="mb-[75px] sm:mb-[87px] z-10 relative">
       <img src={Group} alt="Group" />
     </div>
-  ) : (
-    <span></span>
   )
 }

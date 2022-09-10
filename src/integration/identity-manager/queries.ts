97d6f07408bf1435113e1c3d4c2ad689df5cba45
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-09-10 19:34:26 +0200

    feat([sc-4244]): applications meta mapping in profile

--- src/integration/identity-manager/queries.ts
+++ src/integration/identity-manager/queries.ts
@@ -1,9 +1,16 @@
 import useSWR from "swr"
 
-import { fetchProfile } from "."
+import { fetchApplications, fetchProfile } from "."
 
 export function useAccount() {
   return useSWR("account/fetch", fetchProfile, {
+    dedupingInterval: 60_000,
+    focusThrottleInterval: 60_000,
+  })
+}
+
+export function useApplicationsMeta() {
+  return useSWR("applications/meta", fetchApplications, {
     dedupingInterval: 60_000,
     focusThrottleInterval: 60_000,
   })

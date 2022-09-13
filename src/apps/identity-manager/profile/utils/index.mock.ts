851ff809bf70589c88a81dfccfe9295190a97b53
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-09-13 11:34:02 +0200

    fix: added unit tests for groupPersonasByApplications

---
+++ src/apps/identity-manager/profile/utils/index.mock.ts
@@ -0,0 +1,43 @@
+export const mockAccountApplications = [
+  {
+    domain: "https://wzkxy-vyaaa-aaaaj-qab3q-cai.ic0.app",
+    label: "Account #0",
+    accountId: "0",
+  },
+  {
+    domain: "http://localhost:3000",
+    label: "Account #0",
+    accountId: "0",
+  },
+  {
+    domain: "https://wzkxy-vyaaa-aaaaj-qab3q-cai.ic0.app",
+    label: "Account #1",
+    accountId: "1",
+  },
+  {
+    domain: "https://wzkxy-vyaaa-aaaaj-qab3q-cai.ic0.app",
+    label: "Account #2",
+    accountId: "2",
+  },
+]
+
+export const mockApplicationsMeta = [
+  {
+    accountLimit: 5,
+    domain: "http://localhost:3000",
+    name: "My Sweet App",
+    alias: ["http://localhost:3000"],
+  },
+  {
+    accountLimit: 5,
+    domain: "https://wzkxy-vyaaa-aaaaj-qab3q-cai.ic0.app",
+    name: "NFID-Demo",
+    alias: ["https://wzkxy-vyaaa-aaaaj-qab3q-cai.ic0.app"],
+  },
+  {
+    accountLimit: 1,
+    domain: "https://zqrbj-lqaaa-aaaal-aa7ja-cai.ic0.app",
+    name: "NFID-SA-DEMO",
+    alias: [],
+  },
+]

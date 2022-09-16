89b7d1e9016473faebff3d88da6f4cec2902ac81
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-09-16 14:27:47 +0200

    fix([sc-4578]): remove trailing slash

--- src/integration/app-config/index.ts
+++ src/integration/app-config/index.ts
@@ -1,12 +1,13 @@
 // Code allowing third parties to configure their apps will go here
 import { Application, fetchApplications } from "../identity-manager"
+import { rmTrailingSlash } from "../identity-manager/persona/utils"
 
 // NOTE: make sure to sanitize application meta
 function selectApplication(
   hostname: string,
   applications: Application[],
 ): Application | undefined {
-  const app = applications.find((a) => a.domain === hostname)
+  const app = applications.find((a) => rmTrailingSlash(a.domain) === hostname)
   return app
 }
 

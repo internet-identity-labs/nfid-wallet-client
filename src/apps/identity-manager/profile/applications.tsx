e6d361e0b52d869758f5091b55d187fd93274b8c
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-09-20 17:17:32 +0200

    fix([sc-4539]): produce model in mapping function

--- src/apps/identity-manager/profile/applications.tsx
+++ src/apps/identity-manager/profile/applications.tsx
@@ -1,10 +1,10 @@
-import { useAccount } from "frontend/integration/identity-manager/queries"
+import { useApplicationAccounts } from "frontend/integration/identity-manager/queries"
 import ProfileApplicationsPage from "frontend/ui/pages/new-profile/applications"
 
 const ProfileApplications = () => {
-  const { data: account } = useAccount()
+  const { applicationAccounts } = useApplicationAccounts()
 
-  return <ProfileApplicationsPage applications={account?.accounts ?? []} />
+  return <ProfileApplicationsPage applications={applicationAccounts} />
 }
 
 export default ProfileApplications

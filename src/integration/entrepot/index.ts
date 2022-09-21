8abcc57459cdabaadf2e26e0c5f031e3d57f2595
jorgenbuilder <jorgen@buildnode.io>
2022-09-20 18:32:19 -0700

    handle entrepot assets

--- src/integration/entrepot/index.ts
+++ src/integration/entrepot/index.ts
@@ -1,40 +1,88 @@
 import { Principal } from "@dfinity/principal"
-import { principalToAddress } from "ictool"
+import { decodeTokenIdentifier, encodeTokenIdentifier } from "ictool"
 
-import {
-  NFTData,
-  NFTDetails,
-} from "frontend/integration/entrepot/entrepot_interface"
-import {
-  mapToNFTData,
-  toNftDetails,
-} from "frontend/integration/entrepot/mapper"
 import { Account } from "frontend/integration/identity-manager"
-import { restCall } from "frontend/integration/rosetta/util"
 
-const entrepot = "https://us-central1-entrepot-api.cloudfunctions.net/api"
+import {
+  assetFullsize,
+  assetPreview,
+  fetchCollection,
+  fetchCollections,
+  fetchCollectionTokens,
+  fetchNFTsOfPrincipals,
+  getTokenLink,
+} from "./lib"
+import type { EntrepotCollection, EntrepotToken, NFTDetails, UserNFTDetails } from "./types"
 
-export async function getNFTsOfPrincipals(
-  inputData: { principal: Principal; account: Account }[],
-): Promise<NFTData[]> {
-  const response = await Promise.all(
-    inputData.map(async ({ principal, account }) => {
-      const address: string = principalToAddress(principal as any)
-      return await restCall(
-        "GET",
-        `${entrepot}/maddies/getAllNfts/${address}`,
-      ).then((r) => mapToNFTData(r, principal, account))
-    }),
-  )
-  return response.flat()
+/**
+ * Retrieve all known NFT collections.
+ */
+export async function collections(): Promise<EntrepotCollection[]> {
+  return Object.values(await fetchCollections())
 }
 
-export async function getNFTDetails(canisterId: string): Promise<NFTDetails> {
-  let nftDetails = await restCall("GET", `${entrepot}/collections`)
-    .then(toNftDetails)
-    .then((l) => l.find((e) => e.id === canisterId))
-  if (typeof nftDetails === "undefined") {
-    throw Error(`Collection is undefined ${canisterId}`)
+/**
+ * Retrieve details for a specific collection.
+ * @param id Canister ID of the collection.
+ */
+export async function collection(id: string): Promise<EntrepotCollection> {
+  return fetchCollection(id)
+}
+
+/**
+ * Retrieve details for a specific token.
+ */
+export async function token(
+  collection: EntrepotCollection,
+  tokens: EntrepotToken[],
+  index: number,
+): Promise<NFTDetails> {
+  const tokenId = encodeTokenIdentifier(collection.id, index)
+  const token = tokens.find((token) => token.tokenId === tokenId)
+  if (!token) {
+    throw new Error(
+      `Could not find token #${index} of ${collection.name} (${collection.id})`,
+    )
   }
-  return nftDetails
+  return {
+    collection,
+    canisterId: collection.id,
+    index,
+    tokenId,
+    name: `${collection.name} #${index}`,
+    assetPreview: assetPreview(collection, token),
+    assetFullsize: await assetFullsize(collection, token),
+  }
+}
+
+/**
+ * Retrieve tokens for a given collection.
+ */
+export async function tokens(
+  collection: EntrepotCollection,
+): Promise<NFTDetails[]> {
+  return Promise.all(
+    await fetchCollectionTokens(collection).then((tokens) =>
+      tokens.slice(0, 4).map(async ({ tokenId }) => {
+        const { index } = decodeTokenIdentifier(tokenId)
+        return await token(collection, tokens, index)
+      }),
+    ),
+  )
+}
+
+/**
+ * Retrieve link to NFT.
+ */
+export function link(canister: string, index: number): string {
+  return getTokenLink(canister, index)
+}
+
+/**
+ * Retrieve all NFTs for the given principals.
+ */
+export async function principalTokens(
+  inputData: { principal: Principal; account: Account }[],
+): Promise<UserNFTDetails[]> {
+  return (await fetchNFTsOfPrincipals(inputData)).flat()
 }
--- src/integration/entrepot/mapper.ts
+++ src/integration/entrepot/mapper.ts
@@ -1,34 +1,18 @@
 import { Principal } from "@dfinity/principal"
-
-import {
-  EntrepotNFTData,
-  NFTData,
-  NFTDetails,
-} from "frontend/integration/entrepot/entrepot_interface"
 import { Account } from "frontend/integration/identity-manager"
+import { decodeTokenIdentifier } from "ictool"
+import { collection, token } from "."
+import { EntrepotToken, UserNFTDetails } from "./types"
 
 export async function mapToNFTData(
-  response: Response,
+  response: EntrepotToken[],
   principal: Principal,
   account: Account,
-): Promise<NFTData[]> {
-  return await response
-    .json()
-    .then((data) => data as EntrepotNFTData[])
-    .then((responseDataArray: EntrepotNFTData[]) => {
-      return responseDataArray.map((entrepotNFT) => {
-        return {
-          account: account,
-          canisterId: entrepotNFT.canisterId,
-          imageUrl: entrepotNFT.imageUrl,
-          owner: entrepotNFT.owner,
-          principal: principal,
-          tokenId: entrepotNFT.tokenId,
-        } as NFTData
-      })
-    })
-}
-
-export async function toNftDetails(response: Response): Promise<NFTDetails[]> {
-  return await response.json().then((data) => data as NFTDetails[])
+): Promise<UserNFTDetails[]> {
+  return Promise.all(response.map(async (entrepotNFT) => {
+    const _collection = await collection(entrepotNFT.canisterId);
+    const { index } = decodeTokenIdentifier(entrepotNFT.tokenId);
+    const _token = await token(_collection, response, index)
+    return { ..._token, principal, account }
+  }))
 }

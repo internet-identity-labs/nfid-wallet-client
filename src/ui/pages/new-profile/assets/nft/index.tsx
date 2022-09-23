0f20bdda0e35611d0f3617f31203f8b12ce99542
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-09-23 20:01:52 +0200

    feat([sc-4441]): undefined nft validation

--- src/ui/pages/new-profile/assets/nft/index.tsx
+++ src/ui/pages/new-profile/assets/nft/index.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 
+import { NFTDetails } from "frontend/integration/entrepot/types"
+
 import { ProfileNFTNotPresent } from "./not-present-nft"
 import { ProfileNFTPresent } from "./present-nft"
 
 interface IProfileAssetsNFT extends React.HTMLAttributes<HTMLDivElement> {
-  nfts: any[]
+  nfts: NFTDetails[]
 }
 
 export const ProfileAssetsNFT: React.FC<IProfileAssetsNFT> = ({ nfts }) => {
-  if (nfts.length) return <ProfileNFTPresent nfts={nfts} />
+  if (nfts && nfts.length) return <ProfileNFTPresent nfts={nfts} />
   else return <ProfileNFTNotPresent />
 }

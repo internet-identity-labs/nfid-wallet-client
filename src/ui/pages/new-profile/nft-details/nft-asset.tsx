b9e5d1acdf90b4950de0db4135a6d81e1242e158
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-09-25 16:57:29 +0200

    feat([sc-4413]): nft details without transactions

---
+++ src/ui/pages/new-profile/nft-details/nft-asset.tsx
@@ -0,0 +1,38 @@
+import { DisplayFormat } from "frontend/integration/entrepot/types"
+
+export const NFTAsset = (assetFullsize: {
+  url: string
+  format: DisplayFormat
+}) => {
+  switch (assetFullsize.format) {
+    case "img":
+      return (
+        <div
+          style={{ backgroundImage: `url(${assetFullsize.url})` }}
+          className="relative h-full bg-center bg-no-repeat bg-contain"
+        />
+      )
+    case "iframe":
+      return (
+        <iframe
+          title="nft"
+          frameBorder="0"
+          src={assetFullsize.url}
+          alt=""
+          style={{
+            border: "none",
+            height: "100%",
+            width: "100%",
+            display: "block",
+          }}
+        />
+      )
+    default:
+      return (
+        <div
+          style={{ backgroundImage: `url(${assetFullsize.url})` }}
+          className="relative z-20 h-full bg-center bg-no-repeat bg-contain"
+        />
+      )
+  }
+}

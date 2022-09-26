87580c165d040798a17b1030ebf67cabc0e05086
jorgenbuilder <jorgen@buildnode.io>
2022-09-26 15:08:17 -0700

    chore: move hooks

--- src/apps/identity-manager/profile/nfts.tsx
+++ src/apps/identity-manager/profile/nfts.tsx
@@ -1,73 +1,8 @@
-import { decodeTokenIdentifier } from "ictool"
-import useSWR from "swr"
-
-import {
-  principalTokens,
-  collection,
-  tokens,
-} from "frontend/integration/entrepot"
-import { fetchPrincipals } from "frontend/integration/facade"
-import {
-  fetchAccounts,
-  fetchApplications,
-} from "frontend/integration/identity-manager"
 import {
   useApplicationsMeta,
-  useProfile,
 } from "frontend/integration/identity-manager/queries"
 import ProfileNFTsPage from "frontend/ui/pages/new-profile/nfts"
-
-export function useNFT(tokenid: string) {
-  const { canister } = decodeTokenIdentifier(tokenid)
-  const _collection = useSWR(`collection/${canister}`, () =>
-    collection(canister),
-  )
-  const _tokens = useSWR(
-    _collection.data ? `collection/${canister}/tokens` : null,
-    () => {
-      if (!_collection.data) throw new Error("unreachable")
-      return tokens(_collection.data)
-    },
-  )
-  return useSWR(
-    _collection.data && _tokens.data ? `token/${tokenid}` : null,
-    () => {
-      if (!_collection.data || !_tokens.data) throw new Error("unreachable")
-      return _tokens.data.find((token) => token.tokenId === tokenid)
-    },
-  )
-}
-
-export function useAllNFTs() {
-  const { profile } = useProfile()
-
-  const accounts = useSWR(`accounts`, fetchAccounts)
-  const applications = useSWR(`applications`, () => {
-    return fetchApplications().then((r) => r.filter((app) => app.isNftStorage))
-  })
-  const principals = useSWR(
-    profile?.anchor && accounts?.data && applications.data
-      ? `principals${profile.anchor}`
-      : null,
-    () => {
-      if (!profile || !accounts.data || !applications.data)
-        throw new Error("Unreachable")
-      return fetchPrincipals(
-        BigInt(profile.anchor),
-        accounts.data,
-        applications.data,
-      )
-    },
-  )
-  return useSWR(principals?.data ? `userTokers` : null, () => {
-    if (!principals.data) throw new Error("unreachable")
-    console.debug(
-      "Searched for NFTs at principals",
-      principals.data.map((p) => [p.account, p.principal.toText()]),
-    )
-    return principalTokens(principals.data)
-  })
-}
+import { useAllNFTs } from "frontend/state/hooks/nfts"
 
 const ProfileNFTs = () => {
   const tokens = useAllNFTs()

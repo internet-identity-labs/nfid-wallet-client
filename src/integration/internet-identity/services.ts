7692446b8f43bb3662d5c65adf1e35aabd71a17f
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2022-09-28 15:19:26 +0400

    fix([sc-4580]): prod Dev Windows 10 11 Chrome Brave Edge
    * Prod Dev Windows 10 11 Chrome Brave Edge [sc-4580]
    
    * Prod Dev Windows 10 11 Chrome Brave Edge [sc-4580]
    
    * fix([sc-4580]): prod Dev Windows 10 11 Chrome Brave Edge
    
    * fix([sc-4580]): prod Dev Windows 10 11 Chrome Brave Edge

--- src/integration/internet-identity/services.ts
+++ src/integration/internet-identity/services.ts
@@ -253,7 +253,10 @@ export async function registerService(
 export async function fetchAuthenticatorDevicesService(context: {
   profile: Profile
 }) {
-  const devices = await lookup(Number(context.profile.anchor), false)
+  const devices = await lookup(
+    Number(context.profile.anchor),
+    (x) => x.purpose === "authentication",
+  )
   console.debug(`fetchAuthenticatorDevicesService lookup`, { devices })
   return devices
 }

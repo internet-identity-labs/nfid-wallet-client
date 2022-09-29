8ecff1abe230124f63b0d6933bc581a055c5ab79
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-09-29 23:22:28 +0200

    feat: pavlo's ui changes

--- src/ui/pages/new-profile/nft-details/index.tsx
+++ src/ui/pages/new-profile/nft-details/index.tsx
@@ -1,11 +1,12 @@
 import clsx from "clsx"
 import React from "react"
+import { To } from "react-router-dom"
 import { toast } from "react-toastify"
 
 import { ITransaction } from "frontend/apps/identity-manager/profile/nft-details/utils"
-import { ProfileConstants } from "frontend/apps/identity-manager/profile/routes"
 import { link } from "frontend/integration/entrepot"
 import { NFTDetails, UserNFTDetails } from "frontend/integration/entrepot/types"
+import { Application } from "frontend/integration/identity-manager"
 import { Loader } from "frontend/ui/atoms/loader"
 import Table from "frontend/ui/atoms/table"
 import ProfileContainer from "frontend/ui/templates/profile-container/Container"
@@ -20,14 +21,14 @@ interface IProfileNFTDetails {
   nft: UserNFTDetails | NFTDetails
   isTransactionsFetching?: boolean
   transactions: ITransaction[]
+  applications: Application[]
 }
-
-const containerClassName = "max-w-[100vw] px-4 sm:px-[30px]"
 
 export const ProfileNFTDetailsPage = ({
   nft,
   isTransactionsFetching = false,
   transactions,
+  applications,
 }: IProfileNFTDetails) => {
   const copyToClipboard = React.useCallback(() => {
     toast.info("NFT URL copied to clipboard", { toastId: "NFTCopied" })
@@ -35,9 +36,11 @@ export const ProfileNFTDetailsPage = ({
   }, [nft.collection.id, nft.index])
 
   console.log({ nft })
+
   return (
     <ProfileTemplate
-      onBack={`${ProfileConstants.base}/${ProfileConstants.nfts}`}
+      // I have not found any other ways to avoid as To. This is exactly what navigate expect
+      onBack={-1 as To}
       icon={CopyIcon}
       onIconClick={copyToClipboard}
       className="w-full min-w-fit z-[1]"
@@ -45,19 +48,18 @@ export const ProfileNFTDetailsPage = ({
     >
       <div
         className={clsx(
-          "grid gap-[30px]",
-          "grid-cols-1 sm:grid-cols-[30vw,1fr]",
-          containerClassName,
+          "grid gap-[30px] max-w-[100vw]",
+          "grid-cols-1 sm:grid-cols-[auto,1fr]",
         )}
       >
-        <div className="relative overflow-hidden bg-gray-50 rounded-xl aspect-square">
+        <div className="relative overflow-hidden bg-gray-50 rounded-xl h-[342px] aspect-square">
           <div
             style={{
               backgroundImage: `url(${nft.assetPreview})`,
             }}
             className={clsx(
               "bg-cover bg-center bg-no-repeat blur-md brightness-150",
-              "h-full absolute z-10 w-full opacity-90",
+              "h-full absolute z-10 w-full opacity-70",
               nft.assetFullsize.format === "video" && "hidden",
             )}
           />
@@ -73,7 +75,11 @@ export const ProfileNFTDetailsPage = ({
             <div className="flex items-center mt-4 space-x-2">
               <img src={WalletIcon} alt="wallet" />
               <p className="text-sm font-semibold text-gray-400">
-                {nft?.account?.label}
+                {
+                  applications.find((x) => x.domain === nft.account.domain)
+                    ?.name
+                }{" "}
+                account {Number(nft.account.accountId) + 1}
               </p>
             </div>
           ) : null}
@@ -84,35 +90,34 @@ export const ProfileNFTDetailsPage = ({
               >
                 <p className="mb-1 text-gray-400">Standard</p>
                 <p className={clsx("w-full sm:w-[80%]")}>
-                  {nft.collection.standard}
+                  {nft.collection.standard === "legacy"
+                    ? "Legacy EXT"
+                    : nft.collection.standard}
                 </p>
               </div>
               <div
                 className={clsx("flex items-center justify-between flex-wrap")}
               >
-                <p className="mb-1 text-gray-400">ID</p>
+                <p className="mb-1 text-gray-400">NFT ID</p>
                 <p className="w-full sm:w-[80%]">{nft.tokenId}</p>
               </div>
               <div
                 className={clsx("flex items-center justify-between flex-wrap")}
               >
-                <p className="mb-1 text-gray-400">Canister</p>
+                <p className="mb-1 text-gray-400">Collection ID</p>
                 <p className="w-full sm:w-[80%]">{nft.canisterId}</p>
               </div>
             </div>
           </ProfileContainer>
         </div>
       </div>
-      <div className={clsx("mt-[30px]", containerClassName)}>
+      <div className={clsx("mt-[30px] max-w-[100vw]")}>
         <ProfileContainer title="About">
           <p className="text-sm">{nft.collection.description}</p>
         </ProfileContainer>
       </div>
       <div
-        className={clsx(
-          "block border border-gray-200 rounded-xl",
-          "my-[30px] mx-4 sm:mx-[30px]",
-        )}
+        className={clsx("block border border-gray-200 rounded-xl", "my-[30px]")}
       >
         <div
           className={clsx(
@@ -130,9 +135,10 @@ export const ProfileNFTDetailsPage = ({
             val: Object.values(transaction).map((value, i) => (
               <span
                 className={clsx(
+                  "text-sm",
                   i === 0 && "capitalize",
                   (i === 1 || i === 4) && "whitespace-nowrap",
-                  (i === 2 || i === 3) && "w-[216px] break-words inline-block",
+                  (i === 2 || i === 3) && "w-[286px] break-words inline-block",
                 )}
               >
                 {value}

cef4d95c4d69112ab541447bda9633cf9c5301f8
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-10-06 20:13:33 +0200

    feat([sc-4788]): init useBalanceICPAll interface

--- src/integration/rosetta/queries.spec.ts
+++ src/integration/rosetta/queries.spec.ts
@@ -3,32 +3,54 @@
  */
 import { Ed25519KeyIdentity } from "@dfinity/identity"
 import { Principal } from "@dfinity/principal"
-import { renderHook } from "@testing-library/react"
+import { act, renderHook, waitFor } from "@testing-library/react"
 
+import * as imHooks from "frontend/integration/identity-manager/queries"
 import * as iiHooks from "frontend/integration/internet-identity/queries"
 
 import * as rosettaMocks from "."
-import { Account, Profile } from "../identity-manager"
-import { useBalanceICPAll } from "./queries"
+import { Account, Application } from "../identity-manager"
+import { sumDecimalValue, useBalanceICPAll } from "./queries"
 
 describe("rosetta queries suite", () => {
+  describe("sumDecimalValue", () => {
+    it("shoudl sum decimal values", () => {
+      expect(
+        sumDecimalValue(
+          { value: "0.0001", decimals: 4 },
+          { value: "0.0002", decimals: 4 },
+        ),
+      ).toBe("0.0003")
+    })
+    it("should throw when different decimals", () => {
+      expect(() =>
+        sumDecimalValue(
+          { value: "0.0001", decimals: 2 },
+          { value: "0.0002", decimals: 4 },
+        ),
+      ).toThrow("decimals must match")
+    })
+  })
   describe("useBalanceICPAll", () => {
-    it("should work", () => {
+    it("should return application balances with accumulated balance greater than zero", async () => {
+      const commonFields = {
+        currency: {
+          symbol: "ICP",
+          decimals: 8,
+          metadata: {
+            Issuer: "",
+          },
+        },
+        metadata: {},
+      }
+      const call1Promise = Promise.resolve({ value: "0.0001", ...commonFields })
+      const call2Promise = Promise.resolve({ value: "0.0002", ...commonFields })
+      const call3Promise = Promise.resolve({ value: "0.0000", ...commonFields })
       const getBalanceSpy = jest
         .spyOn(rosettaMocks, "getBalance")
-        .mockImplementation(() =>
-          Promise.resolve({
-            value: "0",
-            currency: {
-              symbol: "ICP",
-              decimals: 1,
-              metadata: {
-                Issuer: "",
-              },
-            },
-            metadata: {},
-          }),
-        )
+        .mockImplementationOnce(() => call1Promise)
+        .mockImplementationOnce(() => call2Promise)
+        .mockImplementationOnce(() => call3Promise)
 
       const useAllPrincipals = jest
         .spyOn(iiHooks, "useAllPrincipals")
@@ -57,10 +79,69 @@ describe("rosetta queries suite", () => {
             },
           ],
         }))
-      const { result, rerender } = renderHook(() => useBalanceICPAll())
-      expect(useAllPrincipals).toHaveBeenCalled()
-      expect(result.current.isLoading).toBe(true)
-      expect(getBalanceSpy).toBeCalledTimes(3)
+
+      const useApplicationsMeta = jest
+        .spyOn(imHooks, "useApplicationsMeta")
+        .mockImplementation(() => ({
+          applicationsMeta: [
+            {
+              accountLimit: 1,
+              domain: "domain-1",
+              name: "Application 1",
+              isNftStorage: false,
+            } as Application,
+            {
+              accountLimit: 1,
+              domain: "domain-2",
+              name: "Application 2",
+              isNftStorage: false,
+            } as Application,
+          ],
+          isLoading: false,
+          refreshApplicationMeta: jest.fn(),
+        }))
+
+      const { result } = renderHook(() => useBalanceICPAll())
+
+      await act(async () => {
+        await call1Promise
+        await call2Promise
+        await call3Promise
+      })
+
+      waitFor(() => {
+        expect(useAllPrincipals).toHaveBeenCalled()
+        expect(useApplicationsMeta).toHaveBeenCalled()
+        expect(result.current.isLoading).toBe(true)
+        expect(getBalanceSpy).toBeCalledTimes(3)
+      })
+      expect(result.current.isLoading).toBe(false)
+
+      expect(result.current.balanceICPRaw?.length).toBe(3)
+      expect(result.current.appAccountBalance).toEqual({
+        "Application 1": {
+          totalBalance: {
+            value: "0.0003",
+            ...commonFields,
+          },
+          accounts: [
+            {
+              accountId: "0",
+              balance: {
+                value: "0.0001",
+                ...commonFields,
+              },
+            },
+            {
+              accountId: "1",
+              balance: {
+                value: "0.0002",
+                ...commonFields,
+              },
+            },
+          ],
+        },
+      })
     })
   })
 })
--- src/integration/rosetta/queries.ts
+++ src/integration/rosetta/queries.ts
@@ -2,19 +2,145 @@ import React from "react"
 import useSWR from "swr"
 
 import { getBalance } from "."
+import { Account, Application } from "../identity-manager"
+import { useApplicationsMeta } from "../identity-manager/queries"
 import { useAllPrincipals } from "../internet-identity/queries"
+import { Balance } from "./rosetta_interface"
 
-export const useBalanceICPAll = () => {
+interface RawBalance {
+  principalId: string
+  account: Account
+  balance: Balance
+}
+
+interface AccountBalance {
+  accountId: string
+  balance: Balance
+}
+
+interface AppBalance {
+  totalBalance: Balance
+  accounts: AccountBalance[]
+}
+
+interface AppAccountBalanceRecords {
+  [applicationName: string]: AppBalance | undefined
+}
+
+export const sumDecimalValue = (
+  a: { value: string; decimals: number },
+  b: { value: string; decimals: number },
+) => {
+  if (a.decimals !== b.decimals) throw new Error("decimals must match")
+  const multiplier = Math.pow(10, a.decimals)
+  return `${
+    (multiplier * parseFloat(a.value) + multiplier * parseFloat(b.value)) /
+    multiplier
+  }`
+}
+
+const reduceRawToAppAccountBalance = (
+  rawBalance: RawBalance[],
+  applications: Application[],
+  filterZeroAccount: boolean,
+): AppAccountBalanceRecords => {
+  const commonFields = {
+    currency: {
+      symbol: "ICP",
+      decimals: 8,
+      metadata: {
+        Issuer: "",
+      },
+    },
+    metadata: {},
+  }
+  const appAccountBalance = {
+    "Application 1": {
+      totalBalance: {
+        value: "0.0003",
+        ...commonFields,
+      },
+      accounts: [
+        {
+          accountId: "0",
+          balance: {
+            value: "0.0001",
+            ...commonFields,
+          },
+        },
+        {
+          accountId: "1",
+          balance: {
+            value: "0.0002",
+            ...commonFields,
+          },
+        },
+      ],
+    },
+  }
+  // return appAccountBalance
+  return rawBalance.reduce<AppAccountBalanceRecords>((acc, rawBalance) => {
+    const applicationMatch = applications.find(
+      (a) => a.domain === rawBalance.account.domain,
+    )
+
+    const appName = applicationMatch
+      ? applicationMatch.name
+      : rawBalance.account.domain
+
+    const currentApp: AppBalance | undefined = acc[appName]
+
+    const totalBalanceValue = sumDecimalValue(
+      {
+        value: currentApp?.totalBalance?.value || "0",
+        decimals:
+          currentApp?.totalBalance?.currency?.decimals ||
+          rawBalance.balance.currency.decimals,
+      },
+      {
+        value: rawBalance.balance.value,
+        decimals: rawBalance.balance.currency.decimals,
+      },
+    )
+
+    if (filterZeroAccount && totalBalanceValue === "0") return acc
+
+    return {
+      ...acc,
+      [appName]: {
+        totalBalance: {
+          ...rawBalance.balance,
+          value: totalBalanceValue,
+        } as Balance,
+        accounts: [
+          ...(currentApp ? currentApp.accounts : []),
+          {
+            accountId: rawBalance.account.accountId,
+            balance: rawBalance.balance,
+          },
+        ],
+      },
+    }
+  }, {})
+}
+
+/**
+ * returns map of applications and there accumulated balance across all accounts
+ *
+ * @param filterZeroAccount only include applications with non zero balance
+ */
+export const useBalanceICPAll = (filterZeroAccount: boolean = true) => {
   const { principals } = useAllPrincipals()
+  const { applicationsMeta } = useApplicationsMeta()
 
-  const { data: balanceICPAll, isValidating: isLoading } = useSWR(
+  const { data: balanceICPRaw, isValidating: isLoading } = useSWR(
     principals ? "balanceICPAll" : null,
     async () => {
       if (!principals) throw new Error("principals required")
 
       return Promise.all(
         principals.map(async ({ principal, account }) => ({
-          principal,
+          principalId: principal.toText(),
           account,
           balance: await getBalance(principal),
         })),
@@ -22,7 +148,17 @@ export const useBalanceICPAll = () => {
     },
   )
 
-  // const posBalanceICP = React.useMemo(() => {}, [balanceICPAll])
+  const appAccountBalance = React.useMemo(
+    () =>
+      balanceICPRaw
+        ? reduceRawToAppAccountBalance(
+            balanceICPRaw,
+            applicationsMeta || [],
+            filterZeroAccount,
+          )
+        : null,
+    [balanceICPRaw, applicationsMeta],
+  )
 
-  return { isLoading, balanceICPAll }
+  return { isLoading, balanceICPRaw, appAccountBalance }
 }

ea91aad0830fdca53bfd0a61cd145ca719b0cb81
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-10-06 20:15:59 +0200

    feat([sc-4788]): update reduced data structure

--- src/integration/rosetta/queries.ts
+++ src/integration/rosetta/queries.ts
@@ -20,17 +20,24 @@ interface RawBalance {
 }
 
 interface AccountBalance {
-  accountId: string
-  balance: Balance
+  accountName: string
+  icpBalance: string
+  usdBalance: string
 }
 
 interface AppBalance {
-  totalBalance: Balance
+  icon?: string
+  appName: string
+  icpBalance: string
   accounts: AccountBalance[]
 }
 
-interface AppAccountBalanceRecords {
-  [applicationName: string]: AppBalance | undefined
+interface ICPBalanceSheet {
+  label: string
+  token: string
+  icpBalance: string
+  usdBalance: string
+  applications: { [applicationName: string]: AppBalance | undefined }
 }
 
 export const sumE8sICPString = (a: string, b: string) => {
@@ -41,42 +48,62 @@ const reduceRawToAppAccountBalance = (
   rawBalance: RawBalance[],
   applications: Application[],
   filterZeroAccount: boolean,
-): AppAccountBalanceRecords => {
-  return rawBalance.reduce<AppAccountBalanceRecords>((acc, rawBalance) => {
-    const applicationMatch = applications.find(
-      (a) => a.domain === rawBalance.account.domain,
-    )
+): ICPBalanceSheet => {
+  return rawBalance.reduce<ICPBalanceSheet>(
+    (acc, rawBalance) => {
+      const applicationMatch = applications.find(
+        (a) => a.domain === rawBalance.account.domain,
+      )
 
-    const appName = applicationMatch
-      ? applicationMatch.name
-      : rawBalance.account.domain
+      const appName = applicationMatch
+        ? applicationMatch.name
+        : rawBalance.account.domain
 
-    const currentApp: AppBalance | undefined = acc[appName]
+      const currentApp: AppBalance | undefined = acc.applications[appName]
 
-    const totalBalanceValue = sumE8sICPString(
-      currentApp?.totalBalance?.value || "0",
-      rawBalance.balance.value,
-    )
+      const totalBalanceValue = sumE8sICPString(
+        acc.icpBalance,
+        rawBalance.balance.value,
+      )
+      const currentAppTotalBalance = sumE8sICPString(
+        acc.applications[appName]?.icpBalance || "0",
+        rawBalance.balance.value,
+      )
 
-    if (filterZeroAccount && totalBalanceValue === "0") return acc
+      if (filterZeroAccount && currentAppTotalBalance === "0") return acc
 
-    return {
-      ...acc,
-      [appName]: {
-        totalBalance: {
-          ...rawBalance.balance,
-          value: totalBalanceValue,
-        } as Balance,
-        accounts: [
-          ...(currentApp ? currentApp.accounts : []),
-          {
-            accountId: rawBalance.account.accountId,
-            balance: rawBalance.balance,
+      return {
+        ...acc,
+        icpBalance: `${totalBalanceValue} ${acc.token}`,
+        usdBalance: `TODO: convert ${totalBalanceValue} to USD`,
+        applications: {
+          ...acc.applications,
+          [appName]: {
+            icon: applicationMatch?.icon,
+            appName: appName,
+            icpBalance: `${currentAppTotalBalance} ${acc.token}`,
+            accounts: [
+              ...(currentApp ? currentApp.accounts : []),
+              {
+                accountName:
+                  rawBalance.account.label ||
+                  `account ${rawBalance.account.accountId}`,
+                icpBalance: `${rawBalance.balance.value} ${acc.token}`,
+                usdBalance: `TODO: convert to usd ${rawBalance.balance.value} USD`,
+              },
+            ],
           },
-        ],
-      },
-    }
-  }, {})
+        },
+      }
+    },
+    {
+      label: "Internet Computer",
+      token: "ICP",
+      icpBalance: "0",
+      usdBalance: "0",
+      applications: {},
+    },
+  )
 }
 
 /**

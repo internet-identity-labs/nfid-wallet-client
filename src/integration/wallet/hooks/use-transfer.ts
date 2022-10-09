61e9523b20273227fa2cf3b2d026b07555c64d99
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-10-09 11:25:33 +0200

    refactor([sc-4794]): separate into files

--- src/integration/wallet/hooks/use-transfer.ts
+++ src/integration/wallet/hooks/use-transfer.ts
@@ -1,46 +1,12 @@
 import { DelegationIdentity } from "@dfinity/identity"
-import { Principal } from "@dfinity/principal"
-import { principalToAddress } from "ictool"
-import { useEffect, useMemo } from "react"
 import React from "react"
-import useSWR, { mutate } from "swr"
 
-import {
-  getWalletDelegation,
-  getWalletPrincipal,
-  WALLET_SESSION_TTL_2_MIN_IN_NS,
-} from "frontend/integration/facade/wallet"
 import { useProfile } from "frontend/integration/identity-manager/queries"
-import {
-  getBalance,
-  getTransactionHistory,
-  transfer,
-} from "frontend/integration/rosetta"
-import { useICPExchangeRate } from "frontend/integration/rosetta/queries"
+import { transfer } from "frontend/integration/rosetta"
 
-import { stringICPtoE8s } from "./utils"
-
-export const useWalletDelegation = (
-  userNumber?: number,
-  hostName?: string,
-  personaId?: string,
-) => {
-  console.debug("useWalletDelegation", { userNumber })
-  return useSWR(
-    userNumber ? [userNumber, hostName, personaId] : null,
-    (userNumber, hostName, personaId) =>
-      getWalletDelegation(userNumber, hostName, personaId),
-    {
-      dedupingInterval: WALLET_SESSION_TTL_2_MIN_IN_NS / 2,
-      focusThrottleInterval: WALLET_SESSION_TTL_2_MIN_IN_NS / 2,
-    },
-  )
-}
-
-export interface TransferAccount {
-  domain?: string
-  accountId?: string
-}
+import { stringICPtoE8s } from "../utils"
+import { TransferAccount } from "./index"
+import { useWalletDelegation } from "./use-wallet-delegation"
 
 export const useTransfer = ({ domain, accountId }: TransferAccount = {}) => {
   const { profile } = useProfile()
@@ -130,55 +96,5 @@ export const useTransfer = ({ domain, accountId }: TransferAccount = {}) => {
   return {
     isValidatingWalletDelegation,
     transfer: handleTransfer,
-  }
-}
-
-export const useWallet = () => {
-  const { profile } = useProfile()
-
-  const { data: principal, isValidating: isWalletPrincipalLoading } = useSWR(
-    "walletPrincipal",
-    () => getWalletPrincipal(profile?.anchor as number),
-  )
-
-  const { data: balance, isValidating: isWalletBalanceLoading } = useSWR(
-    "walletBalance",
-    () => getBalance(principal as Principal),
-  )
-
-  const { data: transactions, isValidating: isWalletTransactionsLoading } =
-    useSWR("walletTransactions", () =>
-      getTransactionHistory(principal as Principal),
-    )
-
-  const { exchangeRate, isValidating: isWalletExchangeRateLoading } =
-    useICPExchangeRate()
-
-  const address = useMemo(() => {
-    if (!principal) return ""
-    return principalToAddress(principal as any)
-  }, [principal])
-
-  useEffect(() => {
-    if (profile?.anchor) mutate("walletPrincipal")
-  }, [profile])
-
-  useEffect(() => {
-    if (!principal) return
-    mutate("walletBalance")
-    mutate("walletTransactions")
-  }, [principal])
-
-  return {
-    walletPrincipal: principal,
-    walletBalance: balance,
-    walletExchangeRate: exchangeRate,
-    walletTransactions: transactions,
-    walletAddress: address,
-    isWalletLoading:
-      isWalletPrincipalLoading ||
-      isWalletBalanceLoading ||
-      isWalletTransactionsLoading ||
-      isWalletExchangeRateLoading,
   }
 }

3303a599aebd899ec9b28caee17cd29d2943150f
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-10-13 13:12:37 +0200

    feat([sc-4412]): updated send&receive modal

---
+++ src/ui/organisms/transfer-modal/index.tsx
@@ -0,0 +1,69 @@
+import clsx from "clsx"
+import React, { useEffect, useState } from "react"
+
+import { UserNFTDetails } from "frontend/integration/entrepot/types"
+import { IWallet } from "frontend/integration/identity-manager/wallet/types"
+
+import { TransferModalReceive } from "./receive"
+import { TransferModalSend } from "./send"
+import { ITransferNFT } from "./send/send-nft"
+import { ITransferToken } from "./send/send-token"
+import { TransferModalSuccess } from "./sucess"
+import { TransferModalTabs } from "./tabs"
+
+interface ITransferModal {
+  wallets: IWallet[] | undefined
+  successMessage: string
+  onTokenSubmit: (values: ITransferToken) => void
+  onNFTSubmit: (values: ITransferNFT) => void
+  onClose: () => void
+  nfts: UserNFTDetails[]
+}
+
+export type modalTypes = "Send" | "Receive" | "Success" | string
+
+export const TransferModal: React.FC<ITransferModal> = ({
+  successMessage,
+  wallets,
+  onTokenSubmit,
+  onNFTSubmit,
+  onClose,
+  nfts,
+}) => {
+  const [modalType, setModalType] = useState<modalTypes>("Send")
+
+  useEffect(() => {
+    if (successMessage?.length) setModalType("Success")
+  }, [successMessage])
+
+  return (
+    <div
+      className={clsx(
+        "rounded-xl shadow-lg p-5 text-gray-400",
+        "z-20 bg-white absolute flex flex-col",
+        "left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2",
+        "w-[95%] sm:w-[420px] h-[510px]",
+      )}
+      onClick={(e) => e.stopPropagation()}
+    >
+      {modalType !== "Success" && (
+        <TransferModalTabs activeTab={modalType} setActiveTab={setModalType} />
+      )}
+      {modalType === "Send" && (
+        <TransferModalSend
+          nfts={nfts}
+          wallets={wallets}
+          onTokenSubmit={onTokenSubmit}
+          onNFTSubmit={onNFTSubmit}
+        />
+      )}
+      {modalType === "Receive" && <TransferModalReceive wallets={wallets} />}
+      {modalType === "Success" && (
+        <TransferModalSuccess
+          transactionMessage={successMessage}
+          onClose={onClose}
+        />
+      )}
+    </div>
+  )
+}

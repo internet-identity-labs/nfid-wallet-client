c5fe915018b12537d1d3152ec78e2593f39e1f5a
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-10-19 15:06:05 +0200

    fix([sc-4760]): fixed copy icons everywhere

--- src/ui/pages/new-profile/nft-details/index.tsx
+++ src/ui/pages/new-profile/nft-details/index.tsx
@@ -1,18 +1,20 @@
 import clsx from "clsx"
-import React from "react"
+import { useAtom } from "jotai"
+import { useCallback } from "react"
 import { To } from "react-router-dom"
-import { toast } from "react-toastify"
 
 import { ITransaction } from "frontend/apps/identity-manager/profile/nft-details/utils"
+import { transferModalAtom } from "frontend/apps/identity-manager/profile/transfer-modal/state"
 import { link } from "frontend/integration/entrepot"
 import { NFTDetails, UserNFTDetails } from "frontend/integration/entrepot/types"
 import { Application } from "frontend/integration/identity-manager"
+import { Copy } from "frontend/ui/atoms/copy"
 import { Loader } from "frontend/ui/atoms/loader"
 import Table from "frontend/ui/atoms/table"
 import ProfileContainer from "frontend/ui/templates/profile-container/Container"
 import ProfileTemplate from "frontend/ui/templates/profile-template/Template"
 
-import CopyIcon from "./assets/copy.svg"
+import TransferIcon from "./assets/transfer.svg"
 import WalletIcon from "./assets/wallet.svg"
 
 import { NFTAsset } from "./nft-asset"
@@ -30,17 +32,36 @@ export const ProfileNFTDetailsPage = ({
   transactions,
   applications,
 }: IProfileNFTDetails) => {
-  const copyToClipboard = React.useCallback(() => {
-    toast.info("NFT URL copied to clipboard", { toastId: "NFTCopied" })
-    navigator.clipboard.writeText(link(nft.collection.id, nft.index))
-  }, [nft.collection.id, nft.index])
+  const [transferModalState, setTransferModalState] = useAtom(transferModalAtom)
+
+  const onTransferNFT = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      e.preventDefault()
+      setTransferModalState({
+        ...transferModalState,
+        isModalOpen: true,
+        sendType: "nft",
+        selectedNFT: [nft.tokenId],
+      })
+    },
+    [nft.tokenId, setTransferModalState, transferModalState],
+  )
 
   return (
     <ProfileTemplate
       // I have not found any other ways to avoid as To. This is exactly what navigate expect
       onBack={-1 as To}
-      icon={CopyIcon}
-      onIconClick={copyToClipboard}
+      headerMenu={
+        <div className="flex items-center space-x-4">
+          <img
+            className="transition-opacity cursor-pointer hover:opacity-50"
+            src={TransferIcon}
+            alt="transfer"
+            onClick={onTransferNFT}
+          />
+          <Copy value={link(nft.collection.id, nft.index)} />
+        </div>
+      }
       className="w-full z-[1]"
     >
       <div

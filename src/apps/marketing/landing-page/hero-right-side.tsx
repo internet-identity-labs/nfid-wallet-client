8dff69ad0384dd4a81a90460f6087dcd356ca7f9
Pavlo Chorniy <39991357+chorniy8pavlo@users.noreply.github.com>
2022-10-25 16:08:13 +0200

    Fast/15 (#922)

--- src/apps/marketing/landing-page/hero-right-side.tsx
+++ src/apps/marketing/landing-page/hero-right-side.tsx
@@ -4,6 +4,10 @@ import React from "react"
 import NFIDAuthenticationCoordinator from "frontend/coordination/nfid-authentication"
 import { ElementProps } from "frontend/types/react"
 import { NFIDGradientBar } from "frontend/ui/atoms/gradient-bar"
+import { ApplicationMeta } from "frontend/ui/molecules/application-meta"
+import { BlurOverlay } from "frontend/ui/molecules/blur-overlay"
+import { MaintenanceError } from "frontend/ui/molecules/maintenance-error"
+import { useMaintenance } from "frontend/ui/utils/use-maintenance"
 
 import Group from "./assets/Group.svg"
 
@@ -14,6 +18,7 @@ interface HeroRightSideProps extends ElementProps<HTMLDivElement> {
 export const NFIDAuthentication: React.FC<HeroRightSideProps> = ({
   isUnregistered,
 }) => {
+  const { isDown } = useMaintenance()
   console.debug("HeroRightSide", { isUnregistered })
 
   return isUnregistered ? (
@@ -26,10 +31,24 @@ export const NFIDAuthentication: React.FC<HeroRightSideProps> = ({
       )}
     >
       <NFIDGradientBar className="w-full h-0.5 z-20" rounded={false} />
-      <div className="relative mt-[2px] flex flex-col w-full h-full">
+      <div className="relative mt-[2px] flex flex-col w-full h-full z-10">
+        <MaintenanceError isDown={isDown} />
         <NFIDAuthenticationCoordinator />
+        {isDown && (
+          <BlurOverlay className={clsx("absolute w-full h-full mt-12 pt-8")}>
+            <ApplicationMeta
+              title="Scheduled maintenance"
+              subTitle="Please check back later"
+            />
+          </BlurOverlay>
+        )}
       </div>
-      <div className={clsx("p-6 text-xs text-gray-400 text-center")}>
+      <div
+        className={clsx(
+          "p-6 text-xs text-gray-400 text-center",
+          "relative z-20",
+        )}
+      >
         NFID is a privacy-preserving, one-touch multi-factor wallet protocol
         developed by Internet Identity Labs.
       </div>

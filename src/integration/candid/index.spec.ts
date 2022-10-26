744890151dad5db7084b08daf669f8b2a7278f42
Dmitrii <dmitrii@identityLabs.ooo>
2022-10-26 16:39:11 +0400

    feat([sc-5141]): parse candid file and retrieve metadata

--- src/integration/candid/index.spec.ts
+++ src/integration/candid/index.spec.ts
@@ -39,4 +39,66 @@ describe("candid runner suite", () => {
     let evalResult = await evaluateMethod(actor, calledMethodName)
     expect(evalResult.status_code).toEqual(200)
   })
+
+  it("should parse did file with multiline comment into an object", async function () {
+    const did: string = `
+    service : () -> {
+      /**
+     * Function to retrieve transaction history of the token. Returns array
+     * of transactions and isLastPage boolean
+     * @param canisterId
+     * @param tokenId
+     * @param from
+     * @param to
+     */
+      read_applications: () -> (HTTPApplicationResponse) query;
+    }
+    `
+    let commentsByMethodNames: Map<string, Block> =
+      getCommentsByMethodNames(did)
+    let comment: Block | undefined =
+      commentsByMethodNames.get("read_applications")
+    expect(comment?.description).toContain(
+      "Function to retrieve transaction history of the token.",
+    )
+    expect(comment?.tags.length).toBe(4)
+    expect(comment?.tags[0].tag).toBe("param")
+    expect(comment?.tags[0].name).toBe("canisterId")
+    expect(comment?.tags[1].tag).toBe("param")
+    expect(comment?.tags[1].name).toBe("tokenId")
+    expect(comment?.tags[2].tag).toBe("param")
+    expect(comment?.tags[2].name).toBe("from")
+    expect(comment?.tags[3].tag).toBe("param")
+    expect(comment?.tags[3].name).toBe("to")
+  })
+
+  it("should parse did file with two multiline comments and throw error", async function () {
+    const did: string = `
+    service : () -> {
+      /**
+     * Function to retrieve transaction history of the token. Returns array
+     * of transactions and isLastPage boolean
+     * @param canisterId
+     * @param tokenId
+     * @param from
+     * @param to
+     */
+      /**
+     * Function to retrieve transaction history of the token. Returns array
+     * of transactions and isLastPage boolean
+     * @param canisterId
+     * @param tokenId
+     * @param from
+     * @param to
+     */
+      read_applications: () -> (HTTPApplicationResponse) query;
+    }
+    `
+    try {
+      getCommentsByMethodNames(did)
+    } catch (e) {
+      let error = e as Error
+      expect(error.message).toBe("More than one multiline comments were found.")
+    }
+  })
 })

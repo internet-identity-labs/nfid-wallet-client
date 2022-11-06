ac36ac53b36439962715db9573fbbdfd043a832d
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-11-06 16:34:43 +0100

    feat([sc-5181]): added storybook ui

---
+++ src/ui/pages/request-accounts/index.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, Story } from "@storybook/react"
+
+import logo from "frontend/assets/distrikt.svg"
+
+import { SDKRequestAccountsPage, SDKRequestAccountsPageProps } from "./index"
+
+const meta: Meta = {
+  title: "Pages/SDKRequestAccountsPage",
+  component: SDKRequestAccountsPage,
+  argTypes: {},
+  parameters: {
+    controls: { expanded: true },
+  },
+}
+
+export default meta
+
+const Template: Story<SDKRequestAccountsPageProps> = (args) => (
+  <SDKRequestAccountsPage {...args} />
+)
+
+export const Default = Template.bind({})
+
+Default.args = {
+  applicationName: "My Application",
+  applicationLogo: logo,
+  accountsOptions: [],
+  isLoading: false,
+}

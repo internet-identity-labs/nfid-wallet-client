c9b6ca423d6f4b0d0f20c9107398c32f16be9522
raghuidentity <raghu@identitylabs.ooo>
2022-11-11 09:11:25 +0000

    test: wdio - browser logs updated in config file

--- apps/nfid-frontend-e2e/wdio.mobile.conf.ts
+++ apps/nfid-frontend-e2e/wdio.mobile.conf.ts
@@ -18,6 +18,9 @@ export const config = {
           "user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3",
         ],
       },
+      "goog:loggingPrefs": {
+        "browser": "ALL"
+      },
       acceptInsecureCerts: true,
     },
   ],

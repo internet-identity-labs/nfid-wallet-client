cec95d92d9056ebabcab4d3700383fe6aa6b03f1
Pavlo Chorniy <39991357+chorniy8pavlo@users.noreply.github.com>
2022-11-11 15:44:19 +0100

    fix([sc-5368]): fixed getAccounts scroll (#960)
    Co-authored-by: Pavlo Chorniy <pchorniy@identitylabs.ooo>

--- apps/nfid-frontend/src/ui/pages/request-accounts/index.tsx
+++ apps/nfid-frontend/src/ui/pages/request-accounts/index.tsx
@@ -34,73 +34,76 @@ export const SDKRequestAccountsPage: React.FC<SDKRequestAccountsPageProps> = ({
 }) => {
   return (
     <ScreenResponsive frameLabel="Approve with NFID">
-      <BlurredLoader isLoading={!accountsOptions.length} />
-      <div
-        className={clsx(
-          "flex flex-col flex-grow lg:justify-between",
-          "h-full p-5",
-        )}
-      >
-        <div>
-          <ApplicationMeta
-            applicationLogo={applicationLogo}
-            title={applicationName}
-            subTitle="wants to perform the following actions:"
-          />
-          <div
-            className={clsx("border border-gray-300 rounded-md", "p-3 mt-4")}
-          >
-            <div className="flex space-x-2.5 mb-2.5">
-              <div
-                className={clsx(
-                  "w-10 h-10 bg-gray-50 rounded-full",
-                  "flex items-center justify-center",
-                  "relative",
-                )}
-              >
-                <img className="w-6" src={NFIDLogo} alt="nfid" />
-              </div>
-              <div>
-                <p className="text-sm font-semibold">View your Web3 balance</p>
-                <p className="text-xs text-gray-400">
-                  This site will see tokens and history from your selected
-                  accounts
-                </p>
-              </div>
-            </div>
-            <DropdownSelect
-              disabled={timer !== -1}
-              isSearch
-              options={accountsOptions}
-              selectedValues={selectedAccounts}
-              setSelectedValues={setSelectedAccounts}
-              placeholder="None selected - connect anonymously"
-              showSelectAllOption
-            />
-          </div>
-        </div>
+      <BlurredLoader isLoading={!accountsOptions.length}>
         <div
           className={clsx(
-            "grid grid-cols-2 gap-5 mt-5 lg:mt-32",
-            timer !== -1 && "hidden",
+            "flex flex-col flex-grow lg:justify-between",
+            "h-full p-5",
           )}
         >
-          <Button stroke onClick={onReject}>
-            Reject
-          </Button>
-          <Button primary onClick={onApprove}>
-            Approve
+          <div>
+            <ApplicationMeta
+              applicationLogo={applicationLogo}
+              title={applicationName}
+              subTitle="wants to perform the following actions:"
+            />
+            <div
+              className={clsx("border border-gray-300 rounded-md", "p-3 mt-4")}
+            >
+              <div className="flex space-x-2.5 mb-2.5">
+                <div
+                  className={clsx(
+                    "w-10 h-10 bg-gray-50 rounded-full",
+                    "flex items-center justify-center",
+                    "relative",
+                  )}
+                >
+                  <img className="w-6" src={NFIDLogo} alt="nfid" />
+                </div>
+                <div>
+                  <p className="text-sm font-semibold">
+                    View your Web3 balance
+                  </p>
+                  <p className="text-xs text-gray-400">
+                    This site will see tokens and history from your selected
+                    accounts
+                  </p>
+                </div>
+              </div>
+              <DropdownSelect
+                disabled={timer !== -1}
+                isSearch
+                options={accountsOptions}
+                selectedValues={selectedAccounts}
+                setSelectedValues={setSelectedAccounts}
+                placeholder="None selected - connect anonymously"
+                showSelectAllOption
+              />
+            </div>
+          </div>
+          <div
+            className={clsx(
+              "grid grid-cols-2 gap-5 mt-5 lg:mt-32",
+              timer !== -1 && "hidden",
+            )}
+          >
+            <Button stroke onClick={onReject}>
+              Reject
+            </Button>
+            <Button primary onClick={onApprove}>
+              Approve
+            </Button>
+          </div>
+
+          <Button
+            className={clsx("mt-5 lg:mt-32", timer === -1 && "hidden")}
+            block
+            primary
+          >
+            Success! Closing in {timer}
           </Button>
         </div>
-
-        <Button
-          className={clsx("mt-5 lg:mt-32", timer === -1 && "hidden")}
-          block
-          primary
-        >
-          Success! Closing in {timer}
-        </Button>
-      </div>
+      </BlurredLoader>
     </ScreenResponsive>
   )
 }

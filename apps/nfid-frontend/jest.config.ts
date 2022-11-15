33ebbc3d278294655836723c3c5b1a85a8753cff
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-11-15 20:16:44 +0100

    fix([sc-4847]): readd IS_DEV

--- apps/nfid-frontend/jest.config.ts
+++ apps/nfid-frontend/jest.config.ts
@@ -19,6 +19,7 @@ export default {
       "https://ia15v0pzlb.execute-api.us-east-1.amazonaws.com/dev/symmetric/",
     IC_HOST: "https://ic0.app",
     II_ENV: "dev",
+    IS_DEV: "",
     IS_E2E_TEST: false,
     GOOGLE_CLIENT_ID:
       "339872286671-87oou3adnvl7hst9gd90r9k7j6enl7vk.apps.googleusercontent.com",

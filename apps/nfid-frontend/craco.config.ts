211d75aa5ece911b3eccee4e04caad03c8653519
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-11-24 16:14:10 +0100

    fix([sc-5490]): setup sentry CSP reporting

--- apps/nfid-frontend/craco.config.ts
+++ apps/nfid-frontend/craco.config.ts
@@ -17,6 +17,8 @@ const setupCSP = () => {
       "default-src": "'none'",
       "object-src": "'none'",
       "base-uri": "'self'",
+      "report-uri":
+        "https://o1255710.ingest.sentry.io/api/6424378/security/?sentry_key=***REMOVED***",
       "connect-src": [
         "'self'",
         "https://ic0.app",

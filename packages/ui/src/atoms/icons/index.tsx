414933b1a77059747768cba14c586efd4e377fdb
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-11-28 09:29:37 +0100

    feat([sc-5197]): configure icons

---
+++ packages/ui/src/atoms/icons/index.tsx
@@ -0,0 +1,85 @@
+import IconSvgArrow from "./arrow.svg"
+import IconSvgCalendar from "./calendar.svg"
+import IconSvgCancel from "./cancel.svg"
+import IconSvgCheckMark from "./check-mark.svg"
+import IconSvgCopy from "./copy.svg"
+import IconSvgDesktop from "./desktop.svg"
+import IconSvgDokument from "./document.svg"
+import IconSvgDots from "./dots.svg"
+import IconSvgGmail from "./gmail.svg"
+import IconSvgGoogle from "./google.svg"
+import IconSvgInfo from "./info.svg"
+import IconSvgKey from "./key.svg"
+import IconSvgLaptop from "./laptop.svg"
+import IconSvgLogout from "./logout.svg"
+import IconSvgMapPin from "./map-pin.svg"
+import IconSvgMobile from "./mobile.svg"
+import IconSvgPencil from "./pencil.svg"
+import IconSvgPlus from "./plus.svg"
+import IconSvgRecovery from "./recovery.svg"
+import IconSvgRefresh from "./refresh.svg"
+import IconSvgSuccess from "./success.svg"
+import IconSvgTablet from "./tablet.svg"
+import IconSvgTouchId from "./touch-id.svg"
+import IconSvgTrash from "./trash.svg"
+import IconSvgUnknown from "./unknown.svg"
+import IconSvgUsb from "./usb.svg"
+import IconSvgWarning from "./warning.svg"
+
+export {
+  IconSvgArrow,
+  IconSvgCalendar,
+  IconSvgCancel,
+  IconSvgCheckMark,
+  IconSvgCopy,
+  IconSvgDesktop,
+  IconSvgDokument,
+  IconSvgDots,
+  IconSvgGmail,
+  IconSvgGoogle,
+  IconSvgInfo,
+  IconSvgKey,
+  IconSvgLaptop,
+  IconSvgLogout,
+  IconSvgMapPin,
+  IconSvgMobile,
+  IconSvgPencil,
+  IconSvgPlus,
+  IconSvgRecovery,
+  IconSvgRefresh,
+  IconSvgSuccess,
+  IconSvgTablet,
+  IconSvgTouchId,
+  IconSvgTrash,
+  IconSvgUnknown,
+  IconSvgUsb,
+  IconSvgWarning,
+}
+
+export { ReactComponent as IconCmpArrow } from "./arrow.svg"
+export { ReactComponent as IconCmpCalendar } from "./calendar.svg"
+export { ReactComponent as IconCmpCancel } from "./cancel.svg"
+export { ReactComponent as IconCmpCheckMark } from "./check-mark.svg"
+export { ReactComponent as IconCmpCopy } from "./copy.svg"
+export { ReactComponent as IconCmpDesktop } from "./desktop.svg"
+export { ReactComponent as IconCmpDokument } from "./document.svg"
+export { ReactComponent as IconCmpDots } from "./dots.svg"
+export { ReactComponent as IconCmpGmail } from "./gmail.svg"
+export { ReactComponent as IconCmpGoogle } from "./google.svg"
+export { ReactComponent as IconCmpInfo } from "./info.svg"
+export { ReactComponent as IconCmpKey } from "./key.svg"
+export { ReactComponent as IconCmpLaptop } from "./laptop.svg"
+export { ReactComponent as IconCmpLogout } from "./logout.svg"
+export { ReactComponent as IconCmpMapPin } from "./map-pin.svg"
+export { ReactComponent as IconCmpMobile } from "./mobile.svg"
+export { ReactComponent as IconCmpPencil } from "./pencil.svg"
+export { ReactComponent as IconCmpPlus } from "./plus.svg"
+export { ReactComponent as IconCmpRecovery } from "./recovery.svg"
+export { ReactComponent as IconCmpRefresh } from "./refresh.svg"
+export { ReactComponent as IconCmpSuccess } from "./success.svg"
+export { ReactComponent as IconCmpTablet } from "./tablet.svg"
+export { ReactComponent as IconCmpTouchId } from "./touch-id.svg"
+export { ReactComponent as IconCmpTrash } from "./trash.svg"
+export { ReactComponent as IconCmpUnknown } from "./unknown.svg"
+export { ReactComponent as IconCmpUsb } from "./usb.svg"
+export { ReactComponent as IconCmpWarning } from "./warning.svg"

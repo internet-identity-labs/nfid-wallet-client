8db222099299d700338d95bbb0fee604e131e82a
olsemeno <sendtosemenov@gmail.com>
2022-11-29 14:33:18 +0400

    feat([sc-5360]): Create/get vault.

--- packages/integration/jest.config.ts
+++ packages/integration/jest.config.ts
@@ -1,4 +1,6 @@
 /* eslint-disable */
+import { JEST_GLOBALS } from "../../config/jest-globals"
+
 export default {
   displayName: "integration",
   preset: "../../jest.preset.js",
@@ -6,6 +8,7 @@ export default {
     "ts-jest": {
       tsconfig: "<rootDir>/tsconfig.spec.json",
     },
+    ...JEST_GLOBALS,
   },
   transform: {
     "^.+\\.[tj]s$": "ts-jest",
--- packages/integration/src/lib/vault/index.spec.ts
+++ packages/integration/src/lib/vault/index.spec.ts
@@ -1,23 +1,22 @@
 /**
  * @jest-environment jsdom
  */
-import { DelegationIdentity, Ed25519KeyIdentity } from "@dfinity/identity"
 import { principalToAddress } from "ictool"
 import { generateDelegationIdentity } from "src/integration/test-util"
-import { getVaults, registerVault } from "src/integration/vault/index"
+import { getVaults, registerVault } from "./index"
 
 import { replaceIdentity } from "@nfid/integration"
-
+import { DelegationIdentity, Ed25519KeyIdentity } from "@dfinity/identity"
 describe("Vault suite", () => {
   jest.setTimeout(100000)
 
   it("vault register test", async function () {
-    let mockedIdentity = Ed25519KeyIdentity.generate()
+    const mockedIdentity = Ed25519KeyIdentity.generate()
     const delegationIdentity: DelegationIdentity =
       await generateDelegationIdentity(mockedIdentity)
     replaceIdentity(delegationIdentity)
-    let vaultFirst = await registerVault("first")
-    let address = principalToAddress(
+    const vaultFirst = await registerVault("first")
+    const address = principalToAddress(
       mockedIdentity.getPrincipal() as any,
       Array(32).fill(1),
     )
@@ -27,14 +26,14 @@ describe("Vault suite", () => {
     expect(vaultFirst.members[0].role.hasOwnProperty("VaultOwner")).toEqual(
       true,
     )
-    let vaultSecond = await registerVault("second")
+    const vaultSecond = await registerVault("second")
     expect(vaultSecond.name).toEqual("second")
     expect(vaultFirst.members.length).toEqual(1)
     expect(vaultFirst.members[0].user_uuid).toEqual(address)
     expect(vaultFirst.members[0].role.hasOwnProperty("VaultOwner")).toEqual(
       true,
     )
-    let vaults = await getVaults()
+    const vaults = await getVaults()
     expect(vaults[0]).toEqual(vaultFirst)
     expect(vaults[1]).toEqual(vaultSecond)
   })

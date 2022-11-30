72068fefd51eb551e4d8e95dc40a6e22c53a51d9
olsemeno <sendtosemenov@gmail.com>
2022-11-30 16:06:09 +0400

    feat([sc-5360]): Create/get vault.

--- packages/integration/src/lib/vault/index.spec.ts
+++ packages/integration/src/lib/vault/index.spec.ts
@@ -6,12 +6,13 @@ import { principalToAddress } from "ictool"
 
 import { replaceIdentity } from "../auth-state"
 import { generateDelegationIdentity } from "../test-utils"
-import { getVaults, registerVault } from "./index"
+import { addMemberToVault, getVaultMembers, getVaults, registerVault } from "./index"
+import { VaultMember, VaultRole } from "./types"
 
 describe("Vault suite", () => {
   jest.setTimeout(100000)
 
-  it("vault register test", async function () {
+  it("vault register test", async function() {
     const mockedIdentity = Ed25519KeyIdentity.generate()
     const delegationIdentity: DelegationIdentity =
       await generateDelegationIdentity(mockedIdentity)
@@ -24,18 +25,31 @@ describe("Vault suite", () => {
     expect(vaultFirst.name).toEqual("first")
     expect(vaultFirst.members.length).toEqual(1)
     expect(vaultFirst.members[0].user_uuid).toEqual(address)
-    expect(vaultFirst.members[0].role.hasOwnProperty("VaultOwner")).toEqual(
-      true,
+    expect(vaultFirst.members[0].role).toEqual(
+      VaultRole.Admin,
     )
     const vaultSecond = await registerVault("second")
     expect(vaultSecond.name).toEqual("second")
-    expect(vaultFirst.members.length).toEqual(1)
-    expect(vaultFirst.members[0].user_uuid).toEqual(address)
-    expect(vaultFirst.members[0].role.hasOwnProperty("VaultOwner")).toEqual(
-      true,
+    expect(vaultSecond.members.length).toEqual(1)
+    expect(vaultSecond.members[0].user_uuid).toEqual(address)
+    expect(vaultSecond.members[0].role).toEqual(
+      VaultRole.Admin,
     )
     const vaults = await getVaults()
     expect(vaults[0]).toEqual(vaultFirst)
     expect(vaults[1]).toEqual(vaultSecond)
+
+    let members = await getVaultMembers(vaultFirst.id)
+    expect(members.length).toEqual(1)
+    const memberAddress = principalToAddress(
+      Ed25519KeyIdentity.generate().getPrincipal() as any,
+      Array(32).fill(1),
+    )
+    await addMemberToVault(vaultFirst.id, memberAddress, "Test Name", VaultRole.Member)
+    members = await getVaultMembers(vaultFirst.id)
+    expect(members.length).toEqual(2)
+    const member = members.find(l=>l.user_uuid === memberAddress) as VaultMember
+    expect(member.name).toEqual("Test Name")
+    expect(member.role).toEqual(VaultRole.Member)
   })
 })
---
+++ packages/integration/src/lib/vault/mapper.ts
@@ -0,0 +1,45 @@
+import { Vault, VaultMember, VaultRole } from "./types"
+import {
+  Vault as VaultResponse,
+  VaultMember as VaultMemberResponse,
+  VaultRole as VaultRoleTransport,
+} from "../_ic_api/vault.d"
+import { hasOwnProperty } from "@nfid/integration"
+
+export function responseToVault(response: VaultResponse): Vault {
+  return {
+    id: response.id,
+    members: response.members.map(m => responseToMember(m)),
+    name: response.name,
+    policies: response.policies,
+    wallets: response.wallets,
+  }
+}
+
+export function responseToMember(response: VaultMemberResponse): VaultMember {
+  return {
+    name: response.name.length === 0 ? undefined : response.name[0],
+    role: responseToRole(response.role),
+    user_uuid: response.user_uuid,
+  }
+}
+
+function responseToRole(response: VaultRoleTransport): VaultRole {
+  if (hasOwnProperty(response, "Admin")) {
+    return VaultRole.Admin
+  }
+  if (hasOwnProperty(response, "Member")) {
+    return VaultRole.Member
+  }
+  throw Error("Unexpected enum value")
+}
+
+export function roleToRequest(response: VaultRole): VaultRoleTransport {
+  if (response === VaultRole.Admin) {
+    return { "Admin": null } as VaultRoleTransport
+  }
+  if (response === VaultRole.Member) {
+    return { "Member": null } as VaultRoleTransport
+  }
+  throw Error("Unexpected enum value")
+}

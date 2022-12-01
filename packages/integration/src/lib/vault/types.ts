2677a2a34db4c02d1d083b14405496b414d26843
olsemeno <33149496+olsemeno@users.noreply.github.com>
2022-12-01 15:08:17 +0200

    feat([sc-5360]): Create/get wallets. (#1013)
    * feat([sc-5360]): Create/get vault (wallets).
    
    * feat([sc-5360]): Create/get vault (wallets).

--- packages/integration/src/lib/vault/types.ts
+++ packages/integration/src/lib/vault/types.ts
@@ -5,11 +5,19 @@ export interface Vault {
   wallets: Array<bigint>
   policies: Array<bigint>
 }
+
+export interface Wallet {
+  id: bigint
+  name: string | undefined
+  vaults: Array<bigint>
+}
+
 export interface VaultMember {
   userId: string
   name: string | undefined
   role: VaultRole
 }
+
 export enum VaultRole {
   Admin,
   Member,

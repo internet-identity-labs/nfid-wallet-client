d601d5ceddd3ab40f834346e8495b953c3738972
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-12-01 16:46:06 +0100

    chore(e2e): add browser logs

---
+++ apps/nfid-frontend-e2e/src/steps/hooks.ts
@@ -0,0 +1,10 @@
+import { After, Status } from "@cucumber/cucumber"
+import cucumberJson from "wdio-cucumberjs-json-reporter"
+
+After(async function () {
+  // @ts-ignore
+  if (browser.capabilities.browserName === "chrome") {
+    const browserLogs = await browser.getLogs("browser")
+    cucumberJson.attach(JSON.stringify(browserLogs), "application/json")
+  }
+})

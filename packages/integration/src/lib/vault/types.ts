68c193c3341921298c72e1e0b1ea974f8b46387f
olsemeno <sendtosemenov@gmail.com>
2022-12-05 12:52:48 +0100

    feat([sc-5360]): Create/get vault (policy).

--- packages/integration/src/lib/vault/types.ts
+++ packages/integration/src/lib/vault/types.ts
@@ -1,3 +1,5 @@
+import { Currency as CurrencyTransport } from "../_ic_api/vault.d"
+
 export interface Vault {
   id: bigint
   members: Array<VaultMember>
@@ -21,4 +23,26 @@ export interface VaultMember {
 export enum VaultRole {
   Admin,
   Member,
+}
+
+export enum PolicyType {
+  ThresholdPolicy,
+}
+
+export enum Currency {
+  ICP
+}
+
+export interface Policy {
+  "id": bigint,
+  "type": PolicyType,
+}
+
+export interface ThresholdPolicy extends Policy {
+  "id": bigint,
+  "type": PolicyType,
+  "memberThreshold": number,
+  "amountThreshold": bigint,
+  "walletIds": undefined | [Array<bigint>],
+  "currency": Currency,
 }

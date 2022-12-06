931fd422141691336fa0f655ef5d197b2ec7964a
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-12-06 14:54:15 +0100

    test: fixed metamask and balance tests

--- apps/nfid-frontend/src/integration/rosetta/hooks/use-balance-icp-all.mocks.ts
+++ apps/nfid-frontend/src/integration/rosetta/hooks/use-balance-icp-all.mocks.ts
@@ -1,18 +1,18 @@
 export const APP_ACC_BALANCE_SHEET = {
   label: "Internet Computer",
   token: "ICP",
-  icpBalance: "1.30 ICP",
+  icpBalance: "1.3 ICP",
   usdBalance: "$6.50",
   applications: {
     NFID: {
       appName: "NFID",
-      icpBalance: "1.00 ICP",
+      icpBalance: "1 ICP",
       accounts: [
         {
           address:
             "7d3b6612f09d9464612dae852b32b5169e4d8afb556b7921b49bd79e4b637f88",
           accountName: "account 1",
-          icpBalance: "1.00 ICP",
+          icpBalance: "1.0 ICP",
           principalId:
             "dcxdv-zztqe-t22xz-k4jl6-j7vx5-3b3zh-z645u-mjlg6-lnerc-qaiq2-sae",
           usdBalance: "$5.00",
@@ -22,7 +22,7 @@ export const APP_ACC_BALANCE_SHEET = {
     "Application 1": {
       appName: "Application 1",
       icon: "https://i.picsum.photos/id/652/65/65.jpg?hmac=I1_RisYpAdE77zf_bRKKqMbTRgKaTkoOBiB0avFTMhk",
-      icpBalance: "0.30 ICP",
+      icpBalance: "0.3 ICP",
       accounts: [
         {
           principalId:
@@ -30,7 +30,7 @@ export const APP_ACC_BALANCE_SHEET = {
           address:
             "d9197b5c40cfab4049cdae2dcccfa062d616a25ce85a189e4b6a8c610daa4bc0",
           accountName: "account 1", // NOTE: incremented by 1
-          icpBalance: "0.10 ICP",
+          icpBalance: "0.1 ICP",
           usdBalance: "$0.50",
         },
         {
@@ -39,7 +39,7 @@ export const APP_ACC_BALANCE_SHEET = {
           address:
             "27a716d1a6fad66ddf068e3605e8280b5839e7b7e159d97295f6e1840a0a0a9a",
           accountName: "renamedAccount",
-          icpBalance: "0.20 ICP",
+          icpBalance: "0.2 ICP",
           usdBalance: "$1.00",
         },
       ],

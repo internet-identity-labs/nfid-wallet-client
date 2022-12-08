268087db901b4bd9011887f79540c5183756d2db
Dmitrii <dmitrii@identityLabs.ooo>
2022-12-08 20:56:42 +0400

    feat([sc-5186]): implement logic to register NFID with Metamask

--- packages/integration/src/lib/_ic_api/eth_secret_storage.ts
+++ packages/integration/src/lib/_ic_api/eth_secret_storage.ts
@@ -1,10 +1,12 @@
 export const idlFactory = ({ IDL }: any) => {
-  const Address = IDL.Text;
-  const Signature = IDL.Text;
-  const Secret = IDL.Text;
+  const Address = IDL.Text
+  const Signature = IDL.Text
+  const Secret = IDL.Text
   return IDL.Service({
-    'get_secret' : IDL.Func([Address, Signature], [Secret], ['query']),
-    'init' : IDL.Func([], [], []),
-  });
-};
-export const init = ({ IDL }: any) => { return []; };
+    get_secret: IDL.Func([Address, Signature], [Secret], ["query"]),
+    init: IDL.Func([], [], []),
+  })
+}
+export const init = ({ IDL }: any) => {
+  return []
+}

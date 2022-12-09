55e2f72f5b2f75dd6ccab5c748f96cc04ebbe77d
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-12-09 14:08:33 +0100

    fix([sc-5245]): fixed unit tests

--- apps/nfid-frontend/src/integration/identity-manager/devices/hooks.spec.ts
+++ apps/nfid-frontend/src/integration/identity-manager/devices/hooks.spec.ts
@@ -6,10 +6,12 @@ import { byGoogleDevice, byNotGoogleDevice } from "./hooks"
 describe("hooks test suite", () => {
   describe("filterGoogleDevice", () => {
     it("should filter correctly", () => {
-      expect(byGoogleDevice({ browser: "Google account" })).toBe(true)
-      expect(byGoogleDevice({ browser: "cross platform" })).toBe(true)
-      expect(byNotGoogleDevice({ browser: "Google account" })).toBe(false)
+      expect(byGoogleDevice({ browser: "google account" })).toBe(true)
+      expect(byNotGoogleDevice({ browser: "abc google with account" })).toBe(
+        true,
+      )
       expect(byNotGoogleDevice({ browser: "cross platform" })).toBe(false)
+      expect(byNotGoogleDevice({ browser: "Chrome" })).toBe(false)
     })
   })
 })

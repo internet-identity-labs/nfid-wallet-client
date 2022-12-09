9f14b5c45960ba0643d35f8d4e8df3a1a5aaf3a9
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-12-09 14:08:33 +0100

    fix([sc-5245]): not google device tests

--- apps/nfid-frontend/src/integration/identity-manager/devices/hooks.spec.ts
+++ apps/nfid-frontend/src/integration/identity-manager/devices/hooks.spec.ts
@@ -6,12 +6,12 @@ import { byGoogleDevice, byNotGoogleDevice } from "./hooks"
 describe("hooks test suite", () => {
   describe("filterGoogleDevice", () => {
     it("should filter correctly", () => {
-      expect(byGoogleDevice({ browser: "google account" })).toBe(true)
-      expect(byNotGoogleDevice({ browser: "abc google with account" })).toBe(
+      expect(byGoogleDevice({ browser: "Chrome with google account" })).toBe(
         true,
       )
-      expect(byNotGoogleDevice({ browser: "cross platform" })).toBe(false)
-      expect(byNotGoogleDevice({ browser: "Chrome" })).toBe(false)
+      expect(byNotGoogleDevice({ browser: "abc with account" })).toBe(true)
+      expect(byNotGoogleDevice({ browser: "cross platform" })).toBe(true)
+      expect(byNotGoogleDevice({ browser: "Chrome" })).toBe(true)
     })
   })
 })

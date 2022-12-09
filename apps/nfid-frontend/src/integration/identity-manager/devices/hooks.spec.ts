d939d9172bcde26925ba5a1898318aeb1e9b65b2
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-12-09 14:10:29 +0100

    fix([sc-5245]): google unit tests

--- apps/nfid-frontend/src/integration/identity-manager/devices/hooks.spec.ts
+++ apps/nfid-frontend/src/integration/identity-manager/devices/hooks.spec.ts
@@ -6,9 +6,8 @@ import { byGoogleDevice, byNotGoogleDevice } from "./hooks"
 describe("hooks test suite", () => {
   describe("filterGoogleDevice", () => {
     it("should filter correctly", () => {
-      expect(byGoogleDevice({ browser: "Chrome with google account" })).toBe(
-        true,
-      )
+      expect(byGoogleDevice({ browser: "Chrome google account" })).toBe(true)
+      expect(byGoogleDevice({ browser: "abc with google account" })).toBe(true)
       expect(byNotGoogleDevice({ browser: "abc with account" })).toBe(true)
       expect(byNotGoogleDevice({ browser: "cross platform" })).toBe(true)
       expect(byNotGoogleDevice({ browser: "Chrome" })).toBe(true)

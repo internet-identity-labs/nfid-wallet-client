f986084fac2544ad6843b0fe04a2aa09845653b8
Philipp Litzenberger <philipp@identitylabs.ooo>
2022-12-15 08:24:36 +0100

    fix([sc-5740]): send to SELECT_ACCOUNT

--- apps/nfid-frontend/src/state/machines/authorization/authorization.spec.ts
+++ apps/nfid-frontend/src/state/machines/authorization/authorization.spec.ts
@@ -9,6 +9,8 @@ describe("authorization", () => {
       expect(selectAccountAction(0, 5)).toBe("CREATE_ACCOUNT")
       expect(selectAccountAction(0, 1)).toBe("CREATE_ACCOUNT")
       expect(selectAccountAction(1, 1)).toBe("SELECT_ACCOUNT")
+      expect(selectAccountAction(2, 1)).toBe("SELECT_ACCOUNT")
+      expect(selectAccountAction(2, 2)).toBe("PRESENT_ACCOUNTS")
     })
   })
 })

29b72755690a89d44942e6b0f5dc19646589ea46
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-12-19 13:57:59 +0100

    feat: added checkbox component

--- packages/ui/src/atoms/checkbox/index.stories.tsx
+++ packages/ui/src/atoms/checkbox/index.stories.tsx
@@ -24,11 +24,7 @@ const Template: Story<ICheckbox> = (args) => (
 // https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({})
 
-<<<<<<< HEAD
-Default.args = {}
-=======
 Default.args = {
   labelText: "Date and time",
   value: "datetime",
 }
->>>>>>> 76f9d305 (feat: added checkbox component)

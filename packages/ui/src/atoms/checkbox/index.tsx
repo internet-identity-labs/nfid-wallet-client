e00ea874317870b71329293eef30812a006f3590
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-12-19 13:57:46 +0100

    feat: added checkbox component

--- packages/ui/src/atoms/checkbox/index.tsx
+++ packages/ui/src/atoms/checkbox/index.tsx
@@ -1,10 +1,13 @@
 import clsx from "clsx"
 
+import { Label } from "@nfid-frontend/ui"
+
 export interface ICheckbox {
   value: string
   isChecked: boolean
   onChange: (isChecked: boolean, value: string) => void
   className?: string
+  labelText?: string
 }
 
 export const Checkbox = ({
@@ -12,17 +15,25 @@ export const Checkbox = ({
   isChecked,
   onChange,
   className,
+  labelText,
 }: ICheckbox) => {
   return (
-    <input
-      className={clsx(
-        "rounded-[3px] h-[18px] w-[18px] border border-black-base",
-        className,
+    <div className="flex items-center space-x-2">
+      <input
+        className={clsx(
+          "rounded-[3px] h-[18px] w-[18px] border border-black-base",
+          className,
+        )}
+        type="checkbox"
+        id={value}
+        checked={isChecked}
+        onChange={() => onChange(isChecked, value)}
+      />
+      {labelText && (
+        <Label className="cursor-pointer" htmlFor={value}>
+          {labelText}
+        </Label>
       )}
-      type="checkbox"
-      id={value}
-      checked={isChecked}
-      onChange={() => onChange(isChecked, value)}
-    />
+    </div>
   )
 }

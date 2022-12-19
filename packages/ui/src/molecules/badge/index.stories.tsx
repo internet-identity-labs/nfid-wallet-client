77a0f0666f1864d90367a2856b35797e9ff85ecd
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2022-12-19 13:57:46 +0100

    feat([sc-5576]): badge component

---
+++ packages/ui/src/molecules/badge/index.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, Story } from "@storybook/react"
+
+import { Badge, IBadge } from "."
+
+const meta: Meta = {
+  title: "Atoms/Badge",
+  argTypes: {
+    children: {
+      control: {
+        type: "text",
+        default: "Pending",
+      },
+      defaultValue: "Pending",
+    },
+    type: {
+      options: ["warning", "success", "error", "cancel"],
+      control: { type: "radio" },
+      defaultValue: "warning",
+    },
+  },
+}
+
+export default meta
+
+const BadgeComponent: Story<IBadge> = (args) => {
+  return <Badge type={args.type}>{args.children}</Badge>
+}
+
+export const Overview = BadgeComponent.bind({
+  type: "warning",
+})

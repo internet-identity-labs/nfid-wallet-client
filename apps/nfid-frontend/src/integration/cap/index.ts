007599d1e18d6723bffc93b4d793b7e79ac78584
Dmitrii <dmitrii@identityLabs.ooo>
2022-12-29 14:56:28 +0400

    feat([sc-5784]): investigation Querying DIP20 transactions [sc-5784]

--- apps/nfid-frontend/src/integration/cap/index.ts
+++ apps/nfid-frontend/src/integration/cap/index.ts
@@ -4,6 +4,85 @@ import { principalToAddress } from "ictool"
 
 import { getCapRootTransactions } from "frontend/integration/cap/cap_util"
 
+import { Transaction } from "../rosetta/rosetta_interface"
+import { Operation } from "../rosetta/rosetta_interface"
+
+export interface Token {
+  symbol: string
+  canisterId: string
+  name: string
+  decimals: number
+  standard: string
+}
+
+export const TOKENS = {
+  XTCToken: {
+    symbol: "XTC",
+    canisterId: "aanaa-xaaaa-aaaah-aaeiq-cai",
+    name: "Cycles",
+    decimals: 12,
+    standard: "xtc",
+  } as Token,
+  WTC: {
+    symbol: "WTC",
+    canisterId: "5ymop-yyaaa-aaaah-qaa4q-cai",
+    name: "Wrapped Cycles",
+    decimals: 12,
+    standard: "dip20",
+  } as Token,
+  WICP: {
+    symbol: "WICP",
+    canisterId: "utozz-siaaa-aaaam-qaaxq-cai",
+    name: "Wrapped ICP",
+    decimals: 8,
+    standard: "wicp",
+  } as Token,
+  BTKN: {
+    symbol: "BTKN",
+    canisterId: "cfoim-fqaaa-aaaai-qbcmq-cai",
+    name: "Beta Token",
+    decimals: 8,
+    standard: "dip20",
+  } as Token,
+  DUST: {
+    symbol: "DUST",
+    canisterId: "4mvfv-piaaa-aaaak-aacia-cai",
+    name: "Dust Token",
+    decimals: 8,
+    standard: "dip20",
+  } as Token,
+}
+
+export async function getTokenTransactions(
+  token: Token,
+  user: Principal,
+  from: number,
+  to: number,
+): Promise<{ txHistory: Transaction[]; isLastPage: boolean }> {
+  let address = principalToAddress(user as any)
+  let transactionHistory = await Promise.all(
+    Array.from(Array(to).keys())
+      .slice(from, to)
+      .map(async (page) => {
+        let allHistory = await getCapRootTransactions(token.canisterId, page)
+        let txHistory: Transaction[] = allHistory
+          .filter(
+            (l) =>
+              l.details.from.toString() === address ||
+              l.details.to.toString() === address,
+          )
+          .map((x) => getTransaction(x, token))
+        return { txHistory, isLastPage: allHistory.length === 0 }
+      }),
+  )
+  return transactionHistory.reduce((x, y) => {
+    return {
+      txHistory: x.txHistory.concat(y.txHistory),
+      isLastPage: x.isLastPage || y.isLastPage,
+    }
+  })
+}
+
 export async function getTokenTransactionHistory(
   canisterId: string,
   page: number,
@@ -74,4 +153,93 @@ export async function getTokenTxHistoryOfTokenIndex(
       isLastPage: x.isLastPage || y.isLastPage,
     }
   })
+}
+
+function getOperation(
+  type: string,
+  status: string,
+  address: string,
+  amount: string,
+  symbol: string,
+  decimals: number,
+): Operation {
+  return {
+    operationIdentifier: {
+      index: 0,
+    },
+    type: type,
+    status: status,
+    account: {
+      address: address,
+    },
+    amount: {
+      value: amount,
+      currency: {
+        symbol: symbol,
+        decimals: decimals,
+      },
+    },
+  }
+}
+
+function lebDecode(pipe: any): bigint {
+  let weight = BigInt(1)
+  let value = BigInt(0)
+  let byte
+  do {
+    if (pipe.length < 1) {
+      throw new Error("unexpected end of buffer")
+    }
+    byte = pipe[0]
+    pipe = pipe.slice(1)
+    value += BigInt(byte & 0x7f).valueOf() * weight
+    weight *= BigInt(128)
+  } while (byte >= 0x80)
+  return value
+}
+
+function getTransaction(tx: TransactionPrettified, token: Token): Transaction {
+  const amount = lebDecode(tx.details.amount).toString()
+  return {
+    blockIdentifier: {
+      index: 0,
+      hash: "",
+    },
+    transaction: {
+      transactionIdentifier: {
+        hash: "",
+      },
+      operations: [
+        getOperation(
+          tx.operation,
+          tx.details.status.toString(),
+          tx.details.from.toString(),
+          amount,
+          token.symbol,
+          token.decimals,
+        ),
+        getOperation(
+          tx.operation,
+          tx.details.status.toString(),
+          tx.details.to.toString(),
+          amount,
+          token.symbol,
+          token.decimals,
+        ),
+        getOperation(
+          tx.operation,
+          tx.details.status.toString(),
+          tx.details.from.toString(),
+          lebDecode(tx.details.fee).toString(),
+          token.symbol,
+          token.decimals,
+        ),
+      ],
+      metadata: {
+        blockHeight: 0,
+        memo: 0,
+        timestamp: Number(tx.time),
+      },
+    },
+  }
 }

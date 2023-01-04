c5f7caf4e32f933cb53cef1fb955edb5575341ac
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2023-01-04 19:11:46 +0100

    feat([sc-5562]): changed general table component

--- packages/ui/src/molecules/table/index.stories.tsx
+++ packages/ui/src/molecules/table/index.stories.tsx
@@ -1,6 +1,10 @@
 import { Meta, Story } from "@storybook/react"
 
+import { IconCmpArrowRight } from "@nfid-frontend/ui"
+
 import Table, { TableProps } from "."
+import { TableCell } from "./table-cell"
+import { TableRow } from "./table-row"
 
 const meta: Meta = {
   title: "Molecules/Table",
@@ -12,13 +16,28 @@ export default meta
 
 const DefaultTemplate: Story<TableProps> = (args) => (
   <Table
-    headings={["Make", "Model", "Year", "Color"]}
-    rows={[
-      { key: "0", val: ["Mazda", "Miata", "2004", "Silver"] },
-      { key: "1", val: ["Volkswagon", "GTI", "2015", "Dark Gray"] },
-    ]}
-    handleHeaderClick={() => []}
-  />
+    tableHeader={
+      <TableRow className="font-bold border-black">
+        <TableCell isLeft className="w-auto">
+          Name
+        </TableCell>
+        <TableCell className="text-center">Wallets</TableCell>
+        <TableCell className="text-center">Members</TableCell>
+        <TableCell className="w-52">Last activity</TableCell>
+        <TableCell isRight />
+      </TableRow>
+    }
+  >
+    <TableRow onClick={() => {}}>
+      <TableCell isLeft>Some title</TableCell>
+      <TableCell className="text-center">1</TableCell>
+      <TableCell className="text-center">2</TableCell>
+      <TableCell>Some date</TableCell>
+      <TableCell isRight>
+        <IconCmpArrowRight className="text-gray-400" />
+      </TableCell>
+    </TableRow>
+  </Table>
 )
 
 export const Default = DefaultTemplate.bind({})

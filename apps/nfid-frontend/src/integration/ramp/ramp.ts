a902a6cfe9e1a819f02b55a82c34ec7a5290c500
Dmitrii <dmitrii@identityLabs.ooo>
2023-01-12 16:54:01 +0400

    feat([sc-5909]): investigation Review possible API to integrate [sc-5909]

--- apps/nfid-frontend/src/integration/ramp/ramp.ts
+++ apps/nfid-frontend/src/integration/ramp/ramp.ts
@@ -4,7 +4,7 @@ import { TAllEvents } from "@ramp-network/ramp-instant-sdk/dist/types/types"
 declare const RAMP_WALLET_API_KEY: string
 declare const RAMP_WALLET_SDK_URL: string
 
-export type EmbeddedType = 'embedded-desktop' | 'embedded-mobile'
+export type EmbeddedType = "embedded-desktop" | "embedded-mobile"
 
 export function openRampWallet(
   address: string,
@@ -20,7 +20,7 @@ export function openRampWallet(
     hostApiKey: RAMP_WALLET_API_KEY,
     userAddress: address,
     variant: embeddedType,
-    containerNode: element!,
+    containerNode: element,
   })
     .on(eventType, eventHandler)
     .show()

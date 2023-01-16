0a8ec6f2a55b7c1fb62c07407394b9f2afc95831
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2023-01-16 16:15:29 +0100

    fix(vaults): delegation expiration issue

--- apps/nfid-frontend/src/integration/wallet/hooks/use-wallet-delegation.ts
+++ apps/nfid-frontend/src/integration/wallet/hooks/use-wallet-delegation.ts
@@ -4,6 +4,7 @@ import { replaceActorIdentity, vault } from "@nfid/integration"
 
 import {
   getWalletDelegation,
+  WALLET_SESSION_TTL_2_MIN_IN_MS,
   WALLET_SESSION_TTL_2_MIN_IN_NS,
 } from "frontend/integration/facade/wallet"
 
@@ -20,8 +21,10 @@ export const useWalletDelegation = (
     {
       dedupingInterval: WALLET_SESSION_TTL_2_MIN_IN_NS / 2,
       focusThrottleInterval: WALLET_SESSION_TTL_2_MIN_IN_NS / 2,
+      refreshInterval: WALLET_SESSION_TTL_2_MIN_IN_MS,
       onSuccess: (data) => {
-        replaceActorIdentity(vault, data)
+        console.log(123, { data })
+        if (data) replaceActorIdentity(vault, data)
         return data
       },
     },

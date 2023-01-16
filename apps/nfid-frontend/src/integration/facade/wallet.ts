ee2034ba38573001f09751a04fbd5141963bbc81
Pavlo Chorniy <pchorniy@identitylabs.ooo>
2023-01-16 16:15:29 +0100

    fix(vaults): delegation expiration issue

--- apps/nfid-frontend/src/integration/facade/wallet.ts
+++ apps/nfid-frontend/src/integration/facade/wallet.ts
@@ -10,6 +10,7 @@ import {
 
 const WALLET_SCOPE = "nfid.one"
 export const WALLET_SESSION_TTL_2_MIN_IN_NS = 2 * 60 * 1e9
+export const WALLET_SESSION_TTL_2_MIN_IN_MS = 60 * 1e6
 
 export async function getWalletPrincipal(anchor: number): Promise<Principal> {
   return fetchPrincipal(anchor, WALLET_SCOPE).catch((e) => {

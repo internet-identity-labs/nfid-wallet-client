c5c162493bd934fbc537f071e0c2f453357af10f
sataninja <viktor.identitylabs.ooo>
2023-01-30 10:34:19 +0200

    feat([sc-6154]):  Add member scenario refactor

--- apps/nfid-frontend-e2e/src/pages/vault.ts
+++ apps/nfid-frontend-e2e/src/pages/vault.ts
@@ -2,6 +2,30 @@ import { Vaults } from "./vaults";
 
 export class Vault extends Vaults {
 
+  private get membersTab() {
+    return $("#tab_members");
+  }
+
+  private get addMemberButton() {
+    return $("#add-member-trigger");
+  }
+
+  private get memberNameInput() {
+    return $("[name='name']");
+  }
+
+  private get memberNfidAddressInput() {
+    return $("[name='address']");
+  }
+
+  private get addMemberConfirmation() {
+    return $("#add-member-button");
+  }
+
+  private get memberName() {
+    return "[id*='member_";
+  }
+
   private get addWalletButton() {
     return $("#create-wallet-trigger");
   }
@@ -14,8 +38,30 @@ export class Vault extends Vaults {
     return $("#create-wallet-button");
   }
 
-  private get walletId() {
+  private get walletName() {
     return "[id*='wallet_";
+  }
+
+  public async openMembersTab() {
+    await this.membersTab.waitForDisplayed({ timeout: 8000, timeoutMsg: "Members tab is missing!" })
+    await this.membersTab.click();
+  }
+
+  public async addMember(memberName: string, address: string) {
+    await this.addMemberButton.waitForDisplayed({ timeout: 6000, timeoutMsg: "Add Member button is missing!" });
+    await this.addMemberButton.click();
+    await this.memberNameInput.waitForDisplayed({ timeoutMsg: "Member name input is missing" });
+    await this.memberNameInput.setValue(memberName);
+    await this.memberNfidAddressInput.waitForDisplayed({ timeoutMsg: "NFID adress input is missing!" });
+    await this.memberNfidAddressInput.setValue(address);
+    await this.addMemberConfirmation.waitForDisplayed({ timeoutMsg: "Add member confirmation is required!" });
+    await this.addMemberConfirmation.click();
+  }
+
+  public async getMemberByName(memberName: string) {
+    await $(this.memberName + `${memberName}` + "']")
+      .waitForDisplayed({ timeout: 7000, timeoutMsg: "Member has not been created! Missing member name!" });
+    return await $(this.memberName + `${memberName}` + "']");
   }
 
   public async addWallet(walletName: string) {
@@ -28,9 +74,9 @@ export class Vault extends Vaults {
   }
 
   public async getWalletByName(name: string) {
-    await $(this.walletId + `${name}` + "']")
-      .waitForDisplayed({ timeout: 7000, timeoutMsg: "Vault has not been created! Missing vault id!" });
-    return await $(this.walletId + `${name}` + "']");
+    await $(this.walletName + `${name}` + "']")
+      .waitForDisplayed({ timeout: 7000, timeoutMsg: "Wallet has not been created! Missing wallet name!" });
+    return await $(this.walletName + `${name}` + "']");
   }
 
 }

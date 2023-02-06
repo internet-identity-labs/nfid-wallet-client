7f23f90b01581a8ab64c60ba4262a6e0feb826eb
olsemeno <sendtosemenov@gmail.com>
2023-02-06 18:32:49 +0300

    feat([sc-6233]): Optimistically sign a transaction and keep it in canister until user approves

--- apps/nfid-frontend/src/integration/lambda/auth-stats/index.ts
+++ apps/nfid-frontend/src/integration/lambda/auth-stats/index.ts
@@ -1,21 +1,19 @@
-import {ic} from "@nfid/integration"
+import { ic } from "@nfid/integration"
 
 declare const AWS_AUTH_STATS: string
 
-export async function storeSignIn(
-  event: {
-    principal: string;
-    blockchainAddress: string;
-    chain: string;
-    application: string;
-    billable: boolean;
-  }
-) {
+export async function storeSignIn(event: {
+  principal: string
+  blockchainAddress: string
+  chain: string
+  application: string
+  billable: boolean
+}) {
   const url = ic.isLocal ? "/auth" : AWS_AUTH_STATS
   console.log(url)
   const response = await fetch(url, {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(event),
   })
 

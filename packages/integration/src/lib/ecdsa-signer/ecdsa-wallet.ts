bcbe46f7e743daa4f20f9d483752ac82bc0df59e
olsemeno <sendtosemenov@gmail.com>
2023-02-08 20:17:10 +0300

    feat([sc-6233]): Optimistically sign a transaction and keep it in canister until user approves

--- packages/integration/src/lib/ecdsa-signer/ecdsa-wallet.ts
+++ packages/integration/src/lib/ecdsa-signer/ecdsa-wallet.ts
@@ -1,137 +1,161 @@
-import {ActorMethod} from "@dfinity/agent"
-import {Bytes, ethers, Signer, TypedDataDomain, TypedDataField} from "ethers"
-import {Provider, TransactionRequest} from "@ethersproject/abstract-provider"
-import {getEcdsaPublicKey, getSignature, prepareSignature, signEcdsaMessage} from "./index"
-import {_TypedDataEncoder, arrayify, hashMessage, keccak256, resolveProperties, splitSignature} from "ethers/lib/utils"
-import {hexZeroPad, joinSignature} from "@ethersproject/bytes"
-import {serialize} from "@ethersproject/transactions"
-import {UnsignedTransaction} from "ethers-ts"
-import * as BN from "bn.js"
+import { ActorMethod } from "@dfinity/agent";
+import { Bytes, ethers, Signer, TypedDataDomain, TypedDataField } from "ethers";
+import { Provider, TransactionRequest } from "@ethersproject/abstract-provider";
+import { getEcdsaPublicKey, getSignature, prepareSignature, signEcdsaMessage } from "./index";
+import {
+  arrayify,
+  hashMessage,
+  keccak256,
+  resolveProperties,
+  splitSignature
+} from "ethers/lib/utils";
+import { hexZeroPad, joinSignature } from "@ethersproject/bytes";
+import { serialize } from "@ethersproject/transactions";
+import { UnsignedTransaction } from "ethers-ts";
+import * as BN from "bn.js";
+import { SignTypedDataVersion, TypedDataUtils } from "@metamask/eth-sig-util";
 
 const ABI_721 = [
-  'function setApprovalForAll(address operator, bool _approved)',
-  'function safeTransferFrom(address from, address to, uint256 tokenId)',
-  'function transfer(address from, address to, uint256 tokenId)',
-  'function approve(address to, uint256 tokenId)',
-  'function isApprovedForAll(address owner, address operator)'
+  "function setApprovalForAll(address operator, bool _approved)",
+  "function safeTransferFrom(address from, address to, uint256 tokenId)",
+  "function transfer(address from, address to, uint256 tokenId)",
+  "function approve(address to, uint256 tokenId)",
+  "function isApprovedForAll(address owner, address operator)"
 ];
 
 export class EthWallet<T = Record<string, ActorMethod>> extends Signer {
-  override provider?: Provider
-  private address?: string
+  override provider?: Provider;
+  private address?: string;
 
   constructor(provider?: Provider) {
-    super()
-    this.provider = provider
+    super();
+    this.provider = provider;
   }
 
   async getAddress(): Promise<string> {
     if (typeof this.address !== "undefined") {
-      return this.address
+      return this.address;
     }
     return getEcdsaPublicKey()
       .then(pk => {
-        this.address = ethers.utils.computeAddress(pk)
-        return this.address
-      })
+        this.address = ethers.utils.computeAddress(pk);
+        return this.address;
+      });
   }
 
   async signMessage(message: Bytes | string): Promise<string> {
-    const keccakHash = hashMessage(message)
-    const messageHashAsBytes = arrayify(keccakHash)
+    const keccakHash = hashMessage(message);
+    const messageHashAsBytes = arrayify(keccakHash);
     return signEcdsaMessage([...messageHashAsBytes])
       .then(signature => {
-        const ethersSignature = this._splitSignature(signature, messageHashAsBytes)
-        return joinSignature(ethersSignature)
-      })
+        const ethersSignature = this._splitSignature(signature, messageHashAsBytes);
+        return joinSignature(ethersSignature);
+      });
   }
 
   async prepareSignature(message: Bytes | string): Promise<string> {
-    const keccakHash = hashMessage(message)
-    const messageHashAsBytes = arrayify(keccakHash)
-    return prepareSignature([...messageHashAsBytes])
+    const keccakHash = hashMessage(message);
+    const messageHashAsBytes = arrayify(keccakHash);
+    return prepareSignature([...messageHashAsBytes]);
   }
 
   async getPreparedSignature(hash: string, message: Bytes | string): Promise<string> {
-    const keccakHash = hashMessage(message)
-    const messageHashAsBytes = arrayify(keccakHash)
+    const keccakHash = hashMessage(message);
+    const messageHashAsBytes = arrayify(keccakHash);
     return getSignature(hash).then(signature => {
-      const ethersSignature = this._splitSignature(signature, messageHashAsBytes)
-      return joinSignature(ethersSignature)
-    })
+      const ethersSignature = this._splitSignature(signature, messageHashAsBytes);
+      return joinSignature(ethersSignature);
+    });
   }
 
   async signTransaction(transaction: TransactionRequest): Promise<string> {
     return resolveProperties(transaction).then((tx) => {
       if (tx.from != null) {
-        delete tx.from
+        delete tx.from;
       }
-      const keccakHash = keccak256(serialize(<UnsignedTransaction>tx))
-      const messageHashAsBytes = arrayify(keccakHash)
+      const keccakHash = keccak256(serialize(<UnsignedTransaction>tx));
+      const messageHashAsBytes = arrayify(keccakHash);
       return signEcdsaMessage([...messageHashAsBytes])
         .then(signature => {
-          const ethersSignature = this._splitSignature(signature, messageHashAsBytes)
-          return serialize(<UnsignedTransaction>tx, ethersSignature)
-        })
-    })
+          const ethersSignature = this._splitSignature(signature, messageHashAsBytes);
+          return serialize(<UnsignedTransaction>tx, ethersSignature);
+        });
+    });
+  }
+
+  async signTypedData(data: any): Promise<string> {
+    const typedDataHash = TypedDataUtils.eip712Hash(
+      {
+        types: data.types,
+        primaryType: data.primaryType,
+        domain: data.domain,
+        message: data.message
+      },
+      SignTypedDataVersion.V4
+    );
+    return signEcdsaMessage([...typedDataHash])
+      .then(signature => {
+        const ethersSignature = this._splitSignature(signature, typedDataHash);
+        return joinSignature(ethersSignature);
+      });
   }
 
   async safeTransferFrom(to: string, contractAddress: string, tokenId: string) {
-    const contract = new ethers.Contract(contractAddress, ABI_721, this.provider)
-    const connectedWallet = contract.connect(this)
+    const contract = new ethers.Contract(contractAddress, ABI_721, this.provider);
+    const connectedWallet = contract.connect(this);
     return connectedWallet["safeTransferFrom"](this.getAddress(), to, tokenId);
   }
 
   async approve(to: string, contractAddress: string, tokenId: string) {
-    const contract = new ethers.Contract(contractAddress, ABI_721, this.provider)
-    const connectedWallet = contract.connect(this)
+    const contract = new ethers.Contract(contractAddress, ABI_721, this.provider);
+    const connectedWallet = contract.connect(this);
     return connectedWallet["approve"](to, tokenId);
   }
 
   async setApprovalForAll(operator: string, contractAddress: string, approved: boolean) {
-    const contract = new ethers.Contract(contractAddress, ABI_721, this.provider)
-    const connectedWallet = contract.connect(this)
+    const contract = new ethers.Contract(contractAddress, ABI_721, this.provider);
+    const connectedWallet = contract.connect(this);
     return connectedWallet["setApprovalForAll"](operator, approved);
   }
 
   async isApprovedForAll(owner: string, contractAddress: string, operator: string) {
-    const contract = new ethers.Contract(contractAddress, ABI_721, this.provider)
-    const connectedWallet = contract.connect(this)
+    const contract = new ethers.Contract(contractAddress, ABI_721, this.provider);
+    const connectedWallet = contract.connect(this);
     return connectedWallet["isApprovedForAll"](owner, operator);
   }
 
   connect(provider: Provider): Signer {
-    this.provider = provider
-    return this
+    this.provider = provider;
+    return this;
   }
 
   _toEllipticSignature(signature: Array<number>) {
-    const bytes: Uint8Array = arrayify(signature)
-    let v = 27 + (bytes[32] >> 7)
+    const bytes: Uint8Array = arrayify(signature);
+    let v = 27 + (bytes[32] >> 7);
     // Allow a recid to be used as the v
     if (v < 27) {
       if (v === 0 || v === 1) {
-        v += 27
+        v += 27;
       }
     }
     // Compute recoveryParam from v
-    const recoveryParam: number = 1 - (v % 2)
+    const recoveryParam: number = 1 - (v % 2);
     return {
       r: new BN(new Uint8Array(signature.slice(0, 32))),
       recoveryParam: recoveryParam,
-      s: new BN(new Uint8Array(signature.slice(32, 64))),
-    }
+      s: new BN(new Uint8Array(signature.slice(32, 64)))
+    };
   }
 
   _splitSignature(signature: Array<number>, digestBytes: Uint8Array) {
-    const elliptic_signature = this._toEllipticSignature(signature)
+    const elliptic_signature = this._toEllipticSignature(signature);
     let ethersSignature = splitSignature({
       recoveryParam: elliptic_signature.recoveryParam,
       r: hexZeroPad("0x" + elliptic_signature.r.toString(16), 32),
-      s: hexZeroPad("0x" + elliptic_signature.s.toString(16), 32),
-    })
-    const address = ethers.utils.recoverAddress([...digestBytes], ethersSignature)
-    const isEqualAddress = address === this.address
+      s: hexZeroPad("0x" + elliptic_signature.s.toString(16), 32)
+    });
+    const address = ethers.utils.recoverAddress([...digestBytes], ethersSignature);
+    const isEqualAddress = address === this.address;
     // according to EIP-2098 we need to get recovery param from the first byte of s
     // but it does not work for some reasons.
     // With this workaround we can try to recover address with different yParity
@@ -140,28 +164,14 @@ export class EthWallet<T = Record<string, ActorMethod>> extends Signer {
       ethersSignature = splitSignature({
         recoveryParam: 1 - (ethersSignature.recoveryParam % 2),
         r: hexZeroPad("0x" + elliptic_signature.r.toString(16), 32),
-        s: hexZeroPad("0x" + elliptic_signature.s.toString(16), 32),
-      })
+        s: hexZeroPad("0x" + elliptic_signature.s.toString(16), 32)
+      });
     }
-    return ethersSignature
+    return ethersSignature;
   }
 
   async _signTypedData(domain: TypedDataDomain, types: Record<string, Array<TypedDataField>>, value: Record<string, any>): Promise<string> {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const populated = await _TypedDataEncoder.resolveNames(domain, types, value, (name: string) => {
-      return this.provider!.resolveName(name);
-    });
-    const abc = _TypedDataEncoder.hash(populated.domain, types, populated.value);
-    return signEcdsaMessage([...arrayify(abc)])
-      .then(signature => {
-        const ethersSignature = this._splitSignature(signature, arrayify(abc))
-        return joinSignature(ethersSignature)
-      })
-  }
-
-  async signTypedData(data: any): Promise<string> {
-    throw new Error("We did not decide what to do with this for now. Please contact BE team if you face it (:")
+    throw new Error("We did not decide what to do with this for now. Please contact BE team if you face it (:");
   }
 
 }
--- packages/integration/src/lib/ecdsa-signer/index.spec.ts
+++ packages/integration/src/lib/ecdsa-signer/index.spec.ts
@@ -1,11 +1,17 @@
 /**
  * @jest-environment jsdom
  */
-import {JsonnableEd25519KeyIdentity} from "@dfinity/identity/lib/cjs/identity/ed25519"
-import {BigNumber, ethers, Wallet} from "ethers"
-import {EthWallet} from "./ecdsa-wallet"
-import {signTypedData, SignTypedDataVersion} from "@metamask/eth-sig-util";
-import * as crypto from "crypto";
+import { JsonnableEd25519KeyIdentity } from "@dfinity/identity/lib/cjs/identity/ed25519"
+import {
+  recoverTypedSignature,
+  SignTypedDataVersion,
+} from "@metamask/eth-sig-util"
+import { BigNumber, ethers, Wallet } from "ethers"
+import { arrayify, hashMessage } from "ethers/lib/utils"
+
+import { getEcdsaPublicKey, signEcdsaMessage } from "."
+import { EthWallet } from "./ecdsa-wallet"
+
 const idd: JsonnableEd25519KeyIdentity = [
   "0402f7e13e782ad8bb2c4da69d00c14af52d4bf0f1cc20ddb52f117d7fff2e3678c950145102d87915c5688a218cdc4348407cd7b1fdb8256dade044309a2552cd",
   "fa1e290e2524ec98e24e49e95c0e30b43e9c96504715cea4d802269f80f638e6",
@@ -17,36 +23,54 @@ describe("ECDSA suite", () => {
     "https://ethereum-goerli-rpc.allthatnode.com",
   )
   const nfidWallet = new EthWallet(rpcProvider)
-  // it("ecdsa public key", async () => {
-  //   const pk = await getEcdsaPublicKey()
-  //   expect(pk.length > 0).toBe(true)
-  // })
-  //
-  // it("ecdsa sign message", async () => {
-  //   const message = Array(32).fill(1)
-  //   const signature = await signEcdsaMessage(message)
-  //   expect(signature.length > 0).toBe(true)
-  // })
-  //
+  it("ecdsa public key", async () => {
+    const pk = await getEcdsaPublicKey()
+    expect(pk.length > 0).toBe(true)
+  })
+
+  it("ecdsa sign message", async () => {
+    const message = Array(32).fill(1)
+    const signature = await signEcdsaMessage(message)
+    expect(signature.length > 0).toBe(true)
+  })
+
+  it("ecdsa prepare signature", async () => {
+    const message = "test_message"
+    const hash = await nfidWallet.prepareSignature(message)
+    const signature = await nfidWallet.getPreparedSignature(hash, message)
+    expect(signature.length > 0).toBe(true)
+  })
 
   it("nfid-wallet verify message", async () => {
-    let a = JSON.parse("{\"primaryType\":\"Order\",\"domain\":{\"name\":\"Exchange\",\"version\":\"2\",\"verifyingContract\":\"0x02afbd43cad367fcb71305a2dfb9a3928218f0c1\",\"chainId\":5},\"types\":{\"EIP712Domain\":[{\"type\":\"string\",\"name\":\"name\"},{\"type\":\"string\",\"name\":\"version\"},{\"type\":\"uint256\",\"name\":\"chainId\"},{\"type\":\"address\",\"name\":\"verifyingContract\"}],\"AssetType\":[{\"name\":\"assetClass\",\"type\":\"bytes4\"},{\"name\":\"data\",\"type\":\"bytes\"}],\"Asset\":[{\"name\":\"assetType\",\"type\":\"AssetType\"},{\"name\":\"value\",\"type\":\"uint256\"}],\"Order\":[{\"name\":\"maker\",\"type\":\"address\"},{\"name\":\"makeAsset\",\"type\":\"Asset\"},{\"name\":\"taker\",\"type\":\"address\"},{\"name\":\"takeAsset\",\"type\":\"Asset\"},{\"name\":\"salt\",\"type\":\"uint256\"},{\"name\":\"start\",\"type\":\"uint256\"},{\"name\":\"end\",\"type\":\"uint256\"},{\"name\":\"dataType\",\"type\":\"bytes4\"},{\"name\":\"data\",\"type\":\"bytes\"}]},\"message\":{\"maker\":\"0x5d88229726c01f00fdefed1e70bd628407dc07ce\",\"makeAsset\":{\"assetType\":{\"assetClass\":\"0x73ad2146\",\"data\":\"0x000000000000000000000000d8560c88d1dc85f9ed05b25878e366c49b68bef9c3217ef1d6027b5ad5404b21a911b952b5f728b4000000000000000000000002\"},\"value\":\"1\"},\"taker\":\"0x0000000000000000000000000000000000000000\",\"takeAsset\":{\"assetType\":{\"assetClass\":\"0xaaaebeba\",\"data\":\"0x\"},\"value\":\"1000000000000000\"},\"salt\":\"0x735d4f76838af5686a9a127ec4200b5682f72ebb0b8732e422cfa4d6d47fb4a6\",\"start\":0,\"end\":0,\"dataType\":\"0x2fa3cfd3\",\"data\":\"0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003e812345678900000000000000000000000000123456789face09616c6c64617461\"}}")
-    const domain = a.domain;
-    const message = a.message;
-    const types = a.types;
-    const pk = crypto.randomBytes(32);
+    const address = await nfidWallet.getAddress()
+    const message = "test_message"
 
-    const signature = signTypedData({
+    const signature = await nfidWallet.signMessage(message)
+
+    const keccak = hashMessage(message)
+    const digestBytes = arrayify(keccak)
+    const pk = ethers.utils.recoverPublicKey([...digestBytes], signature)
+    const actual = ethers.utils.computeAddress(ethers.utils.arrayify(pk))
+
+    expect(actual).toEqual(address)
+  })
+
+  it("nfid-wallet sign typed data", async () => {
+    const data = JSON.parse(
+      '{"primaryType":"Order","domain":{"name":"Exchange","version":"2","verifyingContract":"0x02afbd43cad367fcb71305a2dfb9a3928218f0c1","chainId":5},"types":{"EIP712Domain":[{"type":"string","name":"name"},{"type":"string","name":"version"},{"type":"uint256","name":"chainId"},{"type":"address","name":"verifyingContract"}],"AssetType":[{"name":"assetClass","type":"bytes4"},{"name":"data","type":"bytes"}],"Asset":[{"name":"assetType","type":"AssetType"},{"name":"value","type":"uint256"}],"Order":[{"name":"maker","type":"address"},{"name":"makeAsset","type":"Asset"},{"name":"taker","type":"address"},{"name":"takeAsset","type":"Asset"},{"name":"salt","type":"uint256"},{"name":"start","type":"uint256"},{"name":"end","type":"uint256"},{"name":"dataType","type":"bytes4"},{"name":"data","type":"bytes"}]},"message":{"maker":"0x5d88229726c01f00fdefed1e70bd628407dc07ce","makeAsset":{"assetType":{"assetClass":"0x73ad2146","data":"0x000000000000000000000000d8560c88d1dc85f9ed05b25878e366c49b68bef9c3217ef1d6027b5ad5404b21a911b952b5f728b4000000000000000000000002"},"value":"1"},"taker":"0x0000000000000000000000000000000000000000","takeAsset":{"assetType":{"assetClass":"0xaaaebeba","data":"0x"},"value":"1000000000000000"},"salt":"0x735d4f76838af5686a9a127ec4200b5682f72ebb0b8732e422cfa4d6d47fb4a6","start":0,"end":0,"dataType":"0x2fa3cfd3","data":"0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003e812345678900000000000000000000000000123456789face09616c6c64617461"}}',
+    )
+    const nfidSignature = await nfidWallet.signTypedData(data)
+    const actual = recoverTypedSignature({
       data: {
-        types: types,
-        primaryType: a.primaryType,
-        domain,
-        message,
+        types: data.types,
+        primaryType: data.primaryType,
+        domain: data.domain,
+        message: data.message,
       },
+      signature: nfidSignature,
       version: SignTypedDataVersion.V4,
-      privateKey: pk,
-    });
-    console.log("Generated signature", signature);
+    })
+    expect(actual).toEqual((await nfidWallet.getAddress()).toLowerCase())
   })
 
   it.skip("nfid-wallet safeTransfer eip721", async () => {
@@ -62,31 +86,31 @@ describe("ECDSA suite", () => {
       )
     }
   })
-  //
-  // it.skip("nfid-wallet approve eip721", async () => {
-  //   const contract = "0xd8560c88d1dc85f9ed05b25878e366c49b68bef9"
-  //   const to = "0xdC75e8c3aE765D8947aDBC6698a2403A6141D439"
-  //   const tokenId =
-  //     "80322369037599879817130611650014995038071054105692890356259348959353817268226"
-  //   try {
-  //     await nfidWallet.approve(to, contract, tokenId)
-  //   } catch (e) {
-  //     expect(JSON.stringify(e)).toContain(
-  //       "execution reverted: ERC721: approval to current owner",
-  //     )
-  //   }
-  // })
-  //
-  // it.skip("nfid-wallet approveForAll eip721", async () => {
-  //   const contract = "0x1f419b9469d641d333805c4054ca3b65af54d315"
-  //   const to = "0xdC75e8c3aE765D8947aDBC6698a2403A6141D439"
-  //   const response = await nfidWallet.setApprovalForAll(to, contract, true)
-  //   const wait = await response.wait()
-  //   console.log(wait)
-  //   expect(wait.events[0].data).toEqual(
-  //     "0x0000000000000000000000000000000000000000000000000000000000000001",
-  //   )
-  // })
+
+  it.skip("nfid-wallet approve eip721", async () => {
+    const contract = "0xd8560c88d1dc85f9ed05b25878e366c49b68bef9"
+    const to = "0xdC75e8c3aE765D8947aDBC6698a2403A6141D439"
+    const tokenId =
+      "80322369037599879817130611650014995038071054105692890356259348959353817268226"
+    try {
+      await nfidWallet.approve(to, contract, tokenId)
+    } catch (e) {
+      expect(JSON.stringify(e)).toContain(
+        "execution reverted: ERC721: approval to current owner",
+      )
+    }
+  })
+
+  it.skip("nfid-wallet approveForAll eip721", async () => {
+    const contract = "0x1f419b9469d641d333805c4054ca3b65af54d315"
+    const to = "0xdC75e8c3aE765D8947aDBC6698a2403A6141D439"
+    const response = await nfidWallet.setApprovalForAll(to, contract, true)
+    const wait = await response.wait()
+    console.log(wait)
+    expect(wait.events[0].data).toEqual(
+      "0x0000000000000000000000000000000000000000000000000000000000000001",
+    )
+  })
 
   // to run this test you need to install geth or another local EVM
   // nice example howto:

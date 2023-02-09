05a6abce36b73f3383899bd310f8e5c62d818496
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-02-09 11:55:30 +0100

    chore: extract cucumberOpts

--- apps/nfid-frontend-e2e/wdio.conf.ts
+++ apps/nfid-frontend-e2e/wdio.conf.ts
@@ -1,5 +1,6 @@
 import { hooks } from "./src/wdio-hooks"
 import { chromeBrowser } from "./src/browserOptions"
+import { cucumberOpts } from "./src/cucumber-options";
 
 export const isDebug = process.env.DEBUG === "true"
 export const hostName = process.env.HOST_NAME;
@@ -191,29 +192,6 @@ export const config: WebdriverIO.Config = {
 
   //
   // If you are using Cucumber you need to specify the location of your step definitions.
-  cucumberOpts: {
-    // <string[]> (file/dir) require files before executing features
-    require: ["./src/steps/*.ts"],
-    // <boolean> show full backtrace for errors
-    backtrace: false,
-    // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
-    requireModule: [],
-    // <boolean> invoke formatters without executing steps
-    dryRun: false,
-    // <boolean> abort the run on first failure
-    failFast: false,
-    // <boolean> hide step definition snippets for pending steps
-    snippets: true,
-    // <boolean> hide source uris
-    source: true,
-    // <boolean> fail if there are any undefined or pending steps
-    strict: false,
-    // <string> (expression) only execute the features or scenarios with tags matching the expression
-    tagExpression: "not @pending and not @mobile",
-    // <number> timeout for step definitions
-    timeout: 60000,
-    // <boolean> Enable this config to treat undefined definitions as warnings.
-    ignoreUndefinedDefinitions: false,
-  } as WebdriverIO.CucumberOpts,
+  cucumberOpts,
   ...hooks,
 }

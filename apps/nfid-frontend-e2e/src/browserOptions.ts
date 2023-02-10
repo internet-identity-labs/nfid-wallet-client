c6c365173bc82774b0cd36cb1e19674e1a965442
sataninja <viktor.identitylabs.ooo>
2023-02-10 17:29:25 +0200

    feat([sc-6200]): screen resolution change in order to run in actions

--- apps/nfid-frontend-e2e/src/browserOptions.ts
+++ apps/nfid-frontend-e2e/src/browserOptions.ts
@@ -1,14 +1,14 @@
 export const chromeBrowserOptions: IChromeOption = {
   args: [
-    "--window-size=1920,1080",
     "--no-sandbox",
     // '--use-fake-device-for-media-stream',
     "--disable-notifications",
     "--disable-web-security",
     "--allow-insecure-localhost",
     "--disable-infobars",
-    // '--disable-dev-shm-usage',
+    '--disable-dev-shm-usage',
     "--ignore-certificate-errors",
+    "--start-maximized",
     "--disable-gpu",
     // "--enable-features=NetworkService,NetworkServiceInProcess"
     `--user-data-dir=${process.env.USER_DATA_DIR}`,

a7b6acb9a3242bdbffa2ba615695c3fdd3cbe64f
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2023-02-23 20:02:43 +0400

    feat: [sc-6220] [Integration] Retrieve MATIC transaction history (native MATIC fungible token) (#1215)

---
+++ packages/integration/src/lib/asset/asset-ethereum.ts
@@ -0,0 +1,321 @@
+import {
+  Activity as RaribleActivity,
+  TransferActivity,
+  ActivitySort,
+  ActivityType,
+  OrderMatchSell,
+  Item as RaribleItem,
+  UserActivityType,
+} from "@rarible/api-client"
+import { Blockchain } from "@rarible/api-client"
+import { EthersEthereum } from "@rarible/ethers-ethereum"
+import { createRaribleSdk, IRaribleSdk } from "@rarible/sdk"
+import { EthereumWallet } from "@rarible/sdk-wallet"
+import {
+  convertEthereumToUnionAddress,
+  convertEthereumItemId,
+  EVMBlockchain,
+} from "@rarible/sdk/build/sdk-blockchains/ethereum/common"
+import { toCurrencyId, UnionAddress } from "@rarible/types"
+import { toBn } from "@rarible/utils"
+import {
+  Network,
+  Alchemy,
+  SortingOrder,
+  AssetTransfersCategory,
+} from "alchemy-sdk"
+import { ethers } from "ethers-ts"
+
+import { EthWallet } from "../ecdsa-signer/ecdsa-wallet"
+import {
+  Asset,
+  ChainBalance,
+  ActivityRecord,
+  NonFungibleActivityRecords,
+  NonFungibleItem,
+  NonFungibleItems,
+  FungibleActivityRecords,
+  Tokens,
+  FungibleActivityRequest,
+  Configuration,
+  ActivitiesByItemRequest,
+  PageRequest,
+  SortRequest,
+} from "./types"
+
+declare const FRONTEND_MODE: string
+declare const ALCHEMY_API_KEY: string
+
+class EthereumAsset implements Asset {
+  readonly blockchain: EVMBlockchain
+  readonly unionBlockchain: EVMBlockchain
+  readonly currencyId: string
+  readonly raribleSdk: IRaribleSdk
+  readonly wallet: EthWallet
+  readonly alchemySdk: Alchemy
+
+  constructor(config: Configuration) {
+    const [raribleSdk, wallet] = this.getRaribleSdk(FRONTEND_MODE, config)
+    this.alchemySdk = this.getAlchemySdk(FRONTEND_MODE, config)
+    this.raribleSdk = raribleSdk
+    this.wallet = wallet
+    this.blockchain = config.blockchain
+    this.currencyId = config.currencyId
+    this.unionBlockchain = config.unionBlockchain
+  }
+
+  public async getActivitiesByItem({
+    tokenId,
+    contract,
+    cursor,
+    size,
+    sort,
+  }: ActivitiesByItemRequest): Promise<NonFungibleActivityRecords> {
+    const itemId = convertEthereumItemId(
+      `${contract}:${tokenId}`,
+      this.blockchain,
+    )
+    const raribleActivities =
+      await this.raribleSdk.apis.activity.getActivitiesByItem({
+        type: [ActivityType.SELL, ActivityType.TRANSFER],
+        itemId,
+        cursor,
+        size,
+        sort:
+          "asc" === sort
+            ? ActivitySort.EARLIEST_FIRST
+            : ActivitySort.LATEST_FIRST,
+      })
+
+    return {
+      activities: raribleActivities.activities.map(this.mapActivity),
+      cursor: raribleActivities.cursor,
+    }
+  }
+
+  public async getActivitiesByUser({
+    cursor,
+    size,
+    sort,
+  }: PageRequest & SortRequest = {}): Promise<NonFungibleActivityRecords> {
+    const address = await this.wallet.getAddress()
+    const unionAddress: UnionAddress = convertEthereumToUnionAddress(
+      address,
+      this.unionBlockchain,
+    )
+    const raribleActivities =
+      await this.raribleSdk.apis.activity.getActivitiesByUser({
+        type: [
+          UserActivityType.SELL,
+          UserActivityType.TRANSFER_FROM,
+          UserActivityType.TRANSFER_TO,
+          UserActivityType.BUY,
+        ],
+        user: [unionAddress],
+        cursor,
+        size,
+        blockchains: [this.blockchain],
+        sort:
+          "asc" === sort
+            ? ActivitySort.EARLIEST_FIRST
+            : ActivitySort.LATEST_FIRST,
+      })
+    return {
+      activities: raribleActivities.activities.map(this.mapActivity),
+      cursor: raribleActivities.cursor,
+    }
+  }
+
+  public async getItemsByUser({
+    cursor,
+    size,
+  }: PageRequest = {}): Promise<NonFungibleItems> {
+    const address = await this.wallet.getAddress()
+    const unionAddress: UnionAddress = convertEthereumToUnionAddress(
+      address,
+      this.unionBlockchain,
+    )
+    const raribleItems = await this.raribleSdk.apis.item.getItemsByOwner({
+      owner: unionAddress,
+      size,
+      continuation: cursor,
+      blockchains: [this.blockchain],
+    })
+    return {
+      total: raribleItems.total,
+      items: raribleItems.items.map(this.mapItem),
+    }
+  }
+
+  public async getBalance(): Promise<ChainBalance> {
+    const address = await this.wallet.getAddress()
+    const unionAddress: UnionAddress = convertEthereumToUnionAddress(
+      address,
+      this.unionBlockchain,
+    )
+    const now = new Date()
+    const [balance, currencyRate] = await Promise.all([
+      this.raribleSdk.balances.getBalance(
+        unionAddress,
+        toCurrencyId(this.currencyId),
+      ),
+      this.raribleSdk.apis.currency.getCurrencyUsdRateByCurrencyId({
+        currencyId: this.currencyId,
+        at: now,
+      }),
+    ])
+    const balanceBN = toBn(balance)
+    const balanceinUsd = toBn(currencyRate.rate).multipliedBy(balanceBN)
+    return { balance: balanceBN, balanceinUsd }
+  }
+
+  public async transferNft(
+    tokenId: string,
+    contract: string,
+    receiver: string,
+  ): Promise<void> {
+    this.wallet.safeTransferFrom(receiver, contract, tokenId)
+  }
+
+  public async getErc20TokensByUser({
+    cursor,
+  }: PageRequest = {}): Promise<Tokens> {
+    const address = await this.wallet.getAddress()
+    const tokens = await this.alchemySdk.core.getTokensForOwner(address, {
+      pageKey: cursor,
+    })
+    return {
+      cursor: tokens.pageKey,
+      tokens: tokens.tokens
+        .filter((x) => x.rawBalance !== undefined && 0 != +x.rawBalance)
+        .map((x) => ({
+          name: x.name || "N/A",
+          symbol: x.symbol || "N/A",
+          logo: x.logo,
+          balance: x.balance || "0.0",
+          contractAddress: x.contractAddress,
+        })),
+    }
+  }
+
+  public async getFungibleActivityByTokenAndUser({
+    direction = "from",
+    contract,
+    cursor,
+    size,
+    sort = "desc",
+  }: FungibleActivityRequest = {}): Promise<FungibleActivityRecords> {
+    const address = await this.wallet.getAddress()
+    const transfers = await this.alchemySdk.core.getAssetTransfers({
+      fromAddress: "from" == direction ? address : undefined,
+      toAddress: "to" == direction ? address : undefined,
+      category: contract
+        ? [AssetTransfersCategory.ERC20]
+        : [AssetTransfersCategory.EXTERNAL],
+      withMetadata: true,
+      order: "asc" == sort ? SortingOrder.ASCENDING : SortingOrder.DESCENDING,
+      maxCount: size,
+      pageKey: cursor,
+    })
+    return {
+      cursor: transfers.pageKey,
+      activities: transfers.transfers.map((x) => ({
+        id: x.uniqueId,
+        date: x.metadata.blockTimestamp,
+        to: x.to || "N/A",
+        from: x.from,
+        transactionHash: x.hash,
+        price: x.value || 0,
+      })),
+    }
+  }
+
+  private mapActivity(activity: RaribleActivity): ActivityRecord {
+    switch (activity["@type"]) {
+      case "SELL": {
+        const sell = activity as OrderMatchSell
+        return {
+          id: sell.id.toString(),
+          type: sell["@type"].toString(),
+          date: sell.date,
+          to: sell.buyer.toString(),
+          from: sell.seller.toString(),
+          transactionHash: activity.transactionHash,
+          price: sell.price.toString(),
+          priceUsd: sell.priceUsd?.toString(),
+        }
+      }
+      case "TRANSFER": {
+        const transfer = activity as TransferActivity
+        return {
+          id: transfer.id.toString(),
+          type: transfer["@type"].toString(),
+          date: transfer.date,
+          from: transfer.from.toString(),
+          to: transfer.owner.toString(),
+          transactionHash: activity.transactionHash,
+        }
+      }
+      default: {
+        throw Error("Not supported Activity Type.")
+      }
+    }
+  }
+
+  private mapItem(raribleItem: RaribleItem): NonFungibleItem {
+    return {
+      id: raribleItem.id.toString(),
+      blockchain: raribleItem.blockchain.toString(),
+      collection: raribleItem.collection?.toString(),
+      contract: raribleItem.contract?.toString(),
+      tokenId: raribleItem.tokenId?.toString(),
+      lastUpdatedAt: raribleItem.lastUpdatedAt.toString(),
+    }
+  }
+
+  private getRaribleSdk(
+    mode: string,
+    config: Configuration,
+  ): [IRaribleSdk, EthWallet] {
+    const network = "production" == FRONTEND_MODE ? "prod" : "testnet"
+    const url =
+      "production" == mode ? config.provider.mainnet : config.provider.testnet
+    const rpcProvider = new ethers.providers.JsonRpcProvider(url)
+    const wallet = new EthWallet(rpcProvider)
+    const ethersWallet = new EthereumWallet(new EthersEthereum(wallet))
+    const raribleSdk = createRaribleSdk(ethersWallet, network)
+
+    return [raribleSdk, wallet]
+  }
+
+  private getAlchemySdk(mode: string, config: Configuration): Alchemy {
+    const alchemyNetwork: Network =
+      "production" == mode ? config.alchemy.mainnet : config.alchemy.testnet
+    return new Alchemy({
+      apiKey: ALCHEMY_API_KEY,
+      network: alchemyNetwork,
+    })
+  }
+}
+
+export const ethereumAsset = new EthereumAsset({
+  currencyId: "ETHEREUM:0x0000000000000000000000000000000000000000",
+  blockchain: Blockchain.ETHEREUM as EVMBlockchain,
+  unionBlockchain: Blockchain.ETHEREUM as EVMBlockchain,
+  provider: {
+    mainnet: "https://ethereum.publicnode.com",
+    testnet: "https://ethereum-goerli-rpc.allthatnode.com",
+  },
+  alchemy: { mainnet: Network.ETH_MAINNET, testnet: Network.ETH_GOERLI },
+})
+
+export const polygonAsset = new EthereumAsset({
+  currencyId: "POLYGON:0x0000000000000000000000000000000000000000",
+  blockchain: Blockchain.POLYGON as EVMBlockchain,
+  unionBlockchain: Blockchain.ETHEREUM as EVMBlockchain,
+  provider: {
+    mainnet: "https://polygon-mainnet.infura.io",
+    testnet: "https://rpc-mumbai.maticvigil.com",
+  },
+  alchemy: { mainnet: Network.MATIC_MAINNET, testnet: Network.MATIC_MUMBAI },
+})
---
+++ packages/integration/src/lib/asset/asset-polygon.spec.ts
@@ -0,0 +1,147 @@
+import { BigNumber } from "@rarible/utils"
+
+import { polygonAsset } from "./asset-ethereum"
+import { ChainBalance } from "./types"
+
+describe("Polygon Asset", () => {
+  jest.setTimeout(20000)
+
+  it("should return one fungible native tx", async function () {
+    const actual = await polygonAsset.getFungibleActivityByTokenAndUser({
+      size: 1,
+      sort: "asc",
+    })
+    expect(actual).toEqual({
+      cursor: expect.any(String),
+      activities: [
+        {
+          id: "0x1a821c20741e9ff6dfe148cf4ff2b44c5064e2b3b86cbe720c8a638c8b93ad1c:external",
+          date: "2023-02-07T15:40:17.000Z",
+          to: "0xdc75e8c3ae765d8947adbc6698a2403a6141d439",
+          from: "0x382901144a77bec53493fa090053b9c63da5dd07",
+          transactionHash:
+            "0x1a821c20741e9ff6dfe148cf4ff2b44c5064e2b3b86cbe720c8a638c8b93ad1c",
+          price: 0.01,
+        },
+      ],
+    })
+  })
+
+  it("should return one fungible erc20 tx", async function () {
+    const actual = await polygonAsset.getFungibleActivityByTokenAndUser({
+      contract: "0x326c977e6efc84e512bb9c30f76e30c160ed06fb",
+      size: 1,
+      sort: "asc",
+      direction: "to",
+    })
+    expect(actual).toEqual({
+      cursor: expect.any(String),
+      activities: [
+        {
+          id: "0xc569a7bd93bb29138b1374ce293469f7da1e2b662314e5ae27c44253a847c524:log:16",
+          date: "2023-02-17T14:45:35.000Z",
+          to: "0x382901144a77bec53493fa090053b9c63da5dd07",
+          from: "0xd5b31fb565d608692d6422beb31bf0875dad4fc3",
+          transactionHash:
+            "0xc569a7bd93bb29138b1374ce293469f7da1e2b662314e5ae27c44253a847c524",
+          price: 100,
+        },
+      ],
+    })
+  })
+
+  it("should return one fungible erc20 token", async function () {
+    const actual = await polygonAsset.getErc20TokensByUser()
+    expect(actual).toEqual({
+      cursor: undefined,
+      tokens: [
+        {
+          name: "USD Coin",
+          symbol: "USDC",
+          logo: undefined,
+          balance: "100.0",
+          contractAddress: "0xe097d6b3100777dc31b34dc2c58fb524c2e76921",
+        },
+      ],
+    })
+  })
+
+  it("should request balance", async function () {
+    const balance: ChainBalance = await polygonAsset.getBalance()
+    expect(balance).toMatchObject({
+      balance: expect.any(BigNumber),
+      balanceinUsd: expect.any(BigNumber),
+    })
+  })
+
+  it("should request activities by item", async function () {
+    const contract = "0x2953399124f0cbb46d2cbacd8a89cf0599974963"
+    const tokenId =
+      "25340927624470057327886108971339045949703948342541401788851146193678729479144"
+    const activities = await polygonAsset.getActivitiesByItem({
+      contract,
+      tokenId,
+      size: 1,
+      sort: "asc",
+    })
+
+    expect(activities).toEqual({
+      cursor: expect.any(String),
+      activities: [
+        {
+          id: "POLYGON:63bfd52391b2831cffcff0be",
+          type: "TRANSFER",
+          date: "2023-01-05T07:07:09Z",
+          from: "ETHEREUM:0x380674ee168d258baa2825be55b57628c150099b",
+          to: "ETHEREUM:0x722fe0d1dbe893edd76a63832c08cb3801069c00",
+          transactionHash:
+            "0x15c9d5bd5389a3f551cd92ba4478956e5c5edb8ed282fae54a51f7230fa129c4",
+        },
+      ],
+    })
+  })
+
+  it("should request activities by user", async function () {
+    const activities = await polygonAsset.getActivitiesByUser({
+      size: 1,
+      sort: "asc",
+    })
+
+    expect(activities).toEqual({
+      cursor: expect.any(String),
+      activities: [
+        {
+          id: "POLYGON:63ef8fc49b7e157fa48bedff",
+          type: "TRANSFER",
+          date: "2023-02-17T14:31:31Z",
+          from: "ETHEREUM:0xdc75e8c3ae765d8947adbc6698a2403a6141d439",
+          to: "ETHEREUM:0x382901144a77bec53493fa090053b9c63da5dd07",
+          transactionHash:
+            "0xa317ed05be5fd7c9d420e273759fdc6094908d5322b6cfd6e287433f61b5419d",
+        },
+      ],
+    })
+  })
+
+  it("should request items by user", async function () {
+    const items = await polygonAsset.getItemsByUser({ size: 1 })
+    expect(items).toEqual({
+      total: undefined,
+      items: expect.any(Array),
+    })
+  })
+
+  it("should request transfer", async function () {
+    const contract = "0xd8560c88d1dc85f9ed05b25878e366c49b68bef9"
+    const to = "0xdC75e8c3aE765D8947aDBC6698a2403A6141D439"
+    const tokenId =
+      "80322369037599879817130611650014995038071054105692890356259348959353817268226"
+    try {
+      await polygonAsset.transferNft(to, contract, tokenId)
+    } catch (e) {
+      expect(JSON.stringify(e)).toContain(
+        "transfer caller is not owner nor approved",
+      )
+    }
+  })
+})

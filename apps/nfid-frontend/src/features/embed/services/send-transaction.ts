fff639ca0ac3a605957640de2cc1ac2a1db19933
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-02-27 13:15:23 +0100

    feat([sc-6233]): optimistically prepare signature

--- apps/nfid-frontend/src/features/embed/services/send-transaction.ts
+++ apps/nfid-frontend/src/features/embed/services/send-transaction.ts
@@ -1,40 +1,27 @@
-import { ethers } from "ethers"
-
-import { ecdsaSigner, EthWallet, replaceActorIdentity } from "@nfid/integration"
+import { nfidEthWallet } from "@nfid/integration"
 
 import { CheckoutMachineContext } from "frontend/features/checkout/machine"
-import { getWalletDelegation } from "frontend/integration/facade/wallet"
 
 import { RPCMessage, RPC_BASE } from "../rpc-service"
 
 export const sendTransactionService = async (
-  { signatureId }: CheckoutMachineContext,
+  { preparedSignature }: CheckoutMachineContext,
   event: { type: string; data?: RPCMessage },
 ) => {
   if (!event.data) throw new Error("No event data")
-  // const identity = await getWalletDelegation(authSession.anchor)
-  // replaceActorIdentity(ecdsaSigner, identity)
-
-  // const rawMessage = event.data.params[0]
-  // const message = Object.keys(rawMessage).reduce(
-  //   (acc, key) => ({
-  //     ...acc,
-  //     ...(rawMessage[key] ? { [key]: rawMessage[key] } : {}),
-  //   }),
-  //   {},
-  // )
-
-  // console.debug("SendTransactionService", { message: rawMessage, req: message })
-
-  const rpcProvider = new ethers.providers.JsonRpcProvider(
-    "https://ethereum-goerli-rpc.allthatnode.com",
+  if (
+    !preparedSignature?.hash ||
+    !preparedSignature?.message ||
+    !preparedSignature?.tx
   )
-  const nfidWallet = new EthWallet(rpcProvider)
+    throw new Error("No prepared signature")
 
   try {
     console.time("SendTransactionService sendTransaction:")
-    const { wait, ...result } = await nfidWallet.getPreparedSignature(
-      signatureId,
+    const { wait, ...result } = await nfidEthWallet.sendPreparedTransaction(
+      preparedSignature?.hash,
+      preparedSignature?.message,
+      preparedSignature?.tx,
     )
     console.timeEnd("SendTransactionService sendTransaction:")
     console.debug("SendTransactionService", { result })
---
+++ packages/integration/src/lib/ecdsa-signer/types.ts
@@ -0,0 +1,5 @@
+import { TransactionRequest } from "@ethersproject/abstract-provider"
+
+export type PreparedSignatureResponse = {
+  hash: string; message: Uint8Array; tx: TransactionRequest;
+}

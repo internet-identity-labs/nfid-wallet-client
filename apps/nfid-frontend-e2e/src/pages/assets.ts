da491c5fd9dbf10ad9c223b22824c558fff109a4
olsemeno <sendtosemenov@gmail.com>
2023-03-02 16:22:08 +0300

    feat([sc-6370]): [UI logic] Display BTC balance on the Asset screen

--- apps/nfid-frontend-e2e/src/pages/assets.ts
+++ apps/nfid-frontend-e2e/src/pages/assets.ts
@@ -22,7 +22,7 @@ export class Assets {
             return equality ? l.includes(expected) : !l.includes(expected)
           }),
       {
-        timeout: 20000,
+        timeout: 30000,
         timeoutMsg:
           "Asset equality error! " +
           (await $(this.assetElement + `${name}` + "']").getText()),
@@ -75,14 +75,14 @@ export class Assets {
       this.assetLabel + `${name}` + "_balance']",
     ).getText()
     let calc = 0
-    while (expectedBalance === "0 BTC" && calc < 20) {
+    while (expectedBalance === "0 BTC" && calc < 30) {
       expectedBalance = await $(
         this.assetLabel + `${name}` + "_balance']",
       ).getText()
       await wait(1000)
       calc++
     }
-    if (calc === 20) {
+    if (calc === 30) {
       fail("Balance did not change")
     }
   }

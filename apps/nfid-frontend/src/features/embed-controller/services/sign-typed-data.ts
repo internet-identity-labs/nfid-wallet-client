4df7982dd431f10518e090139f0cb2cc7974878d
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-03-06 14:25:43 +0100

    feat([sc-6360]): refactor SignTypedDataService

--- apps/nfid-frontend/src/features/embed-controller/services/sign-typed-data.ts
+++ apps/nfid-frontend/src/features/embed-controller/services/sign-typed-data.ts
@@ -1,6 +1,8 @@
-import { ethers } from "ethers"
-
-import { ecdsaSigner, EthWallet, replaceActorIdentity } from "@nfid/integration"
+import {
+  ecdsaSigner,
+  nfidEthWallet,
+  replaceActorIdentity,
+} from "@nfid/integration"
 
 import { RPCResponse, RPC_BASE } from "frontend/features/embed/rpc-service"
 import { getWalletDelegation } from "frontend/integration/facade/wallet"
@@ -21,23 +23,20 @@ export const SignTypedDataService = async ({
 
   console.debug("SignTypedDataService", { parsedMessage })
 
-  const rpcProvider = new ethers.providers.JsonRpcProvider(
-    "https://ethereum-goerli-rpc.allthatnode.com",
-  )
-  const nfidWallet = new EthWallet(rpcProvider)
-
-  try {
-    const result = await nfidWallet.signTypedData(parsedMessage)
-    console.debug("SignTypedDataService", { result })
-    return Promise.resolve({ ...RPC_BASE, id: rpcMessage.id, result })
-  } catch (e: any) {
-    console.error("SignTypedDataService", { e })
-    return Promise.resolve({
-      ...RPC_BASE,
-      id: rpcMessage.id,
-      // FIXME:
-      // define which errors could happen
-      error: { code: -1, message: e.message, data: e },
-    })
-  }
+  return new Promise(async (resolve) => {
+    try {
+      const result = await nfidEthWallet.signTypedData(parsedMessage)
+      console.debug("SignTypedDataService", { result })
+      return resolve({ ...RPC_BASE, id: rpcMessage.id, result })
+    } catch (e: any) {
+      console.error("SignTypedDataService", { e })
+      return resolve({
+        ...RPC_BASE,
+        id: rpcMessage.id,
+        // FIXME:
+        // define which errors could happen
+        error: { code: -1, message: e.message, data: e },
+      })
+    }
+  })
 }

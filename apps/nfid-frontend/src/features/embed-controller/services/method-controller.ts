dfffd1899b1a43eb5e7fe9c78d601798d8ec96d7
Pavlo Chorniy <pavlo@identitylabs.ooo>
2023-03-08 13:26:25 +0100

    :wq eat([sc-6362]): lazy mint

--- apps/nfid-frontend/src/features/embed-controller/services/method-controller.ts
+++ apps/nfid-frontend/src/features/embed-controller/services/method-controller.ts
@@ -6,7 +6,8 @@ export const MethodControllerService = async ({
   let dataString: string
   if (rpcMessage?.params && rpcMessage.params.length > 1) {
     const data = JSON.parse(rpcMessage.params[1])
-    dataString = data.message.makeAsset.assetType.assetClass
+    if (data?.message?.tokenURI.length) dataString = "LazyMint"
+    else dataString = data.message.makeAsset.assetType.assetClass
   } else {
     dataString = rpcMessage?.params[0].data
   }
@@ -17,6 +18,7 @@ export const MethodControllerService = async ({
   if (dataString.startsWith("0x973bb640")) return "Sell"
   if (dataString.startsWith("0x27050d1f")) return "DeployCollection"
   if (dataString.startsWith("0x22a775b6")) return "Mint"
+  if (dataString.startsWith("LazyMint")) return "LazyMint"
 
   return "Success"
 }

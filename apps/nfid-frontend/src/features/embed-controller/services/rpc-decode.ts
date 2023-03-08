2419ff82dbf7fd6e6966076cb60e8cda37e32a9f
Pavlo Chorniy <pavlo@identitylabs.ooo>
2023-03-08 13:26:25 +0100

    :wq eat([sc-6362]): lazy mint

--- apps/nfid-frontend/src/features/embed-controller/services/rpc-decode.ts
+++ apps/nfid-frontend/src/features/embed-controller/services/rpc-decode.ts
@@ -10,6 +10,8 @@ export const decodeRPCRequestService = async ({
   try {
     if (rpcMessage?.params.length > 1) {
       const data = JSON.parse(rpcMessage?.params[1])
+      if (data?.message?.tokenURI.length) return data.message
+
       return await decodeTokenByAssetClass(
         data.message.makeAsset.assetType.assetClass,
         data.message.makeAsset.assetType.data,

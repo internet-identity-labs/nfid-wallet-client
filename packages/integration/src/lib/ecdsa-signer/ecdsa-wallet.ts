a835a6d428e2aec248933464e311a837871ae86c
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-03-09 16:49:31 +0100

    chore: update rpc provider url

--- packages/integration/src/lib/ecdsa-signer/ecdsa-wallet.ts
+++ packages/integration/src/lib/ecdsa-signer/ecdsa-wallet.ts
@@ -209,6 +209,6 @@ export class EthWallet<T = Record<string, ActorMethod>> extends Signer {
 }
 
 const rpcProvider = new ethers.providers.JsonRpcProvider(
-  "https://ethereum-goerli-rpc.allthatnode.com",
+  "https://eth-goerli.g.alchemy.com/v2/KII7f84ZxFDWMdnm_CNVW5hI8NfbnFhZ",
 )
 export const nfidEthWallet = new EthWallet(rpcProvider)

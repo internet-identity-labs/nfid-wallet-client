0a05695b55ce56a010f195d8a45642d33ef17fd9
olsemeno <33149496+olsemeno@users.noreply.github.com>
2023-03-14 10:29:48 +0200

    feat([sc-6298]): [Integration] Create a method to store all signature… (#1198)
    * feat([sc-6298]): [Integration] Create a method to store all signatures made by user
    
    * feat([sc-6298]): [Integration] Create a method to store all signatures made by user

--- apps/nfid-frontend/src/integration/lambda/signature-event/basic-event.ts
+++ apps/nfid-frontend/src/integration/lambda/signature-event/basic-event.ts
@@ -11,6 +11,19 @@ export interface TransferSignatureEvent extends BasicSignatureEvent {
   token: string
 }
 
+export interface TransferNftSignatureEvent extends BasicSignatureEvent {
+  from: string
+  to: string
+  token: string
+}
+
+export interface MessageSignatureEvent extends BasicSignatureEvent {
+  message: string
+  blockchainAddress: string
+}
+
 export enum SignatureEvent {
   TRANSFER = "transfer",
+  TRANSFER_NFT = "transfer_nft",
+  MESSAGE = "message",
 }
--- apps/nfid-frontend/src/integration/lambda/signature-event/index.spec.ts
+++ apps/nfid-frontend/src/integration/lambda/signature-event/index.spec.ts
@@ -2,7 +2,9 @@
  * @jest-environment jsdom
  */
 import {
+  MessageSignatureEvent,
   SignatureEvent,
+  TransferNftSignatureEvent,
   TransferSignatureEvent,
 } from "src/integration/lambda/signature-event/basic-event"
 import { storeSignatureEvent } from "src/integration/lambda/signature-event/index"
@@ -21,6 +23,37 @@ describe("Signature event suite", () => {
           to: "b",
           token: "ICP",
           value: 1,
+        }
+        let resp = await storeSignatureEvent(event)
+        expect(resp.status).toBe(200)
+      } catch (e) {
+        throw new Error("Should not fail")
+      }
+    })
+    it("sore nft transfer", async function () {
+      try {
+        const event: TransferNftSignatureEvent = {
+          application: "unit_test_FE",
+          chain: "Internet Computer",
+          eventType: SignatureEvent.TRANSFER_NFT,
+          from: "a",
+          to: "b",
+          token: "CONTRACT:TOKEN",
+        }
+        let resp = await storeSignatureEvent(event)
+        expect(resp.status).toBe(200)
+      } catch (e) {
+        throw new Error("Should not fail")
+      }
+    })
+    it("sore message", async function () {
+      try {
+        const event: MessageSignatureEvent = {
+          blockchainAddress: "something",
+          message: "message to sign",
+          application: "unit_test_FE",
+          chain: "Ethereum",
+          eventType: SignatureEvent.MESSAGE,
         }
         let resp = await storeSignatureEvent(event)
         expect(resp.status).toBe(200)

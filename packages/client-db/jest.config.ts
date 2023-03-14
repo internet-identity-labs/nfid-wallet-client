b446451e2849e1f9b675a2a3d078235b70f434c1
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-03-14 15:30:45 +0100

    chore(deps-dev): upgrade nx

--- packages/client-db/jest.config.ts
+++ packages/client-db/jest.config.ts
@@ -3,14 +3,14 @@ import { JEST_GLOBALS } from "../../config/jest-globals"
 export default {
   displayName: "client-db",
   preset: "../../jest.preset.js",
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.spec.json",
-    },
-    ...JEST_GLOBALS,
-  },
+  globals: { ...JEST_GLOBALS },
   transform: {
-    "^.+\\.[tj]s$": "ts-jest",
+    "^.+\\.[tj]s$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+      },
+    ],
   },
   moduleFileExtensions: ["ts", "js", "html"],
   coverageDirectory: "../../coverage/packages/client-db",

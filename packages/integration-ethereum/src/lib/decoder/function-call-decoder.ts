98c732cf104d9bcb1c327792301d5fd9cfa6bd18
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2023-03-21 14:33:57 +0400

    feat: [sc-6256] [Integration] parse transaction data to retrieve user readable information (#1284)

--- packages/integration-ethereum/src/lib/decoder/function-call-decoder.ts
+++ packages/integration-ethereum/src/lib/decoder/function-call-decoder.ts
@@ -1,7 +1,7 @@
-import { IRaribleSdk, createRaribleSdk } from "@rarible/sdk"
+import { createRaribleSdk } from "@rarible/sdk"
 import { Fragment } from "ethers/lib/utils"
 
-import { AssetDecoder } from "./asset-decoder/asset-decoder"
+import { RPCMessage } from "../decode-ethereum-function-call"
 import { erc721AssetDecoder } from "./asset-decoder/erc721-asset-decoder/erc721-asset-decoder"
 import { erc721AssetLazyDecoder } from "./asset-decoder/erc721-asset-decoder/erc721-asset-lazy-decoder"
 import { erc1155AssetDecoder } from "./asset-decoder/erc1155-asset-decoder/erc1155-asset-decoder"
@@ -15,81 +15,15 @@ import { createTokenPrivateMethodDecoder } from "./method-decoder/create-token/c
 import { createTokenPublicMethodDecoder } from "./method-decoder/create-token/create-token-public/create-token-public-method-decoder"
 import { directAcceptBidMethodDecoder } from "./method-decoder/direct-accept-bid/direct-accept-bid-method-decoder"
 import { directPurchaseMethodDecoder } from "./method-decoder/direct-purchase/direct-purchase-method-decoder"
-import {
-  MethodDecoder,
-  FunctionCall,
-  Method,
-} from "./method-decoder/method-decoder"
+import { FunctionCall, Method } from "./method-decoder/method-decoder"
 import { mintAndTransferMethodDecoder } from "./method-decoder/mint/mint-and-transfer-method-decoder"
 import { safeTransferFromMethodDecoder } from "./method-decoder/safe-transfer-from/safe-transfer-from-method-decoder"
-
-export class FunctionCallDecoder {
-  private sdk: IRaribleSdk
-  private methodDecoders: {
-    [key: string]: MethodDecoder
-  }
-  private assetDecoders: {
-    [key: string]: AssetDecoder
-  }
-
-  constructor(
-    sdk: IRaribleSdk,
-    methodDecoders: {
-      [key: string]: MethodDecoder
-    },
-    assetDecoders: {
-      [key: string]: AssetDecoder
-    },
-  ) {
-    this.sdk = sdk
-    this.methodDecoders = methodDecoders
-    this.assetDecoders = assetDecoders
-  }
-
-  decode(data: string): Promise<FunctionCall> {
-    const method = data.substring(0, 10)
-    const methodDecoder = this.methodDecoders[method]
-
-    if (!methodDecoder) {
-      throw new Error("No method decoder found")
-    }
-
-    const fragment = methodDecoder.getAbi() as Fragment
-    const decodedData = functionCallService.decode(data, fragment)
-    return methodDecoder.map(decodedData)
-  }
-
-  async decodeByAssetClass(
-    type: string,
-    data: string,
-    method: Method,
-  ): Promise<FunctionCall> {
-    const assetDecoder = this.assetDecoders[type]
-
-    if (!assetDecoder) {
-      throw new Error("No asset decoder found")
-    }
-
-    const tokenId = assetDecoder.map(data)
-    const [item, collection] = await Promise.all([
-      await this.sdk.apis.item.getItemById({
-        itemId: `ETHEREUM:${tokenId.collectionId}:${tokenId.tokenId}`,
-      }),
-      await this.sdk.apis.collection.getCollectionById({
-        collection: `ETHEREUM:${tokenId?.collectionId}`,
-      }),
-    ])
-
-    return {
-      interface: "Item",
-      method: method,
-      data: {
-        ...item,
-        collectionData: collection,
-      },
-    }
-  }
-}
+import { ethSendTransactionRpcMessageDecoder } from "./rpc-message-decoder/eth-sendtransaction-rpc-message-decoder"
+import {
+  RpcMessageDecoder,
+  RpcMessageFunctionalCall,
+} from "./rpc-message-decoder/rpc-message-decoder"
+import { signTypedDataV4RpcMessageDecoder } from "./rpc-message-decoder/sign-typed-data-v4-rpc-message-decoder."
 
 const sdk = createRaribleSdk(null, "testnet")
 const methodDecoders = dependencyService.group([
@@ -109,9 +43,66 @@ const assetDecoders = dependencyService.group([
   erc1155AssetDecoder,
   erc1155TokenLazyDecoder,
 ])
+const rpcMessageDecoders: { [key: string]: RpcMessageDecoder } = {
+  eth_sendTransaction: ethSendTransactionRpcMessageDecoder,
+  eth_signTypedData_v4: signTypedDataV4RpcMessageDecoder,
+}
 
-export const functionCallDecoder = new FunctionCallDecoder(
-  sdk,
-  methodDecoders,
-  assetDecoders,
-)
+export const functionCallDecoder = {
+  decode(data: string): Promise<FunctionCall> {
+    const method = data.substring(0, 10)
+    const methodDecoder = methodDecoders[method]
+
+    if (!methodDecoder) {
+      throw new Error("No method decoder found")
+    }
+
+    const fragment = methodDecoder.getAbi() as Fragment
+    const decodedData = functionCallService.decode(data, fragment)
+    return methodDecoder.map(decodedData)
+  },
+
+  async decodeByAssetClass(
+    type: string,
+    data: string,
+    method: Method = "sell",
+  ): Promise<FunctionCall> {
+    const assetDecoder = assetDecoders[type]
+
+    if (!assetDecoder) {
+      throw new Error("No asset decoder found")
+    }
+
+    const tokenId = assetDecoder.map(data)
+    const [item, collection] = await Promise.all([
+      await sdk.apis.item.getItemById({
+        itemId: `ETHEREUM:${tokenId.collectionId}:${tokenId.tokenId}`,
+      }),
+      await sdk.apis.collection.getCollectionById({
+        collection: `ETHEREUM:${tokenId?.collectionId}`,
+      }),
+    ])
+
+    return {
+      interface: "Item",
+      method: method,
+      data: {
+        ...item,
+        collectionData: collection,
+      },
+    }
+  },
+
+  async decodeRpcMessage({
+    method,
+    params,
+  }: RPCMessage): Promise<RpcMessageFunctionalCall> {
+    const rpcMessageDecoder = rpcMessageDecoders[method]
+
+    if (!rpcMessageDecoder) {
+      throw new Error("No rpc message decoder found")
+    }
+
+    return rpcMessageDecoder.decode(params)
+  },
+}

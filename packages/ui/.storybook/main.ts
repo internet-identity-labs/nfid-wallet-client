cc4d3e072ee87407f6350bdc7861f61e6f5ec4cd
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-03-22 10:50:28 +0100

    chore: update storybook ui

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,32 +1,17 @@
-import type { StorybookConfig, Options } from "@storybook/core-common"
-
-import { rootMain } from "../../../.storybook/main"
+import type { StorybookConfig } from "@storybook/core-common"
 
 const config: StorybookConfig = {
-  ...rootMain,
-
-  core: { ...rootMain.core, builder: "webpack5" },
-
-  stories: [
-    ...rootMain.stories,
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  core: { builder: "webpack5" },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-essentials",
-    ...(rootMain.addons || []),
     "@nrwl/react/plugins/storybook",
+    "storybook-addon-react-router-v6",
   ],
-  webpackFinal: async (config, { configType }: Options) => {
-    // apply any global webpack configs that might have been specified in .storybook/main.ts
-    if (rootMain.webpackFinal) {
-      config = await rootMain.webpackFinal(config, { configType } as Options)
-    }
-
-    // add your own webpack tweaks if needed
-
-    return config
-  },
 }
 
 module.exports = config
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/packages/storybook/documents/custom-builder-configs

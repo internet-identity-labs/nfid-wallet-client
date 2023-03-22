a24dc4eb1b0ee549686f5395ba7155035032e6ce
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-03-22 10:50:28 +0100

    chore: configure tailwind

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,12 +1,16 @@
+const { createGlobPatternsForDependencies } = require("@nrwl/react/tailwind")
 const defaultTheme = require("tailwindcss/defaultTheme")
 const { join } = require("path")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [join(__dirname, "src/**/*!(*.stories|*.spec).{ts,tsx,html}")],
+  content: [
+    join(__dirname, "src/**/*!(*.stories|*.spec).{ts,tsx,html}"),
+    ...createGlobPatternsForDependencies(),
+  ],
   plugins: [
-    require("../../packages/ui-tailwind-core"),
     require("../../packages/ui-tailwind-core/src/forms"),
+    require("../../packages/ui-tailwind-core"),
     require("tailwindcss-radix")(),
   ],
   theme: {

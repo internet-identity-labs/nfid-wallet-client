a32d5eb9e90360d1090f7dcdd5c81e3ec7112f02
Pavlo Chorniy <pavlo@identitylabs.ooo>
2023-03-28 11:14:12 +0200

    feat([sc-6069]): eth account sheet

--- apps/nfid-frontend/src/apps/identity-manager/profile/internet-computer-wallets/index.tsx
+++ apps/nfid-frontend/src/apps/identity-manager/profile/internet-computer-wallets/index.tsx
@@ -4,24 +4,28 @@ import { useBtcBalance } from "src/features/fungable-token/btc/hooks/use-btc-bal
 
 import { TokenStandards } from "@nfid/integration/token/types"
 
+import { useEthBalance } from "frontend/features/fungable-token/eth/hooks/use-eth-balances"
 import { useBalanceICPAll } from "frontend/features/fungable-token/icp/hooks/use-balance-icp-all"
 import TokenWalletsDetailPage from "frontend/ui/pages/new-profile/internet-computer-wallets"
 
 const ProfileTokenWalletsDetailPage = () => {
   const { appAccountBalance } = useBalanceICPAll()
   const { balances: btcSheet } = useBtcBalance()
-
+  const { balance: ethSheet } = useEthBalance()
   const { token } = useParams()
+
   const balance = useMemo(() => {
     if (!token) return undefined
 
     switch (token) {
       case TokenStandards.BTC:
         return btcSheet
+      case TokenStandards.ETH:
+        return ethSheet
       default:
         return appAccountBalance ? appAccountBalance[token] : undefined
     }
-  }, [appAccountBalance, btcSheet, token])
+  }, [appAccountBalance, btcSheet, ethSheet, token])
   console.debug(">> ProfileIWallets", { balance })
 
   return <TokenWalletsDetailPage balanceSheet={balance} />

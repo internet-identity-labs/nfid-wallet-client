ec2ac0cc1bb5f26ad74109cfe3b4f1ac224b68f4
Pavlo Chorniy <pavlo@identitylabs.ooo>
2023-04-05 16:41:34 +0200

    test: increased timeout of tests

--- apps/nfid-frontend-e2e/src/pages/profile.ts
+++ apps/nfid-frontend-e2e/src/pages/profile.ts
@@ -114,13 +114,13 @@ export class Profile extends HomePage {
       await browser.waitUntil(
         async () => (await this.tokens.length) === amount,
         {
-          timeout: 13000,
+          timeout: 30000,
           timeoutMsg: "Not all tokens displayed on user profile!",
         },
       )
     } else {
       await browser.waitUntil(async () => (await this.tokens.length) > 1, {
-        timeout: 13000,
+        timeout: 30000,
         timeoutMsg: "Tokens are not displayed on user profile!",
       })
     }

814e4161322b33c922f6b613b4867ab8955bf1b7
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2023-04-06 09:55:57 +0400

    fix: [sc-6735] [Integration] NFID provider handle estimate gas (#1302)

--- apps/nfid-frontend/src/features/embed/coordinator.tsx
+++ apps/nfid-frontend/src/features/embed/coordinator.tsx
@@ -55,6 +55,7 @@ export default function NFIDEmbedCoordinator() {
           authSession={state.context.authSession}
           rpcMessage={state.context.rpcMessage}
           rpcMessageDecoded={state.context.rpcMessageDecoded}
+          populatedTransaction={state.context.populatedTransaction}
           onConfirm={(data) => {
             console.debug("onConfirm", { data })
             send({ type: "APPROVE", data })

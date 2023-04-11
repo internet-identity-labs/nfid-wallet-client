daef1fbd9be50c17655967eb786a17b4d69591bc
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-04-11 15:58:30 +0200

    chore: fix redirect

--- apps/nfid-frontend/src/boot.ts
+++ apps/nfid-frontend/src/boot.ts
@@ -7,7 +7,7 @@ export const initializeConsoleWarnings = () => {
 
 export const redirectFromCanisters = () => {
   const productionHost = "3y5ko-7qaaa-aaaal-aaaaq-cai.ic0.app"
-  const devHost = "n2mln-sqaaa-aaaag-abjoa-cai"
+  const devHost = "n2mln-sqaaa-aaaag-abjoa-cai.ic0.app"
 
   if (window.location.host === productionHost)
     return (window.location.href = "https://nfid.one")

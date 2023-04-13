a581f83f906c3a1b6b481886b4f053112d227212
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-04-13 12:55:55 +0200

    chore([sc-6484]): wait for data-cache-loaded

--- apps/nfid-frontend-e2e/src/steps/support/action/remove-indexed-db.ts
+++ apps/nfid-frontend-e2e/src/steps/support/action/remove-indexed-db.ts
@@ -1,4 +1,16 @@
 export default async () => {
+  const root = $("#root")
+
+  await root.waitUntil(
+    async function () {
+      return (await root.getAttribute("data-cache-loaded")) === "true"
+    },
+    {
+      timeout: 15000,
+      timeoutMsg: "expected data-cache-loaded to be true after 15s",
+    },
+  )
+
   return await browser.execute(function () {
     // @ts-ignore
     if (typeof this.resetAuthState === "function") {

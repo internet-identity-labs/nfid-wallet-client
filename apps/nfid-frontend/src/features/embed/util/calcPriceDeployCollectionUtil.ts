1db66633d88563ec723f4ce6a07a238137a6092c
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2023-04-27 13:47:34 +0400

    feat([sc-6919]): uI Logic Network fee limit [sc-6919] (#1373)

--- apps/nfid-frontend/src/features/embed/util/calcPriceDeployCollectionUtil.ts
+++ apps/nfid-frontend/src/features/embed/util/calcPriceDeployCollectionUtil.ts
@@ -12,23 +12,31 @@ export function calcPriceDeployCollection(
 ) {
   if (!rates["ETH"] || !populatedTransaction || !populatedTransaction[0])
     return {
-      fee: "0",
       feeUsd: "0",
+      fee: "0",
+      maxFeeUsd: "0",
+      maxFee: "0",
       isInsufficientFundsError: false,
       isNetworkIsBusyWarning: false,
     }
   const [transaction, err] = populatedTransaction
 
+  const gasPriceRaw = transaction.gasPrice
+  delete transaction["gasPrice"]
+
   const gasLimit = BigNumber.from(transaction?.gasLimit)
-  const maxFeePerGas = BigNumber.from(
-    transaction?.maxFeePerGas ?? transaction?.gasPrice,
-  )
-  const fee = gasLimit.mul(maxFeePerGas)
+  const gasPrice = BigNumber.from(gasPriceRaw ?? transaction?.maxFeePerGas)
+  const maxFeePerGas = BigNumber.from(transaction?.maxFeePerGas ?? gasPriceRaw)
+  const fee = gasLimit.mul(gasPrice).div(125).mul(100)
   const feeUsd = parseFloat(ethers.utils.formatEther(fee)) * rates["ETH"]
+  const maxFee = gasLimit.mul(maxFeePerGas)
+  const maxFeeUsd = parseFloat(ethers.utils.formatEther(maxFee)) * rates["ETH"]
 
   return {
     feeUsd: feeUsd.toFixed(2),
     fee: ethers.utils.formatEther(fee),
+    maxFeeUsd: maxFeeUsd.toFixed(2),
+    maxFee: ethers.utils.formatEther(maxFee),
     isInsufficientFundsError: err ?? ProviderError.INSUFICIENT_FUNDS === err,
     isNetworkIsBusyWarning: err ?? ProviderError.NETWORK_BUSY === err,
   }

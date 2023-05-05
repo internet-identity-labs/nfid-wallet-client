47d8d69938dd9115e3763e0c8796586c038ed422
sataninja <viktor.identitylabs.ooo>
2023-05-05 10:50:10 +0300

    feat([]): added retry and logs on step

--- apps/nfid-frontend-e2e/src/steps/when.ts
+++ apps/nfid-frontend-e2e/src/steps/when.ts
@@ -137,7 +137,11 @@ When(
 )
 
 When(
-  /^User is already authenticated by ([^"]*) anchor$/,
+  /^User is already authenticated by ([^"]*) anchor$/, {
+    wrapperOptions: {
+      retry: 3
+    },
+  },
   async function (anchor: number) {
     let testUser: TestUser = await userClient.takeStaticUserByAnchor(anchor)
 
@@ -149,6 +153,7 @@ When(
       }
     }, testUser.authstate)
     await HomePage.openPage("/profile/assets")
+    console.log("BROWSER logs: ", browser.getLogs("browser"))
   },
 )
 

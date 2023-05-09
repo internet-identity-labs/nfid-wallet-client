1b7509adcbfae942a49ee945c266d3ff129757a9
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-05-09 13:47:01 +0200

    feat([sc-6957]): remove package

--- packages/nfid-service-worker/src/lib/nfid-service-worker-nfid-service-worker.spec.ts
+++
@@ -1,9 +0,0 @@
-import { nfidServiceWorkerNfidServiceWorker } from "./nfid-service-worker-nfid-service-worker"
-
-describe("nfidServiceWorkerNfidServiceWorker", () => {
-  it("should work", () => {
-    expect(nfidServiceWorkerNfidServiceWorker()).toEqual(
-      "nfid-service-worker-nfid-service-worker",
-    )
-  })
-})
--- packages/nfid-service-worker/src/lib/nfid-service-worker-nfid-service-worker.ts
+++
@@ -1,3 +0,0 @@
-export function nfidServiceWorkerNfidServiceWorker(): string {
-  return "nfid-service-worker-nfid-service-worker"
-}

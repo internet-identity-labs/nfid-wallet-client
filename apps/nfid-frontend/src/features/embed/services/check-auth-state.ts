47dfd255d9d303ffde69fce4c8f9fbf5b595f82f
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-05-10 11:45:43 +0200

    fix([sc-7181]): sync authState in useAuthentication hook
    chore: simplify fromCache handler

--- apps/nfid-frontend/src/features/embed/services/check-auth-state.ts
+++ apps/nfid-frontend/src/features/embed/services/check-auth-state.ts
@@ -4,7 +4,7 @@ import { fetchProfile } from "frontend/integration/identity-manager"
 import { AuthSession } from "frontend/state/authentication"
 
 export const CheckAuthState = async (): Promise<AuthSession> => {
-  const { delegationIdentity } = await authState.loadCachedAuthSession()
+  const { delegationIdentity } = await authState.fromCache()
 
   if (!delegationIdentity)
     throw new Error("CheckAuthState: no auth session in cache")

f3b20019abcc6ee4191cb788de9024b396315145
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-05-11 18:14:35 +0200

    chore: configure browser

--- apps/nfid-frontend-e2e-new/wdio.conf.ts
+++ apps/nfid-frontend-e2e-new/wdio.conf.ts
@@ -60,8 +60,24 @@ export const config: Options.Testrunner = {
   //
   capabilities: [
     {
-      // capabilities for local browser web tests
-      browserName: "chrome", // or "firefox", "microsoftedge", "safari"
+      browserName: "chrome",
+      "goog:chromeOptions": {
+        args: [
+          "--no-sandbox",
+          // "--use-fake-device-for-media-stream",
+          "--disable-notifications",
+          "--disable-web-security",
+          "--allow-insecure-localhost",
+          "--disable-infobars",
+          "--disable-dev-shm-usage",
+          "--start-maximized",
+          "--ignore-certificate-errors",
+          "--disable-gpu",
+          // "--enable-features=NetworkService,NetworkServiceInProcess"
+          "--user-data-dir=chrome-user-data-dir",
+        ],
+      },
+      acceptInsecureCerts: true,
     },
   ],
   //

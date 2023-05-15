cd48cafd78496205751c88f6d7fbce0bc5c3ea6a
sataninja <viktor.identitylabs.ooo>
2023-05-15 13:15:57 +0300

    feat([sc-7240]): new config, sign in fix

--- apps/nfid-frontend-e2e-new/src/steps/then.ts
+++ apps/nfid-frontend-e2e-new/src/steps/then.ts
@@ -1,8 +1,14 @@
 import { Then } from "@wdio/cucumber-framework"
 
 import Profile from "../pages/profile.js"
+import waitForVisible from "./support/actions/waitForDisplayed.js"
 
 Then(/^NFID number is not zero$/, async () => {
   const actualNFID = await Profile.getNFIDnumber()
   expect(actualNFID).not.toBe("0")
 })
+
+Then(
+  /^I expect that element ([^"]*)? becomes( not)* displayed$/,
+  waitForVisible,
+)

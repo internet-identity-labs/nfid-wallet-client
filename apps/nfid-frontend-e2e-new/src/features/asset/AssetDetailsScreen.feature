9e9c279df2845d5108d4ba67ecce76915a102635
sataninja <viktor.identitylabs.ooo>
2023-05-16 18:46:27 +0300

    feat([sc-7240]): given, when, then steps added, support actions added

---
+++ apps/nfid-frontend-e2e-new/src/features/asset/AssetDetailsScreen.feature
@@ -0,0 +1,26 @@
+@assetdetails
+Feature:Fungible Asset Details
+  As a user, I want to see fungible assets details
+
+  @assetdetails1
+  Scenario Outline: User should be able to see <label> in asset details
+    Given User opens NFID site
+    And User is already authenticated by <anchor> anchor
+    And Tokens displayed on user assets
+    And Open asset with label <label>
+    Then Wait while <label> accounts calculated
+    Then <label> with <balance> <currency> in header
+    And NFID app account 1 with <balance> <currency> displayed
+    And 1 row in the table
+    And Identifiers are <principal> and <address>
+    Examples:
+      | label             | currency | balance    | principal | address | anchor |
+      | Bitcoin           | BTC      | 0.00006879 | 5qfm      | mn9c    | 25795  |
+      | Ethereum          | ETH      | 0.09664164 | 5qfm      | 0x00    | 25795  |
+      | FaucetToken       | FAU      | 1          | 5qfm      | 0x00    | 25795  |
+      | ChainLink Token   | LINK     | 20         | 5qfm      | 0x00    | 25795  |
+      | Test Token        | TST      | 1          | 5qfm      | 0x00    | 25795  |
+      | Matic             | MATIC    | 0.2        | 5qfm      | 0x00    | 25795  |
+      | Internet Computer | ICP      | 0.01       | ymhy      | 8f48    | 28542  |
+      | WICP              | WICP     | 0.01       | m5iz      | aaed    | 28565  |
+
--- apps/nfid-frontend-e2e-new/src/steps/when.ts
+++ apps/nfid-frontend-e2e-new/src/steps/when.ts
@@ -2,10 +2,109 @@ import { When } from "@wdio/cucumber-framework"
 
 import { baseUrl } from "../../wdio.conf.js"
 import userClient from "../helpers/accounts-service.js"
+import HomePage from "../pages/home-page.js"
 import Profile from "../pages/profile.js"
-import clickElement from "./support/actions/clickElement.js"
+import RecoveryPage from "../pages/recovery-page.js"
+import Vault from "../pages/vault.js"
+import Vaults from "../pages/vaults.js"
 
-When(/^I (click|doubleclick) on the (link|selector) ([^"]*)?$/, clickElement)
+import clickElement from "./support/actions/clickElement.js"
+import clearInputField from "./support/actions/clearInputField.js"
+import closeLastOpenedWindow from "./support/actions/closeLastOpenedWindow.js"
+import deleteCookies from "./support/actions/deleteCookies.js"
+import dragElement from "./support/actions/dragElement.js"
+import focusLastOpenedWindow from "./support/actions/focusLastOpenedWindow.js"
+import handleModal from "./support/actions/handleModal.js"
+import moveTo from "./support/actions/moveTo.js"
+import pause from "./support/actions/pause.js"
+import pressButton from "./support/actions/pressButton.js"
+import scroll from "./support/actions/scroll.js"
+import selectOption from "./support/actions/selectOption.js"
+import selectOptionByIndex from "./support/actions/selectOptionByIndex.js"
+import setCookie from "./support/actions/setCookie.js"
+import setInputField from "./support/actions/setInputField.js"
+import setPromptText from "./support/actions/setPromptText.js"
+
+When(/^User enters a captcha$/, async function () {
+  await HomePage.captchaPass()
+  await HomePage.waitForLoaderDisappear()
+  await HomePage.waitForLoaderDisappear()
+})
+
+When(/^User trusts this device$/, async () => {
+  await HomePage.iTrustThisDevice()
+  await browser.addVirtualAuthenticator("ctap2", "internal", true, true, true, true)
+  await HomePage.waitForLoaderDisappear()
+})
+
+When(/^It log's me in$/, async () => {
+  await HomePage.dontTrustThisDevice()
+  await HomePage.waitForLoaderDisappear()
+})
+
+When(/^Tokens displayed on user assets$/, async () => {
+  await Profile.waitForTokensAppear()
+})
+
+When(
+  /^([^"]*) NFT displayed on assets page and ([^"]*) at all$/,
+  async (amount: string, view: string) => {
+    await Profile.waitForNFTsAppear()
+    const nftAmount = await Profile.getNftAmount()
+    await nftAmount.waitUntil(async () =>
+      (await nftAmount).getText().then((l) => {
+        return l === amount
+      }),
+    )
+    const actualAmount = await Profile.getNftsLength()
+    if (parseInt(view) < 4) {
+      expect(view).toEqual(actualAmount)
+    } else {
+      expect(parseInt(view)).toBeGreaterThan(2)
+    }
+  },
+)
+
+When(/^User opens burger menu$/, async () => {
+  await HomePage.openHomeBurgerMenu()
+})
+
+When(/^User opens mobile profile menu$/, async () => {
+  await Profile.openMobileProfileMenu()
+})
+
+When(/^User opens profile menu$/, async () => {
+  await Profile.openProfileMenu()
+})
+
+When(/^User has account stored in localstorage$/, async () => {
+  const localStorage = await browser.getLocalStorageItem("account")
+  expect(localStorage.length).toBeGreaterThan(1)
+  expect(localStorage).toContain("account")
+})
+
+When(
+  /^User has authState$/,
+  //use this method to pick a random available user from the list
+  async function () {
+    for (let i = 0; i < userClient.userMap.size; i++) {
+      if (userClient.userMap.get(userClient.users[i]) === false) {
+        await userClient.takeUser(userClient.users[i])
+        this.testUser = userClient.users[i]
+      }
+    }
+
+    let testUser: TestUser = this.testUser
+
+    await browser.execute(function (authState: AuthState) {
+      // @ts-ignore
+      if (typeof this.setAuthState === "function") {
+        // @ts-ignore
+        this.setAuthState(authState)
+      }
+    }, testUser.authstate)
+  },
+)
 
 When(
   /^User is already authenticated with ?(?:(.*))?$/,
@@ -69,10 +168,159 @@ When(
   },
 )
 
-When(/^Tokens displayed on user assets$/, async () => {
-  await Profile.waitForTokensAppear()
+When(
+  /^User is already authenticated by ([^"]*) anchor$/,
+  {
+    wrapperOptions: {
+      retry: 2,
+    },
+  },
+  async function (anchor: number) {
+    const testUser: TestUser = await userClient.takeStaticUserByAnchor(anchor)
+
+    await browser.execute(function (authState: AuthState) {
+      // @ts-ignore
+      if (typeof this.setAuthState === "function") {
+        // @ts-ignore
+        this.setAuthState(authState)
+      }
+    }, testUser.authstate)
+    await HomePage.openPage("/profile/assets")
+  },
+)
+
+When(/^I open Vaults$/, async () => {
+  await Profile.openVaults()
+  await Profile.waitForLoaderDisappear()
 })
 
-When(/^User opens profile menu$/, async () => {
-  await Profile.openProfileMenu()
+When(/^I open Members tab$/, async () => {
+  await Vault.openMembersTab()
 })
+
+When(
+  /^I add new member to this vault with ([^"]*) and ([^"]*)$/,
+  async (name: string, address: string) => {
+    await Vault.addMember(name, address)
+  },
+)
+
+When(/^I create a new Vault with name ([^"]*)$/, async (vaultName: string) => {
+  await Vaults.createVault(vaultName)
+  await Vaults.waitForLoaderDisappear()
+})
+
+When(/^I click on vault with name ([^"]*)$/, async (vaultName: string) => {
+  await (await Vaults.getVaultByName(vaultName)).click()
+  await Vaults.waitForLoaderDisappear()
+})
+
+When(
+  /^I create a new wallet with name ([^"]*)$/,
+  async (walletName: string) => {
+    await Vault.addWallet(walletName)
+    await Vault.waitForLoaderDisappear()
+  },
+)
+
+When(/^I open Policies tab$/, async () => {
+  await Vault.openPoliciestab()
+})
+
+When(
+  /^I create new Policy for this vault with ([^"]*), ([^"]*) and ([^"]*) included$/,
+  async (walletName: string, greaterThan: number, approvers: number) => {
+    await Vault.addPolicy(walletName, greaterThan, approvers)
+  },
+)
+
+When(/^User opens Credentials$/, async () => {
+  await Profile.openCredentials()
+})
+
+When(/^User connects mobile number$/, async () => {
+  await Profile.connectMobilePhoneNumber()
+})
+
+When(/^User inputs a phone number (.*)$/, async (phoneNumber: string) => {
+  await Profile.inputAndVerifyPhoneNumber(phoneNumber)
+  await Profile.waitForLoaderDisappear()
+})
+
+When(/^Phone number error appears "(.*)"$/, async (errorMsg: string) => {
+  await $("#phone-number-error").waitForDisplayed({
+    timeout: 17000,
+    timeoutMsg: "Phone number error is missing",
+  })
+  expect(await $("#phone-number-error").getText()).toContain(errorMsg)
+})
+
+When(/^User enters pincode "(.*)"$/, async (pinCode: string) => {
+  await Profile.enterPin(pinCode)
+})
+
+When(/^Pin code error message appears "(.*)"$/, async (errorMsg: string) => {
+  await Profile.waitForLoaderDisappear()
+  await $("#pin-input-error").waitForDisplayed({
+    timeout: 15000,
+    timeoutMsg: "Pin Error message is not displayed",
+  })
+  const text = await $("#pin-input-error").getText()
+  await expect(text).toHaveText(errorMsg)
+})
+
+When(/^User goes to recover account with FAQ$/, async () => {
+  await HomePage.recoverAccountWithFAQ()
+  await HomePage.switchToWindow("last")
+})
+
+When(/^User authenticates with a phrase/, async function () {
+  for (let i = 0; i < userClient.userMap.size; i++) {
+    if (userClient.userMap.get(userClient.users[i]) === false) {
+      await userClient.takeUser(userClient.users[i])
+      this.testUserSeed = userClient.users[i]
+    }
+  }
+  await RecoveryPage.recoverAccountWithThePhrase(this.testUserSeed.seed)
+})
+
+When(/^I (click|doubleclick) on the (link|selector) ([^"]*)?$/, clickElement)
+
+When(/^I (add|set) "([^"]*)?" to the inputfield "([^"]*)?"$/, setInputField)
+
+When(/^I clear the inputfield "([^"]*)?"$/, clearInputField)
+
+When(/^I drag element "([^"]*)?" to element "([^"]*)?"$/, dragElement)
+
+When(/^I pause for (\d+)ms$/, pause)
+
+When(/^I set a cookie "([^"]*)?" with the content "([^"]*)?"$/, setCookie)
+
+When(/^I delete the cookie "([^"]*)?"$/, deleteCookies)
+
+When(/^I press "([^"]*)?"$/, pressButton)
+
+When(/^I (accept|dismiss) the (alertbox|confirmbox|prompt)$/, handleModal)
+
+When(/^I enter "([^"]*)?" into the prompt$/, setPromptText)
+
+When(/^I scroll to element "([^"]*)?"$/, scroll)
+
+When(/^I close the last opened (window|tab)$/, closeLastOpenedWindow)
+
+When(/^I focus the (previous|last) opened (window|tab)$/, focusLastOpenedWindow)
+
+When(
+  /^I select the (\d+)(st|nd|rd|th) option for element "([^"]*)?"$/,
+  selectOptionByIndex,
+)
+
+When(
+  /^I select the option with the (name|value|text) "([^"]*)?" for element "([^"]*)?"$/,
+  selectOption,
+)
+
+When(
+  /^I move to element "([^"]*)?"(?: with an offset of (\d+),(\d+))*$/,
+  moveTo,
+)

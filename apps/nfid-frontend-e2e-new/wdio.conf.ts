8e2ba3f4205c4281b11efd1852d31d194d6c16f5
sataninja <viktor.identitylabs.ooo>
2023-05-17 10:36:04 +0300

    feat([sc-7240]): google user path fix

--- apps/nfid-frontend-e2e-new/wdio.conf.ts
+++ apps/nfid-frontend-e2e-new/wdio.conf.ts
@@ -179,7 +179,7 @@ export const config: Options.Testrunner = {
     // <boolean> fail if there are any undefined or pending steps
     strict: false,
     // <string> (expression) only execute the features or scenarios with tags matching the expression
-    tagExpression: "",
+    tagExpression: "not @pending and not @mobile",
     // <number> timeout for step definitions
     timeout: 150000,
     // <boolean> Enable this config to treat undefined definitions as warnings.

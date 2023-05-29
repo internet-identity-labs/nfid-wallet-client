7668fa854e1d9531d7fe1167d50d57cca53f14a3
Pavlo Chorniy <pavlo@identitylabs.ooo>
2023-05-29 16:07:36 +0200

    fix: fixed profile freezing

--- apps/nfid-frontend/src/features/fungable-token/icp/hooks/use-user-balances.ts
+++ apps/nfid-frontend/src/features/fungable-token/icp/hooks/use-user-balances.ts
@@ -1,5 +1,3 @@
-import { useErc20 } from "src/features/fungable-token/erc-20/hooks/use-erc-20"
-import { useErc20Polygon } from "src/features/fungable-token/erc-20/hooks/use-erc-20-polygon"
 import useSWR from "swr"
 
 import { fetchBalances } from "frontend/features/fungable-token/fetch-balances"
@@ -10,23 +8,18 @@ import { useAllDip20Token } from "../../dip-20/hooks/use-all-token-meta"
 export const useUserBalances = () => {
   const { principals } = useAllPrincipals()
   const { token: dip20Token } = useAllDip20Token()
-  const { erc20 } = useErc20()
-  const { erc20: erc20Polygon } = useErc20Polygon()
+
   const {
     data: balances,
     isValidating: isLoading,
     mutate: refreshBalances,
   } = useSWR(
-    dip20Token && principals && erc20 && erc20Polygon
-      ? [principals, dip20Token, erc20, erc20Polygon, `AllBalanceRaw`]
-      : null,
+    dip20Token && principals ? [principals, dip20Token, `AllBalanceRaw`] : null,
     async ([principals, dip20Token]) => {
-      console.debug("AllBalanceRaw", { principals, dip20Token, erc20 })
+      console.debug("AllBalanceRaw", { principals, dip20Token })
       return await fetchBalances({
         principals,
         dip20Token,
-        erc20: erc20 ? erc20 : [],
-        erc20Polygon: erc20Polygon ? erc20Polygon : [],
       })
     },
     { dedupingInterval: 30_000, refreshInterval: 60_000 },

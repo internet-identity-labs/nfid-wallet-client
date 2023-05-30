bea05524447903dab88ee767c66d0dd43d7cd1d3
Pavlo Chorniy <pavlo@identitylabs.ooo>
2023-05-30 17:08:57 +0200

    test: fixed unit to support not e8s

--- packages/integration/src/lib/asset/asset-polygon.spec.ts
+++ packages/integration/src/lib/asset/asset-polygon.spec.ts
@@ -1,6 +1,5 @@
 import { BigNumber } from "@rarible/utils"
 
-import { E8S } from "../token/icp"
 import { polygonAsset } from "./asset-polygon"
 import { ChainBalance } from "./types"
 
@@ -59,6 +58,7 @@ describe("Polygon Asset", () => {
     const actual = await polygonAsset.getErc20TokensByUser({
       identity: "0x382901144a77bec53493fa090053b9c63da5dd07",
     })
+    console.log(actual.tokens[0].balance)
     expect(actual).toEqual({
       cursor: undefined,
       tokens: [
@@ -66,7 +66,7 @@ describe("Polygon Asset", () => {
           name: "USD Coin",
           symbol: "USDC",
           logo: undefined,
-          balance: BigInt(10000000000 / E8S),
+          balance: "100.0",
           contractAddress: "0xe097d6b3100777dc31b34dc2c58fb524c2e76921",
           address: "0x382901144a77bec53493fa090053b9c63da5dd07",
           balanceinUsd: expect.any(String),

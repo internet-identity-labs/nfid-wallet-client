c67cbc5a8c95a038f8578a549d485ed089dab15d
olsemeno <33149496+olsemeno@users.noreply.github.com>
2023-06-19 14:53:47 +0300

    feat([sc-7300]): Implement Global Keys (#1446)
    * feat([sc-7300]): Implement NFID wallet solution without Internet Identity
    
    * feat([sc-7300]): Implement NFID wallet solution without Internet Identity
    
    * Update packages/integration/src/lib/lambda/ecdsa.ts
    
    Co-authored-by: Philipp Litzenberger <philipp@identitylabs.ooo>
    
    * feat([sc-7300]): Implement NFID wallet solution without Internet Identity
    
    ---------
    
    Co-authored-by: Philipp Litzenberger <philipp@identitylabs.ooo>

--- packages/integration/src/lib/lambda/ecdsa.spec.ts
+++ packages/integration/src/lib/lambda/ecdsa.spec.ts
@@ -13,19 +13,28 @@ import { ethers } from "ethers"
 import { arrayify, hashMessage } from "ethers/lib/utils"
 import fetch from "node-fetch"
 
+import { WALLET_SCOPE } from "@nfid/config"
 import {
   ecdsaSigner,
   generateDelegationIdentity,
+  ii,
+  im,
   replaceActorIdentity,
 } from "@nfid/integration"
 
-import { Chain, getPublicKey, registerECDSA, signECDSA } from "./ecdsa"
+import {
+  Chain,
+  getPublicKey,
+  registerECDSA,
+  getGlobalKeys,
+  signECDSA,
+} from "./ecdsa"
 
 const identity: JsonnableEd25519KeyIdentity = [
   "302a300506032b65700321003b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29",
   "00000000000000000000000000000000000000000000000000000000000000003b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29",
 ]
-describe.skip("Lambda Sign/Register ECDSA", () => {
+describe("Lambda Sign/Register ECDSA", () => {
   jest.setTimeout(50000)
 
   describe("lambdaECDSA", () => {
@@ -58,6 +67,31 @@ describe.skip("Lambda Sign/Register ECDSA", () => {
       const txx = Transaction.fromHex(signedTxHex)
       expect(txx.ins.length).toEqual(1)
       expect(txx.outs[0].value).toEqual(10)
+    })
+
+    it("get global IC keys", async function () {
+      const mockedIdentity = Ed25519KeyIdentity.fromParsedJson(identity)
+      const sessionKey = Ed25519KeyIdentity.generate()
+      const globalICIdentity = await getGlobalKeys(
+        mockedIdentity,
+        sessionKey,
+        Chain.IC,
+        ["74gpt-tiaaa-aaaak-aacaa-cai"],
+      )
+      expect(globalICIdentity.getPrincipal().toText()).toEqual(
+        "5froz-eldwx-manjh-jgfni-kzgqa-eah4z-mgs4t-ozk3e-57b6o-jrsn4-bae",
+      )
+      await replaceActorIdentity(ii, globalICIdentity)
+      try {
+        await ii.get_principal(BigInt(1), WALLET_SCOPE)
+      } catch (e: any) {
+        expect(e.message).toContain("Forbidden")
+      }
+      try {
+        await im.get_account()
+      } catch (e) {
+        throw Error("Should not fail")
+      }
     })
   })
 })

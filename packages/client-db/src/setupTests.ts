d1eb0c7724059724035b11cc714d25a15f4d7470
Dmitrii <dmitrii@identityLabs.ooo>
2023-06-29 17:52:06 +0400

    feat([sc-7307]): backend Create a mechanism for email validation [sc-7307]

--- packages/client-db/src/setupTests.ts
+++ packages/client-db/src/setupTests.ts
@@ -1,5 +1,8 @@
 import "fake-indexeddb/auto"
-import { TextEncoder } from "util"
+import { TextEncoder, TextDecoder } from "util"
 import "whatwg-fetch"
 
 global.TextEncoder = TextEncoder
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+global.TextDecoder = TextDecoder

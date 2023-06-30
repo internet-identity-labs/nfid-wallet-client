2f1ed1cca7734da98b422a858acd658cc91998f5
olsemeno <sendtosemenov@gmail.com>
2023-06-30 12:32:49 +0200

    feat([sc-7515]): Implement 2FA Key and Access management to reflect our new 'single-delegation' architecture

--- packages/integration/src/lib/lambda/ecdsa.spec.ts
+++ packages/integration/src/lib/lambda/ecdsa.spec.ts
@@ -1,34 +1,16 @@
 /**
  * @jest-environment jsdom
  */
-import { DelegationIdentity, Ed25519KeyIdentity } from "@dfinity/identity"
-import { JsonnableEd25519KeyIdentity } from "@dfinity/identity/lib/cjs/identity/ed25519"
-import {
-  networks,
-  payments,
-  Transaction,
-  TransactionBuilder,
-} from "bitcoinjs-lib"
-import { ethers } from "ethers"
-import { arrayify, hashMessage } from "ethers/lib/utils"
+import {DelegationChain, DelegationIdentity, Ed25519KeyIdentity} from "@dfinity/identity"
+import {JsonnableEd25519KeyIdentity} from "@dfinity/identity/lib/cjs/identity/ed25519"
+import {networks, payments, Transaction, TransactionBuilder,} from "bitcoinjs-lib"
+import {ethers} from "ethers"
+import {arrayify, hashMessage} from "ethers/lib/utils"
 import fetch from "node-fetch"
 
-import { WALLET_SCOPE } from "@nfid/config"
-import {
-  ecdsaSigner,
-  generateDelegationIdentity,
-  ii,
-  im,
-  replaceActorIdentity,
-} from "@nfid/integration"
-
-import {
-  Chain,
-  getPublicKey,
-  registerECDSA,
-  getGlobalKeys,
-  signECDSA,
-} from "./ecdsa"
+import {Chain, ecdsaSign, getGlobalKeys, getPublicKey,} from "./ecdsa"
+import {ii, im, replaceActorIdentity} from "@nfid/integration";
+import {WALLET_SCOPE} from "@nfid/config";
 
 const identity: JsonnableEd25519KeyIdentity = [
   "302a300506032b65700321003b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29",
@@ -40,21 +22,32 @@ describe("Lambda Sign/Register ECDSA", () => {
   describe("lambdaECDSA", () => {
     it("register ecdsa ETH", async function () {
       const mockedIdentity = Ed25519KeyIdentity.generate()
-      const delegationIdentity: DelegationIdentity =
-        await generateDelegationIdentity(mockedIdentity)
-      await replaceActorIdentity(ecdsaSigner, delegationIdentity)
-      const publicKey = await registerECDSA(delegationIdentity, Chain.ETH)
+      const sessionKey = Ed25519KeyIdentity.generate()
+      const chainRoot = await DelegationChain.create(
+        mockedIdentity,
+        sessionKey.getPublicKey(),
+        new Date(Date.now() + 3_600_000 * 44),
+        {},
+      )
+      const di =  DelegationIdentity.fromDelegation(sessionKey, chainRoot);
+      const pubKey = await getPublicKey(di, Chain.ETH)
       const keccak = hashMessage("test_message")
-      const signature = await signECDSA(keccak, delegationIdentity, Chain.ETH)
+      const signature = await ecdsaSign(keccak, di,  Chain.ETH)
       const digestBytes = arrayify(keccak)
       const pk = ethers.utils.recoverPublicKey(digestBytes, signature)
-      expect(pk).toEqual(publicKey)
+      expect(pk).toEqual(pubKey)
     })
 
     it("register ecdsa BTC", async function () {
       const mockedIdentity = Ed25519KeyIdentity.fromParsedJson(identity)
-      const delegationIdentity: DelegationIdentity =
-        await generateDelegationIdentity(mockedIdentity)
+      const sessionKey = Ed25519KeyIdentity.generate()
+      const chainRoot = await DelegationChain.create(
+        mockedIdentity,
+        sessionKey.getPublicKey(),
+        new Date(Date.now() + 3_600_000 * 44),
+        {},
+      )
+      const delegationIdentity =  DelegationIdentity.fromDelegation(sessionKey, chainRoot);
       const publicKey = await getPublicKey(delegationIdentity, Chain.BTC)
       const { address } = payments.p2pkh({
         pubkey: Buffer.from(publicKey, "hex"),
@@ -63,7 +56,7 @@ describe("Lambda Sign/Register ECDSA", () => {
       expect(address).toEqual("mujCjK6xVJJYfkVp1u4WVvv8i3LE86giqc")
       const tx = await calc("mujCjK6xVJJYfkVp1u4WVvv8i3LE86giqc")
       const hex = tx.buildIncomplete().toHex()
-      const signedTxHex = await signECDSA(hex, delegationIdentity, Chain.BTC)
+      const signedTxHex = await ecdsaSign(hex, delegationIdentity, Chain.BTC)
       const txx = Transaction.fromHex(signedTxHex)
       expect(txx.ins.length).toEqual(1)
       expect(txx.outs[0].value).toEqual(10)
--- packages/integration/src/lib/lambda/ecdsa.ts
+++ packages/integration/src/lib/lambda/ecdsa.ts
@@ -1,43 +1,14 @@
-import { Cbor, QueryFields, SignIdentity } from "@dfinity/agent"
-import { IDL } from "@dfinity/candid"
-import { toHexString } from "@dfinity/candid/lib/cjs/utils/buffer"
-import {
-  DelegationChain,
-  DelegationIdentity,
-  Ed25519KeyIdentity,
-} from "@dfinity/identity"
+import {SignIdentity} from "@dfinity/agent"
+import {DelegationChain, DelegationIdentity, Ed25519KeyIdentity} from "@dfinity/identity"
 
-import { ONE_MINUTE_IN_MS } from "@nfid/config"
-
-import { KeyPair } from "../_ic_api/ecdsa-signer.d"
-import { btcSigner, ecdsaSigner, replaceActorIdentity } from "../actors"
-import { ic } from "../agent/index"
-import { getTransformedRequest } from "./util"
+import {ONE_MINUTE_IN_MS} from "@nfid/config"
+import {btcSigner, ecdsaSigner, replaceActorIdentity} from "../actors"
+import {ic} from "../agent/index"
 
 export enum Chain {
   BTC = "BTC",
   ETH = "ETH",
   IC = "IC",
-}
-
-export async function registerECDSA(
-  identity: DelegationIdentity,
-  chain: Chain,
-): Promise<string> {
-  const url = ic.isLocal ? `/ecdsa_register` : AWS_ECDSA_REGISTER
-  const signer = defineSigner(chain)
-  await replaceActorIdentity(signer, identity)
-  const request = await getRegisterRequest(identity, chain)
-  return await fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(request),
-  }).then(async (response) => {
-    if (!response.ok) throw new Error(await response.text())
-    const kp: KeyPair = await response.json()
-    await signer.add_kp(kp)
-    return kp.public_key
-  })
 }
 
 export async function getGlobalKeys(
@@ -57,15 +28,12 @@ export async function getGlobalKeys(
     if (!response.ok) throw new Error(await response.text())
     return (await response.json()).public_key
   })
-
-  //delegate lambda to register global keys
   const delegationChainForLambda = await DelegationChain.create(
     identity,
     Ed25519KeyIdentity.fromParsedJson([lambdaPublicKey, ""]).getPublicKey(),
     new Date(Date.now() + ONE_MINUTE_IN_MS * 10),
   )
-  //prepare session key to get global delegation
-  const globalICDelegationRequest = {
+  const request = {
     chain,
     delegationChain: JSON.stringify(delegationChainForLambda.toJSON()),
     sessionPublicKey: sessionKey.toJSON()[0],
@@ -76,7 +44,7 @@ export async function getGlobalKeys(
   const chainResponse = await fetch(signUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(globalICDelegationRequest),
+    body: JSON.stringify(request),
   }).then(async (response) => {
     if (!response.ok) throw new Error(await response.text())
     return await response.json()
@@ -87,29 +55,37 @@ export async function getGlobalKeys(
   )
 }
 
-export async function getPublicKey(
+export async function ecdsaSign(keccak: string,
   identity: DelegationIdentity,
-  chain: Chain,
+  chain: Chain
 ): Promise<string> {
-  const signer = defineSigner(chain)
-  await replaceActorIdentity(signer, identity)
-  const response = await signer.get_kp()
-  if (response.key_pair.length === 0) {
-    return registerECDSA(identity, chain)
-  }
-  return response.key_pair[0].public_key
-}
-
-export async function signECDSA(
-  keccak: string,
-  identity: DelegationIdentity,
-  chain: Chain,
-) {
-  const url = ic.isLocal ? `/ecdsa_sign` : AWS_ECDSA_SIGN
-  const request = await getSignCBORRequest(identity, keccak, chain)
-  return await fetch(url, {
+  const registerUrl = ic.isLocal ? `/ecdsa_register` : AWS_ECDSA_REGISTER
+  const lambdaPublicKey = await fetch(registerUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chain,
+    }),
+  }).then(async (response) => {
+    if (!response.ok) throw new Error(await response.text())
+    return (await response.json()).public_key
+  })
+  const delegationChainForLambda = await DelegationChain.create(
+    identity,
+    Ed25519KeyIdentity.fromParsedJson([lambdaPublicKey, ""]).getPublicKey(),
+    new Date(Date.now() + ONE_MINUTE_IN_MS * 10),
+    { previous: identity.getDelegation()}
+  )
+  const request = {
+    chain,
+    delegationChain: JSON.stringify(delegationChainForLambda.toJSON()),
+    tempPublicKey: lambdaPublicKey,
+    keccak,
+  }
+  const signUrl = ic.isLocal ? `/ecdsa_sign` : AWS_ECDSA_SIGN
+  return await fetch(signUrl, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
     body: JSON.stringify(request),
   }).then(async (response) => {
     if (!response.ok) throw new Error(await response.text())
@@ -117,29 +93,63 @@ export async function signECDSA(
   })
 }
 
-export async function getSignCBORRequest(
+export async function ecdsaRegisterNewKeyPair(
   identity: DelegationIdentity,
-  keccak: string,
-  chain: Chain,
-) {
-  const fields: QueryFields = {
-    methodName: "get_kp",
-    arg: IDL.encode([IDL.Opt(IDL.Text)], [[]]),
-  }
-  const request: any = await getTransformedRequest(
+  chain: Chain
+): Promise<string> {
+  const registerUrl = ic.isLocal ? `/ecdsa_register` : AWS_ECDSA_REGISTER
+  const lambdaPublicKey = await fetch(registerUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chain,
+    }),
+  }).then(async (response) => {
+    if (!response.ok) throw new Error(await response.text())
+    return (await response.json()).public_key
+  })
+  const delegationChainForLambda = await DelegationChain.create(
     identity,
-    defineCanisterId(chain),
-    fields,
+    Ed25519KeyIdentity.fromParsedJson([lambdaPublicKey, ""]).getPublicKey(),
+    new Date(Date.now() + ONE_MINUTE_IN_MS * 10),
+    { previous: identity.getDelegation()}
   )
-  const cbor = Cbor.encode(request.body)
-  return {
-    cbor: toHexString(cbor),
-    keccak,
+  const request = {
     chain,
+    delegationChain: JSON.stringify(delegationChainForLambda.toJSON()),
+    tempPublicKey: lambdaPublicKey,
   }
+  const registerAddressUrl = ic.isLocal ? `/ecdsa_register_address` : AWS_ECDSA_REGISTER_ADDRESS
+  const response = await fetch(registerAddressUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(request),
+  }).then(async (response) => {
+    if (!response.ok) throw new Error(await response.text())
+    return await response.json()
+  })
+  return response.public_key
 }
 
-function defineSigner(chain: Chain) {
+//TODO maybe we need update call ???
+export async function getPublicKey(
+  identity: DelegationIdentity,
+  chain: Chain,
+): Promise<string> {
+  const signer = defineChainCanister(chain)
+  await replaceActorIdentity(signer, identity)
+  let response = await signer.get_kp()
+  if (response.key_pair.length === 0) {
+   await ecdsaRegisterNewKeyPair(identity, chain)
+   response = await signer.get_kp()
+    if (response.key_pair.length === 0) {
+      throw "Unable to preregister keys"
+    }
+  }
+  return response.key_pair[0].public_key
+}
+
+function defineChainCanister(chain: Chain) {
   switch (chain) {
     case Chain.ETH:
       return ecdsaSigner
@@ -147,36 +157,5 @@ function defineSigner(chain: Chain) {
       return btcSigner
     case Chain.IC:
       throw Error("Deprecated")
-  }
-}
-
-function defineCanisterId(chain: Chain) {
-  switch (chain) {
-    case Chain.ETH:
-      return ECDSA_SIGNER_CANISTER_ID
-    case Chain.BTC:
-      return BTC_SIGNER_CANISTER_ID
-    case Chain.IC:
-      throw Error("Deprecated")
-  }
-}
-
-export async function getRegisterRequest(
-  identity: DelegationIdentity,
-  chain: Chain,
-) {
-  const fields: QueryFields = {
-    methodName: "get_principal",
-    arg: IDL.encode([IDL.Opt(IDL.Text)], [[]]),
-  }
-  const request: any = await getTransformedRequest(
-    identity,
-    defineCanisterId(chain),
-    fields,
-  )
-  const body = Cbor.encode(request.body)
-  return {
-    cbor: toHexString(body),
-    chain,
   }
 }

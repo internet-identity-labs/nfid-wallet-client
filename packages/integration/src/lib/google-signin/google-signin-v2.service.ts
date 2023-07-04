abd34541ab69be3295398438689e91dcef8e66e1
Dmitrii <dmitrii@identityLabs.ooo>
2023-07-05 01:55:03 +0400

    feat([sc-7620]): backend Implement logic for signing in for [sc-7620]

---
+++ packages/integration/src/lib/google-signin/google-signin-v2.service.ts
@@ -0,0 +1,41 @@
+import {
+  DelegationChain,
+  DelegationIdentity,
+  Ed25519KeyIdentity,
+} from "@dfinity/identity"
+
+import { ic } from "../agent"
+
+const signinV2 = "/signin/v2"
+
+interface GoogleSigninV2Service {
+  signin(token: string): Promise<DelegationIdentity>
+}
+
+export const googleSigninV2Service: GoogleSigninV2Service = {
+  async signin(token: string): Promise<DelegationIdentity> {
+    const url = ic.isLocal ? signinV2 : AWS_SIGNIN_GOOGLE_V2
+
+    const ed25519KeyIdentity = Ed25519KeyIdentity.generate()
+    const publicKeyDerHex = ed25519KeyIdentity.toJSON()[0]
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ token, publicKey: publicKeyDerHex }),
+    })
+
+    const text = await response.text()
+    if (!response.ok) {
+      throw new Error(text)
+    }
+
+    const json = JSON.parse(text)
+    const delegationChain = DelegationChain.fromJSON(json.delegationChain)
+    const delegation = DelegationIdentity.fromDelegation(
+      ed25519KeyIdentity,
+      delegationChain,
+    )
+
+    return delegation
+  },
+}

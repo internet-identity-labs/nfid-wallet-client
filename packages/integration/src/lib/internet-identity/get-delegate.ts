c128b38be044cc748148281f52da32e180701309
olsemeno <33149496+olsemeno@users.noreply.github.com>
2023-07-05 16:40:28 +0300

    feat([sc-7649]): [Backend] API endpoint for new users to get an app-specific delegation (#1460)
    * feat([sc-7649]): [Backend] API endpoint for new users to get an app-specific delegation
    
    * feat([sc-7649]): change signature to use publicKey
    
    * feat([sc-7649]): add getAnonymousDelegate
    
    ---------
    
    Co-authored-by: Philipp Litzenberger <philipp@identitylabs.ooo>

--- packages/integration/src/lib/internet-identity/get-delegate.ts
+++ packages/integration/src/lib/internet-identity/get-delegate.ts
@@ -1,6 +1,9 @@
+import { DelegationIdentity } from "@dfinity/identity"
+
 import { PublicKey } from "../_ic_api/internet_identity.d"
 import { ii } from "../actors"
 import { mapOptional } from "../ic-utils"
+import { Chain, ecdsaGetAnonymous } from "../lambda/ecdsa"
 import { SignedDelegation } from "./types"
 
 /**
@@ -54,4 +57,25 @@ export async function getDelegateRetry(
     }
   }
   throw new Error(`Failed to retrieve a delegation after ${10} retries.`)
+}
+
+export const getAnonymousDelegate = async (
+  sessionPublicKey: Uint8Array,
+  delegationIdentity: DelegationIdentity,
+): Promise<SignedDelegation> => {
+  const delegationChain = await ecdsaGetAnonymous(
+    "nfid.one",
+    sessionPublicKey,
+    delegationIdentity,
+    Chain.IC,
+  )
+  const { delegation, signature } = delegationChain.delegations[0]
+  return {
+    delegation: {
+      expiration: delegation.expiration,
+      pubkey: Array.from(new Uint8Array(delegation.pubkey)),
+      targets: delegation.targets,
+    },
+    signature: Array.from(new Uint8Array(signature)),
+  }
 }

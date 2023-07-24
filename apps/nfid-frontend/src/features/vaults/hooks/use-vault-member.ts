c8d0e7a94f3e937c5fe6956c6ea786accf9f56c3
Pavlo Chorniy <pavlo@identitylabs.ooo>
2023-07-24 12:18:10 +0200

    feat([sc-7701]): vaults delegation new logic

--- apps/nfid-frontend/src/features/vaults/hooks/use-vault-member.ts
+++ apps/nfid-frontend/src/features/vaults/hooks/use-vault-member.ts
@@ -2,17 +2,14 @@ import { SignIdentity } from "@dfinity/agent"
 import { principalToAddress } from "ictool"
 import { useMemo } from "react"
 
-import { useProfile } from "frontend/integration/identity-manager/queries"
-
 import { useVaultDelegation } from "./use-vault-delegation"
 
 export const useVaultMember = () => {
-  const { profile } = useProfile()
   const {
     data: UserIIDelegation,
     isLoading,
     isValidating,
-  } = useVaultDelegation(profile?.anchor)
+  } = useVaultDelegation()
 
   const userAddress = useMemo(() => {
     if (!UserIIDelegation) return ""
@@ -25,7 +22,6 @@ export const useVaultMember = () => {
 
   return {
     address: userAddress,
-    anchor: profile?.anchor,
     identity: UserIIDelegation as SignIdentity,
     isLoading: isValidating,
     isReady: !isLoading,

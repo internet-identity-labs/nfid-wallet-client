5dafb0ca9e29480ae4be3b752c477f39695493a3
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-07-27 15:29:44 +0200

    feat([sc-7839]): passkey added

--- packages/integration/src/lib/tracking/security.ts
+++ packages/integration/src/lib/tracking/security.ts
@@ -1,11 +1,26 @@
 import posthog from "posthog-js"
 
+type PasskeyAdded = {
+  authenticatorAttachment: AuthenticatorAttachment
+  transports: AuthenticatorTransport[]
+  userPresent: boolean
+  userVerified: boolean
+  backupEligibility: boolean
+  backupState: boolean
+  name: string
+}
+
 export class SecurityTracking {
   addPasskey() {
     posthog.capture("Add passkey modal opened")
   }
   passkeyCreationInitiated(isMultiDevice: boolean) {
+    console.debug("passkeyCreationInitiated", { isMultiDevice })
     posthog.capture("Passkey creation initiated", { isMultiDevice })
+  }
+  passkeyAdded(data: PasskeyAdded) {
+    console.debug("SecurityTracking.passkeyAdded", { data })
+    posthog.capture("Passkey added", data)
   }
 }
 

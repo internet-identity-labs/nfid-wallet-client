337275d04c0a75c33bf7226e9e58e928448debf2
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-07-27 15:30:36 +0200

    feat([sc-7853]): add send token event

--- packages/integration/src/lib/tracking/send-receive.ts
+++ packages/integration/src/lib/tracking/send-receive.ts
@@ -1,10 +1,26 @@
 import posthog from "posthog-js"
 
+type SendToken = {
+  network: string
+  destinationType: "address" | "ENS"
+  tokenName: string
+  tokenType: "fungible" | "non-fungible"
+  tokenStandard: string
+  amount: string | number
+  fee: string
+}
+
 class SendReceiveTracking {
   openModal() {
     const title = "Send tokens modal opened"
     console.debug("SendReceiveTracking.openModal", { title })
     posthog.capture(title)
+  }
+
+  sendToken(data: SendToken) {
+    const title = "Send token"
+    console.debug("SendReceiveTracking.sendToken", { title, data })
+    posthog.capture(title, data)
   }
 }
 

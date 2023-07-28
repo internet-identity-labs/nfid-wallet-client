e5f84cd572f0a37b9285a3ed31280351fc2c6faa
Pavlo Chorniy <pavlo@identitylabs.ooo>
2023-07-28 13:28:05 +0200

    feat: create new landing

--- apps/nfid-frontend/src/apps/marketing/landing-page/index.tsx
+++ apps/nfid-frontend/src/apps/marketing/landing-page/index.tsx
@@ -8,8 +8,24 @@ import AnimationDark1 from "./assets/animations/1_dark.json"
 import AnimationDark2 from "./assets/animations/2_dark.json"
 import AnimationDark3 from "./assets/animations/3_dark.json"
 import AnimationDark4 from "./assets/animations/4_dark.json"
+import Discord from "./assets/new-landing/Discord.png"
+import Github from "./assets/new-landing/Github.png"
+import Twitter from "./assets/new-landing/Twitter.png"
+import Lock from "./assets/new-landing/lock.svg"
+import Blockchain from "./assets/new-landing/sponsors/blockchain.svg"
+import Dfinity from "./assets/new-landing/sponsors/dfinity.svg"
+import Flyrfy from "./assets/new-landing/sponsors/flyrfly.svg"
+import Outliers from "./assets/new-landing/sponsors/outliers.svg"
+import Polychain from "./assets/new-landing/sponsors/polychain.svg"
+import Rarible from "./assets/new-landing/sponsors/rarible.svg"
+import Rubylight from "./assets/new-landing/sponsors/rubylight.svg"
+import Spaceship from "./assets/new-landing/sponsors/spaceship.svg"
+import Tomahawk from "./assets/new-landing/sponsors/tomahawk.svg"
+import Yards from "./assets/new-landing/sponsors/yards.svg"
 import MainBanner from "./assets/photo.png"
+import PoweredBy from "./assets/poweredBy.svg"
 
+import "./index.css"
 import "./index.css"
 
 const container = "max-w-[1280px] mx-auto"
@@ -153,15 +169,84 @@ export const HomeScreen = () => {
           <p className="md:text-[42px] text-[32px] font-bold tracking-[0.32px] md:tracking-[0.42px]">
             Security isn’t a feature. It’s the foundation.
           </p>
-          <p className="text-base md:text-[28px] font-bold  md:font-light tracking-[0.16px] md:tracking-[0.28px] w-[800px] mx-auto">
+          <p className="text-base md:text-[28px] font-bold  md:font-light tracking-[0.16px] md:tracking-[0.28px] max-w-[800px] mx-auto mt-[18px]">
             Breakthroughs in cryptography make owning a self-sovereign digital
             identity easier than ever before.
           </p>
         </div>
-        <div className="mt-[27px] md:mt-[98px] grid grid-cols-2 md:gap-[30px] gap-[20px] ">
-          <div className="px-5 bg-gradient-to-r from-purple-50 to-blue-50 md:px-[74px] py-[50px] md:pt-[100px] md:pb-[120px]"></div>
+        <div className="mt-[27px] md:mt-[98px] grid grid-cols-1 md:grid-cols-2 md:gap-[30px] gap-[20px] ">
+          <div className="px-5 bg-gradient-to-r from-purple-50 to-blue-50 md:px-[74px] py-[50px] md:pt-[100px] md:pb-[120px] rounded-[30px]">
+            <img src={Lock} alt="" className="md:w-[140px] w-[100px]" />
+            <p className="mt-[50px] md:mt-[90px] font-bold text-xl md:text-[28px] tracking-[0.2px} md:tracking-[0.28px]">
+              Secured on decentralized architecture built by 100s of the world’s
+              best cryptographers.
+            </p>
+          </div>
+          <div className="px-5 bg-gradient-to-r from-purple-50 to-blue-50 md:px-[74px] py-[50px] md:pt-[100px] md:pb-[120px] rounded-[30px]">
+            <img src={Lock} alt="" />
+            <p className="mt-[50px] md:mt-[90px] font-bold text-xl md:text-[28px] tracking-[0.2px} md:tracking-[0.28px]">
+              Secured on decentralized architecture built by 100s of the world’s
+              best cryptographers.
+            </p>
+          </div>
+          <div className="px-5 bg-gradient-to-r from-purple-50 to-blue-50 md:px-[74px] py-[50px] md:pt-[100px] md:pb-[120px] rounded-[30px]">
+            <img src={Lock} alt="" />
+            <p className="mt-[50px] md:mt-[90px] font-bold text-xl md:text-[28px] tracking-[0.2px} md:tracking-[0.28px]">
+              Secured on decentralized architecture built by 100s of the world’s
+              best cryptographers.
+            </p>
+          </div>
+          <div className="px-5 bg-gradient-to-r from-purple-50 to-blue-50 md:px-[74px] py-[50px] md:pt-[100px] md:pb-[120px] rounded-[30px]">
+            <img src={Lock} alt="" />
+            <p className="mt-[50px] md:mt-[90px] font-bold text-xl md:text-[28px] tracking-[0.2px} md:tracking-[0.28px]">
+              Secured on decentralized architecture built by 100s of the world’s
+              best cryptographers.
+            </p>
+          </div>
         </div>
       </section>
+      <section className="mt-[70px] md:mt-[165px]">
+        <p className="font-bold tracking-[0.32px] md:tracking-[0.42px] text-[32px] md:text-[42px]  text-center ">
+          NFID is backed by industry leaders
+        </p>
+        <div className="flex items-center justify-between mt-[78px] flex-wrap gap-10 md:gap-5">
+          <img src={Tomahawk} alt="" />
+          <img src={Polychain} alt="" />
+          <img src={Outliers} alt="" />
+          <img src={Spaceship} alt="" />
+        </div>
+        <div className="flex items-center justify-between mt-10 md:mt-[78px] flex-wrap gap-10 md:gap-5">
+          <img src={Dfinity} alt="" />
+          <img src={Blockchain} alt="" />
+          <img src={Flyrfy} alt="" />
+          <img src={Rubylight} alt="" />
+        </div>
+        <p className="font-bold md:text-xl tracking-[0.16px] md:tracking-[0.2px] mt-[100px] md:mt-[115px] text-center">
+          With angel investments from leaders at Rarible, 9Yards Capital, and
+          others
+        </p>
+        <div className="flex flex-wrap items-center justify-center gap-10 mt-10 md:gap-5">
+          <img src={Rarible} alt="" />
+          <img src={Yards} alt="" />
+        </div>
+      </section>
+      <section>
+        <div className="flex items-center gap-x-[15px] md:gap-x-5 justify-center mt-[120px]">
+          <img src={Twitter} alt="" />
+          <img src={Discord} alt="" />
+          <img src={Github} alt="" />
+        </div>
+        <div className="flex flex-wrap items-center justify-center gap-x-5 gap-y-4 mt-[26px] md:mt-8 pb-[25px] md:pb-[30px] border-b border-gray-200">
+          <p>© 2022 Internet Identity Labs, Inc</p>
+          <a href="#!">Terms of service</a>
+          <a href="#!">Privacy policy</a>
+        </div>
+      </section>
+      <div className="mt-[22px] md:mt-6 pb-6 flex justify-center">
+        <a href="https://smartcontracts.org" target="_blank" rel="noreferrer">
+          <img src={PoweredBy} alt="PoweredBy" />
+        </a>
+      </div>
     </div>
   )
 }

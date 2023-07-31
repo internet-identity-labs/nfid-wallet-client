e9afb1445657bf82486d9bf7d778ebe2e859871b
olsemeno <sendtosemenov@gmail.com>
2023-07-31 11:40:50 +0200

    feat: [sc-7604] [Integration] verifyTargetCanisterIds(derivationOrigin: string, targets: string[]): boolean

---
+++ packages/integration/src/lib/lambda/targets.spec.ts
@@ -0,0 +1,22 @@
+import {validateTargets} from "./targets";
+
+describe("Targets validation", () => {
+  jest.setTimeout(50000)
+
+  it("validate", async function () {
+    try {
+      await validateTargets(["txkre-oyaaa-aaaap-qa3za-cai"], "nfid.one")
+    }catch (e) {
+      fail("Should pass")
+    }
+  })
+
+  it("validate fail", async function () {
+    try {
+      await validateTargets(["txkre-oyaaa-aaaap-qa3za-cai"], "hernia.one")
+    }catch (e) {
+      expect((e as Error).message).toContain("Target canister does not support")
+    }
+  })
+
+})
---
+++ packages/integration/src/lib/lambda/targets.ts
@@ -0,0 +1,24 @@
+import {Actor, ActorSubclass, Agent, HttpAgent} from "@dfinity/agent";
+import {IDL} from "@dfinity/candid";
+
+
+export async function validateTargets(targets: string[], origin: string) {
+  const agent: Agent = await new HttpAgent({ host: "https://ic0.app" });
+  const idlFactory: IDL.InterfaceFactory = ({ IDL }) =>
+    IDL.Service({
+      get_origins: IDL.Func([], [IDL.Vec(IDL.Text)], []),
+    })
+  const promises = targets.map(async (canisterId) => {
+    const actor: ActorSubclass = Actor.createActor(idlFactory, {
+      agent,
+      canisterId,
+    });
+    const result = await actor["get_origins"]() as string[]
+    if (!result.includes(origin)) {
+      throw new Error(`Target canister does not support "${origin}"`)
+    }
+  });
+
+  await Promise.all(promises);
+}
+

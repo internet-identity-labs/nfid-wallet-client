57a5671638eb8c91d2b3b9289b242040d8a22e88
olsemeno <33149496+olsemeno@users.noreply.github.com>
2023-08-01 15:22:13 +0300

    feat: [sc-7604] [Integration] verifyTargetCanisterIds(derivationOrigi… (#1505)
    * feat: [sc-7604] [Integration] verifyTargetCanisterIds(derivationOrigin: string, targets: string[]): boolean
    
    * feat: [sc-7604] [Integration] verifyTargetCanisterIds(derivationOrigin: string, targets: string[]): boolean

---
+++ packages/integration/src/lib/lambda/targets.spec.ts
@@ -0,0 +1,21 @@
+import { validateTargets } from "./targets"
+
+describe("Targets validation", () => {
+  jest.setTimeout(50000)
+
+  it("validate", async function () {
+    try {
+      await validateTargets(["txkre-oyaaa-aaaap-qa3za-cai"], "nfid.one")
+    } catch (e) {
+      fail("Should pass")
+    }
+  })
+
+  it("validate fail", async function () {
+    try {
+      await validateTargets(["txkre-oyaaa-aaaap-qa3za-cai"], "hernia.one")
+    } catch (e) {
+      expect((e as Error).message).toContain("Target canister txkre-oyaaa-aaaap-qa3za-cai does not support")
+    }
+  })
+})

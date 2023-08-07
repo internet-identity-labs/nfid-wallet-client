c81753709686e15da5225b4ada57c9edd7c22405
Pavlo Chorniy <pavlo@identitylabs.ooo>
2023-08-07 23:10:53 +0200

    fix([sc-8062]): calculate fee before eth nft transfer

--- apps/nfid-frontend/src/features/transfer-modal/components/send-nft.tsx
+++ apps/nfid-frontend/src/features/transfer-modal/components/send-nft.tsx
@@ -132,6 +132,13 @@ export const TransferNFT = ({
       onTransferPromise({
         assetImg: selectedNFT?.assetPreview.url,
         initialPromise: new Promise(async (resolve) => {
+          await selectedConnector?.getFee({
+            to: getValues("to"),
+            contract: selectedNFT?.contractId ?? "",
+            tokenId: selectedNFT?.tokenId ?? "",
+            standard: selectedNFT?.collection.standard ?? "",
+          })
+
           const res = await selectedConnector.transfer({
             identity: await selectedConnector.getIdentity(
               selectedNFT.account.domain,
@@ -160,6 +167,7 @@ export const TransferNFT = ({
       })
     },
     [
+      getValues,
       handleTrackTransfer,
       onTransferPromise,
       refetchNFTs,

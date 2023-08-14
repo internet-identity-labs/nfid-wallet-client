1a66824a26c95b26cf693f0985a11a9aad3cfcd5
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2023-08-14 16:49:59 +0400

    feat: [sc-3758] [Backend] Validate email address on canister against Lambda thru outgoing http call (#1546)

--- apps/nfid-frontend/src/integration/identity-manager/index.spec.ts
+++ apps/nfid-frontend/src/integration/identity-manager/index.spec.ts
@@ -7,7 +7,7 @@ import {
   Ed25519KeyIdentity,
 } from "@dfinity/identity"
 
-import { im, im as imMock, RootWallet } from "@nfid/integration"
+import { im, im as imMock, replaceActorIdentity, RootWallet } from "@nfid/integration"
 
 import { Application } from "frontend/integration/_ic_api/identity_manager.d"
 import {
@@ -15,32 +15,35 @@ import {
   processApplicationOrigin,
   update2fa,
 } from "frontend/integration/identity-manager/index"
+import { getDelegationIdentity, getIdentity } from "../test-util"
 
 describe("Identity Manager suite", () => {
   jest.setTimeout(80000)
 
   describe("Identity Manager Service Test", () => {
     it("Should create NFID profile", async function () {
-      const mockedIdentity = Ed25519KeyIdentity.generate()
-      const sessionKey = Ed25519KeyIdentity.generate()
-      const chain = await DelegationChain.create(
-        mockedIdentity,
-        sessionKey.getPublicKey(),
-        new Date(Date.now() + 3_600_000 * 44),
-        {},
-      )
-      const delegationIdentity = DelegationIdentity.fromDelegation(
-        sessionKey,
-        chain,
-      )
+      const identityDevice = getIdentity("87654321876543218765432187654318")
+      const identityDeviceDelegationIdentity = await getDelegationIdentity(identityDevice)
+
+      const mockedIdentity = getIdentity("87654321876543218765432187654311")
+      const delegationIdentity = await getDelegationIdentity(mockedIdentity)
+
+      await replaceActorIdentity(im, identityDeviceDelegationIdentity)
+      // Optional disable.
+      try {
+        await update2fa(false)
+      } catch(e) {}
+
+      await replaceActorIdentity(im, delegationIdentity)
+      await im.remove_account()
+
       const nfidProfile = await createNFIDProfile(
         delegationIdentity,
-        "test@email.com",
+        "test@test.test",
       )
       expect(nfidProfile.anchor).not.toEqual(BigInt(0))
       expect(nfidProfile.wallet).toEqual(RootWallet.NFID)
       expect(nfidProfile.accessPoints.length).toEqual(1)
-      const identityDevice = Ed25519KeyIdentity.generate()
       const deviceData = {
         icon: "Icon",
         device: "Global",
@@ -54,6 +57,12 @@ describe("Identity Manager suite", () => {
       await im.create_access_point(deviceData)
       const updatedProfile = await update2fa(true)
       expect(updatedProfile.is2fa).toEqual(true)
+
+      await replaceActorIdentity(im, identityDeviceDelegationIdentity)
+      await update2fa(false)
+
+      await replaceActorIdentity(im, delegationIdentity)
+      await im.remove_account()
     })
 
     it("Should create application", async function () {

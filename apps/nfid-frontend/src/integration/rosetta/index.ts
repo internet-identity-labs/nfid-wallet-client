c087a488c1969465791f30eec78454b654911afc
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2023-08-22 16:50:34 +0400

    feat: [sc-8389] Freecurrconv invalid token. (#1572)

--- apps/nfid-frontend/src/integration/rosetta/index.ts
+++ apps/nfid-frontend/src/integration/rosetta/index.ts
@@ -1,3 +1,1 @@
-export const converter = `https://free.currconv.com/api/v7/convert?q=XDR_USD&compact=ultra&apiKey=${
-  CURRCONV_TOKEN ?? "df6440fc0578491bb13eb2088c4f60c7"
-}`
+export const converter = `https://free.currconv.com/api/v7/convert?q=XDR_USD&compact=ultra&apiKey=${CURRCONV_TOKEN}`

4be4e2a3a0eaa44d4b97ee07c83c97b7d939f2b6
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-09-20 14:04:34 +0200

    feat([sc-8792]): init global auth component

---
+++ apps/nfid-demo/src/hooks/useAuthentication.ts
@@ -0,0 +1,102 @@
+import { Identity } from "@dfinity/agent"
+import React from "react"
+import { useFieldArray, useForm } from "react-hook-form"
+
+import { useAuthenticationContext } from "../context/authentication"
+import { useButtonState } from "./useButtonState"
+
+export const useAuthentication = () => {
+  const { nfid, identity, setIdentity } = useAuthenticationContext()
+  const [error, setError] = React.useState<string>()
+
+  const [authButton, updateAuthButton] = useButtonState({
+    label: "Authenticate",
+  })
+
+  React.useEffect(() => {
+    if (nfid?.isAuthenticated) {
+      const identity = nfid.getIdentity()
+      updateAuthButton({ label: "Logout" })
+      setIdentity(identity as unknown as Identity)
+    }
+  }, [nfid, setIdentity, updateAuthButton])
+
+  const [renewDelegationButton, updateRenewDelegationButton] = useButtonState({
+    label: "Renew Delegation",
+  })
+  const { control, register } = useForm<{
+    items: { canisterId: string; id?: string }[]
+  }>()
+  const { fields, append, remove } = useFieldArray({
+    rules: { minLength: 4 },
+    control,
+    name: "items", // This should match the name of your array field
+  })
+
+  const targetCanisterIds = React.useMemo(
+    () => fields.map((field) => field.canisterId),
+    [fields],
+  )
+
+  const handleAuthenticate = React.useCallback(async () => {
+    setError(undefined)
+    if (!nfid) throw new Error("NFID not initialized")
+
+    console.debug("handleAuthenticate", { targetCanisterIds })
+    updateAuthButton({ loading: true, label: "Authenticating..." })
+    try {
+      const identity = await nfid.getDelegation(
+        targetCanisterIds.length ? { targets: targetCanisterIds } : undefined,
+      )
+      setIdentity(identity as unknown as Identity)
+      updateAuthButton({ loading: false, label: "Logout" })
+      return identity
+    } catch (error: any) {
+      console.debug("handleAuthenticate", { error })
+      updateAuthButton({ loading: false, label: "Authenticate" })
+      setError(error)
+    }
+  }, [nfid, setIdentity, targetCanisterIds, updateAuthButton])
+
+  const handleRenewDelegation = React.useCallback(async () => {
+    setError(undefined)
+    if (!nfid) throw new Error("NFID not initialized")
+
+    console.debug("handleRenewDelegation")
+    updateRenewDelegationButton({
+      loading: true,
+      label: "Refetching Delegation...",
+    })
+    try {
+      const identity = await nfid.renewDelegation({
+        targets: targetCanisterIds,
+      })
+      console.debug("handleRenewDelegation", { identity })
+      setIdentity(identity as unknown as Identity)
+      return identity
+    } catch (error: any) {
+      console.debug("handleRenewDelegation", { error })
+      setError(error.message)
+    }
+    updateRenewDelegationButton({ loading: false, label: "Renew Delegation" })
+  }, [nfid, setIdentity, targetCanisterIds, updateRenewDelegationButton])
+
+  return {
+    identity,
+    setIdentity,
+    error,
+    setError,
+    nfid,
+    fields,
+    register,
+    append,
+    remove,
+    authButton,
+    targetCanisterIds,
+    updateAuthButton,
+    renewDelegationButton,
+    updateRenewDelegationButton,
+    handleAuthenticate,
+    handleRenewDelegation,
+  }
+}

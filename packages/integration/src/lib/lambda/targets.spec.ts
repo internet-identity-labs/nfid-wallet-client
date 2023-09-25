fbd8162b97f5953c401b86f57552b87fda099bee
Dmitrii <dmitrii@identityLabs.ooo>
2023-09-25 18:05:57 +0400

    feat([sc-8634]): as a user I want my Global and Anonymous [sc-8634]

--- packages/integration/src/lib/lambda/targets.spec.ts
+++ packages/integration/src/lib/lambda/targets.spec.ts
@@ -5,7 +5,7 @@ describe("Targets validation", () => {
 
   it("validate", async function () {
     try {
-      await validateTargets(["txkre-oyaaa-aaaap-qa3za-cai"], "nfid.one")
+      await validateTargets(["irshc-3aaaa-aaaam-absla-cai"], "nfid.one")
     } catch (e) {
       fail("Should pass")
     }
@@ -13,10 +13,10 @@ describe("Targets validation", () => {
 
   it("validate fail", async function () {
     try {
-      await validateTargets(["txkre-oyaaa-aaaap-qa3za-cai"], "hernia.one")
+      await validateTargets(["irshc-3aaaa-aaaam-absla-cai"], "hernia.one")
     } catch (e) {
       expect((e as Error).message).toContain(
-        "Target canister txkre-oyaaa-aaaap-qa3za-cai does not support",
+        "Target canister irshc-3aaaa-aaaam-absla-cai does not support",
       )
     }
   })

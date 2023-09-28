ee68b7d047de59ad192befa90eadf82581b010a0
olsemeno <sendtosemenov@gmail.com>
2023-09-28 10:06:06 +0200

    feat [sc-9056]: [FE] Extend origins with 'get_trusted_origins_certified'

--- packages/integration/src/lib/lambda/targets.spec.ts
+++ packages/integration/src/lib/lambda/targets.spec.ts
@@ -5,26 +5,25 @@ import {IDL} from "@dfinity/candid";
 describe("Targets validation", () => {
   jest.setTimeout(50000)
 
-  // it("validate", async function () {
-  //   try {
-  //     await validateTargets(["irshc-3aaaa-aaaam-absla-cai"], "nfid.one")
-  //   } catch (e) {
-  //     fail("Should pass")
-  //   }
-  // })
-  //
-  // it("validate fail", async function () {
-  //   try {
-  //     await validateTargets(["irshc-3aaaa-aaaam-absla-cai"], "hernia.one")
-  //   } catch (e) {
-  //     expect((e as Error).message).toContain(
-  //       "Target canister irshc-3aaaa-aaaam-absla-cai does not support",
-  //     )
-  //   }
-  // })
-
-
   it("validate", async function () {
+    try {
+      await validateTargets(["irshc-3aaaa-aaaam-absla-cai"], "nfid.one")
+    } catch (e) {
+      fail("Should pass")
+    }
+  })
+
+  it("validate fail", async function () {
+    try {
+      await validateTargets(["irshc-3aaaa-aaaam-absla-cai"], "hernia.one")
+    } catch (e) {
+      expect((e as Error).message).toContain(
+        "Target canister irshc-3aaaa-aaaam-absla-cai does not support",
+      )
+    }
+  })
+
+  it("validate certified", async function () {
     const agent: Agent = await new HttpAgent({host: "https://ic0.app"})
     const idlFactory: IDL.InterfaceFactory = ({IDL}) =>
       IDL.Service({
--- packages/integration/src/lib/lambda/targets.ts
+++ packages/integration/src/lib/lambda/targets.ts
@@ -1,26 +1,30 @@
-import {Actor, ActorSubclass, Agent, HttpAgent} from "@dfinity/agent"
-import {IDL} from "@dfinity/candid"
+import { Actor, ActorSubclass, Agent, HttpAgent } from "@dfinity/agent"
+import { IDL } from "@dfinity/candid"
 
 export interface CertifiedResponse {
-  'certificate' : Uint8Array | number[],
-  'witness' : Uint8Array | number[],
-  'response' : Array<string>,
+  certificate: Uint8Array | number[]
+  witness: Uint8Array | number[]
+  response: Array<string>
 }
 export async function validateTargets(targets: string[], origin: string) {
-  const agent: Agent = await new HttpAgent({host: "https://ic0.app"})
-  const idlFactory: IDL.InterfaceFactory = ({IDL}) =>
+  const agent: Agent = await new HttpAgent({ host: "https://ic0.app" })
+  const idlFactory: IDL.InterfaceFactory = ({ IDL }) =>
     IDL.Service({
-      get_trusted_origins_certified: IDL.Func([], [
+      get_trusted_origins_certified: IDL.Func(
+        [],
+        [
           IDL.Record({
-            'certificate': IDL.Vec(IDL.Nat8),
-            'witness': IDL.Vec(IDL.Nat8),
-            'response': IDL.Vec(IDL.Text),
-          })],
-        ['query']),
+            certificate: IDL.Vec(IDL.Nat8),
+            witness: IDL.Vec(IDL.Nat8),
+            response: IDL.Vec(IDL.Text),
+          }),
+        ],
+        ["query"],
+      ),
       get_trusted_origins: IDL.Func([], [IDL.Vec(IDL.Text)], []),
     })
 
-  const uncertifiedTargets : string[] = [];
+  const uncertifiedTargets: string[] = []
 
   const promisesCertified = targets.map(async (canisterId) => {
     const actor: ActorSubclass = Actor.createActor(idlFactory, {
@@ -30,7 +34,9 @@ export async function validateTargets(targets: string[], origin: string) {
 
     let result
     try {
-      result = (await actor["get_trusted_origins_certified"]()) as CertifiedResponse
+      result = (await actor[
+        "get_trusted_origins_certified"
+      ]()) as CertifiedResponse
       if (!result || !result.response.includes(origin)) {
         uncertifiedTargets.push(canisterId)
       }

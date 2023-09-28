33aa35c07d14b3f5ac0d7ce082b7457f4c6922d4
olsemeno <sendtosemenov@gmail.com>
2023-09-28 10:05:06 +0200

    feat [sc-9056]: [FE] Extend origins with 'get_trusted_origins_certified'

--- packages/integration/src/lib/lambda/targets.ts
+++ packages/integration/src/lib/lambda/targets.ts
@@ -1,13 +1,47 @@
-import { Actor, ActorSubclass, Agent, HttpAgent } from "@dfinity/agent"
-import { IDL } from "@dfinity/candid"
+import {Actor, ActorSubclass, Agent, HttpAgent} from "@dfinity/agent"
+import {IDL} from "@dfinity/candid"
 
+export interface CertifiedResponse {
+  'certificate' : Uint8Array | number[],
+  'witness' : Uint8Array | number[],
+  'response' : Array<string>,
+}
 export async function validateTargets(targets: string[], origin: string) {
-  const agent: Agent = await new HttpAgent({ host: "https://ic0.app" })
-  const idlFactory: IDL.InterfaceFactory = ({ IDL }) =>
+  const agent: Agent = await new HttpAgent({host: "https://ic0.app"})
+  const idlFactory: IDL.InterfaceFactory = ({IDL}) =>
     IDL.Service({
+      get_trusted_origins_certified: IDL.Func([], [
+          IDL.Record({
+            'certificate': IDL.Vec(IDL.Nat8),
+            'witness': IDL.Vec(IDL.Nat8),
+            'response': IDL.Vec(IDL.Text),
+          })],
+        ['query']),
       get_trusted_origins: IDL.Func([], [IDL.Vec(IDL.Text)], []),
     })
-  const promises = targets.map(async (canisterId) => {
+
+  const uncertifiedTargets : string[] = [];
+
+  const promisesCertified = targets.map(async (canisterId) => {
+    const actor: ActorSubclass = Actor.createActor(idlFactory, {
+      agent,
+      canisterId,
+    })
+
+    let result
+    try {
+      result = (await actor["get_trusted_origins_certified"]()) as CertifiedResponse
+      if (!result || !result.response.includes(origin)) {
+        uncertifiedTargets.push(canisterId)
+      }
+    } catch (e) {
+      //not implemented - will try with the update call
+      uncertifiedTargets.push(canisterId)
+    }
+  })
+  await Promise.all(promisesCertified)
+
+  const promises = uncertifiedTargets.map(async (canisterId) => {
     const actor: ActorSubclass = Actor.createActor(idlFactory, {
       agent,
       canisterId,

285b7ccbae549c8b6d31691b52c31ae68648daf3
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-10-02 10:00:49 +0200

    feat([sc-6977]): change module

--- packages/config/src/lib/constants.ts
+++ packages/config/src/lib/constants.ts
@@ -6,10 +6,6 @@ export const WALLET_SESSION_TTL = BigInt(10 * 60 * 1e9)
 export const WALLET_SESSION_TTL_2_MIN_IN_NS = 2 * 60 * 1e9
 export const WALLET_SESSION_TTL_1_MIN_IN_MS = 60000
 
-export const ONE_SECOND_IN_MS = 1000
-export const ONE_MINUTE_IN_MS = 60 * ONE_SECOND_IN_MS
-export const TEN_MINUTES_IN_MS = 10 * ONE_MINUTE_IN_MS
-
 export const blockchains = [
   "Internet Computer",
   "Ethereum",

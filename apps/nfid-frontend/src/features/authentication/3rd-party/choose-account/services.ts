8e627a1f61cc00e5dc015e1414ab74c44ba31b03
chorniy8pavlo <pavlo@identitylabs.ooo>
2023-10-20 13:23:23 +0200

    fix: updated getPublicProfile

--- apps/nfid-frontend/src/features/authentication/3rd-party/choose-account/services.ts
+++ apps/nfid-frontend/src/features/authentication/3rd-party/choose-account/services.ts
@@ -1,16 +1,14 @@
 import { Ed25519KeyIdentity } from "@dfinity/identity"
-import { AccountIdentifier } from "@dfinity/ledger-icp"
+import { Principal } from "@dfinity/principal"
+import { principalToAddress } from "ictool"
 import { Chain, getPublicKey } from "packages/integration/src/lib/lambda/ecdsa"
 
-import { truncateString } from "@nfid-frontend/utils"
 import { authState, getBalance } from "@nfid/integration"
 
-import { toUSD } from "frontend/features/fungable-token/accumulate-app-account-balances"
 import { getExchangeRate } from "frontend/integration/rosetta/get-exchange-rate"
 import { e8sICPToString } from "frontend/integration/wallet/utils"
 
 export const getPublicProfile = async (): Promise<{
-  address: string
   balance: string
   balanceUSD: string
 }> => {
@@ -19,15 +17,17 @@ export const getPublicProfile = async (): Promise<{
 
   const publicKey = await getPublicKey(delegationIdentity!, Chain.IC)
   const publicDelegation = Ed25519KeyIdentity.fromParsedJson([publicKey, ""])
+  const principal = Principal.selfAuthenticating(
+    new Uint8Array(publicDelegation.getPublicKey().toDer()),
+  )
 
-  const principal = publicDelegation.getPrincipal()
-  const address = AccountIdentifier.fromPrincipal({ principal }).toHex()
+  const address = principalToAddress(principal as any)
   const balance = e8sICPToString(Number(await getBalance(address)))
   const exchangeRate = await getExchangeRate()
 
   return {
-    address: truncateString(principal.toText(), 6, 4),
     balance: balance,
-    balanceUSD: toUSD(Number(balance), exchangeRate),
+    balanceUSD:
+      balance === "0" ? "0" : `$${(exchangeRate * Number(balance)).toFixed(2)}`,
   }
 }

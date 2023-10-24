e000ebc47a9b23508adeb27dfb3f9bf8cd36520f
Daniil Bolotov <daniil@identitylabs.ooo>
2023-10-24 22:08:54 +0300

    feat([sc-9013]): Setup playground-staging.nfid.one [sc-9013]

--- scripts/cloudflare.sh
+++ scripts/cloudflare.sh
@@ -2,18 +2,19 @@
 
 echo "variable CF_PAGES_BRANCH: $CF_PAGES_BRANCH"
 
-# On production branch
-if [ "$CF_PAGES_BRANCH" = "production" ]; then
-  echo "Building production branch with .env.production"
-  npx env-cmd -f .env.production nx build nfid-frontend
-
-# On staging branch
-elif [ "$CF_PAGES_BRANCH" = "staging" ]; then
-  echo "Building staging branch with .env.staging"
-  npx env-cmd -f .env.staging nx build nfid-frontend
-
-# all other branches
-else
-  echo "Building branch: $CF_PAGES_BRANCH with .env.dev"
-  npx env-cmd -f .env.dev nx build nfid-frontend
-fi
+case ${CF_PAGES_BRANCH} in
+    staging)
+        # On staging branch
+        echo "Building staging branch with .env.staging"
+        npx env-cmd -f .env.staging nx build nfid-frontend
+        ;;
+    production)
+        # On production branch
+        echo "Building production branch with .env.production"
+        npx env-cmd -f .env.production nx build nfid-frontend
+        ;;
+    *)
+        # all other branches
+        echo "Building branch: $CF_PAGES_BRANCH with .env.dev"
+        npx env-cmd -f .env.dev nx build nfid-frontend
+esac

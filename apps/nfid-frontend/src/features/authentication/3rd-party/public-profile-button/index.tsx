eb97c2b5860a456f2e067624c247fef6d3114ed2
chorniy8pavlo <pavlo@identitylabs.ooo>
2023-10-25 13:11:23 +0200

    feat([sc-9402]): auth choose account screen

--- apps/nfid-frontend/src/features/authentication/3rd-party/public-profile-button/index.tsx
+++ apps/nfid-frontend/src/features/authentication/3rd-party/public-profile-button/index.tsx
@@ -1,54 +1,41 @@
 import clsx from "clsx"
-import User from "src/assets/userpics/userpic_6.svg"
 import useSWR from "swr"
 
+import { RadioButton } from "@nfid-frontend/ui"
+
+import { ProfileTypes } from "../choose-account"
 import { getPublicProfile } from "../choose-account/services"
 
 export interface IPublicProfileButton {
   isAvailable: boolean
-  onClick: () => void
+  setSelectedProfile: (value: ProfileTypes) => void
 }
 export const PublicProfileButton = ({
   isAvailable,
-  onClick,
+  setSelectedProfile,
 }: IPublicProfileButton) => {
-  const { data: publicProfile, isLoading } = useSWR(
-    "publicProfile",
-    getPublicProfile,
-  )
+  const { data: publicProfile } = useSWR("publicProfile", getPublicProfile)
 
   return (
     <div
-      id="publicProfileID"
       className={clsx(
-        "flex items-center w-full",
-        isAvailable
-          ? "border border-gray-300 hover:border-blue-600 hover:bg-blue-50 px-2.5 h-[70px] space-x-2.5 transition-all rounded-md cursor-pointer hover:shadow-[0px_0px_2px_0px_#0E62FF] text-sm"
-          : "border border-gray-100 bg-gray-50 px-2.5 h-[70px] space-x-2.5 rounded-md ",
+        "flex justify-between text-xs uppercase font-mono h-5 mt-5",
+        !isAvailable && "!text-gray-400",
       )}
-      onClick={isAvailable ? onClick : undefined}
     >
-      <>
-        <img alt="user" src={User} className="w-10 h-10 shrink-0" />
-        <div
-          className={clsx(
-            "flex items-center justify-between w-full text-sm ",
-            isAvailable ? "text-black" : "text-gray-400",
-          )}
-        >
-          <div className="">My NFID profile</div>
-          {isLoading ? (
-            <div className="flex flex-col"></div>
-          ) : (
-            <div className="flex flex-col">
-              <div className="text-right">{publicProfile?.balance} ICP</div>
-              <div className="text-xs text-right">
-                {publicProfile?.balanceUSD}
-              </div>
-            </div>
-          )}
-        </div>
-      </>
+      <div className="flex items-center">
+        <RadioButton
+          id="profile_public"
+          onChange={(e) => setSelectedProfile(e.target.value as ProfileTypes)}
+          value="public"
+          name={"profile"}
+          disabled={!isAvailable}
+        />
+        <label htmlFor="profile_public" className="ml-2">
+          My NFID profile
+        </label>
+      </div>
+      {publicProfile?.balance ? <div>{publicProfile?.balance} ICP</div> : null}
     </div>
   )
 }

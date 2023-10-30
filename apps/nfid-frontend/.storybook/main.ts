f1c5aebd7c9a5727e6dd86b579be078755090adf
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-10-30 10:58:51 +0100

    chore([sc-9554]): upgrade nx

--- apps/nfid-frontend/.storybook/main.ts
+++ apps/nfid-frontend/.storybook/main.ts
@@ -6,7 +6,7 @@ const config = {
   ],
   addons: [
     "@storybook/addon-essentials",
-    "@nrwl/react/plugins/storybook",
+    "@nx/react/plugins/storybook",
     "storybook-addon-react-router-v6",
   ],
 }

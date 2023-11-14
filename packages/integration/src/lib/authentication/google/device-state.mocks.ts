1b3d4ec949f592d9fdeb3fa6b4aed6d764e6e667
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-11-14 14:28:09 +0100

    feat([sc-9767]): move getPrincipalId to utility
    - add older google device state mocks

--- packages/integration/src/lib/authentication/google/device-state.mocks.ts
+++ packages/integration/src/lib/authentication/google/device-state.mocks.ts
@@ -1,12 +1,13 @@
+// import { getPrincipalId } from "../utils"
+
 export const PROFILE = {
-  anchor: 19154,
+  anchor: 12345,
   accessPoints: [
     {
       deviceType: "Unknown",
       icon: "google",
       device: "Google",
       browser: "Chrome with google account",
-      lastUsed: 1690382080668212200,
       principalId:
         "p3npo-xucqz-rmkhi-zu4ew-zwud7-jatgb-mkwtu-al3eq-buxdb-aww7a-cqe",
     },
@@ -20,36 +21,24 @@ export const PROFILE = {
 
 export const DEVICE_DATA = [
   {
-    alias: "NFID Chrome on Mac OS",
-    protection: { unprotected: null },
     pubkey: new Uint8Array([
       48, 42, 48, 5, 6, 3, 43, 101, 112, 3, 33, 0, 15, 151, 231, 222, 35, 180,
       251, 148, 50, 67, 239, 203, 22, 131, 60, 21, 76, 254, 212, 253, 208, 178,
       62, 234, 150, 217, 214, 87, 218, 74, 240, 162,
     ]),
-    key_type: { unknown: null },
-    purpose: { authentication: null },
-    credential_id: [{}],
     principalId:
       "p3npo-xucqz-rmkhi-zu4ew-zwud7-jatgb-mkwtu-al3eq-buxdb-aww7a-cqe",
   },
   {
-    alias: "Recovery phrase",
-    protection: { protected: null },
     pubkey: new Uint8Array([
       48, 42, 48, 5, 6, 3, 43, 101, 112, 3, 33, 0, 137, 190, 131, 21, 229, 192,
       248, 141, 64, 32, 205, 196, 237, 166, 82, 13, 186, 124, 159, 91, 27, 208,
       153, 222, 232, 213, 183, 197, 177, 120, 228, 207,
     ]),
-    key_type: { seed_phrase: null },
-    purpose: { recovery: null },
-    credential_id: [],
     principalId:
       "nmyu5-if2hf-urymb-4ajam-34yj3-gjy5d-cogql-pgoe5-iaczc-ml27v-hae",
   },
   {
-    alias: "Chrome on Mac",
-    protection: { unprotected: null },
     pubkey: new Uint8Array([
       48, 94, 48, 12, 6, 10, 43, 6, 1, 4, 1, 131, 184, 67, 1, 1, 3, 78, 0, 165,
       1, 2, 3, 38, 32, 1, 33, 88, 32, 106, 202, 106, 248, 140, 18, 7, 205, 211,
@@ -58,36 +47,8 @@ export const DEVICE_DATA = [
       121, 225, 84, 8, 164, 202, 219, 82, 161, 208, 161, 75, 148, 169, 128, 137,
       135, 189, 223, 189, 229, 80, 161, 217, 1, 51,
     ]),
-    key_type: { cross_platform: null },
-    purpose: { authentication: null },
-    credential_id: [
-      {
-        "0": 40,
-        "1": 82,
-        "2": 2,
-        "3": 2,
-        "4": 2,
-        "5": 185,
-        "6": 151,
-        "7": 83,
-        "8": 255,
-        "9": 253,
-        "10": 32,
-        "11": 56,
-        "12": 131,
-        "13": 66,
-        "14": 139,
-        "15": 222,
-        "16": 233,
-        "17": 58,
-        "18": 185,
-        "19": 227,
-      },
-    ],
   },
   {
-    alias: "Chrome on Mac",
-    protection: { unprotected: null },
     pubkey: new Uint8Array([
       48, 94, 48, 12, 6, 10, 43, 6, 1, 4, 1, 131, 184, 67, 1, 1, 3, 78, 0, 165,
       1, 2, 3, 38, 32, 1, 33, 88, 32, 254, 22, 85, 75, 176, 179, 184, 202, 17,
@@ -96,43 +57,65 @@ export const DEVICE_DATA = [
       130, 180, 139, 240, 230, 112, 204, 120, 112, 91, 59, 212, 25, 171, 182,
       152, 23, 95, 177, 65, 26, 22, 54, 106, 65,
     ]),
-    key_type: { platform: null },
-    purpose: { authentication: null },
-    credential_id: [
-      {
-        "0": 90,
-        "1": 0,
-        "2": 46,
-        "3": 104,
-        "4": 229,
-        "5": 17,
-        "6": 171,
-        "7": 131,
-        "8": 218,
-        "9": 196,
-        "10": 245,
-        "11": 155,
-        "12": 29,
-        "13": 57,
-        "14": 70,
-        "15": 210,
-        "16": 239,
-        "17": 225,
-        "18": 175,
-        "19": 10,
-        "20": 123,
-        "21": 30,
-        "22": 10,
-        "23": 177,
-        "24": 83,
-        "25": 207,
-        "26": 215,
-        "27": 250,
-        "28": 254,
-        "29": 249,
-        "30": 5,
-        "31": 119,
-      },
-    ],
+  },
+]
+
+export const OLD_PROFILE = {
+  anchor: 1982367,
+  accessPoints: [
+    {
+      deviceType: "Unknown",
+      icon: "",
+      device: "NFID Chrome on Mac OS",
+      browser: "Chrome",
+      principalId:
+        "r3gab-jjmuh-s72cd-r5g7n-slnjf-yn5sl-mq2tn-zbsvx-pbjse-owutt-vqe",
+    },
+  ],
+  principalId:
+    "tdcpa-2hm6n-vht2t-tb4sx-tmrzk-g4nhc-xs3zv-osr7l-62yef-hedlt-cae",
+  wallet: "II",
+  is2fa: false,
+  email: "test@identitylabs.ooo",
+}
+
+const OLD_DEVICE_0 = new Uint8Array([
+  48, 42, 48, 5, 6, 3, 43, 101, 112, 3, 33, 0, 15, 163, 177, 144, 247, 77, 111,
+  9, 150, 153, 188, 186, 195, 32, 20, 170, 244, 27, 90, 233, 166, 217, 111, 138,
+  250, 210, 176, 165, 186, 65, 135, 43,
+])
+
+const OLD_DEVICE_1 = new Uint8Array([
+  48, 94, 48, 12, 6, 10, 43, 6, 1, 4, 1, 131, 184, 67, 1, 1, 3, 78, 0, 165, 1,
+  2, 3, 38, 32, 1, 33, 88, 32, 173, 50, 110, 212, 249, 6, 64, 167, 15, 173, 27,
+  41, 220, 26, 12, 129, 40, 24, 145, 151, 104, 164, 168, 101, 227, 53, 224, 135,
+  21, 169, 238, 141, 34, 88, 32, 212, 56, 0, 167, 173, 31, 103, 101, 30, 237,
+  228, 136, 51, 45, 243, 101, 73, 182, 200, 238, 187, 109, 217, 199, 2, 194,
+  160, 220, 193, 101, 183, 200,
+])
+
+const OLD_DEVICE_2 = new Uint8Array([
+  48, 94, 48, 12, 6, 10, 43, 6, 1, 4, 1, 131, 184, 67, 1, 1, 3, 78, 0, 165, 1,
+  2, 3, 38, 32, 1, 33, 88, 32, 180, 47, 176, 121, 1, 0, 123, 66, 238, 34, 19,
+  121, 56, 203, 71, 44, 76, 192, 97, 107, 115, 240, 116, 184, 197, 183, 137, 56,
+  62, 156, 35, 67, 34, 88, 32, 207, 241, 252, 230, 109, 48, 156, 129, 68, 189,
+  210, 201, 37, 232, 73, 220, 35, 159, 13, 19, 48, 1, 191, 152, 83, 159, 117,
+  158, 223, 101, 201, 93,
+])
+
+export const OLD_DEVICES = [
+  {
+    pubkey: OLD_DEVICE_0,
+    principalId:
+      "tdcpa-2hm6n-vht2t-tb4sx-tmrzk-g4nhc-xs3zv-osr7l-62yef-hedlt-cae",
+  },
+  {
+    pubkey: OLD_DEVICE_1,
+    principalId: undefined,
+  },
+  {
+    pubkey: OLD_DEVICE_2,
+    // principalId: getPrincipalId(DANS_DEVICE_2 as unknown as Array<number>),
+    principalId: undefined,
   },
 ]

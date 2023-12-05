19174cc690b3d9c66cb36047887a7cde1abf53d6
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2023-12-05 18:52:46 +0400

    feat([sc-9416]): check how and for what we are using CheckApplicationMeta [sc-9416] (#1773)

--- apps/nfid-frontend/src/ui/pages/new-profile/assets/header.tsx
+++ apps/nfid-frontend/src/ui/pages/new-profile/assets/header.tsx
@@ -1,27 +1,4 @@
-import { FilterPopover } from "@nfid-frontend/ui"
-import { DropdownSelect, IconCmpFilters, IOption } from "@nfid-frontend/ui"
-
-import { AssetFilter } from "frontend/ui/connnector/types"
-
-interface IProfileAssetsHeader {
-  blockchainOptions: IOption[]
-  blockchainFilter: string[]
-  setBlockchainFilter: (value: string[]) => void
-  accountsOptions: IOption[]
-  assetFilter: AssetFilter[]
-  setAssetFilter: (value: AssetFilter[]) => void
-  resetFilters: () => void
-}
-
-export const ProfileAssetsHeader = ({
-  accountsOptions,
-  assetFilter,
-  setAssetFilter,
-  blockchainFilter,
-  blockchainOptions,
-  setBlockchainFilter,
-  resetFilters,
-}: IProfileAssetsHeader) => {
+export const ProfileAssetsHeader = () => {
   return (
     <div className="flex items-center justify-between w-full">
       <p>Your tokens</p>

9d57878cb90d3b890920ef46d23e5662feb7785e
Dmitrii <dmitrii@identityLabs.ooo>
2023-12-19 14:43:05 +0400

    feat([sc-10029]): 500 error in console during ICP balance request [sc-10029]

--- packages/integration/src/lib/rosetta/balance/index.ts
+++ packages/integration/src/lib/rosetta/balance/index.ts
@@ -1,3 +1,4 @@
+import { isHex } from "alchemy-sdk"
 import { rosetta } from ".."
 import { restCall } from "../../rest/rest-call"
 import { getRosettaRequest } from "../get-rosetta-request"
@@ -21,6 +22,9 @@ export interface RosettaBalance {
 export type Balance = bigint
 
 export async function getBalance(address: string): Promise<Balance> {
+  if (address.startsWith("0x")) {
+    return Promise.resolve(BigInt(0))
+  }
   const request = getRosettaRequest(address)
   return await restCall("POST", `${rosetta}/account/balance`, request).then(
     mapToBalance,

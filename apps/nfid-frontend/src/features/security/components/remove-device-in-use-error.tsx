d13addc3412a3e33559d2ef69818628fc6d1ea09
Dmitrii <dmitrii@identityLabs.ooo>
2024-01-16 19:11:10 +0400

    feat([sc-9165]): an Issue with user s authstate after deleting [sc-9165]

---
+++ apps/nfid-frontend/src/features/security/components/remove-device-in-use-error.tsx
@@ -0,0 +1,36 @@
+
+import { Button } from "@nfid-frontend/ui"
+import { ModalComponent } from "frontend/ui/molecules/modal/index-v0"
+
+export interface RemoveDeviceInUseErrorProps {
+  isModalVisible: boolean
+  setIsModalVisible: (value: boolean) => void
+}
+
+export const RemoveDeviceInUseError = ({
+  isModalVisible,
+  setIsModalVisible
+}: RemoveDeviceInUseErrorProps) => {
+  return (
+    <ModalComponent
+      isVisible={isModalVisible}
+      onClose={() => setIsModalVisible(false)}
+      className="p-5 w-[95%] md:w-[540px] z-[100] lg:rounded-xl"
+    >
+      <div className="space-y-3.5">
+        <p className="text-2xl font-bold">Cannot remove current device</p>
+        <p className="text-sm leading-5">
+          The device you are trying to remove is the one you’re currently signed in with.
+        </p>
+        <p className="text-sm leading-5">
+          Sign out and sign back in with a different device to remove this one.
+        </p>
+        <div className={"grid p-1.5 !flex justify-end"}>
+          <Button className={"!py-2.5 !px-16"} onClick={() => setIsModalVisible(false)}>
+            Ok
+          </Button>
+        </div>
+      </div>
+    </ModalComponent>
+  )
+}

ac377aad6426112ce9ae07dd5e9da4b46fa081bb
olsemeno <33149496+olsemeno@users.noreply.github.com>
2024-01-30 12:16:48 +0200

    feat [sc-10552]: ICRC1 Adapter (#1799)
    * feat [sc-10552]: ICRC1 Adapter
    
    * feat [sc-10552]: ICRC1 Adapter

--- packages/integration/src/lib/icrc1/index.spec.ts
+++ packages/integration/src/lib/icrc1/index.spec.ts
@@ -1,36 +1,84 @@
 /**
  * @jest-environment jsdom
  */
-import {DelegationIdentity, Ed25519KeyIdentity} from "@dfinity/identity"
+import { DelegationIdentity, Ed25519KeyIdentity } from "@dfinity/identity"
+import { Principal } from "@dfinity/principal"
 
-import {iCRC1, replaceActorIdentity} from '../actors';
-import {generateDelegationIdentity} from "../test-utils"
-import {addICRC1Canister, getICRC1Canisters} from "./index";
-import {mockIdentityA} from "@nfid/integration";
+import { mockIdentityA } from "@nfid/integration"
+
+import { TransferArg } from "../_ic_api/icrc1.d"
+import { HTTPAccountResponse } from "../_ic_api/identity_manager.d"
+import { iCRC1Registry, im, replaceActorIdentity } from "../actors"
+import { generateDelegationIdentity } from "../test-utils"
+import {
+  addICRC1Canister,
+  getICRC1Canisters,
+  getICRC1Data,
+  ICRC1Data,
+  transferICRC1,
+} from "./index"
 
 describe("ICRC1 suite", () => {
   jest.setTimeout(200000)
-
+  const iCRC1TestCanister = "6jq2j-daaaa-aaaap-absuq-cai"
   it("Store/retrieve canister id", async () => {
     const mockedIdentity = Ed25519KeyIdentity.fromParsedJson(mockIdentityA)
     const delegationIdentity: DelegationIdentity =
       await generateDelegationIdentity(mockedIdentity)
-    await replaceActorIdentity(iCRC1, delegationIdentity)
-    const canister_id = generateRandomString(5)
-    await addICRC1Canister(canister_id)
-    const canisters = await getICRC1Canisters() as string[];
-    expect(canisters).toContain(canister_id)
+    await replaceActorIdentity(iCRC1Registry, delegationIdentity)
+    await replaceActorIdentity(im, delegationIdentity)
+    await addICRC1Canister(iCRC1TestCanister)
+    await replaceActorIdentity(im, delegationIdentity)
+    const account = (await im.get_account()) as HTTPAccountResponse
+    const root = account.data[0]!.principal_id
+    const canisters = (await getICRC1Canisters(root)) as string[]
+    expect(canisters).toContain(iCRC1TestCanister)
+  })
+
+  it("Get data", async () => {
+    const mockedIdentity = Ed25519KeyIdentity.fromParsedJson(mockIdentityA)
+    const delegationIdentity: DelegationIdentity =
+      await generateDelegationIdentity(mockedIdentity)
+    await replaceActorIdentity(iCRC1Registry, delegationIdentity)
+    await replaceActorIdentity(im, delegationIdentity)
+    const account = (await im.get_account()) as HTTPAccountResponse
+    const root = account.data[0]!.principal_id
+    const data = (await getICRC1Data(
+      root,
+      "sculj-2sjuf-dxqlm-dcv5y-hin5x-zfyvr-tzngf-bt5b5-dwhcc-zbsqf-rae",
+    )) as Array<ICRC1Data>
+    const testICRC1 = data[0]
+    expect(testICRC1.balance).toBeGreaterThan(0)
+    expect(testICRC1.fee).toEqual(BigInt(10000))
+    expect(testICRC1.canisterId).toEqual(iCRC1TestCanister)
+    expect(testICRC1.decimals).toEqual(8)
+    expect(testICRC1.name).toEqual("ICPTestNfid")
+    expect(testICRC1.symbol).toEqual("ICPTestNfid")
+  })
+
+  it("Transfer", async () => {
+    const mockedIdentity = Ed25519KeyIdentity.fromParsedJson(mockIdentityA)
+    const delegationIdentity: DelegationIdentity =
+      await generateDelegationIdentity(mockedIdentity)
+    const transferArgs: TransferArg = {
+      amount: BigInt(10),
+      created_at_time: [],
+      fee: [],
+      from_subaccount: [],
+      memo: [],
+      to: {
+        subaccount: [],
+        owner: Principal.fromText(
+          "sculj-2sjuf-dxqlm-dcv5y-hin5x-zfyvr-tzngf-bt5b5-dwhcc-zbsqf-rae",
+        ),
+      },
+    }
+    const block = await transferICRC1(
+      delegationIdentity,
+      iCRC1TestCanister,
+      transferArgs,
+    )
+    // @ts-ignore
+    expect(block.Ok).toBeGreaterThan(0)
   })
 })
-
-function generateRandomString(length: number, charset = 'ABCDEFGHIJK'): string {
-  let result = '';
-  const charsetLength = charset.length;
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charsetLength);
-    result += charset.charAt(randomIndex);
-  }
-
-  return result;
-}

86642dc93a6adbbdf06786a60c2e6888cb8bd8e3
Philipp Litzenberger <philipp@identitylabs.ooo>
2024-02-22 17:28:28 +0100

    feat([sc-11068]): fix undefined principal

--- packages/integration/src/lib/internet-identity/fetch-principals.ts
+++ packages/integration/src/lib/internet-identity/fetch-principals.ts
@@ -17,7 +17,8 @@ export async function fetchPrincipals(): Promise<PrincipalAccount[]> {
   const publicKey = await getPublicKey(delegation, Chain.IC)
   const principal = Ed25519KeyIdentity.fromParsedJson([
     publicKey,
-    "",
+    // FIXME: @dmitriiIdentityLabs will have look to get a better fix
+    "0a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0",
   ]).getPrincipal()
 
   const globalAcc = {

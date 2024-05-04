0b7d01d14702e11d003baefa403204738ec98acb
olsemeno <sendtosemenov@gmail.com>
2024-05-04 02:35:14 +0200

    ICRC 1 Implement index canisters [sc-12188]

--- packages/integration/src/lib/icrc1/index.ts
+++ packages/integration/src/lib/icrc1/index.ts
@@ -1,22 +1,30 @@
 import * as Agent from "@dfinity/agent"
-import {HttpAgent, Identity} from "@dfinity/agent"
-import {Principal} from "@dfinity/principal"
+import { HttpAgent, Identity } from "@dfinity/agent"
+import { Principal } from "@dfinity/principal"
 
-import {idlFactory as icrc1IDL} from "../_ic_api/icrc1"
-import {idlFactory as icrc1IndexIDL} from "../_ic_api/index-icrc1"
-import {_SERVICE as ICRC1Service, Icrc1TransferResult, TransferArg,} from "../_ic_api/icrc1.d"
-import {_SERVICE as ICRCIndex, GetAccountTransactionsArgs, TransactionWithId, Transfer,} from "../_ic_api/index-icrc1.d"
-import {agentBaseConfig, iCRC1Registry} from "../actors"
-import {TokenPrice} from "../asset/types";
-import {PriceService} from "../asset/asset-util";
-import {hasOwnProperty} from "@nfid/integration";
-import {ICRC1} from "../_ic_api/icrc1_registry.d";
+import { hasOwnProperty } from "@nfid/integration"
 
+import { idlFactory as icrc1IDL } from "../_ic_api/icrc1"
+import {
+  _SERVICE as ICRC1Service,
+  Icrc1TransferResult,
+  TransferArg,
+} from "../_ic_api/icrc1.d"
+import { ICRC1 } from "../_ic_api/icrc1_registry.d"
+import { idlFactory as icrc1IndexIDL } from "../_ic_api/index-icrc1"
+import {
+  _SERVICE as ICRCIndex,
+  GetAccountTransactionsArgs,
+  TransactionWithId,
+  Transfer,
+} from "../_ic_api/index-icrc1.d"
+import { agentBaseConfig, iCRC1Registry } from "../actors"
+import { PriceService } from "../asset/asset-util"
+import { TokenPrice } from "../asset/types"
 
 const errorText = "This does not appear to be an ICRC1 compatible canister"
 const network = "Internet Computer"
 const icpCanisterId = "ryjl3-tyaaa-aaaaa-aaaba-cai"
-
 
 export interface ICRC1Data {
   balance: bigint
@@ -37,7 +45,7 @@ export interface ICRC1IndexData {
 }
 
 interface TransactionData {
-  type: 'sent' | 'received'
+  type: "sent" | "received"
   timestamp: bigint
   transactionId: bigint
   symbol: string
@@ -55,9 +63,11 @@ export async function getICRC1DataForUser(
   publicKey: string,
 ): Promise<Array<ICRC1Data>> {
   const canisters = await getICRC1Canisters(rootPrincipalId)
-  return getICRC1Data(canisters.map(l=>l.ledger), publicKey)
+  return getICRC1Data(
+    canisters.map((l) => l.ledger),
+    publicKey,
+  )
 }
-
 
 /*
  PTAL "Get index data" test in icrc1/index.spec.ts
@@ -66,14 +76,22 @@ export async function getICRC1DataForUser(
  * maxResults: the maximum number of transactions to return
  * blockNumberToStartFrom: the block number to start from
  */
-export async function getICRC1HistoryDataForUser(rootPrincipalId: string,
-                                                 publicKey: string,
-                                                 maxResults: bigint,
-                                                 blockNumberToStartFrom: bigint | undefined
+export async function getICRC1HistoryDataForUser(
+  rootPrincipalId: string,
+  publicKey: string,
+  maxResults: bigint,
+  blockNumberToStartFrom: bigint | undefined,
 ): Promise<Array<ICRC1IndexData>> {
   const canisters = await getICRC1Canisters(rootPrincipalId)
-  const indexedCanisters = canisters.filter((canister) => canister.index.length > 0)
-  return getICRC1IndexData(indexedCanisters, publicKey, maxResults, blockNumberToStartFrom)
+  const indexedCanisters = canisters.filter(
+    (canister) => canister.index.length > 0,
+  )
+  return getICRC1IndexData(
+    indexedCanisters,
+    publicKey,
+    maxResults,
+    blockNumberToStartFrom,
+  )
 }
 
 export async function isICRC1Canister(
@@ -83,7 +101,7 @@ export async function isICRC1Canister(
 ): Promise<ICRC1Data> {
   const actor = Agent.Actor.createActor<ICRC1Service>(icrc1IDL, {
     canisterId: canisterId,
-    agent: new HttpAgent({...agentBaseConfig}),
+    agent: new HttpAgent({ ...agentBaseConfig }),
   })
   const standards = await actor.icrc1_supported_standards()
   const isICRC1: boolean = standards
@@ -94,7 +112,8 @@ export async function isICRC1Canister(
   }
   await getICRC1Canisters(rootPrincipalId).then((icrc1) => {
     if (
-      icrc1.map((c) => c)
+      icrc1
+        .map((c) => c)
         .map((c) => c.ledger)
         .includes(canisterId)
     ) {
@@ -135,7 +154,10 @@ export async function getICRC1Canisters(
   return iCRC1Registry.get_canisters_by_root(principal)
 }
 
-export async function addICRC1Canister(canisterId: string, indexCanisterId: string | undefined): Promise<void> {
+export async function addICRC1Canister(
+  canisterId: string,
+  indexCanisterId: string | undefined,
+): Promise<void> {
   const indexCandid: [] | [string] = indexCanisterId ? [indexCanisterId] : []
   await iCRC1Registry.store_icrc1_canister(canisterId, indexCandid)
 }
@@ -149,7 +171,7 @@ export async function getICRC1Data(
     canisters.map(async (canisterId) => {
       const actor = Agent.Actor.createActor<ICRC1Service>(icrc1IDL, {
         canisterId: canisterId,
-        agent: new HttpAgent({...agentBaseConfig}),
+        agent: new HttpAgent({ ...agentBaseConfig }),
       })
       const balance = await actor.icrc1_balance_of({
         subaccount: [],
@@ -218,14 +240,13 @@ export async function getICRC1IndexData(
   canisters: Array<ICRC1>,
   publicKeyInPrincipal: string,
   maxResults: bigint,
-  blockNumberToStartFrom: bigint | undefined
+  blockNumberToStartFrom: bigint | undefined,
 ): Promise<Array<ICRC1IndexData>> {
-
   return Promise.all(
     canisters.map(async (canisterId) => {
       const indexActor = Agent.Actor.createActor<ICRCIndex>(icrc1IndexIDL, {
         canisterId: canisterId.index[0]!,
-        agent: new HttpAgent({...agentBaseConfig}),
+        agent: new HttpAgent({ ...agentBaseConfig }),
       })
 
       const args: GetAccountTransactionsArgs = {
@@ -234,37 +255,58 @@ export async function getICRC1IndexData(
           owner: Principal.fromText(publicKeyInPrincipal),
         },
         max_results: maxResults,
-        start: blockNumberToStartFrom === undefined ? [] : [blockNumberToStartFrom]
+        start:
+          blockNumberToStartFrom === undefined ? [] : [blockNumberToStartFrom],
       }
       const response = await indexActor.get_account_transactions(args)
 
       if (hasOwnProperty(response, "Err")) {
-        console.warn("Error " + response.Err + " getting account transactions for canister: " + canisterId)
-        return {transactions: [], oldestTransactionId: undefined}
+        console.warn(
+          "Error " +
+            response.Err +
+            " getting account transactions for canister: " +
+            canisterId,
+        )
+        return { transactions: [], oldestTransactionId: undefined }
       }
 
       if (hasOwnProperty(response, "Ok")) {
-        const ledgerData = await getICRC1Data([canisterId.ledger], publicKeyInPrincipal);
+        const ledgerData = await getICRC1Data(
+          [canisterId.ledger],
+          publicKeyInPrincipal,
+        )
         return {
-          transactions: mapRawTrsToTransaction(response.Ok.transactions, publicKeyInPrincipal, ledgerData[0].symbol),
-          oldestTransactionId: response.Ok.oldest_tx_id.length === 0 ? undefined : response.Ok.oldest_tx_id[0]
+          transactions: mapRawTrsToTransaction(
+            response.Ok.transactions,
+            publicKeyInPrincipal,
+            ledgerData[0].symbol,
+          ),
+          oldestTransactionId:
+            response.Ok.oldest_tx_id.length === 0
+              ? undefined
+              : response.Ok.oldest_tx_id[0],
         }
       }
-      return {transactions: [], oldestTransactionId: undefined}
-    }))
-
+      return { transactions: [], oldestTransactionId: undefined }
+    }),
+  )
 }
 
-function mapRawTrsToTransaction(rawTrss: Array<TransactionWithId>, ownerPrincipal: string, symbol: string): Array<TransactionData> {
-  const filtered = rawTrss.filter((rawTrs) =>
-    rawTrs.transaction.transfer.length !== 0
+function mapRawTrsToTransaction(
+  rawTrss: Array<TransactionWithId>,
+  ownerPrincipal: string,
+  symbol: string,
+): Array<TransactionData> {
+  const filtered = rawTrss.filter(
+    (rawTrs) => rawTrs.transaction.transfer.length !== 0,
   )
   if (filtered.length === 0) {
     return []
   }
   return filtered.map((rawTrs) => {
     const trs: Transfer = rawTrs.transaction.transfer[0]!
-    const type = ownerPrincipal === trs.from.owner.toText() ? "sent" : "received"
+    const type =
+      ownerPrincipal === trs.from.owner.toText() ? "sent" : "received"
     const data: TransactionData = {
       type,
       timestamp: rawTrs.transaction.timestamp,
@@ -272,7 +314,7 @@ function mapRawTrsToTransaction(rawTrss: Array<TransactionWithId>, ownerPrincipa
       amount: trs.amount,
       from: trs.from.owner.toText(),
       to: trs.to.owner.toText(),
-      transactionId: rawTrs.id
+      transactionId: rawTrs.id,
     }
     return data
   })

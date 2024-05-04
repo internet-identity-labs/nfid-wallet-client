5785a054d889fddcd430ab10d118ad3be4e4a2c8
olsemeno <sendtosemenov@gmail.com>
2024-05-04 02:35:14 +0200

    ICRC 1 Implement index canisters [sc-12188]

--- packages/integration/src/lib/_ic_api/icrc1_registry.ts
+++ packages/integration/src/lib/_ic_api/icrc1_registry.ts
@@ -1,15 +1,15 @@
 export const idlFactory = ({ IDL }: any) => {
-  const Conf = IDL.Record({ 'im_canister' : IDL.Opt(IDL.Text) });
+  const Conf = IDL.Record({ im_canister: IDL.Opt(IDL.Text) })
   const ICRC1 = IDL.Record({
-    'ledger' : IDL.Text,
-    'index' : IDL.Opt(IDL.Text),
-  });
+    ledger: IDL.Text,
+    index: IDL.Opt(IDL.Text),
+  })
   return IDL.Service({
-    'get_canisters_by_root' : IDL.Func([IDL.Text], [IDL.Vec(ICRC1)], ['query']),
-    'store_icrc1_canister' : IDL.Func([IDL.Text, IDL.Opt(IDL.Text)], [], []),
-  });
-};
+    get_canisters_by_root: IDL.Func([IDL.Text], [IDL.Vec(ICRC1)], ["query"]),
+    store_icrc1_canister: IDL.Func([IDL.Text, IDL.Opt(IDL.Text)], [], []),
+  })
+}
 export const init = ({ IDL }: any) => {
-  const Conf = IDL.Record({ 'im_canister' : IDL.Opt(IDL.Text) });
-  return [Conf];
-};
+  const Conf = IDL.Record({ im_canister: IDL.Opt(IDL.Text) })
+  return [Conf]
+}

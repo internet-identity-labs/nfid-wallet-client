e04138aaf1d68b954a761616e8ae320339967d1a
olsemeno <sendtosemenov@gmail.com>
2024-05-16 12:30:17 +0200

    ICRC 1 Implement a method to remove already [sc-12673]

--- packages/integration/src/lib/_ic_api/icrc1_registry.d.ts
+++ packages/integration/src/lib/_ic_api/icrc1_registry.d.ts
@@ -1,12 +1,17 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-import type { IDL } from '@dfinity/candid';
+import type { ActorMethod } from "@dfinity/agent"
+import type { IDL } from "@dfinity/candid"
+import type { Principal } from "@dfinity/principal"
 
-export interface Conf { 'im_canister' : [] | [string] }
-export interface ICRC1 { 'ledger' : string, 'index' : [] | [string] }
-export interface _SERVICE {
-  'get_canisters_by_root' : ActorMethod<[string], Array<ICRC1>>,
-  'remove_icrc1_canister' : ActorMethod<[string], undefined>,
-  'store_icrc1_canister' : ActorMethod<[string, [] | [string]], undefined>,
+export interface Conf {
+  im_canister: [] | [string]
 }
-export declare const idlFactory: IDL.InterfaceFactory;
+export interface ICRC1 {
+  ledger: string
+  index: [] | [string]
+}
+export interface _SERVICE {
+  get_canisters_by_root: ActorMethod<[string], Array<ICRC1>>
+  remove_icrc1_canister: ActorMethod<[string], undefined>
+  store_icrc1_canister: ActorMethod<[string, [] | [string]], undefined>
+}
+export declare const idlFactory: IDL.InterfaceFactory

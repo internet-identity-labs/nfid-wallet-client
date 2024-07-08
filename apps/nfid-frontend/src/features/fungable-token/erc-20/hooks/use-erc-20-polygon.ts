616c5a11f6e9d561cb2de89e7efddd25a4a184ac
Vitalii Pustovyi <vitalii@identitylabs.ooo>
2024-07-08 19:09:47 +0300

    feat([sc-13386]): eth/btc/matic removal

--- apps/nfid-frontend/src/features/fungable-token/erc-20/hooks/use-erc-20-polygon.ts
+++
@@ -1,8 +0,0 @@
-import { getErc20Tokens } from "src/features/fungable-token/erc-20/get-erc-20-polygon"
-import useSWR from "swr"
-
-export const useErc20Polygon = () => {
-  const { data: erc20, ...rest } = useSWR("erc20Polygon", getErc20Tokens)
-
-  return { erc20, ...rest }
-}
--- packages/integration/src/lib/token/types.ts
+++ packages/integration/src/lib/token/types.ts
@@ -1,9 +1,4 @@
 export enum TokenStandards {
   "ICP" = "ICP",
   "ICRC1" = "ICRC1",
-  "ETH" = "ETH",
-  "MATIC" = "MATIC",
-  "BTC" = "BTC",
-  "ERC20_ETHEREUM" = "ERC20_ETHEREUM",
-  "ERC20_POLYGON" = "ERC20_POLYGON",
 }

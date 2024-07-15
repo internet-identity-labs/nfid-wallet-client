1d41e71a8270fdad4d6cdd2fc5325e38ef7288bf
Vitalii Pustovyi <vitalii@identitylabs.ooo>
2024-07-15 17:23:58 +0300

    feat([sc-13386]): remove assets

---
+++ apps/nfid-frontend/src/features/embed/services/connect-account-service.ts
@@ -0,0 +1,44 @@
+import { createConnection } from "@nfid/client-db"
+import { ecdsaSigner, replaceActorIdentity } from "@nfid/integration"
+
+import { getWalletDelegation } from "frontend/integration/facade/wallet"
+import { AuthSession } from "frontend/state/authentication"
+import { AuthorizationRequest } from "frontend/state/authorization"
+
+import { getEthAddress } from "../../fungable-token/eth/get-eth-address"
+
+export type ConnectAccountServiceContext = {
+  authSession?: AuthSession
+  authRequest?: AuthorizationRequest
+}
+
+export const ConnectAccountService = async (
+  { authSession, authRequest }: ConnectAccountServiceContext,
+  event: {
+    data: { hostname: string; accountId: string }
+  },
+) => {
+  console.debug("ConnectAccountService", event)
+  if (!authSession)
+    throw new Error("ConnectAccountService: missing authSession")
+  if (!authRequest)
+    throw new Error("ConnectAccountService: missing authRequest")
+
+  const address = await getEthAddress(authSession.anchor)
+  console.debug("ConnectAccountService", { address })
+
+  const identity = await getWalletDelegation(
+    authSession.anchor,
+    event.data.hostname,
+    event.data.accountId,
+  )
+  replaceActorIdentity(ecdsaSigner, identity)
+
+  createConnection({
+    connectionDomain: authRequest.derivationOrigin ?? authRequest.hostname,
+    accountId: event.data.accountId,
+    domain: event.data.hostname,
+  })
+
+  return [address]
+}

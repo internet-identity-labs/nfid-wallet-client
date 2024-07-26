7977203c01a796c447f3642fcd5273fa6dc9f0e6
Vitalii <vitalii@identitylabs.ooo>
2024-07-26 14:58:27 +0300

    feat([sc-13775]): logo video fix

--- packages/ui/src/atoms/nfid-logo/index.tsx
+++ packages/ui/src/atoms/nfid-logo/index.tsx
@@ -20,6 +20,7 @@ export const NFIDLogo = () => {
 
   return (
     <video
+      style={{ backgroundColor: "#0E0F10" }}
       ref={videoRef}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}

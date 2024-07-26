d330b7257ef1b48913d55352b28754ec52a07036
Vitalii <vitalii@identitylabs.ooo>
2024-07-26 18:17:52 +0300

    feat([sc-13899]): hide vaults section and sidebar item

---
+++ apps/nfid-frontend/src/ui/pages/vault-guard/index.tsx
@@ -0,0 +1,23 @@
+import React, { PropsWithChildren } from "react"
+import useSWR from "swr"
+
+import { Loader } from "@nfid-frontend/ui"
+
+import { useVaultMember } from "frontend/features/vaults/hooks/use-vault-member"
+import { getAllVaults } from "frontend/features/vaults/services"
+
+import { NotFound } from "../404"
+
+export const VaultGuard = ({ children }: PropsWithChildren) => {
+  const { isReady } = useVaultMember()
+  const { data: vaults, isLoading } = useSWR(
+    [isReady ? "vaults" : null],
+    getAllVaults,
+  )
+
+  if (!vaults || isLoading) return <Loader isLoading={true} />
+
+  if (vaults && !vaults?.length) return <NotFound />
+
+  return <>{children}</>
+}

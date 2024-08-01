eff02c35f44060fa8c41652b800986e69326dd84
olsemeno <sendtosemenov@gmail.com>
2024-08-01 12:08:33 +0100

    Update logic for NFT price in USD to use [sc-13926]

--- packages/integration/src/lib/exchange/exchange-rate.spec.ts
+++ packages/integration/src/lib/exchange/exchange-rate.spec.ts
@@ -2,7 +2,7 @@ import { exchangeRateService } from "./exchange-rate"
 
 describe("exchange-rate", () => {
   it("should return some rate", async function () {
-    await new Promise((resolve) => setTimeout(resolve, 777))
+    await exchangeRateService.cacheUsdIcpRate()
     expect(exchangeRateService.getICP2USD().toNumber()).toBeGreaterThan(0)
   })
 })

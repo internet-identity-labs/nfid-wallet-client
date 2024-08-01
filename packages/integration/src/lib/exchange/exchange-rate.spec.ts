a2aa33d76e5e5b70efc7809ec7d16769a58c9d6f
olsemeno <sendtosemenov@gmail.com>
2024-08-01 01:46:14 +0100

    Update logic for NFT price in USD to use [sc-13926]

---
+++ packages/integration/src/lib/exchange/exchange-rate.spec.ts
@@ -0,0 +1,8 @@
+import {exchangeRateService} from "./exchange-rate";
+
+describe("exchange-rate", () => {
+  it('should return some rate', async function () {
+    await new Promise(resolve => setTimeout(resolve, 777));
+    expect(exchangeRateService.getICP2USD().toNumber()).toBeGreaterThan(0)
+  });
+})
---
+++ packages/integration/src/lib/exchange/exchange-rate.ts
@@ -0,0 +1,50 @@
+import {actor} from "./../actors";
+import {idlFactory as IDL} from "./idl/ExchangeRate"
+import {_SERVICE as Service, ExchangeRate__1} from "./idl/ExchangeRate.d"
+import * as Agent from "@dfinity/agent";
+import BigNumber from "bignumber.js";
+
+const EXCHANGE_RATE_CANISTER = "2ixw4-taaaa-aaaag-qcpdq-cai"
+type NumberType = string | number | bigint | BigNumber;
+const EXECUTE_TIMEOUT = 60000
+
+export class ExchangeRateService {
+  private exchangeRateActor: Agent.ActorSubclass<Service>
+  private ICP2USD: BigNumber = new BigNumber(0)
+
+  constructor() {
+    this.exchangeRateActor = actor<Service>(EXCHANGE_RATE_CANISTER, IDL)
+  }
+
+  getICP2USD(): BigNumber {
+    return this.ICP2USD
+  }
+
+  async cacheUsdIcpRate() {
+    const result = await this.getExchangeRate("f_USD-c_ICP");
+    this.ICP2USD = this.parseTokenAmount(result.rate, result.decimals);
+  }
+
+  private async getExchangeRate(pair: string): Promise<ExchangeRate__1> {
+    return this.exchangeRateActor.get_exchange_rate(pair)
+  }
+
+  parseTokenAmount(
+    amount: NumberType | null | undefined,
+    decimals: number | bigint = 8
+  ): BigNumber {
+    if (amount !== 0 && !amount) return new BigNumber(0);
+    if (typeof amount === "bigint") amount = Number(amount);
+    if (typeof decimals === "bigint") decimals = Number(decimals);
+    if (Number.isNaN(Number(amount))) return new BigNumber(String(amount));
+    return new BigNumber(String(amount)).dividedBy(10 ** Number(decimals));
+  }
+
+}
+
+export const exchangeRateService = new ExchangeRateService()
+//some trick to make getter sync
+exchangeRateService.cacheUsdIcpRate()
+setInterval(exchangeRateService.cacheUsdIcpRate, EXECUTE_TIMEOUT)
+
+

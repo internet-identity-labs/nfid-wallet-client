bad0801e731d306b0509732ea262681ecda502aa
Vitalii <vitalii@identitylabs.ooo>
2024-08-12 18:47:06 +0300

    feat([sc-13955]): storybook wallet address

--- apps/nfid-frontend/src/ui/templates/profile-template/Template.tsx
+++ apps/nfid-frontend/src/ui/templates/profile-template/Template.tsx
@@ -1,12 +1,28 @@
+import { useActor } from "@xstate/react"
 import clsx from "clsx"
 import ProfileInfo from "packages/ui/src/organisms/profile-info"
-import { HTMLAttributes, useMemo, useCallback, ReactNode, FC } from "react"
+import {
+  HTMLAttributes,
+  useMemo,
+  useCallback,
+  ReactNode,
+  FC,
+  useContext,
+} from "react"
+import useSWR from "swr"
 
 import { ArrowButton, Tooltip } from "@nfid-frontend/ui"
+import { sendReceiveTracking } from "@nfid/integration"
+import { TokenStandards } from "@nfid/integration/token/types"
 
+import { SendReceiveButton } from "frontend/apps/identity-manager/profile/send-receive-button"
 import { useAllToken } from "frontend/features/fungible-token/use-all-token"
 import { TransferModalCoordinator } from "frontend/features/transfer-modal/coordinator"
+import { useAccountsOptions } from "frontend/features/transfer-modal/hooks/use-accounts-options"
+import { getWalletDelegationAdapter } from "frontend/integration/adapters/delegations"
+import { ProfileContext } from "frontend/provider"
 import { Loader } from "frontend/ui/atoms/loader"
+import { Blockchain } from "frontend/ui/connnector/types"
 import ProfileHeader from "frontend/ui/organisms/profile-header"
 import ProfileSidebar from "frontend/ui/organisms/profile-sidebar"
 
@@ -42,7 +58,31 @@ const ProfileTemplate: FC<IProfileTemplate> = ({
     window.history.back()
   }, [])
 
+  const globalServices = useContext(ProfileContext)
   const { token, isLoading: isTokenLoading } = useAllToken()
+  const [, send] = useActor(globalServices.transferService)
+  const {
+    data: identity,
+    isLoading: isIdentityLoading,
+    isValidating,
+  } = useSWR("globalIdentity", () =>
+    getWalletDelegationAdapter("nfid.one", "-1"),
+  )
+
+  const onSendClick = () => {
+    sendReceiveTracking.openModal()
+    send({ type: "ASSIGN_VAULTS", data: false })
+    send({ type: "ASSIGN_SOURCE_WALLET", data: "" })
+    send("SHOW")
+  }
+
+  const onReceiveClick = () => {
+    sendReceiveTracking.openModal()
+    send({ type: "ASSIGN_VAULTS", data: false })
+    send({ type: "ASSIGN_SOURCE_WALLET", data: "" })
+    send({ type: "CHANGE_DIRECTION", data: "receive" })
+    send("SHOW")
+  }
 
   const tokensUsdValue = useMemo(() => {
     return token
@@ -55,6 +95,8 @@ const ProfileTemplate: FC<IProfileTemplate> = ({
 
     // Will add NFT floor price to calculation later!
   }, [token])
+
+  if (!identity) return <Loader isLoading />
 
   return (
     <div className={clsx("relative min-h-screen overflow-hidden", className)}>
@@ -74,7 +116,13 @@ const ProfileTemplate: FC<IProfileTemplate> = ({
           <ProfileSidebar id="desktop" />
         </div>
         <section className={clsx("relative", className)}>
-          <ProfileInfo value={tokensUsdValue} isLoading={isTokenLoading} />
+          <ProfileInfo
+            value={tokensUsdValue}
+            isLoading={isTokenLoading && isIdentityLoading && isValidating}
+            onSendClick={onSendClick}
+            onReceiveClick={onReceiveClick}
+            address={identity.getPrincipal().toString()}
+          />
           <div className="flex justify-between h-[70px] items-center mt-5">
             <div className="sticky left-0 flex items-center space-x-2">
               {showBackButton && (

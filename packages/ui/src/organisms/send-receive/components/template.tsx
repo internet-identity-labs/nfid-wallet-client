de6ac1a3e25ce70655f740f5c50e5f406bbce03f
Pavlo Chorniy <39991357+chorniy8pavlo@users.noreply.github.com>
2024-08-29 14:00:53 +0200

    fix: mobile transfer modal width (#2098)
    Co-authored-by: Pavlo Chorniy <pavlo@identitylabs.ooo>

--- packages/ui/src/organisms/send-receive/components/template.tsx
+++ packages/ui/src/organisms/send-receive/components/template.tsx
@@ -24,7 +24,7 @@ export const TransferTemplate: React.FC<ITransferTemplate> = ({
           "rounded-[24px] shadow-lg p-5 text-black overflow-hidden",
           "z-20 bg-white absolute flex flex-col",
           "left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2",
-          "w-[95%] sm:w-[450px] h-[560px]",
+          "w-[340px] sm:w-[450px] h-[560px]",
         )}
         onClick={(e) => e.stopPropagation()}
       >

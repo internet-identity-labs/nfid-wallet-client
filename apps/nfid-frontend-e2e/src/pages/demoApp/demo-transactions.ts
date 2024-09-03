e0c479cc8c5630bc6f54cbf3fdb7ef42b5d15598
Alex <nearbyg@gmail.com>
2024-09-03 20:19:34 +0300

    test: fixed locator

--- apps/nfid-frontend-e2e/src/pages/demoApp/demo-transactions.ts
+++ apps/nfid-frontend-e2e/src/pages/demoApp/demo-transactions.ts
@@ -5,7 +5,7 @@ import { demoAppPage } from "./demoApp-page.js"
 export class DemoTransactions extends demoAppPage {
   get myNFTSelector() {
     return $(
-      "#option_3hfnf-vakor-uwiaa-aaaaa-b4atg-aaqca-aacgm-a span:first-of-type",
+      "#option_3hfnf-vakor-uwiaa-aaaaa-b4atg-aaqca-aacgm-a",
     )
   }
 
@@ -61,7 +61,6 @@ export class DemoTransactions extends demoAppPage {
   }
 
   async sendNFTTransaction(address: string) {
-    await browser.pause(5000)
     await this.getReceiverICAddressInput("NFT").then(async (it) => {
       await it.waitForDisplayed()
       await it.setValue(address)

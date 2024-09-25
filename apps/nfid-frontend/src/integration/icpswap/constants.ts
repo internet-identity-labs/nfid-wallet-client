8965f580f6e46798228380d1fc3944c6e133d042
Vitalii <vitalii@identitylabs.ooo>
2024-09-25 12:19:10 +0300

    feat([sc-15050]): swap api fixes

--- apps/nfid-frontend/src/integration/icpswap/constants.ts
+++
@@ -1,4 +0,0 @@
-export const UNSUPPORTED_TOKEN =
-  "Provider doesn’t have enough liquidity to complete this swap. Lower the swap amount and try again."
-export const INSUFFICIENT_FUNDS = "Provider can't swap this pair."
-export const SERVICE_UNAVAILABLE = "Service temporarily unavailable"

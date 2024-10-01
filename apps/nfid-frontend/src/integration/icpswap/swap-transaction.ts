a2faa6b0af607449e35253590c9c06129607d3a5
olsemeno <33149496+olsemeno@users.noreply.github.com>
2024-10-01 23:14:36 +0200

    Implement Swap workflow 1 from ICPSwap (Swap Transactions) [sc-15052] (#2201)
    * Remove cap js library and all related code (Swap Transactions) [sc-15179]
    
    * Remove cap js library and all related code (Swap Transactions) [sc-15179]

--- apps/nfid-frontend/src/integration/icpswap/swap-transaction.ts
+++ apps/nfid-frontend/src/integration/icpswap/swap-transaction.ts
@@ -1,5 +1,9 @@
+import { SwapTransaction as SwapTransactionCandid } from "src/integration/icpswap/idl/swap_trs_storage.d"
+import { Quote } from "src/integration/icpswap/quote"
 import { SwapStage } from "src/integration/icpswap/types/enums"
 
 export interface SwapTransaction {
   getStage(): SwapStage
+  toCandid(quote: Quote): SwapTransactionCandid
+  fromCandid(candid: SwapTransactionCandid): SwapTransaction
 }

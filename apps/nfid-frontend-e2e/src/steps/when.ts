6b0732f121903c390534b683bb920349ec3e6705
Alex <nearbyg@gmail.com>
2024-10-17 18:11:49 +0300

    test: update expected currency

--- apps/nfid-frontend-e2e/src/steps/when.ts
+++ apps/nfid-frontend-e2e/src/steps/when.ts
@@ -105,5 +105,6 @@ When(/^User switches send type$/, async () => {
   await Assets.switchSendType.click()
 })
 
-When(/^User click the back button in Send window$/,
-  async () => (Assets.backButtonInSendWindow.click()))
+When(/^User click the back button in Send window$/, async () => {
+  await Assets.waitUntilElementsLoadedProperly(Assets.backButtonInSendWindow, Assets.switchSendType)
+})

8ae3399f0e04073c052003a530a7b436dba9fdff
Vitalii <vitalii@identitylabs.ooo>
2024-10-18 12:12:48 +0300

    feat([sc-15144]): nft only 10 NFT is displayed on the screen

--- apps/nfid-frontend/src/integration/nft/test/nft.spec.ts
+++ apps/nfid-frontend/src/integration/nft/test/nft.spec.ts
@@ -23,7 +23,7 @@ describe("nft test suite", () => {
       jest
         .spyOn(exchangeRateService as any, "getICP2USD")
         .mockReturnValue(new BigNumber(8.957874722))
-      const result = await nftService.getNFTs(principal)
+      const result = await nftService.getNFTs(principal, 1, 10)
       await Promise.all(result.items.map(async (nft) => nft.init()))
       expect(result.items).toHaveLength(10)
       expect(result.totalPages).toEqual(2)

0bf3583da1124960b0870706d7736ce6b8753b4e
Vitalii <vitalii@identitylabs.ooo>
2024-10-25 15:29:13 +0300

    feat([sc-15564]): swap NAN calculated as a price impact [sc-15564]

--- apps/nfid-frontend/src/integration/icpswap/impl/quote-impl.ts
+++ apps/nfid-frontend/src/integration/icpswap/impl/quote-impl.ts
@@ -145,33 +145,10 @@ export class QuoteImpl implements Quote {
     const sourcePriceFormatted = sourcePrice
       .multipliedBy(this.getSourceAmount())
       .div(10 ** this.source.decimals)
-      .decimalPlaces(2)
 
     const targetPriceFormatted = targetPrice
       .multipliedBy(this.getTargetAmount())
       .div(10 ** this.target.decimals)
-      .decimalPlaces(2)
-
-    if (sourcePriceFormatted.eq(0) && targetPriceFormatted.eq(0)) {
-      return {
-        priceImpact: "0.00%",
-        status: PriceImpactStatus.LOW,
-      }
-    }
-
-    if (!sourcePriceFormatted.eq(0) && targetPriceFormatted.eq(0)) {
-      return {
-        priceImpact: "-100.00%",
-        status: PriceImpactStatus.HIGH,
-      }
-    }
-
-    if (sourcePriceFormatted.eq(0) && !targetPriceFormatted.eq(0)) {
-      return {
-        priceImpact: "100.00%",
-        status: PriceImpactStatus.LOW,
-      }
-    }
 
     const priceImpact = targetPriceFormatted
       .minus(sourcePriceFormatted)

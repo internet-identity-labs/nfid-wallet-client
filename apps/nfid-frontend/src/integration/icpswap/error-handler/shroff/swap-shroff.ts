6061cf87793023035206b2b1b21eff44e0066af0
olsemeno <sendtosemenov@gmail.com>
2024-11-04 13:49:22 +0400

    swap Canceling unsuccessful transaction doesn [sc-15498]

--- apps/nfid-frontend/src/integration/icpswap/error-handler/shroff/swap-shroff.ts
+++ apps/nfid-frontend/src/integration/icpswap/error-handler/shroff/swap-shroff.ts
@@ -1,25 +1,30 @@
-import { SignIdentity } from "@dfinity/agent"
-import { ShroffDepositErrorHandler } from "src/integration/icpswap/error-handler/shroff/deposit-shroff"
-import { ShroffBuilder } from "src/integration/icpswap/impl/shroff-impl"
-import { Shroff } from "src/integration/icpswap/shroff"
-import { SwapTransaction } from "src/integration/icpswap/swap-transaction"
+import {SignIdentity} from "@dfinity/agent"
+import {ShroffBuilder, ShroffImpl} from "src/integration/icpswap/impl/shroff-impl"
+import {Shroff} from "src/integration/icpswap/shroff"
+import {SwapTransaction} from "src/integration/icpswap/swap-transaction"
 
-import { replaceActorIdentity } from "@nfid/integration"
+import {replaceActorIdentity} from "@nfid/integration"
 
-export class ShroffSwapErrorHandler extends ShroffDepositErrorHandler {
+export class ShroffSwapErrorHandler extends ShroffImpl {
   async swap(delegationIdentity: SignIdentity): Promise<SwapTransaction> {
     if (!this.swapTransaction) {
       throw new Error("Swap transaction not set")
     }
     try {
       await replaceActorIdentity(this.swapPoolActor, delegationIdentity)
+      this.delegationIdentity = delegationIdentity
       console.debug("Transaction restarted")
-      await this.withdraw()
-      console.debug("Withdraw done")
-      this.swapTransaction.setCompleted()
-      await this.restoreTransaction()
-      console.debug("Transaction stored")
-      return this.swapTransaction
+      if (this.swapTransaction.getError() === undefined) {
+        console.debug("Swap timeout error")
+        return this.handleSwapTimeoutError()
+      } else {
+        await this.withdraw()
+        console.debug("Withdraw done")
+        this.swapTransaction!.setCompleted()
+        await this.restoreTransaction()
+        console.debug("Transaction stored")
+        return this.swapTransaction!
+      }
     } catch (e) {
       console.error("Swap error:", e)
       if (!this.swapTransaction.getError()) {
@@ -28,6 +33,15 @@ export class ShroffSwapErrorHandler extends ShroffDepositErrorHandler {
       await this.restoreTransaction()
       throw e
     }
+  }
+
+  private async handleSwapTimeoutError(): Promise<SwapTransaction> {
+    await super.withdraw()
+    console.debug("Withdraw done")
+    this.swapTransaction!.setCompleted()
+    await this.restoreTransaction()
+    console.debug("Transaction stored")
+    return this.swapTransaction!
   }
 }
 

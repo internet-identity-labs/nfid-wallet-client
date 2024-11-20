98647c1716b988c24772aeb461545e9c7cddec51
olsemeno <sendtosemenov@gmail.com>
2024-11-20 14:55:48 +0400

    Cache improvements [sc-777]

--- packages/integration/src/lib/token/icrc1/service/icrc1-oracle-service.ts
+++ packages/integration/src/lib/token/icrc1/service/icrc1-oracle-service.ts
@@ -1,7 +1,7 @@
-import {ICRC1, ICRC1Request} from "../../../_ic_api/icrc1_oracle.d"
-import {iCRC1OracleActor} from "../../../actors"
-import {localStorageTTL} from "../../../util/local-strage-ttl"
-import {ICRC1 as ICRC1Data} from "../types"
+import { ICRC1, ICRC1Request } from "../../../_ic_api/icrc1_oracle.d"
+import { iCRC1OracleActor } from "../../../actors"
+import { localStorageTTL } from "../../../util/local-strage-ttl"
+import { ICRC1 as ICRC1Data } from "../types"
 
 export const icrc1OracleCacheName = "ICRC1OracleService.getICRC1Canisters"
 
@@ -46,7 +46,7 @@ export class ICRC1OracleService {
   async requestNetworkForCanisters() {
     return await iCRC1OracleActor.count_icrc1_canisters().then((canisters) => {
       return Promise.all(
-        Array.from({length: Math.ceil(Number(canisters) / 25)}, (_, i) =>
+        Array.from({ length: Math.ceil(Number(canisters) / 25) }, (_, i) =>
           iCRC1OracleActor.get_icrc1_paginated(i * 25, 25),
         ),
       ).then((res) => res.flat())
@@ -54,23 +54,8 @@ export class ICRC1OracleService {
   }
 
   serializeCanisters(canister: Array<ICRC1>): string {
-    return JSON.stringify(canister.map((c) => {
-      return {
-        name: c.name,
-        ledger: c.ledger,
-        category: c.category,
-        index: c.index,
-        symbol: c.symbol,
-        logo: c.logo,
-        fee: Number(c.fee),
-        decimals: c.decimals,
-      }
-    }))
-  }
-
-  deserializeCanisters(canister: string): Array<ICRC1> {
-    return (JSON.parse(canister) as Array<ICRC1>)
-      .map((c) => {
+    return JSON.stringify(
+      canister.map((c) => {
         return {
           name: c.name,
           ledger: c.ledger,
@@ -78,10 +63,26 @@ export class ICRC1OracleService {
           index: c.index,
           symbol: c.symbol,
           logo: c.logo,
-          fee: BigInt(c.fee),
+          fee: Number(c.fee),
           decimals: c.decimals,
         }
-      })
+      }),
+    )
+  }
+
+  deserializeCanisters(canister: string): Array<ICRC1> {
+    return (JSON.parse(canister) as Array<ICRC1>).map((c) => {
+      return {
+        name: c.name,
+        ledger: c.ledger,
+        category: c.category,
+        index: c.index,
+        symbol: c.symbol,
+        logo: c.logo,
+        fee: BigInt(c.fee),
+        decimals: c.decimals,
+      }
+    })
   }
 }
 

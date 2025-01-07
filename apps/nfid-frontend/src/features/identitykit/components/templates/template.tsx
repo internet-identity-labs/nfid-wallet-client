bfebee823966ad38d385b9bf9451b8496cbbb0e2
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2025-01-07 12:14:58 +0400

    Implement security verification of target [sc-16197] (#2453)

--- apps/nfid-frontend/src/features/identitykit/components/templates/template.tsx
+++ apps/nfid-frontend/src/features/identitykit/components/templates/template.tsx
@@ -3,8 +3,8 @@ import React from "react"
 
 import { IconCmpNFIDWalletLogo, IconCmpShare } from "@nfid-frontend/ui"
 
-import LeftBgApproval from "./left-bg-approval.png"
-import LeftBg from "./left-bg.png"
+import LeftBgApproval from "../../assets/left-bg-approval.png"
+import LeftBg from "../../assets/left-bg.png"
 
 interface RPCTemplateProps {
   isApproveRequestInProgress: boolean
@@ -67,7 +67,7 @@ export const RPCTemplate: React.FC<RPCTemplateProps> = ({
         </a>
       </div>
       <div className="relative flex justify-center order-1 mt-20 lg:items-center lg:bg-white lg:order-2 lg:mt-0">
-        <div className="h-fit min-h-[520px] lg:min-h-0 mx-auto w-[90%] sm:max-w-[387px] flex flex-col bg-white rounded-3xl lg:rounded-none overflow-hidden lg:overflow-auto relative lg:static p-5 lg:p-[10px]">
+        <div className="h-fit min-h-[520px] lg:min-h-0 mx-auto w-[90%] sm:max-w-[407px] flex flex-col bg-white rounded-3xl lg:rounded-none overflow-hidden lg:overflow-auto relative lg:static p-5 lg:p-[10px]">
           {children}
         </div>
       </div>
---
+++ packages/ui/src/molecules/card/index.tsx
@@ -0,0 +1,100 @@
+import clsx from "clsx"
+import { ReactNode } from "react"
+
+import { IconCmpWarning } from "@nfid-frontend/ui"
+
+export enum CardType {
+  WARNING,
+  NOTICE,
+}
+
+export interface ICard {
+  title?: string
+  text: string | ReactNode
+  link?: {
+    text: string
+    url: string
+  }
+  classNames?: string
+  hasIcon?: boolean
+  green?: boolean
+  type?: CardType
+}
+
+export const Card = ({
+  title,
+  text,
+  link,
+  classNames,
+  hasIcon = true,
+  type = CardType.WARNING,
+}: ICard) => (
+  <div
+    className={clsx(
+      type === CardType.WARNING && "bg-orange-50",
+      type === CardType.NOTICE && "bg-green-50",
+      "w-full p-[15px] my-3 rounded-[12px]",
+      classNames,
+    )}
+  >
+    {title && (
+      <div className={clsx("flex items-center justify-between")}>
+        <div className="flex items-center text-sm font-bold">
+          {hasIcon && (
+            <IconCmpWarning
+              className={clsx(
+                type === CardType.WARNING && "text-orange-900",
+                type === CardType.NOTICE && "text-green-900",
+                "w-[22px] mr-2.5",
+              )}
+            />
+          )}
+
+          <p
+            className={clsx(
+              type === CardType.WARNING && "text-orange-900",
+              type === CardType.NOTICE && "text-green-900",
+              "text-sm font-bold",
+            )}
+          >
+            {title}
+          </p>
+        </div>
+      </div>
+    )}
+    <div className={clsx("space-y-3 text-sm", title && "pl-8")}>
+      <div>
+        <div className="flex gap-[10px]">
+          {!title && hasIcon && (
+            <IconCmpWarning
+              className={clsx(
+                type === CardType.WARNING && "text-orange-900",
+                type === CardType.NOTICE && "text-green-900",
+                "h-[22px] w-[22px] shrink-0",
+              )}
+            />
+          )}
+          <p
+            className={clsx(
+              type === CardType.WARNING && "text-orange-900",
+              type === CardType.NOTICE && "text-green-900",
+              "text-sm",
+              classNames,
+            )}
+          >
+            {link && (
+              <a
+                className="text-primaryButtonColor"
+                href={link.url}
+                target="_blank"
+              >
+                {link.text}&nbsp;
+              </a>
+            )}
+            {text}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+)

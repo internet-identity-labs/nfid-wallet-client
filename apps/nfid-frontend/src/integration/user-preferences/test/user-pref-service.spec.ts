6086dccac9149a68153bf2bc6ca961f034b1cb9c
olsemeno <33149496+olsemeno@users.noreply.github.com>
2025-01-08 10:21:32 +0100

    Implement the show hide zero balance token [sc-16566] (#2474)
    * Implement the show hide zero balance token [sc-16566]
    
    * Implement the show hide zero balance token [sc-16566]
    
    * Implement the show hide zero balance token [sc-16566]
    
    * Implement the show hide zero balance token [sc-16566]
    
    * Implement the show hide zero balance token [sc-16566]
    
    ---------
    
    Co-authored-by: Artem Nadiein <168751040+NadieinNFID@users.noreply.github.com>

---
+++ apps/nfid-frontend/src/integration/user-preferences/test/user-pref-service.spec.ts
@@ -0,0 +1,18 @@
+import { authStorage } from "packages/integration/src/lib/authentication/storage"
+import { userPrefService } from "src/integration/user-preferences/user-pref-service"
+
+describe("userpref service test suite", () => {
+  it("should get user preferences", async () => {
+    await userPrefService.getUserPreferences().then((userPreferences) => {
+      expect(userPreferences.isHideZeroBalance()).toBe(false)
+      userPreferences.setHideZeroBalance(true).then(() => {
+         userPrefService.getUserPreferences().then((userPreferences) => {
+          expect(userPreferences.isHideZeroBalance()).toBe(true)
+        })
+        authStorage.get("userPreferences").then((userPreferencesString) => {
+          expect(userPreferencesString).toBe('{"hideZeroBalance":true}')
+        })
+      })
+    })
+  })
+})

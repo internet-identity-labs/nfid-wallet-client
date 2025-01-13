afc64ddf678f3b9e2ae8652bb0cf471e464b6332
olsemeno <sendtosemenov@gmail.com>
2025-01-13 11:22:56 +0400

    Investigate swap implementation via Kongswap [sc-16394]

--- apps/nfid-frontend/src/integration/swap/kong/impl/kong-calculator.ts
+++ apps/nfid-frontend/src/integration/swap/kong/impl/kong-calculator.ts
@@ -7,13 +7,24 @@ import {
 export class KongCalculator extends CalculatorAbstract {
   calculateSourceSwapAmount(): bigint {
     return (
-      this.userInputAmount - this.sourceFee - this.widgetFee - this.sourceFee
+      this.userInputAmount -
+      this.sourceFee -
+      this.widgetFee -
+      this.sourceFee -
+      this.sourceFee
     )
   }
 
   calculateWidgetFee(): bigint {
     return BigInt(
-      BigNumber(Number(this.userInputAmount - this.sourceFee - this.sourceFee))
+      BigNumber(
+        Number(
+          this.userInputAmount -
+            this.sourceFee -
+            this.sourceFee -
+            this.sourceFee,
+        ),
+      )
         .multipliedBy(WIDGET_FEE)
         .toFixed(0),
     )

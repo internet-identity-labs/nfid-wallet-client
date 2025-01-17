cd60aa6f6a5ad499198894f24f005b825ebbef6c
NadieinNFID <artem@identitylabs.ooo>
2025-01-17 16:25:20 +0100

    Remove husky and commitlint [sc-16668]

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rules: {
-    "header-max-length": [2, "always", 100],
-    "subject-empty": [0],
-    "type-empty": [0],
-    "footer-max-line-length": [2, "always", 100],
-  },
-}

d9192d5d564e18b76ca9d6262c2ad05ece8a1a5e
Artem Nadiein <168751040+NadieinNFID@users.noreply.github.com>
2025-01-21 00:39:15 +1100

    Update landing page and fix warnings [sc-777] (#2543)

--- apps/nfid-frontend/src/features/identitykit/components/templates/template.tsx
+++ apps/nfid-frontend/src/features/identitykit/components/templates/template.tsx
@@ -43,12 +43,12 @@ export const RPCTemplate: React.FC<RPCTemplateProps> = ({
           <p className="gradient-text text-[34px] lg:text-[2.9rem] xl:text-[3.2rem] font-bold leading-[120%]">
             {isApproveRequestInProgress
               ? "Transaction request"
-              : "Your crypto wallet & gateway to ICP apps"}
+              : "The easiest to use and hardest to lose wallet"}
           </p>
           <p className="font-inter text-white text-[20px] lg:text-[1.4rem] mt-[2rem] leading-[140%]">
             {isApproveRequestInProgress
               ? "Verify this transaction’s accuracy and make sure you trust the app requesting its approval."
-              : "NFID Wallet is your on-chain wallet running end-to-end on the Internet Computer Protocol."}
+              : "Secure your assets with the most audited wallet on ICP and start exploring ICP applications in seconds."}
           </p>
         </div>
         <a

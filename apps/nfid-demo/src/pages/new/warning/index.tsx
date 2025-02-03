1a05be3e7ec2919fbdc5123afd21845e8c781c12
Dmitrii <dmitrii@identitylabs.ooo>
2025-02-03 12:35:34 +0400

    Remove nfid embed call canisters from Lambda [sc-17102]

---
+++ apps/nfid-demo/src/pages/new/warning/index.tsx
@@ -0,0 +1,14 @@
+import WarningIcon from "../../../assets/warning.svg"
+
+export const Warning = ({ children }: { children: string }) => {
+  return (
+    <div className="flex items-center space-x-2 p-5 bg-orange-50 h-16 rounded-lg">
+      <img
+        className="w-6 h-6 rounded-full"
+        src={WarningIcon}
+        alt="Deprecated Warning"
+      />
+      <p className="text-sm font-normal text-orange-900">{children}</p>
+    </div>
+  )
+}

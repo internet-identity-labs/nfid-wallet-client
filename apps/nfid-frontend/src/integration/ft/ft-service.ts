0fd936479a624c5e6855da587af42734b04b5015
Vitalii <vitalii@identitylabs.ooo>
2025-02-27 14:59:21 +0200

    NFIDW token isn t included in the default [sc-17558]

--- apps/nfid-frontend/src/integration/ft/ft-service.ts
+++ apps/nfid-frontend/src/integration/ft/ft-service.ts
@@ -49,40 +49,38 @@ export const filterTokens = (ft: FT[], filterText: string): FT[] => {
   )
 }
 
-async getTokens(userId: string): Promise<Array<FT>> {
-  try {
-    let canisters = await icrc1StorageService.getICRC1Canisters(userId);
+export class FtService {
+  async getTokens(userId: string): Promise<Array<FT>> {
+    return icrc1StorageService
+      .getICRC1Canisters(userId)
+      .then(async (canisters) => {
+        const icp = canisters.find(
+          (canister) => canister.ledger === ICP_CANISTER_ID,
+        )
 
-    const icp = canisters.find(
-      (canister) => canister.ledger === ICP_CANISTER_ID
-    );
+        const nfidw = canisters.find(
+          (canister) => canister.ledger === NFIDW_CANISTER_ID,
+        )
 
-    const nfidw = canisters.find(
-      (canister) => canister.ledger === NFIDW_CANISTER_ID
-    );
+        if (!icp || icp.state === State.Inactive) {
+          await icrc1RegistryService.storeICRC1Canister(
+            ICP_CANISTER_ID,
+            State.Active,
+          )
+        }
 
-    // If ICP canister is inactive, update it
-    if (!icp || icp.state === State.Inactive) {
-      await icrc1RegistryService.storeICRC1Canister(
-        ICP_CANISTER_ID,
-        State.Active
-      );
-    }
+        if (!nfidw || nfidw.state === State.Inactive) {
+          await icrc1RegistryService.storeICRC1Canister(
+            NFIDW_CANISTER_ID,
+            State.Active,
+          )
+        }
 
-    // If NFIDW canister is inactive, update it
-    if (!nfidw || nfidw.state === State.Inactive) {
-      await icrc1RegistryService.storeICRC1Canister(
-        NFIDW_CANISTER_ID,
-        State.Active
-      );
-    }
+        canisters = await icrc1StorageService.getICRC1Canisters(userId)
 
-    // Fetch canisters again only if necessary after state changes
-    canisters = await icrc1StorageService.getICRC1Canisters(userId);
-
-    // Map to FT instances and sort tokens
-    const ft = canisters.map((canister) => new FTImpl(canister));
-    return sortTokens(ft);
+        const ft = canisters.map((canister) => new FTImpl(canister))
+        return sortTokens(ft)
+      })
   }
 
   async filterNotActiveNotZeroBalancesTokens(

a269c7c84a06d0ff673e05b140668b5aed72dfe8
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2025-02-28 16:05:57 +0400

    Merge sc 16250 branch into the main [sc-17433] (#2647)

---
+++ apps/nfid-extension/src/features/authentication/3rd-party/choose-account/services.ts
@@ -0,0 +1,34 @@
+import { AccountIdentifier } from "@dfinity/ledger-icp"
+import { Principal } from "@dfinity/principal"
+import { getPublicKey } from "packages/integration/src/lib/delegation-factory/delegation-i"
+
+import { authState, getBalance } from "@nfid/integration"
+
+import { getExchangeRate } from "frontend/integration/rosetta/get-exchange-rate"
+import { e8sICPToString } from "frontend/integration/wallet/utils"
+
+export const getPublicProfile = async (): Promise<{
+  balance: string
+  balanceUSD: string
+  address: string
+  principal: Principal
+}> => {
+  const { delegationIdentity } = authState.get()
+  if (!delegationIdentity) throw new Error("No identity")
+
+  const principalString = await getPublicKey(delegationIdentity!)
+  const principal = Principal.fromText(principalString)
+  const address = AccountIdentifier.fromPrincipal({ principal }).toHex()
+  const balance = e8sICPToString(Number(await getBalance(address)))
+  const exchangeRate = await getExchangeRate("ICP")
+
+  return {
+    balance: balance,
+    balanceUSD:
+      balance === "0"
+        ? "0.00 USD"
+        : `${(exchangeRate * Number(balance)).toFixed(2)} USD`,
+    address,
+    principal,
+  }
+}
---
+++ apps/nfid-extension/src/features/authentication/auth-selection/email-flow/machine.typegen.ts
@@ -0,0 +1,75 @@
+// This file was automatically generated. Edits will be overwritten
+
+export interface Typegen0 {
+  "@@xstate/typegen": true
+  internalEvents: {
+    "done.invoke.authorizeWithEmail": {
+      type: "done.invoke.authorizeWithEmail"
+      data: unknown
+      __tip: "See the XState TS docs to learn how to strongly type this."
+    }
+    "done.invoke.checkEmailVerification": {
+      type: "done.invoke.checkEmailVerification"
+      data: unknown
+      __tip: "See the XState TS docs to learn how to strongly type this."
+    }
+    "done.invoke.sendVerificationEmail": {
+      type: "done.invoke.sendVerificationEmail"
+      data: unknown
+      __tip: "See the XState TS docs to learn how to strongly type this."
+    }
+    "error.platform.authorizeWithEmail": {
+      type: "error.platform.authorizeWithEmail"
+      data: unknown
+    }
+    "error.platform.checkEmailVerification": {
+      type: "error.platform.checkEmailVerification"
+      data: unknown
+    }
+    "error.platform.sendVerificationEmail": {
+      type: "error.platform.sendVerificationEmail"
+      data: unknown
+    }
+    "xstate.init": { type: "xstate.init" }
+  }
+  invokeSrcNameMap: {
+    authorizeWithEmail: "done.invoke.authorizeWithEmail"
+    checkEmailVerification: "done.invoke.checkEmailVerification"
+    sendVerificationEmail: "done.invoke.sendVerificationEmail"
+  }
+  missingImplementations: {
+    actions: never
+    delays: never
+    guards: never
+    services: never
+  }
+  eventsCausingActions: {
+    assignAuthSession: "done.invoke.authorizeWithEmail"
+    assignEmailDelegation: "done.invoke.checkEmailVerification"
+    assignVerificationData: "done.invoke.sendVerificationEmail"
+    stopIntervalVerification:
+      | "BACK"
+      | "RESEND"
+      | "error.platform.checkEmailVerification"
+    toastError: "error.platform.sendVerificationEmail"
+  }
+  eventsCausingDelays: {}
+  eventsCausingGuards: {
+    isRequestNotExpired: "error.platform.sendVerificationEmail"
+  }
+  eventsCausingServices: {
+    authorizeWithEmail: "done.invoke.checkEmailVerification"
+    checkEmailVerification:
+      | "done.invoke.sendVerificationEmail"
+      | "error.platform.sendVerificationEmail"
+    sendVerificationEmail: "RESEND" | "xstate.init"
+  }
+  matchesStates:
+    | "Authenticated"
+    | "EmailVerified"
+    | "End"
+    | "Error"
+    | "PendingEmailVerification"
+    | "SendVerificationEmail"
+  tags: never
+}

8125ba57d78feec680ec34ba2b051e2c9a057fc7
olsemeno <33149496+olsemeno@users.noreply.github.com>
2025-03-06 21:15:52 +0100

    Implement basic logic to create stakes [sc-17569] (#2661)
    * Implement basic logic to create stakes [sc-17569]
    
    * Implement basic logic to create stakes [sc-17569]
    
    * Implement basic logic to create stakes [sc-17569]
    
    * Implement basic logic to create stakes [sc-17569]

---
+++ apps/nfid-frontend/src/integration/staking/redeem-calculator.ts
@@ -0,0 +1,9 @@
+import { NFIDNeuron } from "src/integration/staking/nfid-neuron"
+
+export interface RedeemCalculator {
+  setNeuron(neuron: NFIDNeuron): Promise<void>
+  getInitialStake(): string
+  getReward(): string
+  getProtocolFee(): string
+  getTotalRedemption(): string
+}

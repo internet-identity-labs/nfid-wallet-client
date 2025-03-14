c3aa63b14f2d089c9a14591507f981ff349f39ba
vitaliipustovyi <167295310+vitaliipustovyi@users.noreply.github.com>
2025-03-14 12:08:50 +0200

    Create storybook components for the staking [sc-17573] (#2691)
    Create storybook components for the Staking [sc-17572]

--- apps/nfid-frontend/src/features/staking-details/index.tsx
+++ apps/nfid-frontend/src/features/staking-details/index.tsx
@@ -6,8 +6,26 @@ import { useParams } from "react-router-dom"
 
 import { useSWR } from "@nfid/swr"
 
+<<<<<<< HEAD
 import { stakingService } from "frontend/integration/staking/service/staking-service-impl"
 import { ProfileContext } from "frontend/provider"
+=======
+export interface IStakingOption {
+  id: string
+  initial: string
+  initialInUsd: string
+  rewards: string
+  rewardsInUsd: string
+  total: string
+  totalInUsd: string
+  lockTime: string
+  unlockIn?: string
+  isDiamond?: boolean
+  createdAt: number
+  unlockAt?: number
+  type: StakingOptions
+}
+>>>>>>> 782a690988 (Create storybook components for the staking [sc-17573] (#2691))
 
 import { fetchStakedToken } from "../staking/utils"
 import { ModalType } from "../transfer-modal/types"
@@ -17,6 +35,7 @@ const StakingDetailsPage = () => {
   const { tokenSymbol } = useParams()
   const [identity, setIdentity] = useState<SignIdentity>()
 
+<<<<<<< HEAD
   const globalServices = useContext(ProfileContext)
   const [, send] = useActor(globalServices.transferService)
 
@@ -52,6 +71,66 @@ const StakingDetailsPage = () => {
   const onRedeemOpen = () => {
     send({ type: "CHANGE_DIRECTION", data: ModalType.REDEEM })
     send("SHOW")
+=======
+  const stakeOptions = {
+    Available: [
+      {
+        id: "5695121862339497860",
+        initial: "2,000.00 ICP",
+        initialInUsd: "14,207.03 USD",
+        rewards: "40.08 ICP",
+        rewardsInUsd: "284.71 USD",
+        total: "204.754 ICP",
+        totalInUsd: "2514.47 USD",
+        lockTime: "2 years",
+        createdAt: 1656295343000,
+        isDiamond: true,
+        type: StakingOptions.Available,
+      },
+    ],
+    Unlocking: [
+      {
+        id: "5695121862339497861",
+        initial: "2,000.00 ICP",
+        initialInUsd: "14,207.03 USD",
+        rewards: "40.08 ICP",
+        rewardsInUsd: "284.71 USD",
+        total: "204.754 ICP",
+        totalInUsd: "2514.47 USD",
+        lockTime: "2 years",
+        unlockIn: "4 months, 124 days",
+        createdAt: 1656295343000,
+        unlockAt: 1656295343000,
+        type: StakingOptions.Unlocking,
+      },
+    ],
+    Locked: [
+      {
+        id: "5695121862339497862",
+        initial: "2,000.00 ICP",
+        initialInUsd: "14,207.03 USD",
+        rewards: "40.08 ICP",
+        rewardsInUsd: "284.71 USD",
+        total: "204.754 ICP",
+        totalInUsd: "2514.47 USD",
+        lockTime: "2 years",
+        createdAt: 1656295343000,
+        type: StakingOptions.Locked,
+      },
+      {
+        id: "5695121862339497863",
+        initial: "2,000.00 ICP",
+        initialInUsd: "14,207.03 USD",
+        rewards: "40.08 ICP",
+        rewardsInUsd: "284.71 USD",
+        total: "204.754 ICP",
+        totalInUsd: "2514.47 USD",
+        lockTime: "2 years",
+        createdAt: 1656295343000,
+        type: StakingOptions.Locked,
+      },
+    ],
+>>>>>>> 782a690988 (Create storybook components for the staking [sc-17573] (#2691))
   }
 
   return (

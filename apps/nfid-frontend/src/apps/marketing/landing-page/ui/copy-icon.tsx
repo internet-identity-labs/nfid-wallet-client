01c8e9289d4d60836c6bc4564a6d6c7fb96e30b4
slimHmac <dyukit99@gmail.com>
2025-03-19 16:25:03 +0700

    wallet mobile design discrepancies on landing [sc-17709]

--- apps/nfid-frontend/src/apps/marketing/landing-page/ui/copy-icon.tsx
+++ apps/nfid-frontend/src/apps/marketing/landing-page/ui/copy-icon.tsx
@@ -1,9 +1,9 @@
 export function CopyIcon() {
   return (
     <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -25,7 +25,7 @@ export function CopyIcon() {
       </g>
       <defs>
         <clipPath id="clip0_66199_75256">
-          <rect width="16" height="16" fill="#14B8A6" />
+          <rect width="18" height="18" fill="#14B8A6" />
         </clipPath>
       </defs>
     </svg>

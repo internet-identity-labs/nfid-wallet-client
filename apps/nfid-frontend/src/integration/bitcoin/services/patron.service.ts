ef05346718f35be35719dafb0a23f6f6a0479732
dmytroidentitylabs <dmytroidentitylabs@gmail.com>
2025-05-26 20:40:12 +0400

    Implement BTC sending for the send window [sc-17811] (#2751)

--- apps/nfid-frontend/src/integration/bitcoin/services/patron.service.ts
+++ apps/nfid-frontend/src/integration/bitcoin/services/patron.service.ts
@@ -1,4 +1,4 @@
-import { HttpAgent, SignIdentity } from "@dfinity/agent"
+import { ActorSubclass, HttpAgent, SignIdentity } from "@dfinity/agent"
 import { Principal } from "@dfinity/principal"
 import { authStorage } from "packages/integration/src/lib/authentication/storage"
 
@@ -6,7 +6,12 @@ import { actor, agentBaseConfig } from "@nfid/integration"
 
 import { Account, PaymentType } from "../idl/chain-fusion-signer.d"
 import { idlFactory as patronIDL } from "../idl/patron"
-import { _SERVICE as Patron } from "../idl/patron.d"
+import {
+  _SERVICE as Patron,
+  SelectedUtxosFeeRequest,
+  SelectedUtxosFeeResponse,
+} from "../idl/patron.d"
+import { satoshiService } from "./satoshi.service"
 
 export class PatronService {
   public async askToPayFor(identity: SignIdentity): Promise<void> {
@@ -17,9 +22,7 @@ export class PatronService {
       return
     }
 
-    const patronActor = actor<Patron>(PATRON_CANISTER_ID, patronIDL, {
-      agent: HttpAgent.createSync({ ...agentBaseConfig, identity }),
-    })
+    const patronActor = this.getPatronActor(identity)
 
     await patronActor.allow_signing([])
     await authStorage.set(key, "v1")
@@ -33,6 +36,35 @@ export class PatronService {
 
     const payment: PaymentType = { PatronPaysIcrc2Cycles: patronAccount }
     return payment
+  }
+
+  public async askToCalcUtxosAndFee(
+    identity: SignIdentity,
+    amount: string,
+  ): Promise<SelectedUtxosFeeResponse> {
+    const patronActor = this.getPatronActor(identity)
+    const amountInSatoshis = satoshiService.getInSatoshis(amount)
+
+    const request: SelectedUtxosFeeRequest = {
+      network: { mainnet: null },
+      amount_satoshis: amountInSatoshis,
+      min_confirmations: [6],
+    }
+
+    const response = await patronActor.btc_select_user_utxos_fee(request)
+
+    if ("Err" in response) {
+      console.error(response)
+      throw Error("Unable to calculate transaction fee for BTC.")
+    }
+
+    return response.Ok
+  }
+
+  private getPatronActor(identity: SignIdentity): ActorSubclass<Patron> {
+    return actor<Patron>(PATRON_CANISTER_ID, patronIDL, {
+      agent: HttpAgent.createSync({ ...agentBaseConfig, identity }),
+    })
   }
 }
 

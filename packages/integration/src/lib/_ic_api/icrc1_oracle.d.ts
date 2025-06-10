ade6f4fa401932906aaeef27a55424d21d043144
olsemeno <sendtosemenov@gmail.com>
2025-06-10 10:20:20 +0400

    Implement getFee method for BTC [sc-17979]

--- packages/integration/src/lib/_ic_api/icrc1_oracle.d.ts
+++ packages/integration/src/lib/_ic_api/icrc1_oracle.d.ts
@@ -2,6 +2,13 @@ import type { ActorMethod } from "@dfinity/agent"
 import type { IDL } from "@dfinity/candid"
 import type { Principal } from "@dfinity/principal"
 
+export type BitcoinNetwork =
+  | { mainnet: null }
+  | { regtest: null }
+  | { testnet: null }
+export type BtcSelectUserUtxosFeeResult =
+  | { Ok: SelectedUtxosFeeResponse }
+  | { Err: SelectedUtxosFeeError }
 export type Category =
   | { Sns: null }
   | { Spam: null }
@@ -11,19 +18,20 @@ export type Category =
   | { ChainFusion: null }
   | { Community: null }
 export interface Conf {
-  controllers: [] | [Array<Principal>]
+  operator: [] | [Principal]
   im_canister: [] | [Principal]
 }
 export interface ICRC1 {
   fee: bigint
+  root_canister_id: [] | [string]
   decimals: number
   logo: [] | [string]
   name: string
+  date_added: bigint
   ledger: string
   category: Category
   index: [] | [string]
   symbol: string
-  root_canister_id: [] | [string]
 }
 export interface ICRC1Request {
   fee: bigint
@@ -34,14 +42,47 @@ export interface ICRC1Request {
   index: [] | [string]
   symbol: string
 }
+export interface NeuronData {
+  name: string
+  date_added: bigint
+  ledger: string
+  neuron_id: string
+}
+export interface Outpoint {
+  txid: Uint8Array | number[]
+  vout: number
+}
+export type SelectedUtxosFeeError = { InternalError: { msg: string } }
+export interface SelectedUtxosFeeRequest {
+  network: BitcoinNetwork
+  amount_satoshis: bigint
+  min_confirmations: [] | [number]
+}
+export interface SelectedUtxosFeeResponse {
+  fee_satoshis: bigint
+  utxos: Array<Utxo>
+}
+export interface Utxo {
+  height: number
+  value: bigint
+  outpoint: Outpoint
+}
 export interface _SERVICE {
-  allow_signing : ActorMethod<[], undefined>,
+  allow_signing: ActorMethod<[], undefined>
+  btc_select_user_utxos_fee: ActorMethod<
+    [SelectedUtxosFeeRequest],
+    BtcSelectUserUtxosFeeResult
+  >
   count_icrc1_canisters: ActorMethod<[], bigint>
   get_all_icrc1_canisters: ActorMethod<[], Array<ICRC1>>
-  get_icrc1_paginated: ActorMethod<[number, number], Array<ICRC1>>
+  get_all_neurons: ActorMethod<[], Array<NeuronData>>
+  get_icrc1_paginated: ActorMethod<[bigint, bigint], Array<ICRC1>>
+  remove_icrc1_canister: ActorMethod<[string], undefined>
+  replace_all_neurons: ActorMethod<[Array<NeuronData>], undefined>
   replace_icrc1_canisters: ActorMethod<[Array<ICRC1>], undefined>
+  set_operator: ActorMethod<[Principal], undefined>
   store_icrc1_canister: ActorMethod<[ICRC1Request], undefined>
   store_new_icrc1_canisters: ActorMethod<[Array<ICRC1>], undefined>
-  sync_controllers: ActorMethod<[], Array<string>>
 }
 export declare const idlFactory: IDL.InterfaceFactory
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[]

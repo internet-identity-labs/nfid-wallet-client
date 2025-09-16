5c1749464a44fc3a5ece4095ac6aa82396f5c45d
Vitalii Pustovyi <vetalsd2@gmail.com>
2025-09-16 14:24:26 +0300

    wallet Choose your wallet screen isn t triggered [sc-18336] (#2910)
    wallet Choose your wallet screen isn t triggered [sc-18336]

--- packages/integration/src/lib/authentication/storage.ts
+++ packages/integration/src/lib/authentication/storage.ts
@@ -3,6 +3,9 @@ import { Storage } from "@nfid/client-db"
 const AUTH_DB_NAME = "authstate"
 const OBJECT_STORE_NAME = "ic-keyval"
 
+const WALLET_DB_NAME = "wallets"
+const WALLET_STORE_NAME = "wallets-store"
+
 export const KEY_STORAGE_KEY = "identity"
 export const KEY_STORAGE_DELEGATION = "delegation"
 export const KEY_BTC_ADDRESS = "bitcoin-address"
@@ -13,4 +16,9 @@ export type StoredKey = string | CryptoKeyPair
 export const authStorage = new Storage<StoredKey>({
   dbName: AUTH_DB_NAME,
   storeName: OBJECT_STORE_NAME,
+})
+
+export const walletStorage = new Storage<StoredKey>({
+  dbName: WALLET_DB_NAME,
+  storeName: WALLET_STORE_NAME,
 })

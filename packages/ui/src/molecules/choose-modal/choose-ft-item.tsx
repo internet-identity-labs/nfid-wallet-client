656c205b5f4b53d508be35645a7ecb4d3048ab74
Vitalii Pustovyi <vetalsd2@gmail.com>
2025-11-05 20:05:13 +0200

    Send Modal Optimization [sc-18782] (#2940)
    Send Modal Optimization [sc-18782]

--- packages/ui/src/molecules/choose-modal/choose-ft-item.tsx
+++ packages/ui/src/molecules/choose-modal/choose-ft-item.tsx
@@ -8,21 +8,30 @@ import { TokensAvailableToSwap } from "frontend/integration/ft/ft-service"
 import { IconNftPlaceholder } from "../../atoms/icons"
 import ImageWithFallback from "../../atoms/image-with-fallback"
 import { Skeleton } from "../../atoms/skeleton"
+import { BTC_NATIVE_ID, ETH_NATIVE_ID } from "@nfid/integration/token/constants"
 
 interface IChooseFtItem {
   token: FT
   isSwapTo?: boolean
   tokensAvailableToSwap?: TokensAvailableToSwap
+  isBtcEthLoading?: boolean
 }
 
 export const ChooseFtItem = ({
   token,
   isSwapTo,
   tokensAvailableToSwap,
+  isBtcEthLoading,
 }: IChooseFtItem) => {
-  const isTokenAvailable = isSwapTo
-    ? tokensAvailableToSwap?.to.includes(token.getTokenAddress()) ?? false
-    : tokensAvailableToSwap?.from.includes(token.getTokenAddress()) ?? false
+  const isTokenAvailable =
+    (isSwapTo
+      ? tokensAvailableToSwap?.to.includes(token.getTokenAddress())
+      : tokensAvailableToSwap?.from.includes(token.getTokenAddress())) ??
+    (false ||
+      !(
+        isBtcEthLoading &&
+        [BTC_NATIVE_ID, ETH_NATIVE_ID].includes(token.getTokenAddress())
+      ))
 
   return (
     <div
@@ -31,9 +40,7 @@ export const ChooseFtItem = ({
         "hover:opacity-50 transition-opacity",
         "flex items-center justify-between",
         "py-2.5 h-[60px]",
-        !isTokenAvailable && tokensAvailableToSwap
-          ? "cursor-not-allowed"
-          : "cursor-pointer",
+        !isTokenAvailable ? "cursor-not-allowed" : "cursor-pointer",
       )}
     >
       <div className="flex items-center h-[28px]">
@@ -46,9 +53,7 @@ export const ChooseFtItem = ({
             src={token.getTokenLogo() || "#"}
             className={clsx(
               "mr-[18px] w-[28px] h-[28px] object-cover rounded-full",
-              !isTokenAvailable &&
-                tokensAvailableToSwap &&
-                "grayscale opacity-40",
+              !isTokenAvailable && "grayscale opacity-40",
             )}
           />
         )}
@@ -56,7 +61,7 @@ export const ChooseFtItem = ({
           <p
             className={clsx(
               "text-sm mb-0.5 flex items-center space-x-1",
-              !isTokenAvailable && tokensAvailableToSwap && "text-gray-400",
+              !isTokenAvailable && "text-gray-400",
             )}
           >
             <span className="font-semibold">{token.getTokenSymbol()}</span>
@@ -66,23 +71,21 @@ export const ChooseFtItem = ({
           </p>
         </div>
       </div>
-      {token.isInited() ? (
-        <div
-          className={clsx(
-            !isTokenAvailable && tokensAvailableToSwap && "text-gray-400",
-          )}
-        >
+      {!token.isInited() ||
+      (isBtcEthLoading &&
+        [BTC_NATIVE_ID, ETH_NATIVE_ID].includes(token.getTokenAddress())) ? (
+        <div>
+          <Skeleton className="rounded-[6px] h-[20px] w-[80px] mb-[5px]" />
+          <Skeleton className="rounded-[6px] h-[16px] w-[60px] ml-auto" />
+        </div>
+      ) : (
+        <div className={clsx(!isTokenAvailable && "text-gray-400")}>
           <p className="text-sm text-right">{`${
             token.getTokenBalanceFormatted() || "0"
           } ${token.getTokenSymbol()}`}</p>
           <p className="text-xs text-right text-gray-400 dark:text-zinc-500">
             {token.getUSDBalanceFormatted() ?? "Not listed"}
           </p>
-        </div>
-      ) : (
-        <div>
-          <Skeleton className="rounded-[6px] h-[20px] w-[80px] mb-[5px]" />
-          <Skeleton className="rounded-[6px] h-[16px] w-[60px] ml-auto" />
         </div>
       )}
     </div>

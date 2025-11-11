c03ff0a5846948ae572c472fcaec2ad9f2d8fece
Vitalii Pustovyi <vetalsd2@gmail.com>
2025-11-11 13:59:14 +0200

    wallet Unexpected dark font on the error [sc-18801] (#2945)

--- packages/ui/src/organisms/authentication/error/index.tsx
+++ packages/ui/src/organisms/authentication/error/index.tsx
@@ -14,13 +14,13 @@ export const AuthEmailError: React.FC<AuthEmailErrorProps> = ({
   return (
     <div className="flex flex-col w-full h-full text-sm text-center">
       <IconCmpArrow
-        className="absolute transition-opacity cursor-pointer left-5 top-5 hover:opacity-50 shrink-0"
+        className="absolute transition-opacity cursor-pointer left-5 top-5 hover:opacity-50 shrink-0 dark:text-white"
         onClick={onBack}
       />
-      <p className="text-center font-bold mt-[30px] text-lg">
+      <p className="text-center font-bold mt-[30px] text-lg dark:text-white">
         Something went wrong
       </p>
-      <p className="mt-2.5">
+      <p className="mt-2.5 dark:text-white">
         Magic link verification failed. Please try again.
       </p>
       <img

9fca646fad0ddc649a3ab20686a13aba24bfcc1e
Vitalii Pustovyi <vetalsd2@gmail.com>
2025-11-19 18:14:24 +0200

    Implement Revoke All feature [sc-18837] (#2951)
    Implement Revoke All feature [sc-18837]

--- packages/ui/src/organisms/permissions/PermissionsRow.tsx
+++ packages/ui/src/organisms/permissions/PermissionsRow.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx"
-import { FC, useMemo, useState } from "react"
-
+import { FC } from "react"
 import {
   ImageWithFallback,
   IconNftPlaceholder,
@@ -9,51 +8,18 @@ import {
   Skeleton,
 } from "@nfid-frontend/ui"
 import { Allowance } from "."
-import { Principal } from "@dfinity/principal"
-import { useIdentity } from "frontend/hooks/identity"
-import { Spinner } from "../../atoms/spinner"
-import { mutate } from "@nfid/swr"
-import { FT } from "frontend/integration/ft/ft"
-import { AllowanceDetailDTO } from "@nfid/integration/token/icrc1/types"
-import { updateAllowancesAfterRevoke } from "frontend/features/transfer-modal/utils"
-import toaster from "../../atoms/toast"
-import { ModalComponent } from "../../molecules/modal/index-v0"
 
-export const PermissionsToken: FC<Allowance> = ({
-  token,
-  address,
-  usdAmount,
-  amountFormatted,
+interface PermissionsRowProps {
+  allowance?: Allowance
+  onChooseAllowance: (a: Allowance) => void
+}
+
+export const PermissionsToken: FC<PermissionsRowProps> = ({
+  allowance,
+  onChooseAllowance,
 }) => {
-  const [isLoading, setIsLoading] = useState(false)
-  const { identity, isLoading: identityLoading } = useIdentity()
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const handleRevoke = async () => {
-    setIsLoading(true)
-    try {
-      await token.revokeAllowance(identity!, Principal.from(address))
-      toaster.success(
-        `Access to ${amountFormatted} has been successfully revoked`,
-      )
-      mutate<
-        {
-          token: FT
-          allowances: AllowanceDetailDTO[]
-        }[]
-      >(
-        "allowances",
-        (current) => updateAllowancesAfterRevoke(current, token, address),
-        false,
-      )
-    } catch (e) {
-      toaster.error("An error occurred while revoking access.")
-    } finally {
-      setIsLoading(false)
-      setIsModalOpen(false)
-    }
-  }
-
+  if (!allowance) return null
+  const { token, address, usdAmount, amountFormatted } = allowance
   return (
     <>
       <tr
@@ -119,7 +85,7 @@ export const PermissionsToken: FC<Allowance> = ({
               className="mt-2.5"
               type="stroke"
               isSmall
-              onClick={() => setIsModalOpen(true)}
+              onClick={() => onChooseAllowance(allowance)}
             >
               Revoke
             </Button>
@@ -155,45 +121,15 @@ export const PermissionsToken: FC<Allowance> = ({
           className="hidden md:table-cell w-[24px] min-w-[30px] lg:min-w-[50px] lg:ps-[25px]"
           id={`${token.getTokenName()}_options`}
         >
-          <Button type="stroke" isSmall onClick={() => setIsModalOpen(true)}>
+          <Button
+            type="stroke"
+            isSmall
+            onClick={() => onChooseAllowance(allowance)}
+          >
             Revoke
           </Button>
         </td>
       </tr>
-      <ModalComponent
-        isVisible={isModalOpen}
-        onClose={() => {
-          setIsModalOpen(false)
-        }}
-        className="p-5 w-[95%] md:w-[540px] z-[100] !rounded-[24px]"
-      >
-        <p className="text-[20px] leading-[26px] font-bold dark:text-white mb-[18px]">
-          Revoke approve
-        </p>
-        <p className="leading-[22px] dark:text-white">
-          You are going to revoke approve of {amountFormatted} that you
-          previously give to {address}.
-        </p>
-        <div className="mt-5 flex justify-end gap-2.5">
-          <Button
-            type="stroke"
-            isSmall
-            className="w-[115px]"
-            onClick={() => setIsModalOpen(false)}
-          >
-            Cancel
-          </Button>
-          <Button
-            isSmall
-            className="w-[115px]"
-            onClick={handleRevoke}
-            disabled={identityLoading || !identity || isLoading}
-            icon={isLoading ? <Spinner /> : null}
-          >
-            Revoke
-          </Button>
-        </div>
-      </ModalComponent>
     </>
   )
 }

3871f8b58580202fb7d1575c3027ab902a8240e6
vitaliipustovyi <vetalsd2@gmail.com>
2025-12-16 11:48:49 +0200

    Extend Scan for new tokens with ERC 20 tokens [sc-18815]

--- apps/nfid-frontend/src/features/fungible-token/utils.ts
+++ apps/nfid-frontend/src/features/fungible-token/utils.ts
@@ -1,9 +1,15 @@
 import { Principal } from "@dfinity/principal"
 
 import { authState } from "@nfid/integration"
+import { ChainId, State } from "@nfid/integration/token/icrc1/enum/enums"
+import { arbitrumErc20Service } from "frontend/integration/ethereum/arbitrum/arbitrum-erc20.service"
+import { baseErc20Service } from "frontend/integration/ethereum/base/base-erc20.service"
+import { ethErc20Service } from "frontend/integration/ethereum/eth/eth-erc20.service"
+import { polygonErc20Service } from "frontend/integration/ethereum/polygon/pol-erc20.service"
 
 import { FT } from "frontend/integration/ft/ft"
 import { ftService } from "frontend/integration/ft/ft-service"
+import { tokenFactory } from "frontend/integration/ft/token-creator/token-factory.service"
 import { NFT } from "frontend/integration/nft/nft"
 import { portfolioService } from "frontend/integration/portfolio-balance/portfolio-service"
 
@@ -26,12 +32,54 @@ export const fetchTokens = async () => {
 
 export const filterNotActiveNotZeroBalancesTokens = async (
   allTokens: Array<FT>,
+  ethAddress: string,
+  isEthAddressLoading: boolean,
 ) => {
+  if (isEthAddressLoading) return
   const { publicKey } = await getUserPrincipalId()
-  return await ftService.filterNotActiveNotZeroBalancesTokens(
-    allTokens,
-    Principal.fromText(publicKey),
+
+  const [icrc1Tokens, arbTokens, polTokens, baseTokens, ethTokens] =
+    await Promise.all([
+      ftService.filterNotActiveNotZeroBalancesTokens(
+        allTokens,
+        Principal.fromText(publicKey),
+      ),
+      arbitrumErc20Service.getTokensWithNonZeroBalance(ethAddress),
+      polygonErc20Service.getTokensWithNonZeroBalance(ethAddress),
+      baseErc20Service.getTokensWithNonZeroBalance(ethAddress),
+      ethErc20Service.getTokensWithNonZeroBalance(ethAddress),
+    ])
+
+  const arbTokensErc20 = arbTokens.map((canister) =>
+    tokenFactory.getCreatorByChainID(ChainId.ARB).buildTokens(canister),
   )
+
+  const polTokensErc20 = polTokens.map((canister) =>
+    tokenFactory.getCreatorByChainID(ChainId.ARB).buildTokens(canister),
+  )
+
+  const baseTokensErc20 = baseTokens.map((canister) =>
+    tokenFactory.getCreatorByChainID(ChainId.ARB).buildTokens(canister),
+  )
+
+  const ethTokensErc20 = ethTokens.map((canister) =>
+    tokenFactory.getCreatorByChainID(ChainId.ARB).buildTokens(canister),
+  )
+
+  const nativeTokens: FT[] = [
+    tokenFactory.getCreatorByChainID(ChainId.POL).buildNative(State.Active),
+    tokenFactory.getCreatorByChainID(ChainId.ARB).buildNative(State.Active),
+    tokenFactory.getCreatorByChainID(ChainId.BASE).buildNative(State.Active),
+  ]
+
+  return [
+    ...icrc1Tokens,
+    ...arbTokensErc20,
+    ...polTokensErc20,
+    ...baseTokensErc20,
+    ...ethTokensErc20,
+    ...nativeTokens,
+  ]
 }
 
 export const getFullUsdValue = async (nfts: NFT[], ft: FT[]) => {

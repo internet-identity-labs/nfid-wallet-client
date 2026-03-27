c0ebc56672f863645282085e6c484eaadc414c68
vitaliipustovyi <vetalsd2@gmail.com>
2026-03-27 15:42:44 +0200

    wallet No animation of closing of menu dropdown [sc-17455]

--- packages/ui/src/molecules/tooltip/index.tsx
+++ packages/ui/src/molecules/tooltip/index.tsx
@@ -32,40 +32,35 @@ export const Tooltip: React.FC<TooltipProps> = ({
       >
         {children}
       </RadixTooltip.Trigger>
-      <RadixTooltip.Portal>
-        {isOpen && (
-          <RadixTooltip.Content
-            asChild
-            sideOffset={5}
-            forceMount={true}
-            {...contentProps}
-            onPointerDownOutside={(event) => {
-              if (!getIsMobileDeviceMatch()) event.preventDefault()
-            }}
+      <RadixTooltip.Portal forceMount>
+        <RadixTooltip.Content
+          asChild
+          sideOffset={5}
+          forceMount={true}
+          {...contentProps}
+          onPointerDownOutside={(event) => {
+            if (!getIsMobileDeviceMatch()) event.preventDefault()
+          }}
+        >
+          <motion.div
+            className={clsx(
+              "text-white text-sm bg-black dark:bg-zinc-700 p-[15px] rounded-[6px]",
+              className,
+              ["left", "right"].includes(contentProps.side || "top")
+                ? "my-2"
+                : "mx-2",
+            )}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: isOpen ? 1 : 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            style={{ pointerEvents: isOpen ? "auto" : "none" }}
           >
-            <motion.div
-              className={clsx(
-                "text-white text-sm bg-black dark:bg-zinc-700 p-[15px] rounded-[6px]",
-                className,
-                ["left", "right"].includes(contentProps.side || "top")
-                  ? "my-2"
-                  : "mx-2",
-              )}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.25, ease: "easeInOut" }}
-            >
-              {tip}
-              <RadixTooltip.Arrow
-                className={clsx(
-                  "fill-black dark:fill-zinc-700",
-                  arrowClassname,
-                )}
-              />
-            </motion.div>
-          </RadixTooltip.Content>
-        )}
+            {tip}
+            <RadixTooltip.Arrow
+              className={clsx("fill-black dark:fill-zinc-700", arrowClassname)}
+            />
+          </motion.div>
+        </RadixTooltip.Content>
       </RadixTooltip.Portal>
     </RadixTooltip.Root>
   )

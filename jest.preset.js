ed97215cb1c8defca6ef22cecb0f639e4ba37dcc
dmytroidentitylabs <dmytroidentitylabs@gmail.com>
2026-04-15 14:47:46 +0400

    Update libraries to the latest versions [sc-19054] (#3126)
    * Update libraries to the latest versions [sc-19054]
    
    * Update libraries to the latest versions [sc-19054]

--- jest.preset.js
+++ jest.preset.js
@@ -12,4 +12,7 @@ module.exports = {
    * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
    */
   snapshotFormat: { escapeString: true, printBasicPrototype: true },
+  moduleNameMapper: {
+    "^jose$": "jose-node-cjs-runtime",
+  },
 }
--- packages/integration/src/setup-tests.ts
+++ packages/integration/src/setup-tests.ts
@@ -13,11 +13,6 @@ global.TextEncoder = TextEncoder
 // @ts-ignore
 global.TextDecoder = TextDecoder
 
-// `jose` relies on `structuredClone` in its WebCrypto build.
-// Jest's runtime may not provide it depending on environment/polyfills.
-// @ts-ignore
-global.structuredClone ??= (value: unknown) => JSON.parse(JSON.stringify(value))
-
 Object.defineProperty(BigInt.prototype, "toJSON", {
   get() {
     return () => `${this.toString()}n`
